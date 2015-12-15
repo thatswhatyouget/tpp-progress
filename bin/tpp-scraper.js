7776ba969d34f7074d636ac626e766a040af40ff
Dave Rhodes <dave.was.r@gmail.com>
2015-12-15 13:37:41 +0000

    removed log messages from scraper

--- bin/tpp-scraper.js
+++ bin/tpp-scraper.js
@@ -4,16 +4,14 @@ function Scrape(run) {
     var deferred = $.Deferred();
     $.get("https://crossorigin.me/" + run.ScrapeUrl).then(function (page) {
         var $lastUpdate = $(page).find('.last-update');
-        var $badges = $(page).find("h3 strong:contains(Bosses), h3 strong:contains(Badges), h3 strong:contains('Elite 4')");
+        var $badges = $(page).find("h3 strong:contains(Bosses), h3 strong:contains(Badges), h3 strong:contains('Elite Four')");
         if ($lastUpdate.is('*')) {
-            console.log($lastUpdate.text());
             run.Duration = $lastUpdate.text().split(':').pop().trim();
         }
         $badges.each(function (i, group) {
             var $table = $(group).parent().next('.table-pokemon'), groupName = $(group).text();
             $table.find('th').each(function (i, th) {
                 var title = $(th).text();
-                console.log(title);
                 var $col = $table.find('tr td:nth-child(' + (i + 1) + ')');
                 if (!$col.find('img').is('.greyed-out')) {
                     run.Events.push({
--- src/tpp-scraper.ts
+++ src/tpp-scraper.ts
@@ -4,16 +4,14 @@ function Scrape(run:TPP.Run) {
 	var deferred = $.Deferred<TPP.Run>();
 	$.get("https://crossorigin.me/" + run.ScrapeUrl).then(page=> {
 		var $lastUpdate = $(page).find('.last-update');
-		var $badges = $(page).find("h3 strong:contains(Bosses), h3 strong:contains(Badges), h3 strong:contains('Elite 4')");
+		var $badges = $(page).find("h3 strong:contains(Bosses), h3 strong:contains(Badges), h3 strong:contains('Elite Four')");
 		if ($lastUpdate.is('*')) {
-			console.log($lastUpdate.text());
 			run.Duration = $lastUpdate.text().split(':').pop().trim();
 		}
 		$badges.each((i, group) => {
 			var $table = $(group).parent().next('.table-pokemon'), groupName = $(group).text();
 			$table.find('th').each((i, th)=> {
 				var title = $(th).text();
-				console.log(title);
 				var $col = $table.find('tr td:nth-child(' + (i + 1) + ')');
 				if (!$col.find('img').is('.greyed-out')) {
 					run.Events.push({
