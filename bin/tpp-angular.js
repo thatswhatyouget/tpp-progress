9dc6911766a760e69e0120f4ad8a9127a59e1e71
dave10rhodes <dave.was.r@gmail.com>
2016-02-19 06:00:35 +0000

    brought scraping into Angular, suppressed 404 messages

--- bin/tpp-angular.js
+++ bin/tpp-angular.js
@@ -13,13 +13,14 @@ tppNg.controller("credits", function ($scope) {
     ];
     $scope.live = tppData.filter(function (c) { return c.Runs.filter(function (r) { return r.Scraper && true; }).length > 0; }).length > 0 ? "Live" : "Most";
 });
-tppNg.controller('progressCharts', function ($scope) {
+tppNg.controller('progressCharts', function ($scope, $http) {
     tppData.forEach(function (c) { return c.Runs.forEach(function (r, i) {
         r.StartTime = r.StartTime || (r.StartDate ? Math.floor(Date.parse(r.StartDate) / 1000) : 0);
         if (r.Scraper) {
-            Scrape(r).then(function () { return $scope.$apply(); });
+            var scrape = function () { return $http.get("https://crossorigin.me/" + r.Scraper.url).then(function (p) { return scrapeRun(r, p.data); }); };
+            scrape();
             r.Duration = "0d";
-            setInterval(function () { return Scrape(r).then(function () { return $scope.$apply(); }); }, 15 * 60000);
+            setInterval(function () { return scrape(); }, 15 * 60000);
         }
         if (r.CopyEvents) {
             var events = [];
@@ -44,6 +45,8 @@ tppNg.controller('progressCharts', function ($scope) {
     $scope.ppd = 64;
     $scope.concurrentRuns = function (run, scale) {
         if (scale === void 0) { scale = TPP.Scale.Days; }
+        if (!run.ContainsRunsFrom)
+            return [];
         var baseDuration = Duration.parse(run.Duration), baseEndTime = run.StartTime + baseDuration.TotalSeconds;
         return tppData.filter(function (c) { return run.ContainsRunsFrom.indexOf(c.Name) >= 0; }).map(function (c) { return c.Runs.filter(function (r) { return run.StartTime < r.StartTime && baseEndTime > r.StartTime; }).map(function (r) {
             var runStart = Duration.parse(r.StartDate, run.StartTime), runEnd = Duration.parse(r.Duration, r.StartTime);
@@ -78,5 +81,61 @@ tppNg.controller('progressCharts', function ($scope) {
     $scope.scale = function (s) { return TPP.Scale[s]; };
     $scope.zoomIn = function () { return $scope.ppd *= 2; };
     $scope.zoomOut = function () { return $scope.ppd /= 2; };
-    $scope.dump = function (data) { return JSON.stringify(data); };
 });
+function scrapeRun(run, page) {
+    page = page.replace(/\bsrc\=/gi, "crs=");
+    var $lastUpdate = $(page).find('.last-update');
+    run.Scraper.parts = run.Scraper.parts || ["Badge", "Elite Four"];
+    var $events = $(page).find(run.Scraper.parts.map(function (p) { return "h3 strong:contains(" + p + ")"; }).join(','));
+    if (run.Scraper.runtime && $lastUpdate.is('*')) {
+        run.Duration = ($lastUpdate.text().split(':').pop() || "0d").trim();
+        if (!Duration.canParse(run.Duration))
+            run.Duration = new Date().toISOString();
+    }
+    var knownEvents = {};
+    run.Events.forEach(function (e) { return knownEvents[e.Name + e.Time] = e; });
+    $events.each(function (i, group) {
+        var $table = $(group).parent().next('.table-pokemon'), groupName = $(group).text();
+        $table.find('th').each(function (i, th) {
+            var title = $(th).text();
+            var $col = $table.find('tr td:nth-child(' + (i + 1) + ')');
+            var time = $($col[1]).text().trim();
+            if (!$col.find('img').is('.greyed-out') && !knownEvents[title + time]) {
+                run.Events.push({
+                    Group: groupName,
+                    Image: $col.find('img').attr('crs').replace(/^\//, run.Scraper.url + "/"),
+                    Name: title,
+                    Time: time,
+                    Attempts: parseInt($col.find('strong').text() || '0'),
+                    New: true
+                });
+            }
+        });
+    });
+    if (run.Scraper.hostname) {
+        run.HostName = $(page).find('td.text-left:contains("Trainer Name")').next('td.text-right').text() || run.HostName;
+    }
+    if (run.Scraper.pokemon) {
+        var pkmn = {};
+        run.Events.filter(function (e) { return e.Group == "Pokemon"; }).forEach(function (e) { return pkmn[e.Name] = e; });
+        $(page).find('.history-obtained').each(function () {
+            var $element = $(this);
+            var $img = $element.find('img');
+            if (!$img.is('*')) {
+                var title = $element.attr('title').split('(').shift().trim();
+            }
+            else {
+                var title = $img.attr('title') || "";
+            }
+            var time = $element.text().replace(/[()]/g, '').trim();
+            if (!pkmn[title] && Duration.canParse(time))
+                pkmn[title] = {
+                    Name: title,
+                    Time: time,
+                    Group: "Pokemon",
+                    New: true
+                };
+        });
+        Object.keys(pkmn).forEach(function (mon) { return run.Events.push(pkmn[mon]); });
+    }
+}
--- src/tpp-angular.ts
+++ src/tpp-angular.ts
@@ -16,13 +16,15 @@ tppNg.controller("credits", function($scope) {
     ];
     $scope.live = tppData.filter(c=> c.Runs.filter(r=> r.Scraper && true).length > 0).length > 0 ? "Live" : "Most";
 });
-tppNg.controller('progressCharts', function($scope) {
+
+tppNg.controller('progressCharts', function($scope, $http:angular.IHttpService) {
     tppData.forEach(c=> c.Runs.forEach((r, i) => {
         r.StartTime = r.StartTime || (r.StartDate ? Math.floor(Date.parse(r.StartDate) / 1000) : 0)
         if (r.Scraper) {
-            Scrape(r).then(() => $scope.$apply());
+            var scrape = () => $http.get("https://crossorigin.me/" + r.Scraper.url).then(p=>scrapeRun(r,<string>p.data));
+            scrape();
             r.Duration = "0d";
-            setInterval(() => Scrape(r).then(() => $scope.$apply()), 15 * 60000);
+            setInterval(() => scrape(), 15 * 60000);
         }
         if (r.CopyEvents) {
             var events: TPP.Event[] = [];
@@ -37,6 +39,7 @@ tppNg.controller('progressCharts', function($scope) {
     $scope.width = (time: string, scale = TPP.Scale.Days, baseTime?: number) => $scope.time(time, scale, baseTime) * $scope.ppd;
     $scope.ppd = 64;
     $scope.concurrentRuns = (run: TPP.Run, scale = TPP.Scale.Days) => {
+        if (!run.ContainsRunsFrom) return [];
         var baseDuration = Duration.parse(run.Duration),
             baseEndTime = run.StartTime + baseDuration.TotalSeconds;
         return tppData.filter(c=> run.ContainsRunsFrom.indexOf(c.Name) >= 0).map(c=> c.Runs.filter(r=> run.StartTime < r.StartTime && baseEndTime > r.StartTime).map(r=> {
@@ -71,3 +74,60 @@ tppNg.controller('progressCharts', function($scope) {
     $scope.zoomIn = () => $scope.ppd *= 2;
     $scope.zoomOut = () => $scope.ppd /= 2;
 });
+
+function scrapeRun(run: TPP.Run, page:string) {
+    page = page.replace(/\bsrc\=/gi, "crs=");
+    var $lastUpdate = $(page).find('.last-update');
+    run.Scraper.parts = run.Scraper.parts || ["Badge", "Elite Four"];
+    var $events = $(page).find(run.Scraper.parts.map(p=> "h3 strong:contains(" + p + ")").join(','));
+    if (run.Scraper.runtime && $lastUpdate.is('*')) {
+        run.Duration = ($lastUpdate.text().split(':').pop() || "0d").trim();
+        if (!Duration.canParse(run.Duration)) run.Duration = new Date().toISOString();
+    }
+    var knownEvents: { [key: string]: TPP.Event } = {};
+    run.Events.forEach(e=> knownEvents[e.Name + e.Time] = e);
+    $events.each((i, group) => {
+        var $table = $(group).parent().next('.table-pokemon'), groupName = $(group).text();
+        $table.find('th').each((i, th) => {
+            var title = $(th).text();
+            var $col = $table.find('tr td:nth-child(' + (i + 1) + ')');
+            var time = $($col[1]).text().trim();
+            if (!$col.find('img').is('.greyed-out') && !knownEvents[title + time]) {
+                run.Events.push({
+                    Group: groupName,
+                    Image: $col.find('img').attr('crs').replace(/^\//, run.Scraper.url + "/"),
+                    Name: title,
+                    Time: time,
+                    Attempts: parseInt($col.find('strong').text() || '0'),
+                    New: true
+                });
+            }
+        });
+    });
+    if (run.Scraper.hostname) {
+        run.HostName = $(page).find('td.text-left:contains("Trainer Name")').next('td.text-right').text() || run.HostName;
+    }
+    if (run.Scraper.pokemon) {
+        var pkmn: { [key: string]: TPP.Event } = {};
+        run.Events.filter(e=> e.Group == "Pokemon").forEach(e=> pkmn[e.Name] = e);
+        $(page).find('.history-obtained').each(function() {
+            var $element = $(this);
+            var $img = $element.find('img');
+            if (!$img.is('*')) {
+                var title = $element.attr('title').split('(').shift().trim();
+            }
+            else {
+                var title = $img.attr('title') || "";
+            }
+            var time = $element.text().replace(/[()]/g, '').trim();
+            if (!pkmn[title] /*&& title.toLowerCase() != "egg"*/ && Duration.canParse(time))
+                pkmn[title] = {
+                    Name: title,
+                    Time: time,
+                    Group: "Pokemon",
+                    New: true
+                };
+        });
+        Object.keys(pkmn).forEach(mon=> run.Events.push(pkmn[mon]));
+    }
+}
