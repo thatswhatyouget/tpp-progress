026371f32ca9529b867f4f44ecce3cddfcb2a6dc
Dave Rhodes <dave.was.r@gmail.com>
2016-03-21 00:06:49 +0000

    Stuff happened in EarthBound

--- src/tpp-data.ts
+++ src/tpp-data.ts
@@ -4496,6 +4496,19 @@ var tppData: TPP.Collection[] = [
                 Events: [
                     {
                         Group: "Bosses",
+                        Name: "Frank Fly",
+                        Image: "http://vignette2.wikia.nocookie.net/earthbound/images/8/81/EB_Frank.gif/revision/latest?cb=20111121002553",
+                        Time: "2016-03-20T18:48:00.000Z",
+                    },
+                    {
+                        Group: "Bosses",
+                        Name: "Frankystein MarkII",
+                        Image: "http://vignette1.wikia.nocookie.net/earthbound/images/7/79/FrankysteinMarkII.gif/revision/latest?cb=20090221132344",
+                        Time: "2016-03-20T20:04:00.000Z",
+                        Attempts: 2
+                    },
+                    {
+                        Group: "Bosses",
                         Name: "Titanic Ant",
                         Image: "http://vignette1.wikia.nocookie.net/earthbound/images/9/9f/Titanic_Ant_In-Battle.gif/revision/latest",
                         Time: "???",
