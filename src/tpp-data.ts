ee1c0fbb7f069da4f40f289ca5a79e52661f7a20
Dave Rhodes <dave.was.r@gmail.com>
2016-04-10 14:48:39 +0000

    Saved Howdy and Dexter

--- src/tpp-data.ts
+++ src/tpp-data.ts
@@ -5214,8 +5214,8 @@ var tppData: TPP.Collection[] = [
                 Events: [
                     { Group: "Badges", Name: "Bijou", Image: "img/hamhams/bijou.gif", Time: "2016-04-09T08:53:00Z" },
                     { Group: "Badges", Name: "Cappy", Image: "img/hamhams/cappy.gif", Time: "-1d" },
-                    { Group: "Badges", Name: "Dexter", Image: "img/hamhams/dexter.gif", Time: "-1d" },
-                    { Group: "Badges", Name: "Howdy", Image: "img/hamhams/howdy.gif", Time: "-1d" },
+                    { Group: "Badges", Name: "Howdy", Image: "img/hamhams/howdy.gif", Time: "2016-04-10T14:44:00Z" },
+                    { Group: "Badges", Name: "Dexter", Image: "img/hamhams/dexter.gif", Time: "2016-04-10T14:44:00Z" },
                     { Group: "Badges", Name: "Jingle", Image: "img/hamhams/jingle.gif", Time: "2016-04-10T07:24:00Z" },
                     { Group: "Badges", Name: "Maxwell", Image: "img/hamhams/maxwell.gif", Time: "2016-04-09T10:34:00Z" },
                     { Group: "Badges", Name: "Oxnard", Image: "img/hamhams/oxnard.gif", Time: "2016-04-09T11:43:00Z" },
