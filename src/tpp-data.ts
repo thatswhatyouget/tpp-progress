79bd40b58c4919b1d4ffd439215633963b13a564
Dave Rhodes <dave.was.r@gmail.com>
2016-04-10 00:22:37 +0000

    Added ham-ham milestones to Hamtaro

--- src/tpp-data.ts
+++ src/tpp-data.ts
@@ -5187,7 +5187,7 @@ var tppData: TPP.Collection[] = [
                 Region: "TCG Island",
                 Events: []
             },
-{
+            {
                 RunName: "Hamtaro: Ham-Hams Unite!",
                 StartDate: "2016-04-09T06:15:00Z",
                 Duration: new Date().toISOString(),
@@ -5199,6 +5199,18 @@ var tppData: TPP.Collection[] = [
                 HostName: "98866IQ",
                 Region: "Hamtaro",
                 Events: [
+                    { Group: "Badges", Name: "Bijou", Image: "img/hamhams/bijou.gif", Time: "2016-04-09T08:53:00Z" },
+                    { Group: "Badges", Name: "Cappy", Image: "img/hamhams/cappy.gif", Time: "-1d" },
+                    { Group: "Badges", Name: "Dexter", Image: "img/hamhams/dexter.gif", Time: "-1d" },
+                    { Group: "Badges", Name: "Howdy", Image: "img/hamhams/howdy.gif", Time: "-1d" },
+                    { Group: "Badges", Name: "Jingle", Image: "img/hamhams/jingle.gif", Time: "-1d" },
+                    { Group: "Badges", Name: "Maxwell", Image: "img/hamhams/maxwell.gif", Time: "2016-04-09T10:34:00Z" },
+                    { Group: "Badges", Name: "Oxnard", Image: "img/hamhams/oxnard.gif", Time: "2016-04-09T11:43:00Z" },
+                    { Group: "Badges", Name: "Panda", Image: "img/hamhams/panda.gif", Time: "2016-04-09T19:08:00Z" },
+                    { Group: "Badges", Name: "Pashmina", Image: "img/hamhams/pashmina.gif", Time: "-1d" },
+                    { Group: "Badges", Name: "Penelope", Image: "img/hamhams/penelope.gif", Time: "-1d" },
+                    { Group: "Badges", Name: "Sandy", Image: "img/hamhams/sandy.gif", Time: "-1d" },
+                    { Group: "Badges", Name: "Stan", Image: "img/hamhams/stan.gif", Time: "-1d" },
                 ]
             },
         ]
