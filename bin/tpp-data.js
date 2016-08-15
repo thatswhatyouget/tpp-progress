2a9a7befe7cade74453b030a998f6c8b9985ea4e
Dave Rhodes <dave.was.r@gmail.com>
2016-08-15 17:37:13 +0000

    Caught Combee

--- bin/tpp-data.js
+++ bin/tpp-data.js
@@ -4330,6 +4330,7 @@ var tppData = [
                     { "Group": "Pokemon", "Name": "Totodile", "Time": "14d 13h 36m" },
                     { "Group": "Pokemon", "Name": "Beautifly", "Time": "14d 14h 14m" },
                     { "Group": "Pokemon", "Name": "Zapdos", "Time": "14d 18h 4m" },
+                    { "Group": "Pokemon", "Name": "Combee", "Time": "14d 18h 23m" },
                 ]
             },
         ]
