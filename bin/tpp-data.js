89242dac05baaafbd69699b195cc630cc04308a2
Dave Rhodes <dave.was.r@gmail.com>
2016-10-09 03:40:01 +0000

    Made a new friend

--- bin/tpp-data.js
+++ bin/tpp-data.js
@@ -5243,7 +5243,7 @@ var tppData = [
                 RunName: "Telefang Revisit",
                 Class: "Telefang",
                 StartDate: "2016-10-08T21:37:00Z",
-                Duration: "2016-10-09T21:00:00Z",
+                Duration: "2016-10-09T18:00:00Z",
                 Ongoing: true,
                 ColorPrimary: "#EFFE24",
                 ColorSecondary: "#120291",
@@ -5254,6 +5254,7 @@ var tppData = [
                 Events: [
                     { Group: "Pokemon", Name: "Mierths", Time: "2016-10-09T00:19:00Z" },
                     { Group: "Bosses", Name: "Stream", Time: "2016-10-09T00:38:00Z", Image: "img/telefang/ogawa.gif", Attempts: 1 },
+                    { Group: "Pokemon", Name: "Tlaitlu", Time: "2016-10-09T03:38:00Z" },
                 ],
                 Revisit: { Collection: "Long Intermissions", Run: "Telefang" },
                 CopyEvents: ["Telefang"],
