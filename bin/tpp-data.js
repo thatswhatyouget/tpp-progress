7a027648d7a27e16beac882b821f85acb7b04af3
Dave Rhodes <dave.was.r@gmail.com>
2016-10-10 03:24:25 +0000

    Caught a Marill

--- bin/tpp-data.js
+++ bin/tpp-data.js
@@ -4630,6 +4630,7 @@ var tppData = [
                     { "Group": "Pokemon", "Name": "Larvitar", "Time": "8m" },
                     { "Group": "Pokemon", "Name": "Taillow", "Time": "3h 48m" },
                     { "Group": "Pokemon", "Name": "Ralts", "Time": "4h 55m" },
+                    { "Group": "Pokemon", "Name": "Marill", "Time": "6h 15m" },
                 ]
             }
         ]
