af4350adaef3a6f4189b07a063dd59e91b816751
Dave Rhodes <dave.was.r@gmail.com>
2016-10-13 02:30:42 +0000

    Removed temp data now that .org is up to date

--- src/tpp-data.ts
+++ src/tpp-data.ts
@@ -4705,10 +4705,8 @@ var tppData: TPP.Collection[] = [
                 },
                 Region: "Naljo",
                 Events: [
-                    { "Group": "Pokemon", "Name": "Butterfree", "Time": "2d4h57m" },
-                    { "Group": "Pokemon", "Name": "Kirlia", "Time": "2d 11h 50m" },
-                    { "Group": "Pokemon", "Name": "Totodile", "Time": "2d 15h 1m" },
-                    { "Group": "Badges", "Name": "Charm Badge", "Time": "2d 14h 57m", "Attempts": 1, "Image": "img/badges/charm.png" },
+                    // { "Group": "Pokemon", "Name": "Totodile", "Time": "2d 15h 1m" },
+                    // { "Group": "Badges", "Name": "Charm Badge", "Time": "2d 14h 57m", "Attempts": 1, "Image": "img/badges/charm.png" },
                 ]
             }
         ]
