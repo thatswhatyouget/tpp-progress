9d76a44855273a7eae5b7bbc1e986cb1e70b377e
Dave Rhodes <dave.was.r@gmail.com>
2016-10-18 01:53:24 +0000

    Scraper now returns the run on failure So if tpp.org is down, the (empty) run still shows up

--- bin/tpp-scraper.js
+++ bin/tpp-scraper.js
@@ -1,86 +1,88 @@
 function Scrape(run) {
     $('.doItLive').text('Live');
-    var deferred = $.Deferred(), durationExp = /(?:(\d*)w)? *(?:(\d*)d)? *(?:(\d*)h) *(?:(\d*)m) *(?:(\d*)s)?/im, attemptsExp = /Attempt[^\d]*(\d*)/i;
+    var deferred = $.Deferred(), durationExp = /(?:(\d*)w)? *(?:(\d*)d)? *(?:(\d*)h) *(?:(\d*)m) *(?:(\d*)s)?/im, attemptsExp = /Attempt[^\d]*(\d*)/i, promise = deferred.promise();
     if (run.Scraper.url == "http://twitchplayspokemon.org/")
-        return TppOrgApi(run, deferred);
-    $.ajax({
-        url: "http://crossorigin.me/" + run.Scraper.url,
-        type: "GET",
-        dataType: "text",
-        timeout: 1000
-    }).then(function (r) { return r; }, function (e) { return $.ajax({
-        url: "http://cors.io/?u=" + run.Scraper.url,
-        type: "GET",
-        dataType: "text",
-        timeout: 1000
-    }); }).then(function (page) {
-        page = page.replace(/\bsrc=/ig, 'crs=');
-        var $lastUpdate = $(page).find('.last-update');
-        run.Scraper.parts = run.Scraper.parts || ["Badge", "Elite Four"];
-        var $events = $(page).find(run.Scraper.parts.map(function (p) { return "h3 strong:contains(" + p + ")"; }).join(','));
-        if (run.Scraper.runtime && $lastUpdate.is('*')) {
-            run.Duration = ($lastUpdate.text().split(':').pop() || "0d").trim();
-            if (!Duration.canParse(run.Duration))
-                run.Duration = new Date().toISOString();
-        }
-        var knownEvents = {};
-        run.Events.forEach(function (e) { return knownEvents[(e.Name + e.Time).toLowerCase()] = e; });
-        $events.each(function (i, group) {
-            var groupName = $(group).text();
-            function parseEvents($table) {
-                $table.find('th a').remove();
-                $table.find('th').each(function (i, th) {
-                    var $col = $table.find('tr td:nth-child(' + (i + 1) + ')');
-                    if ($col.find('img').is('.greyed-out'))
-                        return;
-                    var title = $(th).text().trim();
-                    var time = (($col.text().match(durationExp) || []).shift() || '').trim();
-                    if (time && Duration.canParse(time) && !knownEvents[(title + time).toLowerCase()]) {
-                        run.Events.push({
-                            Group: groupName,
-                            Image: ($col.find('img').attr('crs') || '').replace(/^\//, run.Scraper.url + "/"),
+        promise = TppOrgApi(run, deferred);
+    else {
+        $.ajax({
+            url: "http://crossorigin.me/" + run.Scraper.url,
+            type: "GET",
+            dataType: "text",
+            timeout: 1000
+        }).then(function (r) { return r; }, function (e) { return $.ajax({
+            url: "http://cors.io/?u=" + run.Scraper.url,
+            type: "GET",
+            dataType: "text",
+            timeout: 1000
+        }); }).then(function (page) {
+            page = page.replace(/\bsrc=/ig, 'crs=');
+            var $lastUpdate = $(page).find('.last-update');
+            run.Scraper.parts = run.Scraper.parts || ["Badge", "Elite Four"];
+            var $events = $(page).find(run.Scraper.parts.map(function (p) { return "h3 strong:contains(" + p + ")"; }).join(','));
+            if (run.Scraper.runtime && $lastUpdate.is('*')) {
+                run.Duration = ($lastUpdate.text().split(':').pop() || "0d").trim();
+                if (!Duration.canParse(run.Duration))
+                    run.Duration = new Date().toISOString();
+            }
+            var knownEvents = {};
+            run.Events.forEach(function (e) { return knownEvents[(e.Name + e.Time).toLowerCase()] = e; });
+            $events.each(function (i, group) {
+                var groupName = $(group).text();
+                function parseEvents($table) {
+                    $table.find('th a').remove();
+                    $table.find('th').each(function (i, th) {
+                        var $col = $table.find('tr td:nth-child(' + (i + 1) + ')');
+                        if ($col.find('img').is('.greyed-out'))
+                            return;
+                        var title = $(th).text().trim();
+                        var time = (($col.text().match(durationExp) || []).shift() || '').trim();
+                        if (time && Duration.canParse(time) && !knownEvents[(title + time).toLowerCase()]) {
+                            run.Events.push({
+                                Group: groupName,
+                                Image: ($col.find('img').attr('crs') || '').replace(/^\//, run.Scraper.url + "/"),
+                                Name: title,
+                                Time: time,
+                                Attempts: parseInt(($col.text().match(attemptsExp) || []).pop() || '0'),
+                                New: true
+                            });
+                        }
+                    });
+                }
+                var $element = $(group).parent();
+                while ($element.nextAll('h3, .table-pokemon').first().is('.table-pokemon'))
+                    parseEvents($element = $element.nextAll('.table-pokemon').first());
+            });
+            if (run.Scraper.hostname) {
+                run.HostName = $(page).find('td.text-left:contains("Trainer Name")').next('td.text-right').text() || run.HostName;
+            }
+            if (run.Scraper.pokemon) {
+                var pkmn = {};
+                run.Events.filter(function (e) { return e.Group == "Pokemon"; }).forEach(function (e) { return pkmn[e.Name] = e; });
+                $(page).find('.history-obtained').each(function () {
+                    var $element = $(this);
+                    var $img = $element.find('img');
+                    if (!$img.is('*')) {
+                        var title = $element.attr('title').split('(').shift().trim();
+                    }
+                    else {
+                        var title = $img.attr('title') || "";
+                    }
+                    var time = $element.text().replace(/[()]/g, '').trim();
+                    if (!pkmn[title] && Duration.canParse(time))
+                        pkmn[title] = {
                             Name: title,
                             Time: time,
-                            Attempts: parseInt(($col.text().match(attemptsExp) || []).pop() || '0'),
+                            Group: "Pokemon",
                             New: true
-                        });
-                    }
+                        };
                 });
+                Object.keys(pkmn).filter(function (mon) { return !knownEvents[(pkmn[mon].Name + pkmn[mon].Time).toLowerCase()]; }).forEach(function (mon) { return run.Events.push(pkmn[mon]); });
             }
-            var $element = $(group).parent();
-            while ($element.nextAll('h3, .table-pokemon').first().is('.table-pokemon'))
-                parseEvents($element = $element.nextAll('.table-pokemon').first());
-        });
-        if (run.Scraper.hostname) {
-            run.HostName = $(page).find('td.text-left:contains("Trainer Name")').next('td.text-right').text() || run.HostName;
-        }
-        if (run.Scraper.pokemon) {
-            var pkmn = {};
-            run.Events.filter(function (e) { return e.Group == "Pokemon"; }).forEach(function (e) { return pkmn[e.Name] = e; });
-            $(page).find('.history-obtained').each(function () {
-                var $element = $(this);
-                var $img = $element.find('img');
-                if (!$img.is('*')) {
-                    var title = $element.attr('title').split('(').shift().trim();
-                }
-                else {
-                    var title = $img.attr('title') || "";
-                }
-                var time = $element.text().replace(/[()]/g, '').trim();
-                if (!pkmn[title] && Duration.canParse(time))
-                    pkmn[title] = {
-                        Name: title,
-                        Time: time,
-                        Group: "Pokemon",
-                        New: true
-                    };
-            });
-            Object.keys(pkmn).filter(function (mon) { return !knownEvents[(pkmn[mon].Name + pkmn[mon].Time).toLowerCase()]; }).forEach(function (mon) { return run.Events.push(pkmn[mon]); });
-        }
-        deferred.resolve(run);
-        $(page).find('img').attr('src', '');
-    }, function () { return deferred.reject("Could not load live run data."); });
-    return deferred.promise();
+            deferred.resolve(run);
+            $(page).find('img').attr('src', '');
+        }, function () { return deferred.reject("Could not load live run data."); });
+    }
+    return promise.then(function (r) { return r; }, function (e) { return $.when(run); });
 }
 function TppOrgApi(run, deferred) {
     ;
--- src/tpp-scraper.ts
+++ src/tpp-scraper.ts
@@ -4,84 +4,87 @@ function Scrape(run: TPP.Run) {
     $('.doItLive').text('Live');
     var deferred = $.Deferred<TPP.Run>(),
         durationExp = /(?:(\d*)w)? *(?:(\d*)d)? *(?:(\d*)h) *(?:(\d*)m) *(?:(\d*)s)?/im,
-        attemptsExp = /Attempt[^\d]*(\d*)/i;
-    if (run.Scraper.url == "http://twitchplayspokemon.org/") return TppOrgApi(run, deferred);
-    $.ajax({
-        url: "http://crossorigin.me/" + run.Scraper.url,
-        type: "GET",
-        dataType: "text",
-        timeout: 1000
-    }).then(r => r, e => $.ajax({
-        url: "http://cors.io/?u=" + run.Scraper.url,
-        type: "GET",
-        dataType: "text",
-        timeout: 1000
-    })).then(page => {
-        page = page.replace(/\bsrc=/ig, 'crs=');
-        var $lastUpdate = $(page).find('.last-update');
-        run.Scraper.parts = run.Scraper.parts || ["Badge", "Elite Four"];
-        var $events = $(page).find(run.Scraper.parts.map(p => "h3 strong:contains(" + p + ")").join(','));
-        if (run.Scraper.runtime && $lastUpdate.is('*')) {
-            run.Duration = ($lastUpdate.text().split(':').pop() || "0d").trim();
-            if (!Duration.canParse(run.Duration)) run.Duration = new Date().toISOString();
-        }
-        var knownEvents: { [key: string]: TPP.Event } = {};
-        run.Events.forEach(e => knownEvents[(e.Name + e.Time).toLowerCase()] = e);
-        $events.each((i, group) => {
-            var groupName = $(group).text();
-            function parseEvents($table: JQuery) {
-                $table.find('th a').remove();   //remove More Info buttons
-                $table.find('th').each((i, th) => {
-                    var $col = $table.find('tr td:nth-child(' + (i + 1) + ')');
-                    if ($col.find('img').is('.greyed-out')) return;
-                    var title = $(th).text().trim();
-                    var time = (($col.text().match(durationExp) || []).shift() || '').trim();
-                    if (time && Duration.canParse(time) && !knownEvents[(title + time).toLowerCase()]) {
-                        run.Events.push({
-                            Group: groupName,
-                            Image: ($col.find('img').attr('crs') || '').replace(/^\//, run.Scraper.url + "/"),
+        attemptsExp = /Attempt[^\d]*(\d*)/i,
+        promise = deferred.promise();
+    if (run.Scraper.url == "http://twitchplayspokemon.org/") promise = TppOrgApi(run, deferred);
+    else {
+        $.ajax({
+            url: "http://crossorigin.me/" + run.Scraper.url,
+            type: "GET",
+            dataType: "text",
+            timeout: 1000
+        }).then(r => r, e => $.ajax({
+            url: "http://cors.io/?u=" + run.Scraper.url,
+            type: "GET",
+            dataType: "text",
+            timeout: 1000
+        })).then(page => {
+            page = page.replace(/\bsrc=/ig, 'crs=');
+            var $lastUpdate = $(page).find('.last-update');
+            run.Scraper.parts = run.Scraper.parts || ["Badge", "Elite Four"];
+            var $events = $(page).find(run.Scraper.parts.map(p => "h3 strong:contains(" + p + ")").join(','));
+            if (run.Scraper.runtime && $lastUpdate.is('*')) {
+                run.Duration = ($lastUpdate.text().split(':').pop() || "0d").trim();
+                if (!Duration.canParse(run.Duration)) run.Duration = new Date().toISOString();
+            }
+            var knownEvents: { [key: string]: TPP.Event } = {};
+            run.Events.forEach(e => knownEvents[(e.Name + e.Time).toLowerCase()] = e);
+            $events.each((i, group) => {
+                var groupName = $(group).text();
+                function parseEvents($table: JQuery) {
+                    $table.find('th a').remove();   //remove More Info buttons
+                    $table.find('th').each((i, th) => {
+                        var $col = $table.find('tr td:nth-child(' + (i + 1) + ')');
+                        if ($col.find('img').is('.greyed-out')) return;
+                        var title = $(th).text().trim();
+                        var time = (($col.text().match(durationExp) || []).shift() || '').trim();
+                        if (time && Duration.canParse(time) && !knownEvents[(title + time).toLowerCase()]) {
+                            run.Events.push({
+                                Group: groupName,
+                                Image: ($col.find('img').attr('crs') || '').replace(/^\//, run.Scraper.url + "/"),
+                                Name: title,
+                                Time: time,
+                                Attempts: parseInt(($col.text().match(attemptsExp) || []).pop() || '0'),
+                                New: true
+                            });
+                        }
+                    });
+                }
+                var $element = $(group).parent();
+                while ($element.nextAll('h3, .table-pokemon').first().is('.table-pokemon'))
+                    parseEvents($element = $element.nextAll('.table-pokemon').first());
+            });
+            if (run.Scraper.hostname) {
+                run.HostName = $(page).find('td.text-left:contains("Trainer Name")').next('td.text-right').text() || run.HostName;
+            }
+            if (run.Scraper.pokemon) {
+                var pkmn: { [key: string]: TPP.Event } = {};
+                run.Events.filter(e => e.Group == "Pokemon").forEach(e => pkmn[e.Name] = e);
+                $(page).find('.history-obtained').each(function () {
+                    var $element = $(this);
+                    var $img = $element.find('img');
+                    if (!$img.is('*')) {
+                        var title = $element.attr('title').split('(').shift().trim();
+                    }
+                    else {
+                        var title = $img.attr('title') || "";
+                    }
+                    var time = $element.text().replace(/[()]/g, '').trim();
+                    if (!pkmn[title] /*&& title.toLowerCase() != "egg"*/ && Duration.canParse(time))
+                        pkmn[title] = {
                             Name: title,
                             Time: time,
-                            Attempts: parseInt(($col.text().match(attemptsExp) || []).pop() || '0'),
+                            Group: "Pokemon",
                             New: true
-                        });
-                    }
+                        };
                 });
+                Object.keys(pkmn).filter(mon => !knownEvents[(pkmn[mon].Name + pkmn[mon].Time).toLowerCase()]).forEach(mon => run.Events.push(pkmn[mon]));
             }
-            var $element = $(group).parent();
-            while ($element.nextAll('h3, .table-pokemon').first().is('.table-pokemon'))
-                parseEvents($element = $element.nextAll('.table-pokemon').first());
-        });
-        if (run.Scraper.hostname) {
-            run.HostName = $(page).find('td.text-left:contains("Trainer Name")').next('td.text-right').text() || run.HostName;
-        }
-        if (run.Scraper.pokemon) {
-            var pkmn: { [key: string]: TPP.Event } = {};
-            run.Events.filter(e => e.Group == "Pokemon").forEach(e => pkmn[e.Name] = e);
-            $(page).find('.history-obtained').each(function () {
-                var $element = $(this);
-                var $img = $element.find('img');
-                if (!$img.is('*')) {
-                    var title = $element.attr('title').split('(').shift().trim();
-                }
-                else {
-                    var title = $img.attr('title') || "";
-                }
-                var time = $element.text().replace(/[()]/g, '').trim();
-                if (!pkmn[title] /*&& title.toLowerCase() != "egg"*/ && Duration.canParse(time))
-                    pkmn[title] = {
-                        Name: title,
-                        Time: time,
-                        Group: "Pokemon",
-                        New: true
-                    };
-            });
-            Object.keys(pkmn).filter(mon => !knownEvents[(pkmn[mon].Name + pkmn[mon].Time).toLowerCase()]).forEach(mon => run.Events.push(pkmn[mon]));
-        }
-        deferred.resolve(run);
-        $(page).find('img').attr('src', '');
-    }, () => deferred.reject("Could not load live run data."));
-    return deferred.promise();
+            deferred.resolve(run);
+            $(page).find('img').attr('src', '');
+        }, () => deferred.reject("Could not load live run data."));
+    }
+    return promise.then(r => r, e => $.when(run));
 }
 
 function TppOrgApi(run: TPP.Run, deferred: JQueryDeferred<TPP.Run>) {
