9af71af1fccb46ed787096b7794a12c62be41975
Dave Rhodes <dave.was.r@gmail.com>
2016-10-24 20:32:13 +0000

    Caught Donphan

--- bin/tpp-data.js
+++ bin/tpp-data.js
@@ -4757,6 +4757,7 @@ var tppData = [
                     { "Group": "Pokemon", "Name": "Lairon", "Time": "14d 15h 21m" },
                     { "Group": "Pokemon", "Name": "Articuno", "Time": "14d 15h 29m" },
                     { "Group": "Pokemon", "Name": "Slugma", "Time": "14d 18h 22m" },
+                    { "Group": "Pokemon", "Name": "Donphan", "Time": "14d 23h 30m" },
                 ]
             }
         ]
--- src/tpp-data.ts
+++ src/tpp-data.ts
@@ -4834,6 +4834,7 @@ var tppData: TPP.Collection[] = [
                     { "Group": "Pokemon", "Name": "Lairon", "Time": "14d 15h 21m" },
                     { "Group": "Pokemon", "Name": "Articuno", "Time": "14d 15h 29m" },
                     { "Group": "Pokemon", "Name": "Slugma", "Time": "14d 18h 22m" },
+                    { "Group": "Pokemon", "Name": "Donphan", "Time": "14d 23h 30m" },
                 ]
             }
         ]
