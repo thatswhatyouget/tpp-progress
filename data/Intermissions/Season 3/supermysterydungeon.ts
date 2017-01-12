3d36e80a1b55974af1c8fce5bc7538b574bc8151
Dave Rhodes <dave.was.r@gmail.com>
2017-01-12 22:36:31 +0000

    Added run for Super Mystery Dungeon

---
+++ data/Intermissions/Season 3/supermysterydungeon.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Super Mystery Dungeon",
+        StartDate: "2017-01-12T22:21:54Z",
+        ColorPrimary: "#24BBEC",
+        ColorSecondary: "#B872B4",
+        Duration: "2016-01-13T17:00:00Z",
+        Ongoing: true,
+        HostName: "¡",
+        HostImage: "http://cdn.bulbagarden.net/upload/thumb/6/6c/653Fennekin_PSMD.png/106px-653Fennekin_PSMD.png",
+	Class: "squarehost",
+        Region: "Pokemon World",
+        Events: []
+    }
+);
