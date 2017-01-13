1e5c9cc5024ed453654e104f1397aa7f19617092
Dave Rhodes <dave.was.r@gmail.com>
2017-01-13 18:25:54 +0000

    Got Rowlet, updated protagonist and name

--- data/Season 3/moon.ts
+++ data/Season 3/moon.ts
@@ -8,9 +8,9 @@ Season3.Runs.push(
         Duration: "255d",
         Ongoing: true,
         BackgroundImage: "linear-gradient(to bottom, #a8e5fe 0%,white 30%,white 33%,#47b1e3 40%,#cdeff8 100%)",
-        HostName: "???",
-        // HostImage: "img/hosts/sun.png",
-        HostImage: "img/hosts/moon.png",
+        HostName: "\"?p ",
+        HostImage: "img/hosts/sun.png",
+        //HostImage: "img/hosts/moon.png",
         HostImageSource: "http://drawnamu.deviantart.com/art/Sun-Moon-characters-612994192",
         TPPOrgLink: "http://twitchplayspokemon.org/",
         // Scraper: {
@@ -21,6 +21,7 @@ Season3.Runs.push(
         // },
         Region: "Alola",
         Events: [
+            { "Group": "Pokemon", "Name": "Rowlet", "Time": "24m" },
             //{ "Group": "Badges", "Name": "Normalium Z", "Image": "img/z-crystals/Normalium Z.png", "Time": "", "Attempts": 0 },
             //{ "Group": "Pokemon", "Name": "", "Time": "" },
         ]
