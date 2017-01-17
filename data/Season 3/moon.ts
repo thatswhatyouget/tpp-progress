3aac94691956ca357bcd1dfadd28386f6e9973b0
Dave Rhodes <dave.was.r@gmail.com>
2017-01-17 06:40:12 +0000

    Fixed time for Seismitoad

--- data/Season 3/moon.ts
+++ data/Season 3/moon.ts
@@ -147,7 +147,7 @@ Season3.Runs.push(
             { "Group": "Pokemon", "Name": "Bonsly", "Time": "3d10h55m" },
             { "Group": "Pokemon", "Name": "Tranquill", "Time": "3d11h7m" },
             { "Group": "Pokemon", "Name": "Swanna", "Time": "3d12h30m" },
-            { "Group": "Pokemon", "Name": "Seismitoad", "Time": "3d9h31m" },
+            { "Group": "Pokemon", "Name": "Seismitoad", "Time": "3d12h31m" },
             //{ "Group": "Badges", "Name": "Rockium Z", "Image": "img/z-crystals/Rockium Z.png", "Time": "", "Attempts": 7 },
         ]
     }
