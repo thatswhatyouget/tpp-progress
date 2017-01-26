3c11c086cfef019336c72e79a4495cbd3bfeb7c2
Dave Rhodes <dave.was.r@gmail.com>
2017-01-26 13:21:21 +0000

    Caught Sneasel

--- data/Season 3/moon.ts
+++ data/Season 3/moon.ts
@@ -373,6 +373,7 @@ Season3.Runs.push(
             { "Group": "Elite Four Rematch", "Name": "Acerola", "Time": "12d17h45m", "Attempts": 1, "Image": "img/trainers/sun/rematch/acerola.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
             { "Group": "Pokemon", "Name": "Miltank", "Time": "12d18h7m" },
             { "Group": "Pokemon", "Name": "Snover", "Time": "12d19h7m" },
+            { "Group": "Pokemon", "Name": "Sneasel", "Time": "12d19h20m" },
             // { "Group": "Elite Four Rematch", "Name": "Hala", "Time": "", "Attempts": 2, "Image": "img/trainers/sun/rematch/hala.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
         ]
     }
