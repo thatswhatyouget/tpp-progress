6c02ec19b75e33fa6f5455fe5b5f512748153e67
Dave Rhodes <dave.was.r@gmail.com>
2017-01-31 13:52:12 +0000

    Defeated Jasmine

--- data/Sidegames/puzzlechallenge.ts
+++ data/Sidegames/puzzlechallenge.ts
@@ -19,9 +19,9 @@ Sidegames.Runs.push(
             { Group: "Badges", Name: "Hive Badge", Image: "img/badges/hive.png", Time: "2017-01-29T01:03:50Z", Attempts: 1 },
             { Group: "Badges", Name: "Plain Badge", Image: "img/badges/plain.png", Time: "2017-01-29T23:23:52Z", Attempts: 1 },
             { Group: "Badges", Name: "Fog Badge", Image: "img/badges/fog.png", Time: "2017-01-30T13:14:30Z", Attempts: 1 },
-            // { Group: "Badges", Name: "Glacier Badge", Image: "img/badges/glacier.png", Time: "", Attempts: 1 },
+            { Group: "Badges", Name: "Mineral Badge", Image: "img/badges/mineral.png", Time: "2017-01-31T10:15:17Z", Attempts: 2 },
             // { Group: "Badges", Name: "Storm Badge", Image: "img/badges/storm.png", Time: "", Attempts: 1 },
-            // { Group: "Badges", Name: "Mineral Badge", Image: "img/badges/mineral.png", Time: "", Attempts: 1 },
+            // { Group: "Badges", Name: "Glacier Badge", Image: "img/badges/glacier.png", Time: "", Attempts: 1 },
             // { Group: "Badges", Name: "Rising Badge", Image: "img/badges/rising.png", Time: "", Attempts: 1 },
         ]
     }
