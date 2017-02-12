21ed3f97224e6a1be363c1ea951e293357985c56
Dave Rhodes <dave.was.r@gmail.com>
2017-02-12 05:21:59 +0000

    Added more hosts to GB Roulette

--- data/Intermissions/Season 4/gbroulette.ts
+++ data/Intermissions/Season 4/gbroulette.ts
@@ -5,7 +5,7 @@ Intermissions.Runs.push(
         ColorPrimary: "#a6d410",
         ColorSecondary: "#072b03",
         StartDate: "2017-02-09T21:44:00Z",
-        Duration: "2017-02-12T20:00:00Z",
+        Duration: "2017-02-12T15:00:00Z",
         HostImage: "img/hosts/roulette/madden.gif",
         HostName: "John Madden\nMadden '95",
         Class: "squarehost",
@@ -13,7 +13,19 @@ Intermissions.Runs.push(
             { Group: "Hosts", Name: "Street Racer", Image: "img/hosts/roulette/streetracer.gif", Time: "2017-02-09T21:46:00Z", Class: "squarehost" },
             { Group: "Hosts", Name: "Lucky Luke", Image: "img/hosts/roulette/luckyluke.gif", Time: "2017-02-09T21:47:00Z" },
             { Group: "Hosts", Name: "Aya\nMonster Rancher\nBattle Card GB", Image: "img/hosts/roulette/aya.gif", Time: "2017-02-09T21:51:00Z", Class: "squarehost" },
-
+            //oh so many more...
+            { Group: "Hosts", Name: "Harry Potter\nand the\nSorcerer's Stone", Image: "img/hosts/other/harrypotter.gif", Time: "2017-02-10T03:48:00Z", Class: "squarehost" },
+            { Group: "Hosts", Name: "Pikachu\nPocket Monsters GO! GO!\nThe Pikachu Nightmare", Image: "img/hosts/pmd/pikachu.png", Time: "2017-02-10T09:33:00Z" },
+            { Group: "Hosts", Name: "A!!!!!!!\nTetris DX", Image: "img/hosts/other/tetris.gif", Time: "2017-02-10T16:03:00Z" },
+            { Group: "Hosts", Name: "Pikachu\nPokémon Adventure", Image: "img/hosts/pmd/pikachu.png", Time: "2017-02-11T00:15:00Z" },
+            { Group: "Hosts", Name: "Wario\nWario Land II", Image: "img/hosts/other/wario.png", Time: "2017-02-11T02:36:00Z", Class: "squarehost" },
+            { Group: "Hosts", Name: "Ethan\nPokémon Puzzle Challenge", Image: "img/hosts/ethan.png", Time: "2017-02-11T108:36:00Z" },
+            { Group: "Hosts", Name: "♪\nHarvest Moon 2 GBC", Image: "img/hosts/other/chelsea.gif", Time: "2017-02-11T12:21:00Z" },
+            { Group: "Hosts", Name: "Scooby Doo!\nClassic Creep Capers", Image: "img/hosts/other/scoob.gif", Time: "2017-02-11T20:43:00Z" },
+            { Group: "Hosts", Name: "CASSIE\nAnimorphs", Image: "img/hosts/other/cassie.gif", Time: "2017-02-11T22:23:00Z", Class: "squarehost" },
+            { Group: "Hosts", Name: "DABC\nHarvest Moon 3 GBC", Image: "img/hosts/other/pete.png", Time: "2017-02-12T00:03:00Z" },
+            { Group: "Hosts", Name: "Hamtar\nHamtaro\nHam-Hams Unite!", Image: "img/hosts/other/hamtaro.gif", Time: "2017-02-12T01:08:00Z", Class: "squarehost" },
+            { Group: "Hosts", Name: "AAAAIII\nPokémon Red Advanced", Image: "img/hosts/red.png", Time: "2017-02-12T01:53:00Z" },
         ]
     }
 );
