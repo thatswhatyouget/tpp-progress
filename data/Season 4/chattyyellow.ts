1113232e755e941ef2c4e3d3d5def442d44392f9
Dave Rhodes <dave.was.r@gmail.com>
2017-02-13 07:02:07 +0000

    Got Bulbasaur

--- data/Season 4/chattyyellow.ts
+++ data/Season 4/chattyyellow.ts
@@ -22,13 +22,14 @@ Season4.Runs.push(
             { Name: "Rattata", Time: "5h7m", Group: "Pokemon" },
             { Name: "NidoranF", Time: "5h15m", Group: "Pokemon" },
             { Name: "Magikarp", Time: "7h42m", Group: "Pokemon" },
-            // { "Group": "Badges", "Name": "HeyGuys (Cascade) Badge", "Time": "", "Attempts": 0, "Image": "img/badges/cascade.png" },
-            // { "Group": "Badges", "Name": "Jebaited (Thunder) Badge", "Time": "", "Attempts": 0, "Image": "img/badges/thunder.png" },
-            // { "Group": "Badges", "Name": "Rainbow Badge", "Time": "", "Attempts": 0, "Image": "img/badges/rainbow.png" },
-            // { "Group": "Badges", "Name": "Soul Badge", "Time": "", "Attempts": 0, "Image": "img/badges/soul.png" },
-            // { "Group": "Badges", "Name": "Marsh Badge", "Time": "", "Attempts": 0, "Image": "img/badges/marsh.png" },
-            // { "Group": "Badges", "Name": "Volcano Badge", "Time": "", "Attempts": 0, "Image": "img/badges/volcano.png" },
-            // { "Group": "Badges", "Name": "Earth Badge", "Time": "", "Attempts": 0, "Image": "img/badges/earth.png" },
+            { Name: "Bulbasaur", Time: "10h", Group: "Pokemon" },
+            // { "Group": "Badges", "Name": "HeyGuys (Cascade) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/30259/3.0" },
+            // { "Group": "Badges", "Name": "Jebaited (Thunder) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/114836/3.0" },
+            // { "Group": "Badges", "Name": "VoHiYo (Rainbow) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/81274/3.0" },
+            // { "Group": "Badges", "Name": "MVGame (Soul) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/142140/3.0" },
+            // { "Group": "Badges", "Name": "PMSTwin (Marsh) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/92/1.0" },
+            // { "Group": "Badges", "Name": "DatSheffy (Volcano) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/111700/3.0" },
+            // { "Group": "Badges", "Name": "BigBrother (Earth) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/1904/1.0" },
         ]
     }
 );
