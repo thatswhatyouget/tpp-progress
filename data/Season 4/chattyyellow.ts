290e33d6aebacb57f6a581fef2042e540984aa5b
Dave Rhodes <dave.was.r@gmail.com>
2017-02-15 07:02:06 +0000

    Evolved Diglett

--- data/Season 4/chattyyellow.ts
+++ data/Season 4/chattyyellow.ts
@@ -38,6 +38,7 @@ Season4.Runs.push(
             { Name: "Geodude", Time: "1d17h20m", Group: "Pokemon" },
             { Name: "Golbat", Time: "1d22h35m", Group: "Pokemon" },
             { Name: "Raichu", Time: "2d8h19m", Group: "Pokemon" },
+            { Name: "Dugtrio", Time: "2d10h", Group: "Pokemon" },
             // { "Group": "Badges", "Name": "VoHiYo (Rainbow) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/81274/3.0" },
             // { "Group": "Badges", "Name": "MVGame (Soul) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/142140/3.0" },
             // { "Group": "Badges", "Name": "PMSTwin (Marsh) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/92/1.0" },
