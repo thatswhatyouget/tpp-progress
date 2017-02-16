f46d8aca434c3aa607de082702031699359a217c
Dave Rhodes <dave.was.r@gmail.com>
2017-02-16 17:54:06 +0000

    Picked up Eevee

--- data/Season 4/chattyyellow.ts
+++ data/Season 4/chattyyellow.ts
@@ -46,6 +46,7 @@ Season4.Runs.push(
             { Name: "Caterpie", Time: "2d22h45m", Group: "Pokemon" },
             { Name: "Venusaur", Time: "3d11h26m", Group: "Pokemon" },
             { "Group": "Badges", "Name": "VoHiYo (Rainbow) Badge", "Time": "3d16h43m", "Attempts": 1, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/81274/3.0" },
+            { Name: "Eevee", Time: "3d19h46m", Group: "Pokemon" },
             // { "Group": "Badges", "Name": "MVGame (Soul) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/142140/3.0" },
             // { "Group": "Badges", "Name": "PMSTwin (Marsh) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/92/1.0" },
             // { "Group": "Badges", "Name": "DatSheffy (Volcano) Badge", "Time": "", "Attempts": 0, "Image": "https://static-cdn.jtvnw.net/emoticons/v1/111700/3.0" },
