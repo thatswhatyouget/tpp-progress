412ecd53c44104c5471003ba0813d14ec0d666fc
Dave Rhodes <dave.was.r@gmail.com>
2017-03-13 20:34:26 +0000

    Started new sidegame

---
+++ data/Sidegames/ashgray.ts
@@ -0,0 +1,108 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Ash Gray",
+        ColorPrimary: "#E8E4DF",
+        ColorSecondary: "#97856E",
+        Duration: "255w",
+        StartDate: "2017-03-13 20:26:16.371000",
+        //HostName: "Ash",
+        HostImage: "img/hosts/ash.png",
+        HostImageSource: "http://kensuyjin33.deviantart.com/art/Sprites-Trainer-Ash-Ketchum-Satoshi-Pokemon-Anime-456676605",
+        BaseGame: "FireRed",
+        Region: "Kanto",
+        Events: [
+            //{
+            //    Group: "Badges",
+            //    Name: "Boulder Badge",
+            //    Image: "img/badges/boulder.png",
+            //    Time: "",
+            //    Attempts: 0
+            //},
+//            {
+//                Group: "Badges",
+//                Name: "Cascade Badge",
+//                Image: "img/badges/cascade.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Badges",
+//                Name: "Thunder Badge",
+//                Image: "img/badges/thunder.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Badges",
+//                Name: "Rainbow Badge",
+//                Image: "img/badges/rainbow.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Badges",
+//                Name: "Soul Badge",
+//                Image: "img/badges/soul.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Badges",
+//                Name: "Marsh Badge",
+//                Image: "img/badges/marsh.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Badges",
+//                Name: "Volcano Badge",
+//                Image: "img/badges/volcano.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Badges",
+//                Name: "Earth Badge",
+//                Image: "img/badges/earth.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Elite Four",
+//                Name: "Lorelei",
+//                Image: "img/trainers/firered/lorelei.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Elite Four",
+//                Name: "Bruno",
+//                Image: "img/trainers/firered/bruno.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Elite Four",
+//                Name: "Agatha",
+//                Image: "img/trainers/firered/agatha.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Elite Four",
+//                Name: "Lance",
+//                Image: "img/trainers/firered/lance.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+//            {
+//                Group: "Champions",
+//                Name: "Green",
+//                Image: "img/trainers/firered/green.png",
+//                Time: "",
+//                Attempts: 0
+//            },
+        ]
+    }
+);
