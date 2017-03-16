ce320a45f867c4c468fa5ce67c78a87c826cfa06
Dave Rhodes <dave.was.r@gmail.com>
2017-03-16 22:09:37 +0000

    Added Tunod badges

--- data/Season 4/blazedglazed.ts
+++ data/Season 4/blazedglazed.ts
@@ -18,6 +18,15 @@ Season4.Runs.push(
         //     pokemon: true
         // },
         Events: [
+            { Group: "Pokemon", Name: "", Time: "" },
+            { Group: "Badges", Name: "Jolt Badge", Time: "", Attempts: 0, Image: "http://vignette2.wikia.nocookie.net/fanmadepokemonglazed/images/9/9e/Jolt_badge.png/revision/latest?cb=20140315165128" },
+            { Group: "Badges", Name: "Dirt Badge", Time: "", Attempts: 0, Image: "http://vignette4.wikia.nocookie.net/fanmadepokemonglazed/images/d/dc/Dirt_badge.png/revision/latest?cb=20140315165158" },
+            { Group: "Badges", Name: "Crest Badge", Time: "", Attempts: 0, Image: "http://vignette2.wikia.nocookie.net/fanmadepokemonglazed/images/9/93/Crest_badge.png/revision/latest?cb=20140315165057" },
+            { Group: "Badges", Name: "Stem Badge", Time: "", Attempts: 0, Image: "http://vignette2.wikia.nocookie.net/fanmadepokemonglazed/images/5/54/Stem_badge.png/revision/latest?cb=20160322195957" },
+            { Group: "Badges", Name: "Icicle Badge", Time: "", Attempts: 0, Image: "http://vignette2.wikia.nocookie.net/fanmadepokemonglazed/images/b/b5/Icicle_badge.png/revision/latest?cb=20160323162303" },
+            { Group: "Badges", Name: "Blaze Badge", Time: "", Attempts: 0, Image: "img/badges/blaze.png" },
+            { Group: "Badges", Name: "Core Badge", Time: "", Attempts: 0, Image: "http://vignette1.wikia.nocookie.net/fanmadepokemonglazed/images/b/b4/Core_badge.png/revision/latest?cb=20151114083934" },
+            { Group: "Badges", Name: "Clobber Badge", Time: "", Attempts: 0, Image: "http://vignette2.wikia.nocookie.net/fanmadepokemonglazed/images/7/70/Clobber_badge.png/revision/latest?cb=20151114083924" },
         ]
     }
 );
