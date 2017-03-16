38a812b11f8c890d12fb42d5f10346aadb1c0f11
Dave Rhodes <dave.was.r@gmail.com>
2017-03-16 06:19:10 +0000

    Got Machu

--- data/Sidegames/ashgray.ts
+++ data/Sidegames/ashgray.ts
@@ -12,7 +12,7 @@ Sidegames.Runs.push(
         BaseGame: "FireRed",
         Region: "Kanto",
         Events: [
-            { Name: "Pikachu", Time: "", Group: "Pokemon" },
+            { Name: "Pikachu", Time: "2017-03-16T01:25:35+00:00", Group: "Pokemon" },
             { Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", Time: "", Attempts: 0 },
             { Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", Time: "", Attempts: 0 },
             { Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", Time: "", Attempts: 0 },
