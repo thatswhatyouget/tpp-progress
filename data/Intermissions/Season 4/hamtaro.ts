599bc72241a49bda1cfe01cdce512a45515d20ef
Dave Rhodes <dave.was.r@gmail.com>
2017-04-07 22:18:38 +0000

    It's Hamtaro time!

---
+++ data/Intermissions/Season 4/hamtaro.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Hamtaro: Ham-Ham Heartbreak",
+        StartDate: "2017-04-07T21:17:00Z",
+        Duration: "2017-04-08T20:00:00Z",
+        ColorPrimary: "#e59910",
+        ColorSecondary: "#43AB1E",
+        HostImage: "img/hosts/other/hamtaro.gif",
+        HostImageSource: "http://biggysprites.smackjeeves.com/comics/678645/hamtaro-sprite-sheet-2/",
+        HostName: "Hamtaro",
+        Region: "Hamtaro",
+        Class: "squarehost",
+        Events: [
+            { Group: "Badges", Name: "Bijou", Image: "img/hamhams/bijou.gif", Time: "" },
+        ]
+    }
+);
