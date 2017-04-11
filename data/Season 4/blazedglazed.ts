b1615fd855e5d7fa493431477135d856f66ebbab
Dave Rhodes <dave.was.r@gmail.com>
2017-04-11 19:54:00 +0000

    Finally caught Bagon

--- data/Season 4/blazedglazed.ts
+++ data/Season 4/blazedglazed.ts
@@ -67,6 +67,7 @@ Season4.Runs.push(
             { Group: "Pokemon", Name: "NidoranM", Time: "2d19h33m" },
             { Group: "Pokemon", Name: "Geodude", Time: "2d20h10m" },
             { Group: "Pokemon", Name: "Phanpy", Time: "2d20h43m" },
+            { Group: "Pokemon", Name: "Bagon", Time: "2d22h52m" },
 
             { Group: "Badges", Name: "Stem Badge", Time: "", Attempts: 3, Image: "img/badges/glazed/stem.png" },
             { Group: "Badges", Name: "Icicle Badge", Time: "", Attempts: 0, Image: "img/badges/glazed/icicle.png" },
