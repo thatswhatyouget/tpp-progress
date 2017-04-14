6e43541a1c22dc8cc1c8ce3ab9bf703f2265d009
Dave Rhodes <dave.was.r@gmail.com>
2017-04-14 21:28:57 +0000

    Caught Tepig and Froakie

--- data/Season 4/blazedglazed.ts
+++ data/Season 4/blazedglazed.ts
@@ -138,6 +138,8 @@ Season4.Runs.push(
             { Group: "Pokemon", Name: "Mienfoo", Time: "5d18h45m" },
             { Group: "Pokemon", Name: "Arcanine", Time: "5d20h27m" },
             { Group: "Pokemon", Name: "Zorua", Time: "6d3m" },
+            { Group: "Pokemon", Name: "Tepig", Time: "6d14m" },
+            { Group: "Pokemon", Name: "Froakie", Time: "6d23m" },
 
             { Group: "Badges", Name: "Zephyr Badge", Time: "", Attempts: 0, Image: "img/badges/glazed/zephyr.png" },
             { Group: "Badges", Name: "Hive Badge", Time: "", Attempts: 0, Image: "img/badges/glazed/hive.png" },
