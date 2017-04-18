9b4ac11d94c8b6356d7b17b81bc614efb0c6f183
Dave Rhodes <dave.was.r@gmail.com>
2017-04-18 21:02:58 +0000

    Last of the Day 10 catches

--- data/Season 4/blazedglazed.ts
+++ data/Season 4/blazedglazed.ts
@@ -236,6 +236,9 @@ Season4.Runs.push(
             { Group: "Pokemon", Name: "Jigglypuff", Time: "9d19h37m" },
             { Group: "Pokemon", Name: "Skiploom", Time: "9d19h38m" },
             { Group: "Pokemon", Name: "Pelipper", Time: "9d20h53m" },
+            { Group: "Pokemon", Name: "Magneton", Time: "9d23h16m" },
+            { Group: "Pokemon", Name: "Tangela", Time: "9d23h39m" },
+            { Group: "Pokemon", Name: "Yanma", Time: "9d23h53m" },
 
             { Group: "Badges", Name: "Mineral Badge", Time: "", Attempts: 0, Image: "img/badges/glazed/mineral.png" },
             { Group: "Badges", Name: "Psychic Badge", Time: "", Attempts: 0, Image: "img/badges/glazed/psychic.png" },
