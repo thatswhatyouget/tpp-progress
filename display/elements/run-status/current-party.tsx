15f69bf0f28d08153e2003092e012f28648c7268
Dave Rhodes <dave.was.r@gmail.com>
2017-04-21 05:07:01 +0000

    Fixed type icon names

--- display/elements/run-status/current-party.tsx
+++ display/elements/run-status/current-party.tsx
@@ -96,7 +96,7 @@ namespace TPP.Display.Elements.RunStatus {
     class TypeImg extends React.PureComponent<{ type: string; }, {}> {
         render() {
             let type = ((this.props.type == "???" ? "Fairy" : this.props.type) || '').toLowerCase();
-            return <img src={`img/types/${type}.png`} />
+            return <img src={`img/type-icons/${type}.png`} />
         }
     }
 
