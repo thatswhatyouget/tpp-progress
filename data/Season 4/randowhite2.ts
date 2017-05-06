49228442291f0a752e4dd885d9549eafb36f16ee
Dave Rhodes <dave.was.r@gmail.com>
2017-05-06 21:18:32 +0000

    Added RandoWhite2 file

---
+++ data/Season 4/randowhite2.ts
@@ -0,0 +1,36 @@
+/// <reference path="../tpp-data.ts" />
+Season4.Runs.push(
+    {
+        RunName: "Randomized White 2",
+        ColorPrimary: "white",
+        ColorSecondary: "black",
+        Duration: "255d",
+        StartDate: "2017-06-03T21:00:00Z",
+        HostName: "???",
+        HostImage: "img/hosts/cly.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "New Unova",
+        TPPOrgLink: "http://twitchplayspokemon.org/",
+        Events: [
+            { Group: "Badges", Name: "Toxic Badge", Image: "img/badges/toxic.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Quake Badge", Image: "img/badges/quake.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Jet Badge", Image: "img/badges/jet.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Wave Badge", Image: "img/badges/wave.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Legend Badge", Image: "img/badges/legend.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Grimsley", Image: "img/trainers/black/grimsley.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Caitlin", Image: "img/trainers/black/caitlin.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Marshal", Image: "img/trainers/black/marshal.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "Iris", Image: "img/trainers/black/iris.png", Time: "", Attempts: 0 },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "", Attempts: 0, Party: [],
+                Image: "img/ribbons/champion.png"
+            },
+        ]
+    }
+);
