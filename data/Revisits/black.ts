27c6aade874d24de68993babee2303d766421e02
Dave Rhodes <dave.was.r@gmail.com>
2017-06-01 16:11:05 +0000

    Caught Frillish

--- data/Revisits/black.ts
+++ data/Revisits/black.ts
@@ -24,6 +24,7 @@ Revisits.Runs.push(
             { Name: "Excadrill", Time: "2017-06-01T07:12:00.000Z", Group: "Pokemon"},
             { Name: "Golbat", Time: "2017-06-01T07:14:00.000Z", Group: "Pokemon"},
             { Name: "Clefairy", Time: "2017-06-01T07:43:00.000Z", Group: "Pokemon"},
+            { Name: "Frillish", Time: "2017-06-01T16:07:00.000Z", Group: "Pokemon"},
         ],
         Revisit: { Collection: "Season 1", Run: "Black" },
         CopyEvents: ["Black"],
