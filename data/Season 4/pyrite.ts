5b5139cbd72c9a804745eb661bfb5b2a196a582e
Dave Rhodes <dave.was.r@gmail.com>
2017-08-16 21:31:13 +0000

    Evolved Exeggcute

--- data/Season 4/pyrite.ts
+++ data/Season 4/pyrite.ts
@@ -61,6 +61,7 @@ Season4.Runs.push(
             { Name: "Pineco", Time: "3d18h10m", Group: "Pokemon" },
             { Name: "Murkrow", Time: "3d18h34m", Group: "Pokemon" },
             { Name: "Stantler", Time: "3d18h41m", Group: "Pokemon" },
+            { Name: "Exeggutor", Time: "3d21h55m", Group: "Pokemon" },
 	            
             { Group: "Badges", Name: "Fog Badge", Image: "img/badges/fog.png", Time: "", Attempts: 0 },
             { Group: "Badges", Name: "Glacier Badge", Image: "img/badges/glacier.png", Time: "", Attempts: 0 },
