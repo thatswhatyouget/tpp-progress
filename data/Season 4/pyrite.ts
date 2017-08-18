51da1c0ff56319ab0cf435cd50ed74bc94fed465
Dave Rhodes <dave.was.r@gmail.com>
2017-08-18 12:49:25 +0000

    Earned Storm and Mineral badges

--- data/Season 4/pyrite.ts
+++ data/Season 4/pyrite.ts
@@ -71,9 +71,9 @@ Season4.Runs.push(
             { Name: "Poliwhirl", Time: "5d11m", Group: "Pokemon" },
             { Name: "Feraligatr", Time: "5d19m", Group: "Pokemon" },
             { Group: "Badges", Name: "Glacier Badge", Image: "img/badges/glacier.png", Time: "5d5h34m", Attempts: 13 },
-            { Group: "Badges", Name: "Storm Badge", Image: "img/badges/storm.png", Time: "", Attempts: 0 },
-            
-            { Group: "Badges", Name: "Mineral Badge", Image: "img/badges/mineral.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Storm Badge", Image: "img/badges/storm.png", Time: "5d10h14m", Attempts: 6 },
+            { Group: "Badges", Name: "Mineral Badge", Image: "img/badges/mineral.png", Time: "5d13h23m", Attempts: 5 },
+
             { Group: "Badges", Name: "Rising Badge", Image: "img/badges/rising.png", Time: "", Attempts: 0 },
             { Group: "Elite Four", Name: "Will", Image: "img/trainers/crystal/will.png", Time: "", Attempts: 0 },
             { Group: "Elite Four", Name: "Koga", Image: "img/trainers/crystal/koga.png", Time: "", Attempts: 0 },
