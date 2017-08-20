599d757f1f01d8ba27cb1ad5c05e614d55702de2
Dave Rhodes <dave.was.r@gmail.com>
2017-08-20 11:56:37 +0000

    Updated Storm Badge attempts

--- data/Season 4/pyrite.ts
+++ data/Season 4/pyrite.ts
@@ -71,7 +71,7 @@ Season4.Runs.push(
             { Name: "Poliwhirl", Time: "5d11m", Group: "Pokemon" },
             { Name: "Feraligatr", Time: "5d19m", Group: "Pokemon" },
             { Group: "Badges", Name: "Glacier Badge", Image: "img/badges/glacier.png", Time: "5d5h34m", Attempts: 13 },
-            { Group: "Badges", Name: "Storm Badge", Image: "img/badges/storm.png", Time: "5d10h14m", Attempts: 6 },
+            { Group: "Badges", Name: "Storm Badge", Image: "img/badges/storm.png", Time: "5d10h14m", Attempts: 7 },
             { Group: "Badges", Name: "Mineral Badge", Image: "img/badges/mineral.png", Time: "5d13h23m", Attempts: 5 },
             { Name: "Sunkern", Time: "5d23h28m", Group: "Pokemon" },
             { Name: "NidoranF", Time: "5d23h31m", Group: "Pokemon" },
