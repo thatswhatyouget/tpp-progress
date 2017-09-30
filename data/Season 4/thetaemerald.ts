a01711e916315981750866d1d4f384057b6619b2
Dave Rhodes <dave.was.r@gmail.com>
2017-09-30 22:04:32 +0000

    Started Theta Emerald EX

---
+++ data/Season 4/thetaemerald.ts
@@ -0,0 +1,37 @@
+/// <reference path="../tpp-data.ts" />
+Season4.Runs.push(
+    {
+        RunName: "Theta Emerald EX",
+        ColorPrimary: "#64ab64",
+        ColorSecondary: "darkgreen",
+        Duration: "255d",
+        StartDate: "2017-09-30T21:00:00Z",
+        HostName: "ACCPPQ",
+        HostImage: "img/hosts/a.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Hoenn",
+        Generation: 6,
+        Events: [
+            { Name: "Torchic", Time: "14m", Group: "Pokemon" },
+        
+            { Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Heat Badge", Image: "img/badges/heat.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Feather Badge", Image: "img/badges/feather.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Mind Badge", Image: "img/badges/mind.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Rain Badge", Image: "img/badges/rain.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Sidney", Image: "img/trainers/emerald/sidney.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Phoebe", Image: "img/trainers/emerald/phoebe.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Glacia", Image: "img/trainers/emerald/glacia.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Drake", Image: "img/trainers/emerald/drake.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "Wallace", Image: "img/trainers/emerald/wallace.png", Time: "", Attempts: 0 },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "61415", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion.png"
+            },
+        ]
+    }
+);
