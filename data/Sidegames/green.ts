851039b94e0477165b5398c43915eaeddff5b94f
Dave Rhodes <dave.was.r@gmail.com>
2017-10-15 19:33:40 +0000

    Added next sidegame

---
+++ data/Sidegames/green.ts
@@ -0,0 +1,32 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Green",
+        ColorPrimary: "green",
+        ColorSecondary: "darkgreen",
+        Duration: "255w",
+        StartDate: "2017-10-15T21:00:00Z",
+        HostName: "",
+        HostImage: "https://cdn.bulbagarden.net/upload/6/66/Spr_RG_Red_1.png",
+        BaseGame: "Red",
+        Region: "Kanto",
+        ContainsRunsFrom: ["Revisits", "Intermissions", "Season 4"],
+        SidegameId: "pg",
+        Events: [
+            { Name: "", Time: "", Group: "Pokemon" },
+            { Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Lorelei", Image: "img/trainers/red/lorelei.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Bruno", Image: "img/trainers/red/bruno.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Agatha", Image: "img/trainers/red/agatha.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Lance", Image: "img/trainers/red/lance.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "Rival", Image: "img/trainers/red/green.png", Time: "", Attempts: 0 },
+        ]
+    }
+);
