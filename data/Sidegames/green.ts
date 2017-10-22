91dc30a01227d0042246bd398de556fc7e612084
Dave Rhodes <dave.was.r@gmail.com>
2017-10-22 21:37:07 +0000

    Entered the Hall of Fame

--- data/Sidegames/green.ts
+++ data/Sidegames/green.ts
@@ -16,6 +16,11 @@ Sidegames.Runs.push(
         SidegameId: "pg",
         Events: [
             { Name: "TORTO", Class: "Squirtle", Time: "Tue Oct 17 2017 02:40:22 GMT+0000 (UTC)", Group: "Pokemon" },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2017-10-22T07:54:20+00:00", Attempts: 1, Party: [                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate:"2017-10-22T00:14:08+00:00"
+            },
             { Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", Time: "", Attempts: 0 },
             { Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", Time: "", Attempts: 0 },
             { Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", Time: "", Attempts: 0 },
