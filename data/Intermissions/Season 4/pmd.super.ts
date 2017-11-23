5f4d5c9ff28e0d098d072d2e4c0c2f8bac6ccbaf
Dave Rhodes <dave.was.r@gmail.com>
2017-11-23 05:31:51 +0000

    Added second PSMD Intermission

---
+++ data/Intermissions/Season 4/pmd.super.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Super Mystery Dungeon",
+        StartDate: "2017-11-23T00:49:45Z",
+        ColorPrimary: "#95C492",
+        ColorSecondary: "#153A74",
+        Duration: "2017-11-25T20:00:00Z",
+        // Unfinished: true,
+        HostName: "¡",
+        HostImage: "img/hosts/pmd/fennekin.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/5qo9dy/the_hosts_of_the_voices_before_season_4/",
+        Region: "Pokemon World",
+        Events: []
+    }
+);
