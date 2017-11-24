bed9b17b57ef87de147a47c0d58db119cbb5aca4
Dave Rhodes <dave.was.r@gmail.com>
2017-11-24 20:58:03 +0000

    Added second Tomodachi intermission

---
+++ data/Intermissions/Season 4/tomodachi.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Tomodachi Life",
+        StartDate: "2017-11-24T00:31:00Z",
+        ColorPrimary: "#ffed00",
+        ColorSecondary: "red",
+        Unfinished: true,
+        Duration: "2017-11-25T20:00:00Z",
+        HostName: "twdtm'',k",
+        HostImage: "img/hosts/other/miis/twdtm.png",
+        Region: "jrfc'vffrf Island",
+        Events: [],
+    }
+);
