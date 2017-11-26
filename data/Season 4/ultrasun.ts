14d01856aff0d11eb4f210f93730a22440751e7c
Dave Rhodes <dave.was.r@gmail.com>
2017-11-26 04:42:53 +0000

    Started Ultra Sun

---
+++ data/Season 4/ultrasun.ts
@@ -0,0 +1,67 @@
+/// <reference path="../tpp-data.ts" />
+Season3.Runs.push(
+    {
+        RunName: "Ultra Sun",
+        ColorSecondary: "black",
+        ColorPrimary: "#8b7446",
+        StartDate: "2017-11-25T21:00:00Z",
+        Duration: "255d",
+        BackgroundImage: "linear-gradient(to bottom, #eac07a 0%,#fefcff 30%,#fefcff 33%,#eac07a 40%,#000 100%)",
+        HostName: "Rrwq:",
+        HostImage: "https://cdn.bulbagarden.net/upload/0/05/Ultra_Sun_Ultra_Moon_Protagonist_male.png",
+        // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/5gwfvd/nigel_sprite_v_20/",
+        Region: "Alola",
+        Generation: 7,
+        Pokedex: "New Alola",
+        Events: [
+            { "Group": "Pokemon", "Name": "Popplio", "Time": "12m" },
+            { "Group": "Pokemon", "Name": "Grubbin", "Time": "49m" },
+            { "Group": "Pokemon", "Name": "Ledyba", "Time": "50m" },
+            { "Group": "Pokemon", "Name": "Yungoos", "Time": "57m" },
+            { "Group": "Pokemon", "Name": "Pikipek", "Time": "1h" },
+            { "Group": "Pokemon", "Name": "Caterpie", "Time": "1h8m" },
+            { "Group": "Pokemon", "Name": "Buneary", "Time": "1h11m" },
+            { "Group": "Pokemon", "Name": "Meowth", "Time": "3h40m" },
+            { "Group": "Pokemon", "Name": "Grimer", "Time": "3h47m" },
+            { "Group": "Pokemon", "Name": "Zorua", "Time": "4h8m" },
+            { "Group": "Pokemon", "Name": "Magnemite", "Time": "5h7m" },
+            { "Group": "Pokemon", "Name": "Mime Jr.", "Time": "7h17m" },
+            { "Group": "Pokemon", "Name": "Furfrou", "Time": "7h20m" },
+            { "Group": "Pokemon", "Name": "Wingull", "Time": "7h23m" },
+            
+            { "Group": "Badges", "Name": "Normalium Z", "Image": "img/z-crystals/Normalium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Fightinium Z", "Image": "img/z-crystals/Fightinium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Waterium Z", "Image": "img/z-crystals/Waterium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Firium Z", "Image": "img/z-crystals/Firium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Grassium Z", "Image": "img/z-crystals/Grassium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Rockium Z", "Image": "img/z-crystals/Rockium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Electrium Z", "Image": "img/z-crystals/Electrium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Ghostium Z", "Image": "img/z-crystals/Ghostium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Darkium Z", "Image": "img/z-crystals/Darkium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Groundium Z", "Image": "img/z-crystals/Groundium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Dragonium Z", "Image": "img/z-crystals/Dragonium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Elite Four", "Name": "Hala", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/hala.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
+            { "Group": "Elite Four", "Name": "Acerola", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/acerola.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
+            { "Group": "Elite Four", "Name": "Olivia", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/olivia.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
+            { "Group": "Elite Four", "Name": "Kahili", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/kahili.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Kahili-651482881" },
+            { "Group": "Champions", "Name": "Professor Kukui", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/kukui.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Prof-Kukui-649197164" },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion-alola.png",
+                FirstAttemptDate: ""
+            },
+            { "Group": "Elite Four Rematch", "Name": "Hala", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/rematch/hala.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
+            { "Group": "Elite Four Rematch", "Name": "Kahili", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/rematch/kahili.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Kahili-651482881" },
+            { "Group": "Elite Four Rematch", "Name": "Olivia", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/rematch/olivia.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
+            { "Group": "Elite Four Rematch", "Name": "Acerola", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/rematch/acerola.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
+            { "Group": "Champions", "Name": "Hau", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/hau.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Hau-651693374" },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion-alola.png",
+                FirstAttemptDate: ""
+            },
+        ]
+    }
+);
