0e70887e2efa46d464b24e65a8dcf91e4a7d1ac0
Dave Rhodes <dave.was.r@gmail.com>
2017-12-09 03:57:57 +0000

    Added Mario Maker intermission

---
+++ data/Intermissions/Season 4/mariomaker.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Super Mario Maker 3DS",
+        StartDate: "2017-12-09T01:16:00Z",
+        Duration: "2017-12-09T07:00:00Z",
+        Unfinished: true,
+        ColorPrimary: "yellow",
+        ColorSecondary: "red",
+        HostImage: "https://supermariorun.com/assets/img/stage/mario03.png",
+        HostName: "Mario",
+        Events: [
+        ]
+    }
+);
