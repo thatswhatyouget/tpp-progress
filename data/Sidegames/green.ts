2757d5ef22b39d5f69ebe57af2b893c959679fd5
Dave Rhodes <dave.was.r@gmail.com>
2018-02-04 17:19:26 +0000

    Caught NIDER

--- data/Sidegames/green.ts
+++ data/Sidegames/green.ts
@@ -33,6 +33,7 @@ Sidegames.Runs.push(
             { Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", Time: "2018-01-25T15:33:04+00:00", Attempts: 1 },
             { Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", Time: "2018-02-02T09:24:18+00:00", Attempts: 2 },
             { Name: "RECEN", Class: "Venonat", Time: "2018-02-03T23:31:21+00:00", Group: "Pokemon" },
+            { Name: "NIDER", Class: "NidoranF", Time: "2018-02-04T08:24:52+00:00", Group: "Pokemon" },
 
             { Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", Time: "", Attempts: 0 },
             { Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", Time: "", Attempts: 0 },
