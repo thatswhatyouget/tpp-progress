28ddb9519114e93a0e3b2769af83a96607c262e6
Dave Rhodes <dave.was.r@gmail.com>
2018-04-05 14:18:54 +0000

    Added Katamari

---
+++ data/Intermissions/Season 5/katamari.ts
@@ -0,0 +1,62 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Me & My Katamari",
+        StartDate: "2018-03-31T23:52:53Z",
+        ColorPrimary: "#f7971c",
+        ColorSecondary: "black",
+        Duration: "2018-04-02T00:00:50Z",
+        HostName: "Prince",
+        HostImage: "img/hosts/other/prince.png",
+        Region: "CAT Island",
+        Events: [
+            {
+                Group: "Badges",
+                Name: "35m37cm5mm",
+                Image: "img/katamari.png",
+                ImageSource: "https://www.redbubble.com/people/sonorosan/works/24851828-colorful-sticky-rolling-ball",
+                Time: "2018-04-01T04:39:31Z"
+            },
+            {
+                Group: "Badges",
+                Name: "129m37cm5mm",
+                Image: "img/katamari.png",
+                ImageSource: "https://www.redbubble.com/people/sonorosan/works/24851828-colorful-sticky-rolling-ball",
+                Time: "2018-04-01T10:10:07Z"
+            },
+            {
+                Group: "Badges",
+                Name: "16m26cm0mm",
+                Image: "img/katamari.png",
+                ImageSource: "https://www.redbubble.com/people/sonorosan/works/24851828-colorful-sticky-rolling-ball",
+                Time: "2018-04-01T12:09:46Z"
+            },
+            {
+                Group: "Hosts",
+                Name: "Marcy",
+                Time: "2018-04-01T12:20:00Z",
+                Image: "img/hosts/other/marcy.png"
+            },
+            {
+                Group: "Badges",
+                Name: "4250m21cm7mm",
+                Image: "img/katamari.png",
+                ImageSource: "https://www.redbubble.com/people/sonorosan/works/24851828-colorful-sticky-rolling-ball",
+                Time: "2018-04-01T21:48:50Z"
+            },
+            {
+                Group: "Hosts",
+                Name: "Lalala",
+                Time: "2018-04-01T22:24:38Z",
+                Image: "img/hosts/other/lalala.png"
+            },
+            {
+                Group: "Badges",
+                Name: "23cm2mm",
+                Image: "img/katamari.png",
+                ImageSource: "https://www.redbubble.com/people/sonorosan/works/24851828-colorful-sticky-rolling-ball",
+                Time: "2018-04-01T23:20:30Z"
+            },
+        ],
+    }
+);
