9636225cb10590fbb8069554a1e94e69c2f68a2f
Dave Rhodes <dave.was.r@gmail.com>
2018-04-09 17:28:50 +0000

    That wasn't GRAPE

--- data/Sidegames/green.ts
+++ data/Sidegames/green.ts
@@ -43,7 +43,7 @@ Sidegames.Runs.push(
             { Group: "Elite Four", Name: "XIBA", Image: "img/trainers/red/bruno.png", Time: "2018-04-06T21:28:03+00:00", Attempts: 1 },
             { Group: "Elite Four", Name: "KUCHIC", Image: "img/trainers/red/agatha.png", Time: "2018-04-07T12:15:33+00:00", Attempts: 1 },
             { Group: "Elite Four", Name: "WATERLU", Image: "img/trainers/red/lance.png", Time: "2018-04-08T06:59:43+00:00", Attempts: 1 },            
-            { Group: "Champions", Name: "GRAPE", Image: "img/trainers/red/green.png", Time: "2018-04-09T04:54:48+00:00", Attempts: 1 },
+            { Group: "Champions", Name: "GRAPE", Image: "img/trainers/red/blue.png", Time: "2018-04-09T04:54:48+00:00", Attempts: 1 },
         ]
     }
 );
