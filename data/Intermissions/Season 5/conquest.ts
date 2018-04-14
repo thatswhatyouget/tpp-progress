be08523c9819986b8b885ae1c7984bb478e711ca
Dave Rhodes <dave.was.r@gmail.com>
2018-04-14 16:32:14 +0000

    Added Conquest and Storm Silver

---
+++ data/Intermissions/Season 5/conquest.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Conquest Part 2",
+        StartDate: "Apr 14 2018 00:45:36 AM GMT",
+        ColorPrimary: "goldenrod",
+        ColorSecondary: "black",
+        Duration: "Apr 14 2018 8:00 PM GMT",
+        HostName: "ABnp3a",
+        HostImage: "img/hosts/abnp3a.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Ransei",
+        Events: [
+        ],
+        //Revisit: { Collection: "Intermissions", Run: "Conquest" },
+        CopyEvents: ["Conquest"],
+    }
+);
---
+++ data/Season 5/stormsilver.ts
@@ -0,0 +1,52 @@
+/// <reference path="../tpp-data.ts" />
+Season5.Runs.push(
+    {
+        RunName: "Storm Silver",
+        ColorPrimary: "#7a9cff",
+        ColorSecondary: "#278fe5",
+        BackgroundImage: "linear-gradient(#dfdfdf, #b5bdc5)",
+        StartDate: "2018-04-14T21:00:00Z",
+        Duration: "255d",
+        // HostName: "EWWYPPP",
+        // HostImage: "img/hosts/ewwyppp.png",
+        // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/6tdqij/the_hosts_of_the_voices_4th_midseason/",
+        Region: "Johto",
+        // AdditionalRegions: [{ Name: "Kanto", Time: "9d14h" }],
+        Generation: 4,
+        Events: [
+            { Group: "Badges", Name: "Zephyr Badge", Image: "img/badges/zephyr.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Hive Badge", Image: "img/badges/hive.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Plain Badge", Image: "img/badges/plain.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Fog Badge", Image: "img/badges/fog.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Glacier Badge", Image: "img/badges/glacier.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Storm Badge", Image: "img/badges/storm.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Mineral Badge", Image: "img/badges/mineral.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Rising Badge", Image: "img/badges/rising.png", Time: "", Attempts: 0 },
+
+            { Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", Time: "", Attempts: 0 },
+
+            { Group: "Elite Four", Name: "Will", Image: "img/trainers/crystal/will.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Koga", Image: "img/trainers/crystal/koga.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Bruno", Image: "img/trainers/crystal/bruno.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Karen", Image: "img/trainers/crystal/karen.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "Lance", Image: "img/trainers/crystal/lance.png", Time: "", Attempts: 0 },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, IDNo: "", Party: [
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: ""
+            },
+
+            { Group: "Champions", Name: "Red", Image: "img/trainers/crystal/red.png", Time: "", Attempts: 0 },
+
+            { Name: "", Time: "", Group: "Pokemon" },
+        ]
+    }
+);
