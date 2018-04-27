f8e381425c77a0b6dfa3bef2e8437a589b7aafd2
Dave Rhodes <dave.was.r@gmail.com>
2018-04-27 14:27:28 +0000

    Yet another HoF entry

--- data/Season 5/stormsilver.ts
+++ data/Season 5/stormsilver.ts
@@ -100,7 +100,7 @@ Season5.Runs.push(
             },
             <TPP.HallOfFame>{
                 Group: "Hall of Fame", Name: "Hall of Fame", Time: "12d 11h 37m", Attempts: 1, IDNo: "46212", Party: [
-                    { Pokemon: "Skuntank", Nickname: "F2P", Level: 66, Gender: "Female", Met: "Sprout Tower", OT: "AAEFFFF" },
+                    { Pokemon: "Skuntank", Nickname: "F2P", Level: 68, Gender: "Female", Met: "Sprout Tower", OT: "AAEFFFF" },
                     { Pokemon: "Farfetch'd", Nickname: "0011hIqU", Level: 68, Gender: "Female", Met: "Route 48", OT: "AAEFFFF" },
                     { Pokemon: "Gastrodon", Nickname: "AAAAAAAAA", Level: 71, Gender: "Male", Met: "Slowpoke Well", OT: "AAEFFFF" },
                     { Pokemon: "Lugia", Nickname: "r", Level: 69, Met: "Whirl Islands", OT: "AAEFFFF" },
@@ -109,6 +109,30 @@ Season5.Runs.push(
                 ],
                 Image: "img/ribbons/champion.png",
                 FirstAttemptDate: "12d 10h 51m"
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "12d 13h 53m", Attempts: 1, IDNo: "46212", Party: [
+                    { Pokemon: "Skuntank", Nickname: "F2P", Level: 68, Gender: "Female", Met: "Sprout Tower", OT: "AAEFFFF" },
+                    { Pokemon: "Gastrodon", Nickname: "AAAAAAAAA", Level: 71, Gender: "Male", Met: "Slowpoke Well", OT: "AAEFFFF" },
+                    { Pokemon: "Lugia", Nickname: "r", Level: 71, Met: "Whirl Islands", OT: "AAEFFFF" },
+                    { Pokemon: "Farfetch'd", Nickname: "0011hIqU", Level: 69, Gender: "Female", Met: "Route 48", OT: "AAEFFFF" },
+                    { Pokemon: "Froslass", Nickname: "00      UU", Level: 90, Gender: "Female", Met: "Slowpoke Well", OT: "AAEFFFF" },
+                    { Pokemon: "Typhlosion", Nickname: "222", Level: 95, Gender: "Male", Met: "New Bark Town", OT: "AAEFFFF" },
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "12d 13h 6m"
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 3, IDNo: "46212", Party: [
+                    { Pokemon: "Skuntank", Nickname: "F2P", Level: 68, Gender: "Female", Met: "Sprout Tower", OT: "AAEFFFF" },
+                    { Pokemon: "Gastrodon", Nickname: "AAAAAAAAA", Level: 71, Gender: "Male", Met: "Slowpoke Well", OT: "AAEFFFF" },
+                    { Pokemon: "Lugia", Nickname: "r", Level: 71, Met: "Whirl Islands", OT: "AAEFFFF" },
+                    { Pokemon: "Farfetch'd", Nickname: "0011hIqU", Level: 69, Gender: "Female", Met: "Route 48", OT: "AAEFFFF" },
+                    { Pokemon: "Froslass", Nickname: "00      UU", Level: 90, Gender: "Female", Met: "Slowpoke Well", OT: "AAEFFFF" },
+                    { Pokemon: "Typhlosion", Nickname: "222", Level: 95, Gender: "Male", Met: "New Bark Town", OT: "AAEFFFF" },
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "12d 15h 19m"
             },
 
 
