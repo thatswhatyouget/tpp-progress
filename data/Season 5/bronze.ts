d7b73d7b59c5afebe446cb294ab0b7dee0f0ab4f
Dave Rhodes <dave.was.r@gmail.com>
2018-06-10 18:54:25 +0000

    Earned the Bubble Badge

--- data/Season 5/bronze.ts
+++ data/Season 5/bronze.ts
@@ -11,10 +11,17 @@ Season5.Runs.push(
         // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/6tdqij/the_hosts_of_the_voices_4th_midseason/",
         Region: "Kohto",
         Events: [
+            { Group: "Badges", Name: "Bubble Badge", Image: "img/badges/bronze/bubble.png", Time: "0d 14h 56m", Attempts: 1 },
 
             { "Group": "Pokemon", "Name": "Pichu", "Time": "0d 0h 7m" },
             { "Group": "Pokemon", "Name": "Pidgey", "Time": "0d 3h 27m" },
             { "Group": "Pokemon", "Name": "Sentret", "Time": "0d 3h 31m" },
+            { "Group": "Pokemon", "Name": "Pidgeotto", "Time": "0d 14h 37m" },
+            { "Group": "Pokemon", "Name": "Bellsprout", "Time": "0d 17h 35m" },
+            { "Group": "Pokemon", "Name": "Zubat", "Time": "0d 17h 56m" },
+            { "Group": "Pokemon", "Name": "Sandshrew", "Time": "0d 18h 8m" },
+            { "Group": "Pokemon", "Name": "Pumbloom", "Time": "0d 19h 10m" },
+            { "Group": "Pokemon", "Name": "Caterpie", "Time": "0d 19h 17m" },
         ]
     }
 );
