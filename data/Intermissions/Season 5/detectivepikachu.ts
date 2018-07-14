bd2fd9dc6aab702ab6c3a2083b43dda8d67473a1
Dave Rhodes <dave.was.r@gmail.com>
2018-07-14 01:39:23 +0000

    Added Detecive Pikachu

---
+++ data/Intermissions/Season 5/detectivepikachu.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Detective Pikachu",
+        StartDate: "Jul 14 2018 2:00:00 PM GMT",
+        ColorPrimary: "#ffcd00",
+        ColorSecondary: "#693609",
+        Duration: "255w",
+        HostName: "Tim",
+        HostImage: "img/hosts/detectivepikachu.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/8yp0mk/tim_and_detective_pikachu_countdown_for_detective/",
+        // Region: "",
+        Events: [],
+    }
+);
