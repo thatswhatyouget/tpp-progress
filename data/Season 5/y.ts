62d851eee271aca7487e884353d89f5f9818bd78
Dave Rhodes <dave.was.r@gmail.com>
2018-08-12 03:48:57 +0000

    Began Randomized Y

---
+++ data/Season 5/y.ts
@@ -0,0 +1,47 @@
+/// <reference path="../tpp-data.ts" />
+Season5.Runs.push(
+    {
+        RunName: "Randomized Y",
+        ColorPrimary: "#e1002f",
+        ColorSecondary: "black",
+        Duration: "255d",
+        StartDate: "2018-08-11T21:00:00Z",
+        HostName: "♀266",
+        HostImage: "https://cdn.bulbagarden.net/upload/thumb/e/e1/XY_Serena.png/400px-XY_Serena.png",
+        // HostImageSource: "",
+        Region: "Kalos",
+        Generation: 6,
+        Events: [
+            { Group: "Badges", Name: "Bug Badge", Image: "img/badges/bug.png", Time: "0d 3h 56m", Attempts: 2 },
+            { Group: "Badges", Name: "Cliff Badge", Image: "img/badges/cliff.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Rumble Badge", Image: "img/badges/rumble.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Plant Badge", Image: "img/badges/plant.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Voltage Badge", Image: "img/badges/voltage.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Fairy Badge", Image: "img/badges/fairy.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Psychic Badge", Image: "img/badges/psychic.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Iceberg Badge", Image: "img/badges/iceberg.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Malva", Image: "img/trainers/x/malva.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Wikstrom", Image: "img/trainers/x/wikstrom.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Drasna", Image: "", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Siebold", Image: "img/trainers/x/siebold.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "Diantha", Image: "img/trainers/x/diantha.png", Time: "", Attempts: 0 },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion-kalos.png"
+            },
+            { "Group": "Pokemon", "Name": "Phantump", "Time": "0d 0h 11m" },
+            { "Group": "Pokemon", "Name": "Kabuto", "Time": "0d 0h 38m" },
+            { "Group": "Pokemon", "Name": "Drifloon", "Time": "0d 0h 43m" },
+            { "Group": "Pokemon", "Name": "Silcoon", "Time": "0d 0h 47m" },
+            { "Group": "Pokemon", "Name": "Claydol", "Time": "0d 0h 57m" },
+            { "Group": "Pokemon", "Name": "Xatu", "Time": "0d 1h 13m" },
+            { "Group": "Pokemon", "Name": "Probopass", "Time": "0d 1h 39m" },
+            { "Group": "Pokemon", "Name": "Magcargo", "Time": "0d 1h 44m" },
+            { "Group": "Pokemon", "Name": "Cleffa", "Time": "0d 1h 47m" },
+            { "Group": "Pokemon", "Name": "Omanyte", "Time": "0d 5h 5m" },
+            { "Group": "Pokemon", "Name": "Dewott", "Time": "0d 5h 27m" },
+            { "Group": "Pokemon", "Name": "Shinx", "Time": "0d 6h 10m" },
+        ]
+    }
+);
