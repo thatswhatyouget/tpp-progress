c4f3c260c0464c22d41671e5b8fc8b47047ba15f
Dave Rhodes <dave.was.r@gmail.com>
2018-08-13 05:47:45 +0000

    Filled out Sweet data

--- data/Sidegames/sweet.ts
+++ data/Sidegames/sweet.ts
@@ -2,10 +2,10 @@
 // Sidegames.Runs.push(
 //     {
 //         RunName: "Sweet",
-//         ColorPrimary: "",
-//         ColorSecondary: "",
+//         ColorPrimary: "#ffbcfc",
+//         ColorSecondary: "#5e2d40",
 //         Duration: "255w",
-//         StartDate: "",
+//         StartDate: "May 4 2018 12:12 AM",
 //         HostName: "Paula",
 //         HostImage: "img/hosts/paula.png",
 //         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/8qzejc/sweet_beta_green_bronze_host_sprites/",
@@ -14,7 +14,19 @@
 //         ContainsRunsFrom: ["Revisits", "Intermissions", "Season 5"],
 //         SidegameId: "psweet",
 //         Events: [
-//             { Name: "Pikachu", Time: "2017-03-16T01:25:35+00:00", Group: "Pokemon" },
+//             { Name: "Squirpie", Time: "May 5 2018 6:42 AM", Group: "Pokemon" },
+//             { Name: "Meowffin", Time: "May 6 2018 2:23 AM", Group: "Pokemon" },
+//             { Name: "Popsichu", Time: "May 16 2018 5:58 AM", Group: "Pokemon" },
+//             { Name: "Tartortle", Time: "Jun 7 2018 10:57 PM", Group: "Pokemon" },
+//             { Name: "Cupop", Time: "Jun 18 2018 11:48 AM", Group: "Pokemon" },
+//             { Name: "Cupcat", Time: "Jun 22 2018 1:11 PM", Group: "Pokemon" },
+//             { Name: "Bananaby", Time: "Jul 16 2018 5:42 PM", Group: "Pokemon" },
+//             { Name: "Piestoise", Time: "Aug 6 2018 8:55 AM", Group: "Pokemon" },
+//             { Name: "Smorelax", Time: "Aug 8 2018 9:23 AM", Group: "Pokemon" },
+            
+//             { Group: "Badges", Name: "Jewel Badge", Image: "img/badges/sweet/jewel.png", Time: "2018-06-20T03:33:26+00:00", Attempts: 6 },
+//             { Group: "Badges", Name: "Soda Badge", Image: "img/badges/sweet/soda.png", Time: "Jul 13 2018 12:09 AM", Attempts: 1 },
+//             { Group: "Badges", Name: "Yellow Badge", Image: "img/badges/sweet/yellow.png", Time: "Jul 23 2018 2:34 AM", Attempts: 1 },
             
 //             { Group: "Elite Four", Name: "Lorelei", Image: "img/trainers/firered/lorelei.png", Time: "", Attempts: 0 },
 //             { Group: "Elite Four", Name: "Bruno", Image: "img/trainers/firered/bruno.png", Time: "", Attempts: 0 },
