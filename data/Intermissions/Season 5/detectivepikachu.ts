b61260eb18dd7dd3a582debebf4504cc6f158fe5
Dave Rhodes <dave.was.r@gmail.com>
2018-09-10 14:16:19 +0000

    Detective Pikachu has ended

--- data/Intermissions/Season 5/detectivepikachu.ts
+++ data/Intermissions/Season 5/detectivepikachu.ts
@@ -5,11 +5,12 @@ Intermissions.Runs.push(
         StartDate: "Sept 8 2018 2:00:00 PM GMT",
         ColorPrimary: "#ffcd00",
         ColorSecondary: "#693609",
-        Duration: "255w",
+        Duration: "1d22h47m",
         HostName: "Tim",
         HostImage: "img/hosts/detectivepikachu.png",
         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/8yp0mk/tim_and_detective_pikachu_countdown_for_detective/",
         Region: "Ryme City",
+        //TODO: Chapters
         Events: [],
     }
 );
