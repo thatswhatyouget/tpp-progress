eaf6c0764a2a540d052ec9a844d7e9b543295b47
Dave Rhodes <dave.was.r@gmail.com>
2018-09-13 05:21:02 +0000

    Added Flora Sky

---
+++ data/Season 5/florasky.ts
@@ -0,0 +1,19 @@
+/// <reference path="../tpp-data.ts" />
+Season5.Runs.push(
+    {
+        RunName: "Flora Sky",
+        StartDate: "2018-10-13T21:00:00Z",
+        Duration: "255d",
+        ColorPrimary: "#71eeb8",
+        ColorSecondary: "#ee71a7",
+        HostImage: "",
+        // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/64fn91/ahuni_our_adorably_sweet_protagonist_animated/",
+        HostName: "",
+        Region: "Flora",
+        BaseGame: "Emerald",
+        // Pokedex: "Flora",
+        Events: [
+
+         ]
+    }
+);
