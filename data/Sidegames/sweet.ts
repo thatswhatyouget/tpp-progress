c0a57036ad820e2695e9d287eb7a6db2775a05ee
Dave Rhodes <dave.was.r@gmail.com>
2019-01-25 07:38:24 +0000

    Caught Mewberry

--- data/Sidegames/sweet.ts
+++ data/Sidegames/sweet.ts
@@ -28,6 +28,7 @@ Sidegames.Runs.push(
             { Name: "Reginger", Time: "2018-09-23T09:30:13+00:00", Group: "Pokemon" },
             { Name: "Airblim", Time: "2018-09-29T09:16:03+00:00", Group: "Pokemon" },
             { Name: "Cottonbun", Time: "2019-01-20T20:19:16+00:00", Group: "Pokemon" },
+            { Name: "Mewberry", Time: "2019-01-24T14:04:01+00:00", Group: "Pokemon" },
 
             { Group: "Badges", Name: "Jewel Badge", Image: "img/badges/boulder.png", Time: "2018-06-20T03:33:26+00:00", Attempts: 6 },
             { Group: "Badges", Name: "Soda Badge", Image: "img/badges/cascade.png", Time: "Jul 13 2018 12:09 AM", Attempts: 1 },
