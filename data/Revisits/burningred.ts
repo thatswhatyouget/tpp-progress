971e6d9e9d05518e2958c2e032c4ee953ce07ea9
Dave Rhodes <dave.was.r@gmail.com>
2019-03-11 01:17:06 +0000

    Started Burning Red Revisit

---
+++ data/Revisits/burningred.ts
@@ -0,0 +1,20 @@
+/// <reference path="../tpp-data.ts" />
+Revisits.Runs.push(
+    {
+        RunName: "Burning Red Revisit",
+        ColorPrimary: "#ff712d",
+        ColorSecondary: "#8c5856",
+        Duration: "255d",
+        StartDate: "2019-03-11T00:40:00Z",
+        HostName: "KAY",
+        HostImage: "img/hosts/kay.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/aq2qmu/kay_the_planeswalker/",
+        BaseGame: "FireRed",
+        Region: "Kanto",
+        Events: [
+
+        ],
+        Revisit: { Collection: "Season 6", Run: "Burning Red" },
+        CopyEvents: ["Burning Red"],
+    }
+);
