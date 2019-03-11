90315099da3359dd0c8e695803207b057ce470ee
Dave Rhodes <dave.was.r@gmail.com>
2019-03-11 13:42:16 +0000

    Sevii Island Catches

--- data/Revisits/burningred.ts
+++ data/Revisits/burningred.ts
@@ -12,7 +12,16 @@ Revisits.Runs.push(
         BaseGame: "FireRed",
         Region: "Kanto",
         Events: [
-
+            { "Group": "Pokemon", "Name": "Slugma", "Time": "2019-03-11T02:02:00.000Z" },
+            { "Group": "Pokemon", "Name": "Sentret", "Time": "2019-03-11T03:56:00.000Z" },
+            { "Group": "Pokemon", "Name": "Natu", "Time": "2019-03-11T04:10:00.000Z" },
+            { "Group": "Pokemon", "Name": "Yanma", "Time": "2019-03-11T04:12:00.000Z" },
+            { "Group": "Pokemon", "Name": "Wooper", "Time": "2019-03-11T04:28:00.000Z" },
+            { "Group": "Pokemon", "Name": "Swinub", "Time": "2019-03-11T04:55:00.000Z" },
+            { "Group": "Pokemon", "Name": "Blissey", "Time": "2019-03-11T05:00:00.000Z" },
+            { "Group": "Pokemon", "Name": "Delibird", "Time": "2019-03-11T06:31:00.000Z" },
+            { "Group": "Pokemon", "Name": "Wobbuffet", "Time": "2019-03-11T08:01:00.000Z" },
+            { "Group": "Pokemon", "Name": "Hoppip", "Time": "2019-03-11T09:37:00.000Z" },
         ],
         Revisit: { Collection: "Season 6", Run: "Burning Red" },
         CopyEvents: ["Burning Red"],
