4704a54a7297f406b65d6a13a60bb596fb8d5f25
Dave Rhodes <dave.was.r@gmail.com>
2019-03-31 08:28:45 +0000

    Earned the Stone Badge

--- data/Intermissions/Season 6/metrosapph.ts
+++ data/Intermissions/Season 6/metrosapph.ts
@@ -2,8 +2,8 @@
 Intermissions.Runs.push(
     {
         RunName: "Metronome Sapphire",
-        ColorPrimary: "#9b59bb",
-        ColorSecondary: "#713f89",
+        ColorPrimary: "#5b59bb",
+        ColorSecondary: "#313f89",
         StartDate: "2019-03-31T00:43:00Z",
         Duration: "2019-04-03T00:00:00Z",
         HostName: "MARINA",
@@ -13,6 +13,13 @@ Intermissions.Runs.push(
         Generation: 3,
         Pokedex: "National",
         Events: [
+            { Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png", Time: "2019-03-31T06:04:00.000Z", Attempts: 1 },
+
+            { "Group": "Pokemon", "Name": "Gligar", "Time": "2019-03-31T01:05:00.000Z" },
+            { "Group": "Pokemon", "Name": "Slowking", "Time": "2019-03-31T03:16:00.000Z" },
+            { "Group": "Pokemon", "Name": "Kingdra", "Time": "2019-03-31T03:32:00.000Z" },
+            { "Group": "Pokemon", "Name": "Mew", "Time": "2019-03-31T04:35:00.000Z" },
+            { "Group": "Pokemon", "Name": "Gengar", "Time": "2019-03-31T04:37:00.000Z" }
         ],
     }
 );
