1e19593fcf32fd899103074c9352d32ab517f1de
Dave Rhodes <dave.was.r@gmail.com>
2019-03-31 03:14:12 +0000

    Started Metronome Sapphire

---
+++ data/Intermissions/Season 6/metrosapph.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Metronome Sapphire",
+        ColorPrimary: "#9b59bb",
+        ColorSecondary: "#713f89",
+        StartDate: "2019-03-31T00:43:00Z",
+        Duration: "2019-04-03T00:00:00Z",
+        HostName: "MARINA",
+        HostImage: "img/hosts/hnv.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/9pkxol/hannah_the_ninja_master/",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "National",
+        Events: [
+        ],
+    }
+);
