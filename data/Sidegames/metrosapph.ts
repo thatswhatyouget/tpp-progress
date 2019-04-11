d893817787cc5ec243530e399e729f5cd9e0ee04
Dave Rhodes <dave.was.r@gmail.com>
2019-04-11 15:53:37 +0000

    Entered the Hall of Fame again

--- data/Sidegames/metrosapph.ts
+++ data/Sidegames/metrosapph.ts
@@ -13,7 +13,20 @@ Sidegames.Runs.push(
         Region: "Hoenn",
         Generation: 3,
         Pokedex: "National",
-        Events: [],
+        Events: [
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2019-04-11T11:23:42-04:00", IDNo: "01228", Attempts: 1, Party: [
+                    { Pokemon: "Gengar", Nickname: "IIHBAAAMMM", Level: 100, Gender: "Female", Number: 94, IDNo: "01228" },
+                    { Pokemon: "Slowking", Nickname: "AAAAAAAAAA", Level: 100, Gender: "Female", Number: 199, IDNo: "01228" },
+                    { Pokemon: "Mew", Nickname: "MMMMMMMMGG", Level: 100, Number: 151, IDNo: "01228" },
+                    { Pokemon: "Scizor", Nickname: "JJJJJJJJJJ", Level: 100, Gender: "Female", Number: 212, IDNo: "01228" },
+                    { Pokemon: "Furret", Nickname: "CCCCCCCBBB", Level: 100, Gender: "Female", Number: 162, IDNo: "01228" },
+                    { Pokemon: "Volbeat", Nickname: "YYRRRRRQQ", Level: 100, Gender: "Male", Number: 313, IDNo: "01228" },
+                ],
+                FirstAttemptDate: "2019-04-06T02:42:56+00:00",
+                Image: "img/ribbons/champion.png"
+            },
+        ],
         SidegameId: "metrosapph",
         Revisit: { Collection: "Intermissions", Run: "Metronome Sapphire" },
         CopyEvents: ["Metronome Sapphire"],
