8b7345a991c09ff1c278ac5e79bdba50ca1f1f8f
Dave Rhodes <dave.was.r@gmail.com>
2019-04-14 00:57:49 +0000

    Started Volt White

---
+++ data/Season 6/voltwhite.ts
@@ -0,0 +1,69 @@
+/// <reference path="../tpp-data.ts" />
+Season6.Runs.push(
+    {
+        RunName: "Volt White",
+        ColorPrimary: "#E5E5E5",
+        ColorSecondary: "#C5C5C5",
+        Duration: "255w",
+        StartDate: "2019-04-13T21:00:00Z",
+        HostName: "AAQ",
+        HostImage: "https://cdn.bulbagarden.net/upload/1/16/Spr_BW_Hilda_2.png",
+        // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "National",
+        Events: [
+            { Group: "Badges", Name: "Trio Badge", Image: "img/badges/trio.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Quake Badge", Image: "img/badges/quake.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Jet Badge", Image: "img/badges/jet.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Freeze Badge", Image: "img/badges/freeze.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Legend Badge", Image: "img/badges/legend.png", Time: "", Attempts: 0 },
+
+            { Group: "Elite Four", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Grimsley", Image: "img/trainers/black/grimsley.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Caitlin", Image: "img/trainers/black/caitlin.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Marshal", Image: "img/trainers/black/marshal.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "N", Image: "img/trainers/black/n.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "Ghetsis", Image: "img/trainers/black/ghetsis.png", Time: "", Attempts: 0 },
+
+            { Group: "Elite Four Rematch", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Time: "", Attempts: 0, Class: "Rematch" },
+            { Group: "Elite Four Rematch", Name: "Grimsley", Image: "img/trainers/black/grimsley.png", Time: "", Attempts: 0, Class: "Rematch" },
+            { Group: "Elite Four Rematch", Name: "Caitlin", Image: "img/trainers/black/caitlin.png", Time: "", Attempts: 0, Class: "Rematch" },
+            { Group: "Elite Four Rematch", Name: "Marshal", Image: "img/trainers/black/marshal.png", Time: "", Attempts: 0, Class: "Rematch" },
+
+            { Group: "Champions", Name: "Alder", Image: "img/trainers/black/alder.png", Time: "", Attempts: 0 },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "First Game Clear", Time: "", IDNo: "07180", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/event.png"
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "07180", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion.png"
+            },
+
+            { "Group": "Pokemon", "Name": "Snivy", "Time": "2019-04-13T21:04:00.000Z" },
+            { "Group": "Pokemon", "Name": "Zigzagoon", "Time": "2019-04-13T21:41:00.000Z" },
+            { "Group": "Pokemon", "Name": "Bidoof", "Time": "2019-04-13T21:53:00.000Z" },
+            { "Group": "Pokemon", "Name": "Sentret", "Time": "2019-04-13T21:54:00.000Z" },
+            { "Group": "Pokemon", "Name": "Starly", "Time": "2019-04-13T21:58:00.000Z" },
+            { "Group": "Pokemon", "Name": "Pidgey", "Time": "2019-04-13T22:05:00.000Z" },
+            { "Group": "Pokemon", "Name": "Lillipup", "Time": "2019-04-13T22:12:00.000Z" },
+            { "Group": "Pokemon", "Name": "Patrat", "Time": "2019-04-13T22:26:00.000Z" },
+            { "Group": "Pokemon", "Name": "Caterpie", "Time": "2019-04-13T22:27:00.000Z" },
+            { "Group": "Pokemon", "Name": "Weedle", "Time": "2019-04-13T23:02:00.000Z" },
+            { "Group": "Pokemon", "Name": "Panpour", "Time": "2019-04-14T00:36:00.000Z" },
+            { "Group": "Pokemon", "Name": "Spearow", "Time": "2019-04-14T00:41:00.000Z" },
+            { "Group": "Pokemon", "Name": "Poochyena", "Time": "2019-04-14T00:44:00.000Z" },
+            { "Group": "Pokemon", "Name": "Meowth", "Time": "2019-04-14T00:46:00.000Z" },
+            { "Group": "Pokemon", "Name": "Kricketot", "Time": "2019-04-14T00:48:00.000Z" },
+            { "Group": "Pokemon", "Name": "Wurmple", "Time": "2019-04-14T00:53:00.000Z" },
+        ]
+    }
+);
