a84dcfd209327446a0f3db6ab80274467fe010bc
m4_used_rollout <dave.was.r@gmail.com>
2019-07-13 21:25:43 +0000

    Started Pokemon XG

---
+++ data/Season 6/xg.ts
@@ -0,0 +1,128 @@
+/// <reference path="../tpp-data.ts" />
+Season6.Runs.push(
+    {
+        RunName: "XG",
+        ColorPrimary: "#793165",
+        ColorSecondary: "#532245",
+        Duration: "255w",
+        StartDate: "2019-07-13T21:00:00Z",
+        HostName: "Stars",
+        HostImage: "img/hosts/abbbcc.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3woea1/alphab%C4%93tos_av%C4%ABci/",
+        Region: "Orre",
+        Generation: 3,
+        Events: [
+            {
+                "Group": "Bosses",
+                "Image": "img/trainers/xd/lovrina.png",
+                "ImageSource": "http://djshop.smackjeeves.com/comics/381730/rui-lovrina/",
+                "Name": "Lovrina",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Bosses",
+                "Image": "img/trainers/xd/mirorb.png",
+                "ImageSource": "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/MirorB.png.html",
+                "Name": "Miror B.",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Bosses",
+                "Image": "img/trainers/xd/exol.png",
+                "ImageSource": "http://s1292.photobucket.com/user/Apollothemuse/media/Sprites/BulkyPeonSprite_zps84376303.png.html",
+                "Name": "Exol",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Bosses",
+                "Image": "img/trainers/xd/snattle.png",
+                "ImageSource": "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherSnattle.png.html",
+                "Name": "Snattle",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Bosses",
+                "Image": "img/trainers/xd/mirorb.png",
+                "ImageSource": "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/MirorB.png.html",
+                "Name": "Miror B.",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Bosses",
+                "Image": "img/trainers/colosseum/gonzap.png",
+                "ImageSource": "http://forum.yugiohcardmaker.net/topic/200460-colosseum-sprites/",
+                "Name": "Gonzap",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Bosses",
+                "Image": "img/trainers/xd/gorigan.png",
+                "ImageSource": "http://billyk40.deviantart.com/art/Gorigan-BW-257880611",
+                "Name": "Gorigan",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Final Bosses",
+                "Image": "img/trainers/xd/lovrina.png",
+                "ImageSource": "http://djshop.smackjeeves.com/comics/381730/rui-lovrina/",
+                "Name": "Lovrina",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Final Bosses",
+                "Image": "img/trainers/xd/snattle.png",
+                "ImageSource": "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherSnattle.png.html",
+                "Name": "Snattle",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Final Bosses",
+                "Image": "img/trainers/xd/ardos.png",
+                "Name": "Ardos",
+                "ImageSource": "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherArdos.png.html",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Final Bosses",
+                "Image": "img/trainers/xd/gorigan.png",
+                "ImageSource": "http://billyk40.deviantart.com/art/Gorigan-BW-257880611",
+                "Name": "Gorigan",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Final Bosses",
+                "Image": "img/trainers/xd/eldes.png",
+                "ImageSource": "http://billyk40.deviantart.com/art/Eldes-BW-262784197",
+                "Name": "Eldes",
+                "Time": "",
+                "Attempts": 0
+            },
+            {
+                "Group": "Champions",
+                "Image": "img/trainers/xd/greevil.png",
+                "ImageSource": "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/greevil.gif.html",
+                "Name": "Greevil",
+                "Time": "",
+                "Attempts": 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "First Game Clear", Time: "", Party: [
+                ],
+                Image: "img/ribbons/national.png"
+            },
+            { "Group": "Pokemon", "Name": "Eevee", "Time": "2019-07-13T21:00:00Z" },
+            { "Group": "Pokemon", "Name": "Zigzagoon", "Time": "2019-07-13T21:00:00Z" },
+        ]
+    }
+);
