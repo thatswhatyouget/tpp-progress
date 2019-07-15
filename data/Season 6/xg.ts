67ef833cee60b0dbd636801ed9099ddee960d66f
m4_used_rollout <dave.was.r@gmail.com>
2019-07-15 14:09:40 +0000

    Defeated Miror B

--- data/Season 6/xg.ts
+++ data/Season 6/xg.ts
@@ -25,8 +25,8 @@ Season6.Runs.push(
                 "Image": "img/trainers/xd/mirorb.png",
                 "ImageSource": "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/MirorB.png.html",
                 "Name": "Miror B.",
-                "Time": "",
-                "Attempts": 0
+                "Time": "2019-07-14T17:37:49.747Z",
+                "Attempts": 1
             },
             {
                 "Group": "Bosses",
@@ -34,7 +34,7 @@ Season6.Runs.push(
                 "ImageSource": "http://s1292.photobucket.com/user/Apollothemuse/media/Sprites/BulkyPeonSprite_zps84376303.png.html",
                 "Name": "Exol",
                 "Time": "",
-                "Attempts": 0
+                "Attempts": 1 //Turns out Exol is loseable
             },
             {
                 "Group": "Bosses",
@@ -143,6 +143,26 @@ Season6.Runs.push(
             { Group: "Pokemon", Name: "Murkrow", Time: "2019-07-14T11:37:20.526Z" },
             { Group: "Pokemon", Name: "Misdreavus", Time: "2019-07-14T12:13:45.759Z" },
             { Group: "Pokemon", Name: "Clefairy", Time: "2019-07-14T13:17:07.217Z" },
+            { Group: "Pokemon", Name: "Grovyle", Time: "2019-07-14T13:53:45.460Z" },
+            { Group: "Pokemon", Name: "Linoone", Time: "2019-07-14T16:17:01.492Z" },
+            { Group: "Pokemon", Name: "Ludicolo", Time: "2019-07-14T17:38:25.297Z" },
+            { Group: "Pokemon", Name: "Pelipper", Time: "2019-07-14T18:47:45.958Z" },
+            { Group: "Pokemon", Name: "Octillery", Time: "2019-07-14T19:03:20.572Z" },
+            { Group: "Pokemon", Name: "Crawdaunt", Time: "2019-07-14T19:42:07.102Z" },
+            { Group: "Pokemon", Name: "Torkoal", Time: "2019-07-14T20:00:49.917Z" },
+            { Group: "Pokemon", Name: "Metang", Time: "2019-07-14T20:30:37.342Z" },
+            { Group: "Pokemon", Name: "Scyther", Time: "2019-07-14T20:37:22.688Z" },
+            { Group: "Pokemon", Name: "Gligar", Time: "2019-07-14T20:40:49.555Z" },
+            { Group: "Pokemon", Name: "Nosepass", Time: "2019-07-14T22:16:02.910Z" },
+            { Group: "Pokemon", Name: "Staryu", Time: "2019-07-14T22:55:58.819Z" },
+            { Group: "Pokemon", Name: "Sneasel", Time: "2019-07-14T23:09:50.746Z" },
+            { Group: "Pokemon", Name: "Combusken", Time: "2019-07-15T00:29:30.040Z" },
+            { Group: "Pokemon", Name: "Swellow", Time: "2019-07-15T01:54:13.136Z" },
+            { Group: "Pokemon", Name: "Granbull", Time: "2019-07-15T03:26:26.790Z" },
+            { Group: "Pokemon", Name: "Marshtomp", Time: "2019-07-15T05:19:29.467Z" },
+            { Group: "Pokemon", Name: "Nincada", Time: "2019-07-15T07:44:14.017Z" },
+            { Group: "Pokemon", Name: "Exeggutor", Time: "2019-07-15T08:11:59.969Z" },
+            { Group: "Pokemon", Name: "Vibrava", Time: "2019-07-15T08:27:10.222Z" },
         ]
     }
 );
