a9af4785be9359801bc4c5c45a96a844bd38807f
m4_used_rollout <dave.was.r@gmail.com>
2019-08-10 01:18:14 +0000

    Added Pokemon Rubin

---
+++ data/Intermissions/Season 6/rubin.ts
@@ -0,0 +1,46 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Rubin (Debug Version)",
+        ColorPrimary: "#bb595b",
+        ColorSecondary: "#893f31",
+        StartDate: "2019-08-09T18:24:03Z",
+        Duration: "2019-08-09T20:09:03Z",
+        HostName: "PETRA",
+        HostImage: "https://cdn.bulbagarden.net/upload/thumb/0/05/May_Go-Goggles_ORAS_OD.png/200px-May_Go-Goggles_ORAS_OD.png",
+        // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/9pkxol/hannah_the_ninja_master/",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "National",
+        Events: [
+            { Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png", Time: "2019-03-31T06:04:00.000Z", Attempts: 0 },
+            { Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png", Time: "2019-03-31T10:07:00.000Z", Attempts: 0 },
+            { Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png", Time: "2019-03-31T11:43:00.000Z", Attempts: 0 },
+            { Group: "Badges", Name: "Heat Badge", Image: "img/badges/heat.png", Time: "2019-03-31T20:25:00.000Z", Attempts: 0 },
+            { Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png", Time: "2019-03-31T21:48:00.000Z", Attempts: 0 },
+            { Group: "Badges", Name: "Feather Badge", Image: "img/badges/feather.png", Time: "2019-04-01T04:11:00.000Z", Attempts: 0 },
+            { Group: "Badges", Name: "Mind Badge", Image: "img/badges/mind.png", Time: "2019-04-01T18:23:00.000Z", Attempts: 0 },
+            { Group: "Badges", Name: "Rain Badge", Image: "img/badges/rain.png", Time: "2019-04-02T01:24:00.000Z", Attempts: 0 },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2019-08-09T19:15:30.00Z", IDNo: "39091", Attempts: 0, Party: [
+                    { Pokemon: "Geckarbor", Class: "Treecko", Level: 3, Gender: "Male", Number: 1, IDNo: "39091" },
+                ],
+                Image: "img/ribbons/champion.png"
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2019-08-09T19:30:06.00Z", IDNo: "39091", Attempts: 0, Party: [
+                    { Pokemon: "Despotar", Class: "Tyranitar", Level: 11, Gender: "Female", IDNo: "39091" },
+                    { Pokemon: "Zigzachs", Class: "Zigzagoon", Level: 95, Number: 12, Gender: "Male", IDNo: "39091" },
+                    { Pokemon: "Hornliu", Class: "Weedle", Level: 2, Gender: "Male", IDNo: "39091" },
+                    { Pokemon: "NidoranM", Level: 3, Gender: "Male", IDNo: "39091" },
+                    { Pokemon: "Jirachi", Level: 1, Number: 201, IDNo: "39091" },
+                    { Pokemon: "Latios", Level: 1, Number: 197, Gender: "Male", IDNo: "39091" },
+
+                ],
+                Image: "img/ribbons/champion.png"
+            },
+
+        ],
+    }
+);
