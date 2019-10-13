134bcc2fda7f8239c7db6f7006ca91fa83351026
m4_used_rollout <dave.was.r@gmail.com>
2019-10-13 03:47:46 +0000

    Started Randomized Ultra Moon

---
+++ data/Season 6/rum.ts
@@ -0,0 +1,43 @@
+/// <reference path="../tpp-data.ts" />
+Season6.Runs.push(
+    {
+        RunName: "Randomized Ultra Moon",
+        ColorSecondary: "black",
+        ColorPrimary: "#44a8e1",
+        StartDate: "2019-10-12T21:00:00Z",
+        Duration: "255w",
+        BackgroundImage: "linear-gradient(to bottom, #94e9e8 0%,#e4fcff 30%,#fefcff 33%,#44a8e1 40%,#94e9e8 100%)",
+        HostName: "VRG",
+        Class: "squarehost",
+        HostImage: "img/hosts/vrg.png",
+        // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/7g24ka/rorick_rotor_roark_rowans_sprites/",
+        Region: "Alola",
+        Generation: 7,
+        Pokedex: "New Alola",
+        Events: [
+            { "Group": "Badges", "Name": "Normalium Z", "Image": "img/z-crystals/Normalium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Fightinium Z", "Image": "img/z-crystals/Fightinium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Waterium Z", "Image": "img/z-crystals/Waterium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Firium Z", "Image": "img/z-crystals/Firium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Grassium Z", "Image": "img/z-crystals/Grassium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Rockium Z", "Image": "img/z-crystals/Rockium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Electrium Z", "Image": "img/z-crystals/Electrium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Ghostium Z", "Image": "img/z-crystals/Ghostium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Darkium Z", "Image": "img/z-crystals/Darkium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Dragonium Z", "Image": "img/z-crystals/Dragonium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Fairium Z", "Image": "img/z-crystals/Fairium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Badges", "Name": "Groundium Z", "Image": "img/z-crystals/Groundium Z.png", "Time": "", "Attempts": 0 },
+            { "Group": "Elite Four", "Name": "Molayne", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/molayne.png", "ImageSource": "https://www.deviantart.com/art/Custom-sprite-Molayne-653031564" },
+            { "Group": "Elite Four", "Name": "Olivia", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/olivia.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
+            { "Group": "Elite Four", "Name": "Acerola", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/acerola.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621" },
+            { "Group": "Elite Four", "Name": "Kahili", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/kahili.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Kahili-651482881" },
+            { "Group": "Champions", "Name": "Hau", "Time": "", "Attempts": 0, "Image": "img/trainers/sun/hau.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Hau-651693374" },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion-alola.png",
+                FirstAttemptDate: ""
+            },
+        ]
+    }
+)
