b9e4f1308ff5cf181a9ae64c878c34d7264a59ff
m4_used_rollout <dave.was.r@gmail.com>
2020-02-15 14:43:21 +0000

    Earned the Thunder and Rainbow Badges

--- data/Season 7/gauntlet-red.ts
+++ data/Season 7/gauntlet-red.ts
@@ -4,7 +4,7 @@ Season7.Runs.push(
         RunName: "Gauntlet Red",
         ColorPrimary: "#c0504d",
         ColorSecondary: "#8c3836",
-        BackgroundImage: "radial-gradient(ellipse at bottom, rgba(255,255,255,0) 0%, rgba(255,255,255,0) 41%, rgba(255,255,255,1) 45%, rgba(255,255,255,0) 70%)",
+        BackgroundImage: "radial-gradient(ellipse at left bottom, rgba(255,255,255,0) 0%, rgba(255,255,255,0) 41%, rgba(255,255,255,1) 45%, rgba(255,255,255,0) 70%)",
         StartDate: "2020-02-13T01:22:00Z",
         Duration: "16d 7h 52m",
         HostName: "RED",
@@ -35,14 +35,16 @@ Season7.Runs.push(
                 Name: "Thunder Badge",
                 Image: "img/badges/thunder.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: ""
+                Time: "2020-02-14T04:41:11.307Z",
+                Attempts: 2
             },
             {
                 Group: "Badges",
                 Name: "Rainbow Badge",
                 Image: "img/badges/rainbow.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: ""
+                Time: "2020-02-15T05:33:26.036Z",
+                Attempts: 1
             },
             {
                 Group: "Badges",
@@ -112,45 +114,35 @@ Season7.Runs.push(
                 ],
                 Image: "img/ribbons/champion.png"
             },
-            { Name: "Charmander", Time: "2020-02-13T01:30:31.250Z", Group: "Pokemon" },
-            { Name: "Spearow", Time: "2020-02-13T05:14:47.168Z", Group: "Pokemon" },
-            { Name: "NidoranM", Time: "2020-02-13T05:16:57.873Z", Group: "Pokemon" },
-            { Name: "Kakuna", Time: "2020-02-13T05:37:01.063Z", Group: "Pokemon" },
-            { Name: "Weedle", Time: "2020-02-13T05:43:50.479Z", Group: "Pokemon" },
-            { Name: "Charmeleon", Time: "2020-02-13T06:19:03.874Z", Group: "Pokemon" },
-            {
-                "Group": "Pokemon",
-                "Name": "Pidgey",
-                "Time": "2020-02-13T07:27:37.727Z"
-            },
-            {
-                "Group": "Pokemon",
-                "Name": "Zubat",
-                "Time": "2020-02-13T08:34:19.391Z"
-            },
-            {
-                "Group": "Pokemon",
-                "Name": "Magikarp",
-                "Time": "2020-02-13T09:24:15.954Z"
-            },
-            {
-                "Group": "Pokemon",
-                "Name": "Rattata",
-                "Time": "2020-02-13T13:45:34.623Z"
-            },
-            {
-                "Group": "Pokemon",
-                "Name": "Fearow",
-                "Time": "2020-02-13T14:03:43.552Z"
-            },
-            {
-                "Group": "Pokemon",
-                "Name": "Beedrill",
-                "Time": "2020-02-13T15:12:23.663Z"
-            },
-            { Name: "", Time: "", Group: "Pokemon" },
-
-
+            { "Group": "Pokemon", "Name": "Charmander", "Time": "2020-02-13T01:30:31.250Z" },
+            { "Group": "Pokemon", "Name": "Spearow", "Time": "2020-02-13T05:14:47.168Z" },
+            { "Group": "Pokemon", "Name": "NidoranM", "Time": "2020-02-13T05:16:57.873Z" },
+            { "Group": "Pokemon", "Name": "Kakuna", "Time": "2020-02-13T05:37:01.063Z" },
+            { "Group": "Pokemon", "Name": "Weedle", "Time": "2020-02-13T05:43:50.479Z" },
+            { "Group": "Pokemon", "Name": "Charmeleon", "Time": "2020-02-13T06:19:03.874Z" },
+            { "Group": "Pokemon", "Name": "Pidgey", "Time": "2020-02-13T07:27:37.727Z" },
+            { "Group": "Pokemon", "Name": "Zubat", "Time": "2020-02-13T08:34:19.391Z" },
+            { "Group": "Pokemon", "Name": "Magikarp", "Time": "2020-02-13T09:24:15.954Z" },
+            { "Group": "Pokemon", "Name": "Rattata", "Time": "2020-02-13T13:45:34.623Z" },
+            { "Group": "Pokemon", "Name": "Fearow", "Time": "2020-02-13T14:03:43.552Z" },
+            { "Group": "Pokemon", "Name": "Beedrill", "Time": "2020-02-13T15:12:23.663Z" },
+            { "Group": "Pokemon", "Name": "Mankey", "Time": "2020-02-13T17:40:03.392Z" },
+            { "Group": "Pokemon", "Name": "Diglett", "Time": "2020-02-13T19:39:52.499Z" },
+            { "Group": "Pokemon", "Name": "Drowzee", "Time": "2020-02-13T20:09:10.959Z" },
+            { "Group": "Pokemon", "Name": "Oddish", "Time": "2020-02-13T23:49:10.634Z" },
+            { "Group": "Pokemon", "Name": "Ekans", "Time": "2020-02-14T07:07:22.401Z" },
+            { "Group": "Pokemon", "Name": "Dugtrio", "Time": "2020-02-14T07:27:37.444Z" },
+            { "Group": "Pokemon", "Name": "Gloom", "Time": "2020-02-14T07:36:46.431Z" },
+            { "Group": "Pokemon", "Name": "Voltorb", "Time": "2020-02-14T09:00:49.042Z" },
+            { "Group": "Pokemon", "Name": "Machop", "Time": "2020-02-14T11:38:44.102Z" },
+            { "Group": "Pokemon", "Name": "Doduo", "Time": "2020-02-14T19:11:30.694Z" },
+            { "Group": "Pokemon", "Name": "Nidorino", "Time": "2020-02-14T23:32:33.760Z" },
+            { "Group": "Pokemon", "Name": "Nidoking", "Time": "2020-02-15T03:45:34.058Z" },
+            { "Group": "Pokemon", "Name": "Abra", "Time": "2020-02-15T04:27:06.952Z" },
+            { "Group": "Pokemon", "Name": "Exeggcute", "Time": "2020-02-15T13:37:34.198Z" },
+            { "Group": "Pokemon", "Name": "Venonat", "Time": "2020-02-15T13:53:50.895Z" },
+            { "Group": "Pokemon", "Name": "Rhyhorn", "Time": "2020-02-15T13:54:53.616Z" },
+            { "Group": "Pokemon", "Name": "Parasect", "Time": "2020-02-15T13:56:49.532Z" }
         ]
     }
 );
