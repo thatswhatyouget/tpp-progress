0cc71243d98fd95cb94ae4285e1d8def0fc62b1e
m4_used_rollout <dave.was.r@gmail.com>
2020-03-19 05:22:51 +0000

    Added Touhoumon type icons

--- data/Revisits/touhoumon.ts
+++ data/Revisits/touhoumon.ts
@@ -16,6 +16,18 @@ Revisits.Runs.push(
         Events: [
             { "Group": "Pokemon", "Name": "CLuna", "Time": "2020-03-18T21:37:50.076Z" },
             { "Group": "Pokemon", "Name": "CHina", "Time": "2020-03-18T22:00:20.331Z" },
+            { "Group": "Pokemon", "Name": "CUtsuho", "Time": "2020-03-18T23:59:07.100Z" },
+            { "Group": "Pokemon", "Name": "Shanghai", "Time": "2020-03-19T00:46:45.224Z" },
+            { "Group": "Pokemon", "Name": "CLilyBlack", "Time": "2020-03-19T01:35:38.030Z" },
+            { "Group": "Pokemon", "Name": "CAya", "Time": "2020-03-19T01:42:34.845Z" },
+            { "Group": "Pokemon", "Name": "CYuuka", "Time": "2020-03-19T01:53:41.129Z" },
+            { "Group": "Pokemon", "Name": "CWriggle", "Time": "2020-03-19T02:00:20.468Z" },
+            { "Group": "Pokemon", "Name": "CHatate", "Time": "2020-03-19T02:03:43.734Z" },
+            { "Group": "Pokemon", "Name": "CLilyWhite", "Time": "2020-03-19T02:09:25.151Z" },
+            { "Group": "Pokemon", "Name": "CIchirin", "Time": "2020-03-19T02:38:06.012Z" },
+            { "Group": "Pokemon", "Name": "CYoumu", "Time": "2020-03-19T03:23:32.901Z" },
+            { "Group": "Pokemon", "Name": "CIku", "Time": "2020-03-19T04:37:41.584Z" },
+            { "Group": "Pokemon", "Name": "CTewi", "Time": "2020-03-19T05:12:34.102Z" },
         ],
         CopyEvents: ["Touhoumon"]
     }
