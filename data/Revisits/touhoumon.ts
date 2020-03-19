a8bd4bdd06e2d8ecc3cb9cc8b7a1fe25813b6728
m4_used_rollout <dave.was.r@gmail.com>
2020-03-19 20:51:34 +0000

    Earned the Zephyr and Hive badges

--- data/Revisits/touhoumon.ts
+++ data/Revisits/touhoumon.ts
@@ -14,6 +14,62 @@ Revisits.Runs.push(
         Region: "Kanto",
         Pokedex: "Art Book",
         Events: [
+            {
+                Group: "Badges",
+                Name: "Zephyr Badge",
+                Image: "img/badges/zephyr.png",
+                Time: "2020-03-19T12:26:51.781Z",
+                Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Hive Badge",
+                Image: "img/badges/hive.png",
+                Time: "2020-03-19T19:31:58.124Z",
+                Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Plain Badge",
+                Image: "img/badges/plain.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Fog Badge",
+                Image: "img/badges/fog.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Storm Badge",
+                Image: "img/badges/storm.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Mineral Badge",
+                Image: "img/badges/mineral.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Glacier Badge",
+                Image: "img/badges/glacier.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Rising Badge",
+                Image: "img/badges/rising.png",
+                Time: "",
+                Attempts: 0
+            },
             { "Group": "Pokemon", "Name": "CLuna", "Time": "2020-03-18T21:37:50.076Z" },
             { "Group": "Pokemon", "Name": "CHina", "Time": "2020-03-18T22:00:20.331Z" },
             { "Group": "Pokemon", "Name": "CUtsuho", "Time": "2020-03-18T23:59:07.100Z" },
@@ -28,6 +84,34 @@ Revisits.Runs.push(
             { "Group": "Pokemon", "Name": "CYoumu", "Time": "2020-03-19T03:23:32.901Z" },
             { "Group": "Pokemon", "Name": "CIku", "Time": "2020-03-19T04:37:41.584Z" },
             { "Group": "Pokemon", "Name": "CTewi", "Time": "2020-03-19T05:12:34.102Z" },
+            { "Group": "Pokemon", "Name": "CKaguya", "Time": "2020-03-19T05:41:07.687Z" },
+            { "Group": "Pokemon", "Name": "CSuwako", "Time": "2020-03-19T06:02:23.178Z" },
+            { "Group": "Pokemon", "Name": "Medicine", "Time": "2020-03-19T06:37:36.909Z" },
+            { "Group": "Pokemon", "Name": "CKisume", "Time": "2020-03-19T07:35:51.956Z" },
+            { "Group": "Pokemon", "Name": "CTenshi", "Time": "2020-03-19T08:02:15.790Z" },
+            { "Group": "Pokemon", "Name": "Yuugi", "Time": "2020-03-19T08:08:50.423Z" },
+            { "Group": "Pokemon", "Name": "CYuugi", "Time": "2020-03-19T08:11:15.922Z" },
+            { "Group": "Pokemon", "Name": "Rumia", "Time": "2020-03-19T09:21:55.967Z" },
+            { "Group": "Pokemon", "Name": "Kisume", "Time": "2020-03-19T09:40:44.856Z" },
+            { "Group": "Pokemon", "Name": "Yamame", "Time": "2020-03-19T10:10:02.047Z" },
+            { "Group": "Pokemon", "Name": "CEllen", "Time": "2020-03-19T11:07:32.345Z" },
+            { "Group": "Pokemon", "Name": "AChen", "Time": "2020-03-19T11:09:29.689Z" },
+            { "Group": "Pokemon", "Name": "DRemilia", "Time": "2020-03-19T11:51:04.958Z" },
+            { "Group": "Pokemon", "Name": "AFlandre", "Time": "2020-03-19T12:01:14.423Z" },
+            { "Group": "Pokemon", "Name": "SFlandre", "Time": "2020-03-19T12:03:31.732Z" },
+            { "Group": "Pokemon", "Name": "Rin", "Time": "2020-03-19T13:32:34.787Z" },
+            { "Group": "Pokemon", "Name": "Utsuho", "Time": "2020-03-19T13:51:38.852Z" },
+            { "Group": "Pokemon", "Name": "CMai", "Time": "2020-03-19T14:22:12.520Z" },
+            { "Group": "Pokemon", "Name": "CRika", "Time": "2020-03-19T15:00:49.648Z" },
+            { "Group": "Pokemon", "Name": "CMeira", "Time": "2020-03-19T15:05:09.054Z" },
+            { "Group": "Pokemon", "Name": "Meiling", "Time": "2020-03-19T15:59:01.157Z" },
+            { "Group": "Pokemon", "Name": "ASanae", "Time": "2020-03-19T16:56:29.711Z" },
+            { "Group": "Pokemon", "Name": "Star", "Time": "2020-03-19T17:35:53.389Z" },
+            { "Group": "Pokemon", "Name": "Luna", "Time": "2020-03-19T17:37:42.173Z" },
+            { "Group": "Pokemon", "Name": "Chen", "Time": "2020-03-19T18:33:33.148Z" },
+            { "Group": "Pokemon", "Name": "CKana", "Time": "2020-03-19T18:39:40.331Z" },
+            { "Group": "Pokemon", "Name": "Layla", "Time": "2020-03-19T19:11:51.599Z" },
+            { "Group": "Pokemon", "Name": "CFuto", "Time": "2020-03-19T19:55:19.583Z" },
         ],
         CopyEvents: ["Touhoumon"]
     }
