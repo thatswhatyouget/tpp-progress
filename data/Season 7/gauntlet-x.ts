ef8db8c6766c2fdb098d0ce6cc5df35256170358
m4_used_rollout <dave.was.r@gmail.com>
2020-05-03 22:29:28 +0000

    Started Gauntlet X

---
+++ data/Season 7/gauntlet-x.ts
@@ -0,0 +1,126 @@
+/// <reference path="../tpp-data.ts" />
+Season6.Runs.push(
+    {
+        RunName: "Gauntlet X",
+        ColorPrimary: "#007fdb",
+        ColorSecondary: "black",
+        Duration: "255w",
+        StartDate: "2020-05-03T12:00:00Z",
+        HostName: "Etr",
+        HostImage: "img/hosts/etr.png",
+        // HostImageSource: "http://haykira.deviantart.com/art/Li-l-d-from-Twitch-Plays-Pokemon-X-Y-471845274",
+        Region: "Kalos",
+        Generation: 6,
+        Pokedex: "National",
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Bug Badge",
+                Image: "img/badges/bug.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Cliff Badge",
+                Image: "img/badges/cliff.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Rumble Badge",
+                Image: "img/badges/rumble.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Plant Badge",
+                Image: "img/badges/plant.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Voltage Badge",
+                Image: "img/badges/voltage.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Fairy Badge",
+                Image: "img/badges/fairy.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Psychic Badge",
+                Image: "img/badges/psychic.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Iceberg Badge",
+                Image: "img/badges/iceberg.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Malva",
+                Image: "img/trainers/x/malva.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Wikstrom",
+                Image: "img/trainers/x/wikstrom.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Drasna",
+                Image: "img/trainers/x/drasna.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Siebold",
+                Image: "img/trainers/x/siebold.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "Diantha",
+                Image: "img/trainers/x/diantha.png",
+                Time: "",
+                Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                    // { Pokemon: "Lapras", Nickname: "! ♀o?04+6eAd", Level: 68, Gender: "Male" },
+                    // { Pokemon: "Charizard", Nickname: "!taj11yygaaa", Level: 81, Gender: "Male" },
+                    // { Pokemon: "Absol", Nickname: "QQYyy25o6wf7", Level: 70, Gender: "Female" },
+                    // { Pokemon: "Xerneas", Level: 80 },
+                    // { Pokemon: "Hawlucha", Level: 72, Gender: "Female" },
+                    // { Pokemon: "Aegislash", Nickname: "Oi!oiswhhve!", Level: 93, Gender: "Female" },
+                ],
+                Image: "img/ribbons/champion-kalos.png"
+            },
+            { "Group": "Pokemon", "Name": "Froakie", "Time": "2020-05-03T21:14:45.023Z" },
+            { "Group": "Pokemon", "Name": "Scatterbug", "Time": "2020-05-03T21:39:12.985Z" },
+            { "Group": "Pokemon", "Name": "Caterpie", "Time": "2020-05-03T21:42:29.014Z" },
+            { "Group": "Pokemon", "Name": "Pikachu", "Time": "2020-05-03T21:45:20.986Z" },
+            { "Group": "Pokemon", "Name": "Fletchling", "Time": "2020-05-03T21:48:58.101Z" },
+            { "Group": "Pokemon", "Name": "Bunnelby", "Time": "2020-05-03T22:15:34.732Z" }
+        ]
+    }
+);
