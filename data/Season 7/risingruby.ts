29e5e1dcdcbf0a123240b27e0f620873f6329f32
m4_used_rollout <dave.was.r@gmail.com>
2020-08-08 21:15:37 +0000

    Started Rising Ruby

---
+++ data/Season 7/risingruby.ts
@@ -0,0 +1,157 @@
+/// <reference path="../tpp-data.ts" />
+Season7.Runs.push(
+    {
+        RunName: "Rising Ruby",
+        ColorPrimary: "#D1171A",
+        ColorSecondary: "#F47B33",
+        StartDate: "2020-08-08T21:00:00Z",
+        Duration: "255w",
+        HostName: "ébneert",
+        HostImage: "img/hosts/rtyhaszs.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Hoenn",
+        Generation: 6,
+        Pokedex: "National",
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Stone Badge",
+                Image: "img/badges/stone.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Knuckle Badge",
+                Image: "img/badges/knuckle.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Dynamo Badge",
+                Image: "img/badges/dynamo.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Heat Badge",
+                Image: "img/badges/heat.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Balance Badge",
+                Image: "img/badges/balance.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Feather Badge",
+                Image: "img/badges/feather.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Mind Badge",
+                Image: "img/badges/mind.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Rain Badge",
+                Image: "img/badges/rain.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Sidney",
+                Image: "img/trainers/emerald/sidney.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four",
+                Name: "Phoebe",
+                Image: "img/trainers/emerald/phoebe.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four",
+                Name: "Glacia",
+                Image: "img/trainers/emerald/glacia.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four",
+                Name: "Drake",
+                Image: "img/trainers/emerald/drake.png",
+                Time: ""
+            },
+            {
+                Group: "Champions",
+                Name: "Steven",
+                Image: "img/trainers/emerald/steven.png",
+                Time: ""
+            },
+            {
+                Group: "Champions",
+                Name: "Zinnia",
+                Image: "img/trainers/emerald/zinnia.png",
+                ImageSource: "http://kinectedwires.deviantart.com/art/OR-AS-Zinnia-Sprite-505257562",
+                Time: ""
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Sidney",
+                Image: "img/trainers/emerald/sidney.png",
+                Time: "",
+                Attempts: 0,
+                Class: "Rematch"
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Phoebe",
+                Image: "img/trainers/emerald/phoebe.png",
+                Time: "",
+                Attempts: 0,
+                Class: "Rematch"
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Glacia",
+                Image: "img/trainers/emerald/glacia.png",
+                Time: "",
+                Attempts: 0,
+                Class: "Rematch"
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Drake",
+                Image: "img/trainers/emerald/drake.png",
+                Time: "",
+                Attempts: 0,
+                Class: "Rematch"
+            },
+            {
+                Group: "Champions",
+                Name: "Steven",
+                Image: "img/trainers/emerald/steven.png",
+                Time: "",
+                Attempts: 0,
+                Class: "Rematch"
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                    { Pokemon: "Azumarill", Nickname: "!t44444’’’tu", Level: 79, Gender: "Female" },
+                ],
+                Image: "img/ribbons/champion-hoenn.png"
+            },
+        ]
+    }
+);
