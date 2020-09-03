722739db12ec7ed33169e996c0faed66b9db3fd8
Ravendwyr <8914+Ravendwyr@users.noreply.github.com>
2020-09-03 20:58:35 +0100

    Added DemiKids Intermission.

---
+++ data/Intermissions/Season 6/demikids.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "DemiKids: Light Version (Part 1)",
+        ColorPrimary: "orange",
+        ColorSecondary: "black",
+        BackgroundImage: "linear-gradient(to bottom, rgba(255,100,0,1) 30%, rgba(255,255,0,1) 80%)",
+        StartDate: "2019-02-10T00:48:55.000Z",
+        Duration: "2019-02-13T00:55:50.000Z",
+        Unfinished: true,
+        HostImage: "https://vignette.wikia.nocookie.net/megamitensei/images/0/00/JinCalm.png/revision/latest?cb=20140711054358",
+        HostImageSource: "https://megamitensei.fandom.com/wiki/Jin_(Devil_Children)",
+        HostName: "BMMNXc",
+        Events: [],
+    }
+);
+Intermissions.Runs.push(
+    {
+        RunName: "DemiKids: Light Version (Part 2)",
+        ColorPrimary: "orange",
+        ColorSecondary: "black",
+        BackgroundImage: "linear-gradient(to bottom, rgba(255,100,0,1) 30%, rgba(255,255,0,1) 80%)",
+        StartDate: "2019-03-13T13:54:50.000Z",
+        Duration: "2019-03-13T21:37:15.000Z",
+        Unfinished: true,
+        HostImage: "https://vignette.wikia.nocookie.net/megamitensei/images/0/00/JinCalm.png/revision/latest?cb=20140711054358",
+        HostImageSource: "https://megamitensei.fandom.com/wiki/Jin_(Devil_Children)",
+        HostName: "BMMNXc",
+        Events: [],
+    }
+);
