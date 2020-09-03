889da36e03d4a7300c28bcf1415dc887f1cf976b
Ravendwyr <8914+Ravendwyr@users.noreply.github.com>
2020-09-03 22:38:43 +0100

    Added Digimon World Dawn Intermission.

---
+++ data/Intermissions/Season 6/digimonworld.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Digimon World: Dawn",
+        ColorPrimary: "orange",
+        ColorSecondary: "black",
+        BackgroundImage: "linear-gradient(to bottom, rgba(255,255,0,1) 50%, rgba(255,100,0,1) 70%)",
+        StartDate: "2019-04-12T00:59:35.000Z",
+        Duration: "2019-04-13T20:30:45.000Z",
+        Unfinished: true,
+        HostImage: "https://vignette.wikia.nocookie.net/digimon/images/6/67/Coronamon_vg.gif/revision/latest?cb=20130418113437",
+        HostImageSource: "https://digimon.fandom.com/wiki/Coronamon",
+        HostName: "g",
+        Events: [],
+    }
+);
