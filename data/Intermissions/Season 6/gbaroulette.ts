ca0287e4fae546d66a53aa6b1b1a6088f876caa3
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2020-09-08 22:28:00 +0100

    Added Season 6 GBA Roulette.

---
+++ data/Intermissions/Season 6/gbaroulette.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "GameBoy Advance Roulette 1",
+        ColorPrimary: "#a6d410",
+        ColorSecondary: "#072b03",
+        StartDate: "2019-08-09T00:49:25.000Z",
+        Duration: "2019-08-10T20:10:40.000Z",
+        Unfinished: true,
+        ContainsRunsFrom: ["Intermissions", "Short Intermissions"],
+        // HostImage: "img/hosts/roulette/madden.gif",
+        // HostName: "John Madden\nMadden '95",
+        // Class: "squarehost",
+        Events: [
+        ]
+    }
+);
+Intermissions.Runs.push(
+    {
+        RunName: "GameBoy Advance Roulette 2",
+        ColorPrimary: "#a6d410",
+        ColorSecondary: "#072b03",
+        StartDate: "2019-08-20T18:35:54.000Z",
+        Duration: "2019-08-11T03:59:57.000Z",
+        Unfinished: true,
+        ContainsRunsFrom: ["Intermissions", "Short Intermissions"],
+        // HostImage: "img/hosts/roulette/madden.gif",
+        // HostName: "John Madden\nMadden '95",
+        // Class: "squarehost",
+        Events: [
+        ]
+    }
+);
---
+++ data/Intermissions/Season 6/jpemerald.ts
@@ -0,0 +1,23 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Emerald (Japanese)",
+        ColorPrimary: "#9bbb59",
+        ColorSecondary: "#71893f",
+        StartDate: "2019-08-20T21:05:46.926Z",
+        Duration: "2019-08-20T22:20:46.629Z",
+        HostName: "イキワ",
+        HostImage: "img/hosts/a.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "Hoenn",
+        Unfinished: true,
+        Events: [
+            {  Group: "Pokemon", Name: "キモリ", Class: "Treecko", Time: "2019-08-20T20:21:20.349Z" },
+            {  Group: "Pokemon", Name: "ジグザグマ", Class: "Zigzagoon", Time: "2019-08-20T22:06:13.045Z" },
+            {  Group: "Pokemon", Name: "ケムッソ", Class: "Wurmple", Time: "2019-08-20T22:17:33.362Z" },
+            {  Group: "Pokemon", Name: "ポチエナ", Class: "Poochyena", Time: "2019-08-20T22:17:33.362Z" },
+        ]
+    }
+);
