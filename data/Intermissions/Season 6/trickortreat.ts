40b909e2bf002f55966094763d7ecb152e93b1f6
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2020-09-08 16:32:39 +0100

    Added Trick or Treat House 2019.

---
+++ data/Intermissions/Season 6/trickortreat.ts
@@ -0,0 +1,198 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Trick or Treat House 2019",
+        ColorPrimary: "orange",
+        ColorSecondary: "purple",
+        StartDate: "2019-10-31T00:23:20.000Z",
+        Duration: "2019-11-01T06:55:48.000Z",
+        HostName: "AA",
+        HostImage: "img/hosts/aa.png",
+        HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        Region: "Hoenn",
+        Events: [
+//            <TPP.HallOfFame>{
+//                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "34091", Attempts: 0, Party: [
+//                    { Pokemon: "Grovyle", Level: 30, Number: 2, Gender: "Male", IDNo: "34091" },
+//                    { Pokemon: "Marshtomp", Level: 30, Number: 8, Gender: "Male", IDNo: "34091" },
+//                    { Pokemon: "Combusken", Level: 30, Number: 5, Gender: "Male", IDNo: "34091" },
+//                ],
+//                Image: "img/ribbons/champion.png"
+//            },
+
+            { "Group": "Pokemon", "Name": "Grovyle", "Time": "2019-10-31T00:46:51.170Z" },
+            { "Group": "Pokemon", "Name": "Combusken", "Time": "2019-10-31T00:46:51.170Z" },
+            { "Group": "Pokemon", "Name": "Marshtomp", "Time": "2019-10-31T00:46:51.170Z" },
+
+            {
+                Group: "Missions", Name: "Puzzle #1\nShrub Shredding",
+                Time: "2019-10-31T01:06:44.064Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #2\nTrapdoor Trickery",
+                Time: "2019-10-31T01:42:45.679Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #3\nA Song in the Forest",
+                Time: "2019-10-31T02:54:16.576Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #4\nPokémon Says",
+                Time: "2019-10-31T03:17:15.580Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #5\nPeppermints in Petalburg",
+                Time: "2019-10-31T03:57:22.476Z", Attempts: 1, Image: "https://cdn.bulbagarden.net/upload/b/b5/Youngster_RSE_OD.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #6\nDoor Dido Menagerie",
+                Time: "2019-10-31T04:15:56.507Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #7\nThe Mountain Trail",
+                Time: "2019-10-31T05:17:11.426Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #8\nTwin Memories",
+                Time: "2019-10-31T06:11:06.910Z", Attempts: 2, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #9\nBoulder Room Bluff",
+                Time: "2019-10-31T06:35:13.850Z", Attempts: 2, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #10\nCruisin' Down 110",
+                Time: "2019-10-31T06:57:59.289Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #11\nScale the Belfry",
+                Time: "2019-10-31T07:54:17.686Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #12\nMalteasers in Mossdeep",
+                Time: "2019-10-31T09:34:29.312Z", Attempts: 2, Image: "https://cdn.bulbagarden.net/upload/6/6c/Battle_Girl_RSE_OD.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #13\nWaterflow Caverns",
+                Time: "2019-10-31T10:22:22.322Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #14\nRotation Gate Ruse",
+                Time: "2019-10-31T10:50:59.289Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #15\nSpot the Difference",
+                Time: "2019-10-31T11:50:23.612Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #16\nMisplaced",
+                Time: "2019-10-31T12:21:48.619Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #17\nManeuver Mischief",
+                Time: "2019-10-31T12:53:57.324Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #18\nKoga's Labyrinth",
+                Time: "2019-10-31T13:29:31.356Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #19\nShenanigan Samba",
+                Time: "2019-10-31T13:49:12.089Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #20\nI Jump, You Jump, Jack",
+                Time: "2019-10-31T14:09:36.583Z", Attempts: 2, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle #21\nFloor Frolic",
+                Time: "2019-10-31T14:23:11.622Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+
+            {
+                Group: "Missions", Name: "Puzzle Revisit #1\nShrub Shredding", Class: "Rematch",
+                Time: "2019-10-31T14:53:52.201Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #2\nTrapdoor Trickery", Class: "Rematch",
+                Time: "2019-10-31T15:33:27.499Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #3\nA Song in the Forest", Class: "Rematch",
+                Time: "2019-10-31T16:07:18.741Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #4\nPokémon Says", Class: "Rematch",
+                Time: "2019-10-31T16:36:27.192Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #5\nPeppermints in Petalburg", Class: "Rematch",
+                Time: "2019-10-31T17:01:46.591Z", Attempts: 1, Image: "https://cdn.bulbagarden.net/upload/b/b5/Youngster_RSE_OD.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #6\nDoor Dido Menagerie", Class: "Rematch",
+                Time: "2019-10-31T17:18:58.972Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #7\nThe Mountain Trail", Class: "Rematch",
+                Time: "2019-10-31T18:13:29.406Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #8\nTwin Memories", Class: "Rematch",
+                Time: "2019-10-31T19:04:43.689Z", Attempts: 5, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #9\nBoulder Room Bluff", Class: "Rematch",
+                Time: "2019-10-31T19:50:18.356Z", Attempts: 8, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #10\nCruisin' Down 110", Class: "Rematch",
+                Time: "2019-10-31T20:12:51.268Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #11\nScale the Belfry", Class: "Rematch",
+                Time: "2019-10-31T21:02:29.535Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #12\nMalteasers in Mossdeep", Class: "Rematch",
+                Time: "2019-10-31T23:00:29.396Z", Attempts: 1, Image: "https://cdn.bulbagarden.net/upload/6/6c/Battle_Girl_RSE_OD.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #13\nWaterflow Caverns", Class: "Rematch",
+                Time: "2019-11-01T01:04:02.497Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #14\nRotation Gate Ruse", Class: "Rematch",
+                Time: "2019-11-01T01:42:08.471Z", Attempts: 3, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #15\nSpot the Difference", Class: "Rematch",
+                Time: "2019-11-01T03:31:08.519Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #16\nMisplaced", Class: "Rematch",
+                Time: "2019-11-01T04:04:44.214Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #17\nManeuver Mischief", Class: "Rematch",
+                Time: "2019-11-01T04:46:33.036Z", Attempts: 2, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #18\nKoga's Labyrinth", Class: "Rematch",
+                Time: "2019-11-01T05:18:57.088Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #19\nShenanigan Samba", Class: "Rematch",
+                Time: "2019-11-01T05:33:07.782Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #20\nI Jump, You Jump, Jack", Class: "Rematch",
+                Time: "2019-11-01T05:53:42.439Z", Attempts: 1, Image: "img/trainers/emerald/trickmaster.png",
+            },
+            {
+                Group: "Missions", Name: "Puzzle Revisit #21\nFloor Frolic", Class: "Rematch",
+                Time: "2019-11-01T06:43:13.098Z", Attempts: 4, Image: "img/trainers/emerald/trickmaster.png",
+            },
+        ],
+    }
+);
