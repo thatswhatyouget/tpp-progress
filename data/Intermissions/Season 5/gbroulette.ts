025998728659b12f6244eb2683e1a2d5da74f858
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2020-09-08 21:28:18 +0100

    Updated GameBoy Roulettes 3 & 4.

--- data/Intermissions/Season 5/gbroulette.ts
+++ data/Intermissions/Season 5/gbroulette.ts
@@ -4,8 +4,8 @@ Intermissions.Runs.push(
         RunName: "GameBoy Roulette 3",
         ColorPrimary: "#a6d410",
         ColorSecondary: "#072b03",
-        StartDate: "2018-06-08T21:00:00Z", //?
-        Duration: "2018-06-09T20:00:00Z", //?
+        StartDate: "2018-06-08T23:01:30.000Z",
+        Duration: "2018-06-09T20:49:37.000Z",
         Unfinished: true,
         // HostImage: "img/hosts/roulette/madden.gif",
         // HostName: "John Madden\nMadden '95",
@@ -19,8 +19,8 @@ Intermissions.Runs.push(
         RunName: "GameBoy Roulette 4",
         ColorPrimary: "#a6d410",
         ColorSecondary: "#072b03",
-        StartDate: "2018-07-14T14:00:00Z", //ish
-        Duration: "2017-07-15T14:00:00Z",
+        StartDate: "2018-07-14T14:35:24.000Z",
+        Duration: "2018-07-15T14:22:25.000Z",
         Unfinished: true,
         // HostImage: "img/hosts/roulette/madden.gif",
         // HostName: "John Madden\nMadden '95",
