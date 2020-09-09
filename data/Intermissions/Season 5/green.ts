a6d463f015ec08b3cd5ad7eeec2c5c0a3eb23b3f
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2020-09-09 10:54:26 +0100

    Added Bootleg Green intermission to Season 5.

---
+++ data/Intermissions/Season 5/green.ts
@@ -0,0 +1,20 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Bootleg Green (Intermission)",
+        ColorPrimary: "green",
+        ColorSecondary: "darkgreen",
+        StartDate: "2018-06-09T16:26:09.148Z",
+        Duration: "2018-06-09T16:51:09.160Z",
+        HostName: "BBBBB",
+        HostImage: "img/hosts/larry.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/76wtj1/larry_he_is_ginger/",
+        //HostImage: "https://cdn.bulbagarden.net/upload/6/66/Spr_RG_Red_1.png",
+        BaseGame: "Red",
+        Region: "Kanto",
+        Unfinished: true,
+        Events: [
+            { Group: "Pokemon", Name: "INCON", Class: "Bulbasaur", Time: "2018-06-09T16:30:42.497Z" },
+        ],
+    }
+);
