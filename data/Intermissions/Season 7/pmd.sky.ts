cca605437d8eadbfc6677d0a4413d945b7e5a291
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2020-09-11 21:59:07 +0100

    Added PMD: Randomised Sky.

---
+++ data/Intermissions/Season 7/pmd.sky.ts
@@ -0,0 +1,105 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Mystery Dungeon: Explorers of the Sky (Randomised)",
+        ColorPrimary: "#9bbb59",
+        ColorSecondary: "#71893f",
+        StartDate: "2020-03-25T18:23:23.000Z",
+        Duration: "2020-04-11T00:12:25.000Z",
+        HostName: "LLNEHtDaay",
+        HostImage: "img/hosts/pmd/makuhita.png",
+        HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        Region: "Pokemon World",
+        Events: [
+            {
+                Group: "Bosses", Name: "Koffing & Zubat", Time: "2020-03-25T18:58:19.738Z",
+                Class: "pokesprite Koffing", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Drowzee", Time: "2020-03-25T22:56:16.017Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Groudon", Time: "2020-03-27T21:40:49.945Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Manectric's Tribe", Time: "2020-04-05T22:42:35.328Z",
+                Class: "pokesprite Manectric", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Mesprit", Time: "2020-04-06T11:47:09.620Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Grovyle", Time: "2020-04-06T16:38:04.173Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Spiritomb", Time: "2020-04-07T00:20:00.350Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Kabutops & Omastar", Time: "2020-04-07T19:51:13.654Z",
+                Class: "pokesprite Kabutops", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Dusknoir", Time: "2020-04-08T02:02:54.979Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Primal Dialga", Time: "2020-04-08T12:53:18.781Z",
+                Class: "pokesprite Dialga", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Grand Master of All Things Bad", Time: "2020-04-08T16:48:46.872Z",
+                Class: "pokesprite Wigglytuff", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Primal Dialga", Time: "2020-04-09T01:11:20.424Z",
+                Class: "pokesprite Dialga Rematch", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Carnivine", Time: "2020-04-09T08:36:04.813Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Muk", Time: "2020-04-09T09:58:15.377Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Froslass", Time: "2020-04-09T15:30:56.009Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Gyarados", Time: "2020-04-09T22:44:10.460Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Regice", Time: "2020-04-10T01:31:47.756Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Regirock", Time: "2020-04-10T01:52:29.659Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Registeel", Time: "2020-04-10T04:02:44.821Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Regigigas", Time: "2020-04-10T04:18:51.033Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Palkia", Time: "2020-04-10T16:45:30.249Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+            {
+                Group: "Bosses", Name: "Darkrai", Time: "2020-04-10T19:05:51.828Z",
+                Class: "pokesprite", ImageSource: "https://www.pokecommunity.com/showthread.php?t=267728"
+            },
+        ],
+        ContainsRunsFrom: ["Season 7"],
+    }
+);
