b5fe39261d5be64019059c75d577d13b884f108f
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2020-09-11 17:06:31 +0100

    Added AC:WW intermission to Season 7.

---
+++ data/Intermissions/Season 7/acww.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Animal Crossing: Wild World (Part 1)",
+        StartDate: "2020-03-23T04:14:00.000Z",
+        ColorPrimary: "forestgreen",
+        ColorSecondary: "brown",
+        Duration: "2020-03-23T20:15:33.000Z",
+        Unfinished: true,
+        HostName: "1qaaazz",
+        HostImage: "img/hosts/other/villager.png",
+        HostImageSource: "https://nookipedia.com/wiki/File:BoyAAA_CF_Model.png",
+        Region: "Animal Crossing",
+        Events: []
+    }
+);
+Intermissions.Runs.push(
+    {
+        RunName: "Animal Crossing: Wild World (Part 2)",
+        StartDate: "2020-03-25T17:54:15.000Z",
+        ColorPrimary: "forestgreen",
+        ColorSecondary: "brown",
+        Duration: "2020-03-25T18:17:33.000Z",
+        Unfinished: true,
+        HostName: "1qaaazz",
+        HostImage: "img/hosts/other/villager.png",
+        HostImageSource: "https://nookipedia.com/wiki/File:BoyAAA_CF_Model.png",
+        Region: "Animal Crossing",
+        Events: [],
+    }
+);
