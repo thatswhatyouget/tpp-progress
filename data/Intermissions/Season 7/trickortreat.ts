5b3870e699e41f6ea054a252b2a26fb61f6a08d5
m4_used_rollout <dave.was.r@gmail.com>
2020-10-30 23:18:05 +0000

    Added TTH2020

---
+++ data/Intermissions/Season 7/trickortreat.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Trick or Treat House 2020",
+        ColorPrimary: "orange",
+        ColorSecondary: "purple",
+        StartDate: "2020-10-31T00:23:20.000Z",
+        Duration: "2020-11-01T23:59:59.000Z",
+        HostName: "",
+        HostImage: "img/hosts/aa.png",
+        HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        Region: "Hoenn",
+        Events: [
+        ]
+    }
+);
