35c657e202a48730108312df58d4ebf364a5ffcb
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2020-11-17 16:27:04 +0000

    Mark Baba Is You as finished.

--- data/Intermissions/Season 6/babaisyou.ts
+++ data/Intermissions/Season 6/babaisyou.ts
@@ -6,7 +6,6 @@ Intermissions.Runs.push(
         ColorSecondary: "black",
         StartDate: "2019-05-11T22:42:15.000Z",
         Duration: "2019-05-14T22:06:10.000Z",
-        Unfinished: true,
         HostImage: "https://images.shoutwiki.com/8chanmania/4/4a/Baba_Image.png",
         HostImageSource: "http://8chanmania.shoutwiki.com/wiki/Baba",
         HostName: "Baba",
