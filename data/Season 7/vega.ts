df2be3fe4f24feea51d014d06341f2afaff1f0da
m4_used_rollout <dave.was.r@gmail.com>
2020-12-12 20:01:08 +0000

    Ready to start Vega

---
+++ data/Season 7/vega.ts
@@ -0,0 +1,116 @@
+/// <reference path="../tpp-data.ts" />
+Season7.Runs.push(
+    {
+        RunName: "Vega",
+        ColorPrimary: "#CECE08",
+        ColorSecondary: "#087D24",
+        Duration: "255w",
+        StartDate: "2020-12-12T21:00:00Z",
+        HostName: "",
+        // HostImage: "img/hosts/gordon.png",
+        Region: "Tohoak",
+        Pokedex: "Tohoak",
+        //FinalStateLink:"./states/Season 7/vega.json", 
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Elnath Badge",
+                Image: "img/badges/elnath.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Arneb Badge",
+                Image: "img/badges/arneb.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Phact Badge",
+                Image: "img/badges/phact.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Sarfah Badge",
+                Image: "img/badges/sarfah.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Gemma Badge",
+                Image: "img/badges/gemma.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Hadar Badge",
+                Image: "img/badges/hadar.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Prior Badge",
+                Image: "img/badges/prior.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Mirach Badge",
+                Image: "img/badges/mirach.png",
+                Time: "",
+                Attempts: 0
+            },
+            // {
+            //     Group: "Elite Four",
+            //     Name: "Phil",
+            //     Image: "img/trainers/sirius/phil.png",
+            //     Time: "",
+            //     Attempts: 0
+            // },
+            // {
+            //     Group: "Elite Four",
+            //     Name: "Athena",
+            //     Image: "img/trainers/sirius/athena.png",
+            //     Time: "",
+            //     Attempts: 0
+            // },
+            // {
+            //     Group: "Elite Four",
+            //     Name: "Mosmero",
+            //     Image: "img/trainers/sirius/mosmero.png",
+            //     Time: "",
+            //     Attempts: 0
+            // },
+            // {
+            //     Group: "Elite Four",
+            //     Name: "Norman",
+            //     Image: "img/trainers/sirius/norman.png",
+            //     Time: "",
+            //     Attempts: 0
+            // },
+            // {
+            //     Group: "Champions",
+            //     Name: "Wally",
+            //     Image: "img/trainers/sirius/wally.png",
+            //     Time: "",
+            //     Attempts: 0
+            // },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+
+
+        ]
+    }
+);
