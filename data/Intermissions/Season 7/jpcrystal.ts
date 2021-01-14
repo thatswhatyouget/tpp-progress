5268d1059723d3ea66126b643f3d4f1a5b4fed7a
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-01-14 17:49:56 +0000

    Added a few more Season 7 intermissions.

---
+++ data/Intermissions/Season 7/jpcrystal.ts
@@ -0,0 +1,23 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Pocket Monsters Crystal (Debug Version)",
+        ColorPrimary: "#4f81bd",
+        ColorSecondary: "#385d8a",
+        StartDate: "2020-02-09T22:13:49.799Z",
+        Duration: "2020-02-09T22:53:50.040Z",
+        HostName: "- はま ",
+        HostImage: "img/hosts/ajdnnw.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/k9ggdf/the_hosts_of_the_voices_before_the_starry_sky_of/",
+        Region: "Johto",
+        Generation: 2,
+        Pokedex: "New",
+        Unfinished: true,
+        Events: [
+            { Group: "Pokemon", Name: "Bulbasaur", Time: "2020-02-09T22:17:18.281Z" }, // via Debug Menu
+            { Group: "Pokemon", Name: "Tyranitar", Time: "2020-02-09T22:28:51.812Z" }, // via Debug Menu
+            { Group: "Pokemon", Name: "Ho-oh", Time: "2020-02-09T22:28:51.812Z" }, // via Debug Menu
+            { Group: "Pokemon", Name: "Lugia", Time: "2020-02-09T22:49:05.831Z" }, // via Debug Menu
+        ]
+    }
+);
