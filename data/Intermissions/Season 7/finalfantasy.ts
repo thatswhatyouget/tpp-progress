7bd2f3b1b1560dfdd37663354037a9948d39311f
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-02-01 20:35:14 +0000

    Added Final Fantasy intermissions.

---
+++ data/Intermissions/Season 7/finalfantasy.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Final Fantasy VII",
+        StartDate: "2021-01-24T21:45:00Z",
+        Duration: "2021-02-01T00:48:45Z",
+        ColorPrimary: "forestgreen",
+        ColorSecondary: "black",
+        BackgroundImage: "linear-gradient(to bottom, rgba(0,58,35,1) 0%, rgba(22,120,96,1) 65%, rgba(64,143,133,1) 100%)",
+        HostName: "Cloud",
+        HostImage: "img/hosts/other/cloud.png",
+        HostImageSource: "https://finalfantasy.fandom.com/wiki/Cloud_Strife",
+        Region: "Gaia",
+        Events: [],
+    }
+);
