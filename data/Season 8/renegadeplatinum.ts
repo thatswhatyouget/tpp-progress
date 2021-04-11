1a30abdbef7b7a5d928947fae9edc03121fcd9a3
m4_used_rollout <dave.was.r@gmail.com>
2021-04-11 04:50:25 +0000

    Started Renegade Platinum

---
+++ data/Season 8/renegadeplatinum.ts
@@ -0,0 +1,116 @@
+/// <reference path="../tpp-data.ts" />
+Season8.Runs.push(
+    {
+        RunName: "Renegade Platinum",
+        ColorPrimary: "#a6a6a6",
+        ColorSecondary: "#C41018",
+        Duration: "255d",
+        StartDate: "2021-04-10T21:00:00Z",
+        HostName: "Kr ",
+        HostImage: "img/hosts/nqpppnl.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Sinnoh",
+        Generation: 4,
+        Pokedex: "National",
+        // FinalStateLink:"./states/Season 7/gauntlet-platinum.json", 
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Coal Badge",
+                Image: "img/badges/coal.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Forest Badge",
+                Image: "img/badges/forest.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Cobble Badge",
+                Image: "img/badges/cobble.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Fen Badge",
+                Image: "img/badges/fen.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Relic Badge",
+                Image: "img/badges/relic.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Mine Badge",
+                Image: "img/badges/mine.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Icicle Badge",
+                Image: "img/badges/icicle.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Beacon Badge",
+                Image: "img/badges/beacon.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Aaron",
+                Image: "img/trainers/platinum/aaron.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Bertha",
+                Image: "img/trainers/platinum/bertha.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Flint",
+                Image: "img/trainers/platinum/flint.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Lucian",
+                Image: "img/trainers/platinum/lucian.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "Cynthia",
+                Image: "img/trainers/platinum/cynthia.png",
+                Time: "",
+                Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "", Attempts: 0, Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion-sinnoh.png"
+            },
+        ]
+    }
+);
