a6b30916eff81ac3c8a4fb972b339a873e841bb3
m4_used_rollout <dave.was.r@gmail.com>
2021-05-08 20:47:56 +0000

    Added RGB Race Green

---
+++ data/Season 8/rgbrace.ts
@@ -0,0 +1,123 @@
+/// <reference path="../tpp-data.ts" />
+Season8.Runs.push(
+    {
+        RunName: "RGB Race Green",
+        ColorPrimary: "#ceff94",
+        ColorSecondary: "#94ce63",
+        Duration: "255d",
+        StartDate: "2021-05-08T21:00:00Z",
+        HostName: "",
+        HostImage: "img/hosts/larry.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/76wtj1/larry_he_is_ginger/",
+        //HostImage: "https://cdn.bulbagarden.net/upload/6/66/Spr_RG_Red_1.png",
+        BaseGame: "Red",
+        Region: "Kanto",
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Boulder Badge",
+                Image: "img/badges/boulder.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Cascade Badge",
+                Image: "img/badges/cascade.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Thunder Badge",
+                Image: "img/badges/thunder.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Rainbow Badge",
+                Image: "img/badges/rainbow.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Soul Badge",
+                Image: "img/badges/soul.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Marsh Badge",
+                Image: "img/badges/marsh.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Volcano Badge",
+                Image: "img/badges/volcano.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Earth Badge",
+                Image: "img/badges/earth.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Lorelei",
+                Image: "img/trainers/red/lorelei.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Bruno",
+                Image: "img/trainers/red/bruno.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Agatha",
+                Image: "img/trainers/red/agatha.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Lance",
+                Image: "img/trainers/red/lance.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "BLUE",
+                Image: "img/trainers/red/blue.png",
+                Time: "",
+                Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+        ]
+    }
+);
