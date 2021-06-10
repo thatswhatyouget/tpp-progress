f4d7c6399deb27ada407c833ead37f6f42df1ee9
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-06-10 14:22:23 +0100

    Add Miitopia intermission.

---
+++ data/Intermissions/Season 8/miitopia.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Miitopia",
+        ColorPrimary: "#00d0c5",
+        ColorSecondary: "#4a48b5",
+        BackgroundImage: "linear-gradient(90deg, rgba(0,208,197,1) 45%, rgba(74,72,181,1) 70%, rgba(92,37,117,1) 95%)",
+        StartDate: "2021-06-05T15:05:50.000Z",
+        Duration: "2021-06-08T18:01:20.000Z",
+        Events: [],
+    }
+);
