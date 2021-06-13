c3a220f0616074f74316daed07df772f84787808
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-06-13 23:13:05 +0100

    Added Battle Revolution intermission.

---
+++ data/Intermissions/Season 8/pbr.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Battle Revolution",
+        ColorPrimary: "#FFCB05",
+        ColorSecondary: "#3B5CA9",
+        StartDate: "2021-06-12T00:29:50Z",
+        Duration: "2021-06-13T21:22:55Z",
+        Events: [],
+    }
+);
