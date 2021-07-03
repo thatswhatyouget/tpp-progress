67af5eed7d0fb5022149034677e8e6254023afa4
m4_used_rollout <dave.was.r@gmail.com>
2021-07-03 22:43:22 +0000

    Added RandoBlack

---
+++ data/Season 8/randoblack.ts
@@ -0,0 +1,57 @@
+/// <reference path="../tpp-data.ts" />
+Season6.Runs.push(
+    {
+        RunName: "Randomized Black",
+        ColorPrimary: "#111",
+        ColorSecondary: "#CC9BA2",
+        Duration: "255w",
+        StartDate: "2021-07-03T21:00:00Z",
+        HostName: "♂♂♂'",
+        HostImage: "img/hosts/aaq.png",
+        //HostImage: "https://cdn.bulbagarden.net/upload/1/16/Spr_BW_Hilda_2.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/beur93/aqua_kuehl_the_lonely/",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "National",
+        Events: [
+            { Group: "Badges", Name: "Trio Badge", Image: "img/badges/trio.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Quake Badge", Image: "img/badges/quake.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Jet Badge", Image: "img/badges/jet.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Freeze Badge", Image: "img/badges/freeze.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Legend Badge", Image: "img/badges/legend.png", Time: "", Attempts: 0 },
+
+            { Group: "Elite Four", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Grimsley", Image: "img/trainers/black/grimsley.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Caitlin", Image: "img/trainers/black/caitlin.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Marshal", Image: "img/trainers/black/marshal.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "N", Image: "img/trainers/black/n.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "Ghetsis", Image: "img/trainers/black/ghetsis.png", Time: "", Attempts: 0 },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "First Game Clear", Time: "", IDNo: "39604", Attempts: 0, Party: [
+
+                ],
+                Image: "img/ribbons/event.png",
+                FirstAttemptDate: ""
+            },
+
+            { Group: "Elite Four Rematch", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Time: "", Attempts: 0, Class: "Rematch" },
+            { Group: "Elite Four Rematch", Name: "Grimsley", Image: "img/trainers/black/grimsley.png", Time: "", Attempts: 0, Class: "Rematch" },
+            { Group: "Elite Four Rematch", Name: "Caitlin", Image: "img/trainers/black/caitlin.png", Time: "", Attempts: 0, Class: "Rematch" },
+            { Group: "Elite Four Rematch", Name: "Marshal", Image: "img/trainers/black/marshal.png", Time: "", Attempts: 0, Class: "Rematch" },
+
+            { Group: "Champions", Name: "Alder", Image: "img/trainers/black/alder.png", Time: "", Attempts: 0 },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "39604", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: ""
+            },
+
+        ]
+    }
+);
