9501806c02ba5889453dc952ced857f638c82cc5
m4_used_rollout <dave.was.r@gmail.com>
2021-07-05 07:51:56 +0000

    Earned the Bolt Badge

--- data/Season 8/randoblack.ts
+++ data/Season 8/randoblack.ts
@@ -17,7 +17,7 @@ Season8.Runs.push(
             { Group: "Badges", Name: "Trio Badge", Image: "img/badges/trio.png", Time: "2021-07-04T02:39:54.761Z", Attempts: 2 },
             { Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png", Time: "2021-07-04T07:38:29.267Z", Attempts: 2 },
             { Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png", Time: "2021-07-04T14:37:17.060Z", Attempts: 2 },
-            { Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png", Time: "2021-07-04T22:38:22.393Z", Attempts: 1 },
             { Group: "Badges", Name: "Quake Badge", Image: "img/badges/quake.png", Time: "", Attempts: 0 },
             { Group: "Badges", Name: "Jet Badge", Image: "img/badges/jet.png", Time: "", Attempts: 0 },
             { Group: "Badges", Name: "Freeze Badge", Image: "img/badges/freeze.png", Time: "", Attempts: 0 },
