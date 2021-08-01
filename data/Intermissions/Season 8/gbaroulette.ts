f80737188dd47337a133ba2fddab35df19e2a063
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-08-01 10:45:00 +0100

    Added Rubin Hall of Fame.

--- data/Intermissions/Season 8/gbaroulette.ts
+++ data/Intermissions/Season 8/gbaroulette.ts
@@ -10,3 +10,26 @@ Intermissions.Runs.push(
         Events: [],
     }
 );
+Intermissions.Runs.push(
+    {
+        RunName: "GameBoy Advance Roulette 5",
+        ColorPrimary: "#a6d410",
+        ColorSecondary: "#072b03",
+        StartDate: "2021-05-07T21:01:34.094Z",
+        Duration: "2021-05-08T20:26:35.274Z",
+        Unfinished: true,
+        Events: [],
+    }
+);
+Intermissions.Runs.push(
+    {
+        RunName: "GameBoy Advance Roulette 6",
+        ColorPrimary: "#a6d410",
+        ColorSecondary: "#072b03",
+        StartDate: "2021-05-10T08:54:03.739Z",
+        Duration: "2021-05-13T20:57:20.820Z",
+        Unfinished: true,
+        Events: [],
+        ContainsRunsFrom: ["Intermissions"],
+    }
+);
