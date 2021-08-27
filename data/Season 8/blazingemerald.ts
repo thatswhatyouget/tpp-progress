0f06a0bc0fa5207697f2cbe12c4c011f00c3c864
m4_used_rollout <dave.was.r@gmail.com>
2021-08-27 23:54:34 +0000

    Earned all Blazing Emerald badges

--- data/Season 8/blazingemerald.ts
+++ data/Season 8/blazingemerald.ts
@@ -40,57 +40,57 @@ Season8.Runs.push(
                 Group: "Badges",
                 Name: "Heat Badge",
                 Image: "img/badges/heat.png",
-                Time: "",
-                Attempts: 0
+                Time: "2021-08-23T18:41:10.732Z",
+                Attempts: 3
             },
             {
                 Group: "Badges",
                 Name: "Balance Badge",
                 Image: "img/badges/balance.png",
-                Time: "",
-                Attempts: 0
+                Time: "2021-08-24T00:33:20.661Z",
+                Attempts: 2
             },
             {
                 Group: "Badges",
                 Name: "Feather Badge",
                 Image: "img/badges/feather.png",
-                Time: "",
-                Attempts: 0
+                Time: "2021-08-24T20:49:30.333Z",
+                Attempts: 11
             },
             {
                 Group: "Badges",
                 Name: "Mind Badge",
                 Image: "img/badges/mind.png",
-                Time: "",
-                Attempts: 0
+                Time: "2021-08-25T17:14:54.505Z",
+                Attempts: 1
             },
             {
                 Group: "Badges",
-                Name: "Rain Badge",
+                Name: "Rime Badge",
                 Image: "img/badges/rain.png",
-                Time: "",
-                Attempts: 0
-            },
-            {
-                Group: "Elite Four",
-                Name: "Sidney",
-                Image: "img/trainers/emerald/sidney.png",
-                Time: "",
-                Attempts: 0
+                Time: "2021-08-26T07:27:17.907Z",
+                Attempts: 1
             },
             {
                 Group: "Elite Four",
                 Name: "Phoebe",
                 Image: "img/trainers/emerald/phoebe.png",
-                Time: "",
-                Attempts: 0
+                Time: "2021-08-27T00:22:08.732Z",
+                Attempts: 2
             },
             {
                 Group: "Elite Four",
-                Name: "Glacia",
-                Image: "img/trainers/emerald/glacia.png",
-                Time: "",
-                Attempts: 0
+                Name: "Serena",
+                Image: "img/trainers/emerald/serena.png",
+                Time: "2021-08-27T00:59:31.500Z",
+                Attempts: 2
+            },
+            {
+                Group: "Elite Four",
+                Name: "Bede",
+                Image: "img/trainers/emerald/bede.png",
+                Time: "2021-08-27T01:09:29.881Z",
+                Attempts: 1
             },
             {
                 Group: "Elite Four",
