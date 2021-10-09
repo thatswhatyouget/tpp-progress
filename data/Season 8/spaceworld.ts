28cfddfafed69453a621b4c54704df15107aced9
m4_used_rollout <dave.was.r@gmail.com>
2021-10-09 22:24:24 +0000

    Added Spaceworld Gold Reforged

---
+++ data/Season 8/spaceworld.ts
@@ -0,0 +1,76 @@
+/// <reference path="../tpp-data.ts" />
+Season8.Runs.push(
+    {
+        RunName: "Spaceworld Gold Reforged",
+        ColorPrimary: "#AD0021",
+        ColorSecondary: "#F3C69D",
+        StartDate: "2021-10-09T21:00:00Z",
+        Duration: "255w",
+        HostName: "SSS ",
+        HostImage: "img/hosts/ajdnnw.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/m37nzd/the_hosts_of_the_voices_after_the_7th_anniversary/",
+        Region: "Nihon",
+        // FinalStateLink:"./states/Season 8/chattycrystal.json",
+        Events: [
+            {
+                Group: "Badges", Name: "Zephyr Badge", Image: "img/badges/zephyr.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Hive Badge", Image: "img/badges/hive.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Plain Badge)", Image: "img/badges/plain.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Fog Badge", Image: "img/badges/fog.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Mineral Badge", Image: "img/badges/mineral.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Dusk Badge", Image: "img/badges/midnight.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Glacier Badge", Image: "img/badges/glacier.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Indigo Badge", Image: "img/badges/rising.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Will", Image: "img/trainers/crystal/will.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Bruno", Image: "img/trainers/crystal/bruno.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Koga", Image: "img/trainers/crystal/koga.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Karen", Image: "img/trainers/crystal/karen.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Champions", Name: "Lance", Image: "img/trainers/crystal/lance.png",
+                Time: "", Attempts: 0,
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+        ]
+    }
+);
--- poke-styles/pokemon/fakemon.ts
+++ poke-styles/pokemon/fakemon.ts
@@ -40,3 +40,11 @@ addStyles(fixFakeForms(Pokedex.Regional["Tohoak"].map(p => (typeof p === "number
 
 //Blazing Emerald
 addStyles(fixFakeForms(Pokedex.Regional["Blazing Hoenn"].map(p => (typeof p === "number" ? Pokedex.PokeList[p] : p))), defaultMapping("blazingemerald"));
+
+//Spaceworld Gold Reforged
+const spaceworldBetaMons = Pokedex.Regional.Nihon.map((mon, i) => typeof mon == "string" ? { name: mon, number: `${i}-spaceworld` } : null).filter(m => !!m);
+addStyles(spaceworldBetaMons, m => {
+    var clean = dexClean(m.name).replace(/ /g, '');
+    return ".pokesprite." + clean + ' img  { background-image:url("../img/fakemon/gold97/' + m.number + '.png")!important; }';
+});
+addSingleStyle(spaceworldBetaMons.map(m => ".event.pokemon.pokesprite." + dexClean(m.name) + " img").join(", ") + " { background-size: 50%!important; background-position: center!important; }");
