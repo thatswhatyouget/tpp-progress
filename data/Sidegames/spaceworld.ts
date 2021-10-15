100c51bf93f662fbb5bcda2c0e5d04e1bc62b504
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-10-15 15:25:54 +0100

    Add Spaceworld Demo sidegame.

---
+++ data/Sidegames/spaceworld.ts
@@ -0,0 +1,25 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Spaceworld Gold Demo",
+        ColorPrimary: "#AD0021",
+        ColorSecondary: "#8C5208",
+        StartDate: "2019-03-02T00:22:17.532Z",
+        Duration: "2019-03-30T23:55:32.737Z",
+        HostName: "え",
+        HostImage: "img/hosts/ajdnnw.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/m37nzd/the_hosts_of_the_voices_after_the_7th_anniversary/",
+        Region: "Nihon",
+        ContainsRunsFrom: ["Revisits", "Intermissions"],
+        SidegameId: "spaceworld",
+        LastScreenshot: "https://i.tppl.tv/pbr/sidegame/spaceworld/1553990132.png",
+        Events: [
+            { Group: "Pokemon", Name: "Cubburn", Time: "2019-03-03T15:59:14.000Z" },
+            { Group: "Pokemon", Name: "Chikorita", Time: "2019-03-04T11:07:58.000Z" },
+            { Group: "Pokemon", Name: "Palssio", Time: "2019-03-10T03:35:14.000Z" },
+            { Group: "Pokemon", Name: "Charmander", Time: "2019-03-14T00:25:32.000Z" }, // via Debug
+            { Group: "Pokemon", Name: "Butterfree", Time: "2019-03-14T15:39:59.000Z" }, // via Debug
+            { Group: "Pokemon", Name: "Leafeon", Time: "2019-03-14T15:39:59.000Z" }, // via Debug
+        ],
+    }
+);
