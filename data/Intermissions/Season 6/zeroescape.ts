18bf541721dc0574c3088aeb9c3753179b294068
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-10-17 21:37:15 +0100

    Added the other Zero Escape intermissions.

---
+++ data/Intermissions/Season 6/zeroescape.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Zero Escape: Zero Time Dilemma",
+        ColorPrimary: "#223f45",
+        ColorSecondary: "#902a1b",
+        BackgroundImage: "linear-gradient(180deg, rgba(34,63,69,1) 5%, rgba(151,41,28,1) 50%, rgba(137,108,52,1) 95%)",
+        StartDate: "2019-10-11T01:09:10.000Z",
+        Duration: "2019-10-12T13:22:45.000Z",
+        HostName: "Junpei Tenmyouji",
+        HostImage: "img/hosts/other/junpei_adult.png",
+        HostImageSource: "https://zeroescape.fandom.com/wiki/Junpei",
+        Events: [],
+    }
+);
---
+++ data/Intermissions/Season 8/zeroescape.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Zero Escape: Virtue's Last Reward",
+        ColorPrimary: "#223f45",
+        ColorSecondary: "#902a1b",
+        BackgroundImage: "linear-gradient(180deg, rgba(34,63,69,1) 5%, rgba(151,41,28,1) 50%, rgba(137,108,52,1) 95%)",
+        StartDate: "2021-10-15T10:35:00.000Z",
+        Duration: "2021-10-17T15:23:00.000Z",
+        HostName: "Junpei Tenmyouji",
+        HostImage: "img/hosts/other/junpei_old.png",
+        HostImageSource: "https://zeroescape.fandom.com/wiki/Junpei",
+        Events: [],
+    }
+);
