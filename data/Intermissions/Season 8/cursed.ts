b3a4a864f457b8f0bf6e8bd10c14a47e73dfa702
M4 <dave.was.r@gmail.com>
2021-10-31 18:41:12 -0400

    Added Cursed Version

---
+++ data/Intermissions/Season 8/cursed.ts
@@ -0,0 +1,23 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Pokemon Cursed Version",
+        ColorPrimary: "purple",
+        ColorSecondary: "orange",
+        StartDate: "2021-10-31T21:00:00.000Z",
+        Duration: "255w",
+        HostName: "TV",
+        HostImage: "img/hosts/a2.png",
+        HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        Region: "Kanto",
+        Generation:3,
+        Events: [
+            {
+                "Group": "Badge",
+                "Name": "Marsh Badge",
+                "Time": "2021-10-31T22:37:43.684Z",
+                Attempts: 3
+            }
+        ]
+    }
+);
