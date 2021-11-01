f73ae70423a007b51ede80c0b9e1d2833b0093be
M4 <dave.was.r@gmail.com>
2021-11-01 02:27:49 -0400

    Finished Cursed Version, started TTH 2020(1)

--- data/Intermissions/Season 8/cursed.ts
+++ data/Intermissions/Season 8/cursed.ts
@@ -5,12 +5,13 @@ Intermissions.Runs.push(
         ColorPrimary: "purple",
         ColorSecondary: "orange",
         StartDate: "2021-10-31T21:00:00.000Z",
-        Duration: "255w",
+        Duration: "2021-11-01T5:53:00Z",
         HostName: "TV",
         HostImage: "img/hosts/a2.png",
         HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
         Region: "Kanto",
         Generation:3,
+        FinalStateLink: "./states/Season 8/cursed.json",
         Events: [
             {
                 "Group": "Badge",
---
+++ data/Intermissions/Season 8/trickortreat.ts
@@ -0,0 +1,17 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Trick or Treat House 2020(1)",
+        ColorPrimary: "orange",
+        ColorSecondary: "purple",
+        StartDate: "2021-11-01T06:00:00.000Z",
+        Duration: "2031-11-01T00:00:00.000Z",
+        HostName: "Teru",
+        HostImage: "img/hosts/mudkid.png",
+        // HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        //Region: "Hoenn",
+        Generation:3,
+        Events: [
+        ]
+    }
+);
