808d177836fcb94473791508f11b8c38ed0a3ba7
M4 <dave.was.r@gmail.com>
2021-11-02 17:13:58 -0400

    Ended Trick or Treat House 2020(1)

--- data/Intermissions/Season 8/trickortreat.ts
+++ data/Intermissions/Season 8/trickortreat.ts
@@ -5,12 +5,13 @@ Intermissions.Runs.push(
         ColorPrimary: "orange",
         ColorSecondary: "purple",
         StartDate: "2021-11-01T06:00:00.000Z",
-        Duration: "2031-11-01T00:00:00.000Z",
+        Duration: "2021-11-02T16:44:00.000Z",
         HostName: "Teru",
         HostImage: "img/hosts/mudkid.png",
         // HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
         //Region: "Hoenn",
         Generation:3,
+        FinalStateLink: "./states/Season 8/trickortreat.json",
         Events: [
         ]
     }
