a77fd3ae7bc41e2143b5a370cfd08e717bd8a233
m4_used_rollout <dave.was.r@gmail.com>
2021-11-20 14:27:36 +0000

    Added Brilliant Diamond

---
+++ data/Season 8/brilliantdiamond.ts
@@ -0,0 +1,143 @@
+/// <reference path="../tpp-data.ts" />
+Season8.Runs.push(
+    {
+        RunName: "Brilliant Diamond",
+        ColorPrimary: "#026eb7",
+        ColorSecondary: "#c8e8f9",
+        Duration: "255d",
+        StartDate: "2021-11-20T21:00:00Z",
+        // HostName: "",
+        // HostImage: "img/hosts/kr.png",
+        // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/mr8l1r/kristopher_von_karma_the_clockwork_poketch/",
+        Region: "Sinnoh",
+        Generation: 4,
+        Pokedex: "Sinnoh",
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Coal Badge",
+                Image: "img/badges/coal.png",
+                Time: ""
+            },
+            {
+                Group: "Badges",
+                Name: "Forest Badge",
+                Image: "img/badges/forest.png",
+                Time: ""
+            },
+            {
+                Group: "Badges",
+                Name: "Cobble Badge",
+                Image: "img/badges/cobble.png",
+                Time: ""
+            },
+            {
+                Group: "Badges",
+                Name: "Fen Badge",
+                Image: "img/badges/fen.png",
+                Time: ""
+            },
+            {
+                Group: "Badges",
+                Name: "Relic Badge",
+                Image: "img/badges/relic.png",
+                Time: ""
+            },
+            {
+                Group: "Badges",
+                Name: "Mine Badge",
+                Image: "img/badges/mine.png",
+                Time: ""
+            },
+            {
+                Group: "Badges",
+                Name: "Icicle Badge",
+                Image: "img/badges/icicle.png",
+                Time: ""
+            },
+            {
+                Group: "Badges",
+                Name: "Beacon Badge",
+                Image: "img/badges/beacon.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four",
+                Name: "Aaron",
+                Image: "img/trainers/platinum/aaron.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four",
+                Name: "Bertha",
+                Image: "img/trainers/platinum/bertha.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four",
+                Name: "Flint",
+                Image: "img/trainers/platinum/flint.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four",
+                Name: "Lucian",
+                Image: "img/trainers/platinum/lucian.png",
+                Time: ""
+            },
+            {
+                Group: "Champions",
+                Name: "Cynthia",
+                Image: "img/trainers/platinum/cynthia.png",
+                Time: ""
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion-sinnoh.png"
+            },
+            {
+                Group: "Elite Four Rematch",
+                Class: "Rematch",
+                Name: "Aaron",
+                Image: "img/trainers/platinum/aaron.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four Rematch",
+                Class: "Rematch",
+                Name: "Bertha",
+                Image: "img/trainers/platinum/bertha.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four Rematch",
+                Class: "Rematch",
+                Name: "Flint",
+                Image: "img/trainers/platinum/flint.png",
+                Time: ""
+            },
+            {
+                Group: "Elite Four Rematch",
+                Class: "Rematch",
+                Name: "Lucian",
+                Image: "img/trainers/platinum/lucian.png",
+                Time: ""
+            },
+            {
+                Group: "Champions",
+                Class: "Rematch",
+                Name: "Cynthia",
+                Image: "img/trainers/platinum/cynthia.png",
+                Time: ""
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion-sinnoh.png"
+            }
+        ]
+    }
+);
