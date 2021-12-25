58506d8f637df9f5698ee1db8f55d9c4f30fc1b4
m4_used_rollout <dave.was.r@gmail.com>
2021-12-25 06:29:17 +0000

    Added Wooper 2

---
+++ data/Intermissions/Season 8/wooper.ts
@@ -0,0 +1,19 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Wooper 2: Electric Boogaloo",
+        ColorPrimary: "#5b59bb",
+        ColorSecondary: "#313f89",
+        StartDate: "2021-12-25T01:46:48.399Z",
+        Duration: "255w",
+        HostName: "WOOPER",
+        HostImage: "img/hosts/gmmtmt.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/m37nzd/the_hosts_of_the_voices_after_the_7th_anniversary/",
+        Region: "Kanto",
+        Generation: 3,
+        BaseGame: "FireRed",
+        //FinalStateLink: "./states/Season 7/wooper.json",
+        Events: [
+        ],
+    }
+);
