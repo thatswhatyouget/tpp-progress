08611099d71decd30165138ca38d9d020a086aee
m4_used_rollout <dave.was.r@gmail.com>
2021-12-27 08:10:52 +0000

    Finished Wooper 2

--- data/Intermissions/Season 8/wooper2.ts
+++ data/Intermissions/Season 8/wooper2.ts
@@ -5,14 +5,14 @@ Intermissions.Runs.push(
         ColorPrimary: "#5b59bb",
         ColorSecondary: "#313f89",
         StartDate: "2021-12-25T01:46:48.399Z",
-        Duration: "255w",
+        Duration: "2021-12-27T00:00:00Z",
         HostName: "WOOPER",
         HostImage: "img/hosts/gmmtmt.png",
         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/m37nzd/the_hosts_of_the_voices_after_the_7th_anniversary/",
         Region: "Kanto",
         Generation: 3,
         BaseGame: "FireRed",
-        //FinalStateLink: "./states/Season 7/wooper.json",
+        FinalStateLink: "./states/Season 8/wooper2.json",
         Events: [
         ],
     }
