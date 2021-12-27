be05b3a086e3801c3a61c005be084b87577f7429
m4_used_rollout <dave.was.r@gmail.com>
2021-12-27 21:06:16 +0000

    Added Wooper 2 badges and pokemon

--- data/Intermissions/Season 8/wooper2.ts
+++ data/Intermissions/Season 8/wooper2.ts
@@ -14,6 +14,177 @@ Intermissions.Runs.push(
         BaseGame: "FireRed",
         FinalStateLink: "./states/Season 8/wooper2.json",
         Events: [
+            {
+                "Group": "Pokemon",
+                "Name": "Machop",
+                "Time": "2021-12-25T01:49:33.463Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Mankey",
+                "Time": "2021-12-25T01:49:33.463Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Charmander",
+                "Time": "2021-12-25T01:49:33.463Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Wooper",
+                "Time": "2021-12-25T01:49:33.463Z"
+            },
+            {
+                "Group": "Badges",
+                "Name": "Boulder Badge",
+                Image: "img/badges/boulder.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                "Time": "2021-12-25T01:49:38.019Z"
+            },
+            {
+                "Group": "Badges",
+                "Name": "Cascade Badge",
+                Image: "img/badges/cascade.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                "Time": "2021-12-25T01:49:38.019Z"
+            },
+            {
+                "Group": "Badges",
+                "Name": "Thunder Badge",
+                Image: "img/badges/thunder.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                "Time": "2021-12-25T01:49:38.020Z"
+            },
+            {
+                "Group": "Badges",
+                "Name": "Rainbow Badge",
+                Image: "img/badges/rainbow.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                "Time": "2021-12-25T01:49:38.020Z"
+            },
+            {
+                "Group": "Badges",
+                "Name": "Soul Badge",
+                Image: "img/badges/soul.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                "Time": "2021-12-25T01:49:38.020Z"
+            },
+            {
+                "Group": "Badges",
+                "Name": "Marsh Badge",
+                Image: "img/badges/marsh.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                "Time": "2021-12-25T01:49:38.020Z"
+            },
+            {
+                "Group": "Badges",
+                "Name": "Volcano Badge",
+                Image: "img/badges/volcano.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                "Time": "2021-12-25T01:49:38.020Z"
+            },
+            {
+                "Group": "Badges",
+                "Name": "Earth Badge",
+                Image: "img/badges/earth.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                "Time": "2021-12-25T01:49:38.020Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Charmeleon",
+                "Time": "2021-12-25T02:30:29.799Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Caterpie",
+                "Time": "2021-12-25T02:46:28.498Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Sandshrew",
+                "Time": "2021-12-25T03:46:57.352Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Hoothoot",
+                "Time": "2021-12-25T03:48:32.032Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Jigglypuff",
+                "Time": "2021-12-25T04:03:06.613Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Taillow",
+                "Time": "2021-12-25T04:08:47.861Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Rattata",
+                "Time": "2021-12-25T09:51:56.588Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Omanyte",
+                "Time": "2021-12-25T10:11:48.161Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Sandslash",
+                "Time": "2021-12-25T10:26:17.952Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Ekans",
+                "Time": "2021-12-25T10:31:37.456Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Spearow",
+                "Time": "2021-12-25T10:34:31.181Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Charizard",
+                "Time": "2021-12-25T11:07:23.422Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Skitty",
+                "Time": "2021-12-25T13:06:49.680Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Zubat",
+                "Time": "2021-12-25T14:34:33.593Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Grimer",
+                "Time": "2021-12-25T17:39:46.874Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Growlithe",
+                "Time": "2021-12-25T17:44:10.080Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Koffing",
+                "Time": "2021-12-25T17:53:21.849Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Delibird",
+                "Time": "2021-12-25T21:17:01.573Z"
+            },
+            {
+                "Group": "Pokemon",
+                "Name": "Teddiursa",
+                "Time": "2021-12-25T21:55:05.735Z"
+            }
         ],
     }
 );
