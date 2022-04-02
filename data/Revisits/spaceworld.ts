f80417f390406559325eea08df76d4e9fd1d809f
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2022-04-02 18:14:55 +0100

    Populated Spaceworld Gold Revisit.

--- data/Revisits/spaceworld.ts
+++ data/Revisits/spaceworld.ts
@@ -12,7 +12,101 @@ Revisits.Runs.push(
         Region: "Nihon",
         FinalStateLink: "./states/Season 9/spaceworld-revisit.json",
         Events: [
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2022-02-11T09:59:55.480Z", Attempts: 2, Party: [
+                    { Pokemon: "Bruinus", Nickname: "TACO", Level: 88, Gender: "Male", Number: 157, IDNo: "49472" },
+                    { Pokemon: "Raitora", Nickname: "KLUUUUUUUU", Level: 56, Gender: "Female", Number: 171, IDNo: "49472" },
+                    { Pokemon: "Lurreel", Nickname: "EATSMARILL", Level: 67, Gender: "Male", Number: 195, IDNo: "49472" },
+                    { Pokemon: "Lugia", Nickname: "AAAAAA", Level: 71, Number: 250, IDNo: "49472" },
+                    { Pokemon: "Phandarin", Nickname: "STITCHES", Level: 100, Gender: "Male", Number: 221, IDNo: "48926" },
+                    { Pokemon: "Ho-Oh", Nickname: "AMMNNNQ", Level: 49, Number: 251, IDNo: "49472" },
+                ],
+                FirstAttemptDate: "2022-02-11T06:37:54.421Z",
+                Image: "img/ribbons/champion.png"
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2022-02-11T17:44:54.935Z", Attempts: 1, Party: [
+                    { Pokemon: "Bruinus", Nickname: "TACO", Level: 89, Gender: "Male", Number: 157, IDNo: "49472" },
+                    { Pokemon: "Lurreel", Nickname: "EATSMARILL", Level: 68, Gender: "Male", Number: 195, IDNo: "49472" },
+                    { Pokemon: "Phandarin", Nickname: "STITCHES", Level: 100, Gender: "Male", Number: 221, IDNo: "48926" },
+                    { Pokemon: "Ho-Oh", Nickname: "AMMNNNQ", Level: 49, Number: 251, IDNo: "49472" },
+                    { Pokemon: "Chansey", Level: 38, Gender: "Female", Number: 113, IDNo: "49472" },
+                    { Pokemon: "Tearor", Level: 41, Gender: "Female", Number: 212, IDNo: "49472" },
+                ],
+                FirstAttemptDate: "2022-02-11T17:01:47.956Z",
+                Image: "img/ribbons/champion.png"
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2022-02-11T20:54:04.389Z", Attempts: 1, Party: [
+                    { Pokemon: "Bruinus", Nickname: "TACO", Level: 90, Gender: "Male", Number: 157, IDNo: "49472" },
+                    { Pokemon: "Phandarin", Nickname: "STITCHES", Level: 100, Gender: "Male", Number: 221, IDNo: "48926" },
+                    { Pokemon: "Ho-Oh", Nickname: "AMMNNNQ", Level: 49, Number: 251, IDNo: "49472" },
+                    { Pokemon: "Jubiley", Level: 47, Gender: "Female", Number: 249, IDNo: "49472" },
+                    { Pokemon: "Lurreel", Nickname: "EATSMARILL", Level: 68, Gender: "Male", Number: 195, IDNo: "49472" },
+                ],
+                FirstAttemptDate: "2022-02-11T20:01:37.434Z",
+                Image: "img/ribbons/champion.png"
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2022-02-12T14:40:20.069Z", Attempts: 3, Party: [
+                    { Pokemon: "Lurreel", Nickname: "EATSMARILL", Level: 72, Gender: "Male", Number: 195, IDNo: "49472" },
+                    { Pokemon: "Bruinus", Nickname: "TACO", Level: 92, Gender: "Male", Number: 157, IDNo: "49472" },
+                    { Pokemon: "Ho-Oh", Nickname: "AMMNNNQ", Level: 50, Number: 251, IDNo: "49472" },
+                    { Pokemon: "Phandarin", Nickname: "STITCHES", Level: 100, Gender: "Male", Number: 221, IDNo: "48926" },
+                    { Pokemon: "Bellignan", Nickname: "nN", Level: 56, Gender: "Female", Number: 185, IDNo: "49472" },
+                ],
+                FirstAttemptDate: "2022-02-11T23:09:16.721Z",
+                Image: "img/ribbons/champion.png"
+            },
+
+            { "Group": "Pokemon", "Name": "Oddish", "Time": "2022-02-11T01:24:00.561Z" },
+            { "Group": "Pokemon", "Name": "Jigglypuff", "Time": "2022-02-11T01:45:00.929Z" },
+            { "Group": "Pokemon", "Name": "Mr. Mime", "Time": "2022-02-11T01:54:58.375Z" },
+            { "Group": "Pokemon", "Name": "Raticate", "Time": "2022-02-11T02:03:53.563Z" },
+            { "Group": "Pokemon", "Name": "Qwilfish", "Time": "2022-02-11T02:48:40.515Z" },
+            { "Group": "Pokemon", "Name": "Geodude", "Time": "2022-02-11T04:00:39.205Z" },
+            { "Group": "Pokemon", "Name": "Clefairy", "Time": "2022-02-11T04:09:03.305Z" },
+            { "Group": "Pokemon", "Name": "Nidorino", "Time": "2022-02-11T04:29:07.977Z" },
+            { "Group": "Pokemon", "Name": "Nidorina", "Time": "2022-02-11T04:44:09.251Z" },
+            { "Group": "Pokemon", "Name": "Psyduck", "Time": "2022-02-11T05:29:27.607Z" },
+            { "Group": "Pokemon", "Name": "Kangaskhan", "Time": "2022-02-11T06:18:01.356Z" },
+            { "Group": "Pokemon", "Name": "Voltorb", "Time": "2022-02-11T10:21:48.350Z" },
+            { "Group": "Pokemon", "Name": "Ledyba", "Time": "2022-02-11T10:42:01.106Z" },
+            { "Group": "Pokemon", "Name": "Caterpie", "Time": "2022-02-11T10:46:42.692Z" },
+            { "Group": "Pokemon", "Name": "Weedle", "Time": "2022-02-11T10:53:28.507Z" },
+            { "Group": "Pokemon", "Name": "Cubone", "Time": "2022-02-11T11:13:48.960Z" },
+            { "Group": "Pokemon", "Name": "Marowak", "Time": "2022-02-11T11:18:52.947Z" },
+            { "Group": "Pokemon", "Name": "Dragonair", "Time": "2022-02-11T11:49:18.106Z" },
+            { "Group": "Pokemon", "Name": "Sui", "Time": "2022-02-11T12:15:35.126Z" },
+            { "Group": "Pokemon", "Name": "En", "Time": "2022-02-11T12:40:05.255Z" },
+            { "Group": "Pokemon", "Name": "Muk", "Time": "2022-02-11T12:43:02.281Z" },
+            { "Group": "Pokemon", "Name": "Graveler", "Time": "2022-02-11T12:46:34.114Z" },
+            { "Group": "Pokemon", "Name": "Grimer", "Time": "2022-02-11T12:50:44.859Z" },
+            { "Group": "Pokemon", "Name": "Koffing", "Time": "2022-02-11T13:02:18.208Z" },
+            { "Group": "Pokemon", "Name": "Weezing", "Time": "2022-02-11T13:15:28.496Z" },
+            { "Group": "Pokemon", "Name": "Tentacruel", "Time": "2022-02-11T13:48:10.781Z" },
+            { "Group": "Pokemon", "Name": "Bellignan", "Time": "2022-02-11T14:25:36.281Z" },
+            { "Group": "Pokemon", "Name": "Bellossom", "Time": "2022-02-11T16:30:49.547Z" },
+            { "Group": "Pokemon", "Name": "Tearor", "Time": "2022-02-11T17:34:42.221Z" },
+            { "Group": "Pokemon", "Name": "Snubbull", "Time": "2022-02-11T18:26:21.902Z" },
+            { "Group": "Pokemon", "Name": "Jubiley", "Time": "2022-02-11T20:53:51.320Z" },
+            { "Group": "Pokemon", "Name": "Rai", "Time": "2022-02-11T22:14:58.618Z" },
+            { "Group": "Pokemon", "Name": "Chikorita", "Time": "2022-02-11T23:01:31.000Z" },
+            { "Group": "Pokemon", "Name": "Kazappelin", "Time": "2022-02-11T23:31:56.418Z" },
+            { "Group": "Pokemon", "Name": "Petamole", "Time": "2022-02-11T23:43:17.371Z" },
+            { "Group": "Pokemon", "Name": "Meganium", "Time": "2022-02-12T00:42:40.778Z" },
+            { "Group": "Pokemon", "Name": "Hitmonlee", "Time": "2022-02-12T01:32:34.030Z" },
+            { "Group": "Pokemon", "Name": "Hitmonba", "Time": "2022-02-12T05:44:06.701Z" },
+            { "Group": "Pokemon", "Name": "Skarmory", "Time": "2022-02-12T08:38:56.878Z" },
+            { "Group": "Pokemon", "Name": "Growlithe", "Time": "2022-02-12T10:09:43.460Z" },
+            { "Group": "Pokemon", "Name": "Elebabe", "Time": "2022-02-12T10:24:00.048Z" },
+            { "Group": "Pokemon", "Name": "Doduo", "Time": "2022-02-12T15:24:10.571Z" },
+            { "Group": "Pokemon", "Name": "Tauros", "Time": "2022-02-12T16:06:01.556Z" },
         ],
+        Revisit: { Collection: "Season 8", Run: "Spaceworld Gold Reforged" },
         CopyEvents: ["Spaceworld Gold Reforged"]
     }
 );
