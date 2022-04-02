1210968d233de42a2cb47abe476184c2201b3053
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2022-04-02 15:55:19 +0100

    Add AFD intermission runs.
    Plus tweaks to DBZ sidegame data.

---
+++ data/Intermissions/Season 9/sphealteamsix.ts
@@ -0,0 +1,19 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Spheal Team Six",
+        ColorPrimary: "#ff7e1b",
+        ColorSecondary: "#8b5325",
+        StartDate: "2022-04-01T00:40:10.000Z",
+        Duration: "2022-04-01T04:55:30.000Z",
+        HostName: "GHIIIOO",
+        HostImage: "img/hosts/d.png",
+        HostImageSource: "http://haykira.deviantart.com/art/Li-l-d-from-Twitch-Plays-Pokemon-X-Y-471845274",
+        BaseGame: "FireRed",
+        Events: [
+            { "Group": "Pokemon", "Name": "Spheal", "Time": "2022-04-01T00:58:59.469Z" },
+            { "Group": "Pokemon", "Name": "Shinx", "Time": "2022-04-01T02:17:23.860Z" },
+            { "Group": "Pokemon", "Name": "Ivysaur", "Time": "2022-04-01T02:48:27.603Z" },
+        ]
+    }
+);
