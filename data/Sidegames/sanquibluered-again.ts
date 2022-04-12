560823fbec3e6b4ca9b8941c8e5ef4b7d7ca54c5
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2022-04-12 23:05:06 +0100

    Add Rerandomized Sanqui Blue/Red anarchy sidegame.

---
+++ data/Sidegames/sanquibluered-again.ts
@@ -0,0 +1,85 @@
+/// <reference path="../tpp-data.ts" />
+
+Sidegames.Runs.push(
+    {
+        RunName: "Rerandomized Sanqui Blue",
+        ColorPrimary: "#2246a6",
+        ColorSecondary: "#63a5ff",
+        Duration: "9d",
+        StartDate: "2022-04-12T20:50:00Z",
+        HostName: "JACK",
+        HostImage: "img/hosts/b.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/n8sv79/multiverse_champion_basil_benson_an_the_red_and/",
+        Generation: 6,
+        Class: "dual-run",
+        //FinalStateLink: "./states/Season 9/sanquibluered-again.json",
+        APIObjectName: "blue",
+        Events: [
+            { Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", Time: "" },
+            { Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", Time: "" },
+            { Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", Time: "" },
+            { Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", Time: "" },
+            { Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", Time: "" },
+            { Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", Time: "" },
+            { Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", Time: "" },
+            { Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", Time: "" },
+
+            { Group: "Elite Four", Name: "Lorelei", Image: "img/trainers/red/lorelei.png", Time: "" },
+            { Group: "Elite Four", Name: "Bruno", Image: "img/trainers/red/bruno.png", Time: "" },
+            { Group: "Elite Four", Name: "Agatha", Image: "img/trainers/red/agatha.png", Time: "" },
+            { Group: "Elite Four", Name: "Lance", Image: "img/trainers/red/lance.png", Time: "" },
+            { Group: "Champions", Name: "AJJ", Image: "img/trainers/red/blue.png", Time: "" },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 1, Party: [
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "",
+            },
+
+            { Group: "Pokemon", Name: "Snivy", Time: "2022-04-12T20:57:30.018Z" },
+        ]
+    }
+);
+
+Sidegames.Runs.push(
+    {
+        RunName: "Rerandomized Sanqui Red",
+        ColorPrimary: "#cc6b5b",
+        ColorSecondary: "#ff8484",
+        Duration: "9d",
+        StartDate: "2022-04-12T20:44:00Z",
+        HostName: "CUV",
+        HostImage: "img/hosts/b.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/n8sv79/multiverse_champion_basil_benson_an_the_red_and/",
+        Generation: 6,
+        Class: "dual-run",
+        //FinalStateLink: "./states/Season 9/sanquibluered-again.json",
+        APIObjectName: "red",
+        Events: [
+            { Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", Time: "" },
+            { Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", Time: "" },
+            { Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", Time: "" },
+            { Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", Time: "" },
+            { Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", Time: "" },
+            { Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", Time: "" },
+            { Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", Time: "" },
+            { Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", Time: "" },
+
+            { Group: "Elite Four", Name: "Lorelei", Image: "img/trainers/red/lorelei.png", Time: "" },
+            { Group: "Elite Four", Name: "Bruno", Image: "img/trainers/red/bruno.png", Time: "" },
+            { Group: "Elite Four", Name: "Agatha", Image: "img/trainers/red/agatha.png", Time: "" },
+            { Group: "Elite Four", Name: "Lance", Image: "img/trainers/red/lance.png", Time: "" },
+            { Group: "Champions", Name: "DEDEDE", Image: "img/trainers/red/blue.png", Time: "" },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 1, Party: [
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "",
+            },
+
+            { Group: "Pokemon", Name: "Lotad", Time: "2022-04-12T21:01:07.350Z" },
+        ]
+    }
+);
