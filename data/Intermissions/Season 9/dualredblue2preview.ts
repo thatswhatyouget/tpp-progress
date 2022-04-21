c26abd3569d1449b418a590111cb2621e0f66c08
m4_used_rollout <dave.was.r@gmail.com>
2022-04-21 23:56:01 +0000

    Added Dual Red Blue 2 Preview

---
+++ data/Intermissions/Season 9/dualredblue2preview.ts
@@ -0,0 +1,84 @@
+/// <reference path="../../tpp-data.ts" />
+
+Intermissions.Runs.push(
+    {
+        RunName: "Dual Blue 2 Preview",
+        ColorPrimary: "#2246a6",
+        ColorSecondary: "#63a5ff",
+        Duration: "255d",
+        StartDate: "2022-04-21T01:19:00Z",
+        HostName: "UUL",
+        HostImage: "img/hosts/xxwwnnPk.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/7xfytx/xena_winipek_cesar_cesis_cee_cee_brackets_sprites/",
+        BaseGame: "Red",
+        Region: "Kanto",
+        Generation: 1,
+        Class: "dual-run",
+        APIObjectName: "blue",
+        Events: [
+            { Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", Time: "2022-04-21T04:28:10.066Z", Attempts: 1 },
+            { Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", Time: "2022-04-21T10:39:33.650Z", Attempts: 1 },
+            { Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", Time: "2022-04-21T16:21:38.193Z", Attempts: 1 },
+            { Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", Time: "2022-04-21T20:27:25.829Z", Attempts: 1 },
+            { Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", Time: "", Attempts: 0 },
+
+            { Group: "Elite Four", Name: "Lorelei", Image: "img/trainers/red/lorelei.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Bruno", Image: "img/trainers/red/bruno.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Agatha", Image: "img/trainers/red/agatha.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Lance", Image: "img/trainers/red/lance.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "YYRAAA", Image: "img/trainers/red/blue.png", Time: "", Attempts: 0 },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "",
+            },
+
+        ]
+    }
+);
+
+Intermissions.Runs.push(
+    {
+        RunName: "Dual Red 2 Preview",
+        ColorPrimary: "#cc6b5b",
+        ColorSecondary: "#ff8484",
+        Duration: "255d",
+        StartDate: "2022-04-21T01:19:00Z",
+        HostName: "ON",
+        HostImage: "img/hosts/ccc.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/7xfytx/xena_winipek_cesar_cesis_cee_cee_brackets_sprites/",
+        BaseGame: "Red",
+        Region: "Kanto",
+        Generation: 1,
+        Class: "dual-run",
+        APIObjectName: "red",
+        Events: [
+            { Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", Time: "2022-04-21T04:57:11.979Z", Attempts: 2 },
+            { Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", Time: "2022-04-21T14:19:13.842Z", Attempts: 3 },
+            { Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", Time: "2022-04-21T21:46:43.093Z", Attempts: 1 },
+            { Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", Time: "", Attempts: 0 },
+            { Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", Time: "", Attempts: 0 },
+
+            { Group: "Elite Four", Name: "Lorelei", Image: "img/trainers/red/lorelei.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Bruno", Image: "img/trainers/red/bruno.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Agatha", Image: "img/trainers/red/agatha.png", Time: "", Attempts: 0 },
+            { Group: "Elite Four", Name: "Lance", Image: "img/trainers/red/lance.png", Time: "", Attempts: 0 },
+            { Group: "Champions", Name: "YYRAAA", Image: "img/trainers/red/blue.png", Time: "", Attempts: 0 },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "",
+            },
+        ]
+    }
+);
