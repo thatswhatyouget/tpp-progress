1a04ede5732fa04595e4cfacac860d68a664bead
m4_used_rollout <dave.was.r@gmail.com>
2022-06-10 10:00:23 +0000

    Added Grand Colosseum

---
+++ data/Season 9/grandocolo.ts
@@ -0,0 +1,101 @@
+/// <reference path="../tpp-data.ts" />
+Season6.Runs.push(
+    {
+        RunName: "Grand Colosseum",
+        ColorPrimary: "#ff9955",
+        ColorSecondary: "#884422",
+        Duration: "255d",
+        StartDate: "2022-06-11T21:00:00Z",
+        HostName: "",
+        HostImage: "img/hosts/accc.gif",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/cu73f6/the_hosts_of_the_voices_after_the_fate_of_hoenn/",
+        Region: "Orre",
+        Generation: 3,
+        Events: [
+            {
+                Group: "Bosses",
+                Name: "Miror B.",
+                Image: "img/trainers/colosseum/mirorb.png",
+                ImageSource: "http://billyk40.deviantart.com/art/Miror-B-BW-258115890",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Bosses",
+                Name: "Dakim",
+                Image: "img/trainers/colosseum/dakim.png",
+                ImageSource: "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherDakim.png.html",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Bosses",
+                Name: "Venus",
+                Image: "img/trainers/colosseum/venus.png",
+                ImageSource: "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherVenus.png.html",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Bosses",
+                Name: "Ein",
+                Image: "img/trainers/colosseum/ein.png",
+                ImageSource: "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherEin.png.html",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Final Bosses",
+                Name: "Venus",
+                Image: "img/trainers/colosseum/venus.png",
+                ImageSource: "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherVenus.png.html",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Final Bosses",
+                Name: "Miror B.",
+                Image: "img/trainers/colosseum/mirorb.png",
+                ImageSource: "http://billyk40.deviantart.com/art/Miror-B-BW-258115890",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Final Bosses",
+                Name: "Dakim",
+                Image: "img/trainers/colosseum/dakim.png",
+                ImageSource: "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherDakim.png.html",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Final Bosses",
+                Name: "Ein",
+                Image: "img/trainers/colosseum/ein.png",
+                ImageSource: "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherEin.png.html",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Final Bosses",
+                Name: "Gonzap",
+                Image: "img/trainers/colosseum/gonzap.png",
+                ImageSource: "http://forum.yugiohcardmaker.net/topic/200460-colosseum-sprites/",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Final Bosses",
+                Name: "Nascour",
+                Image: "img/trainers/colosseum/nascour.png",
+                ImageSource: "http://s1292.photobucket.com/user/Apollothemuse/media/Sprites/NascourSprite_zpse80724b9.png.html",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "Evice",
+                Image: "img/trainers/colosseum/evice.png",
+                ImageSource: "https://www.reddit.com/r/pokemon/comments/2z9gmy/cipher_head_evice_would_like_to_battle_oc_trainer/",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "First Game Clear", Attempts: 0, Time: "", Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/national.png"
+            },
+
+        ]
+    }
+);
