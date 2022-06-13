c63b80b0294251c974b777097489ef1565068d53
m4_used_rollout <dave.was.r@gmail.com>
2022-06-13 07:01:07 +0000

    Beat Miror B. and Dakim

--- data/Season 9/grandocolo.ts
+++ data/Season 9/grandocolo.ts
@@ -6,7 +6,7 @@ Season9.Runs.push(
         ColorSecondary: "#884422",
         Duration: "255d",
         StartDate: "2022-06-11T21:00:00Z",
-        HostName: "",
+        HostName: "U!!!?",
         HostImage: "img/hosts/accc.gif",
         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/cu73f6/the_hosts_of_the_voices_after_the_fate_of_hoenn/",
         Region: "Orre",
@@ -17,14 +17,14 @@ Season9.Runs.push(
                 Name: "Miror B.",
                 Image: "img/trainers/colosseum/mirorb.png",
                 ImageSource: "http://billyk40.deviantart.com/art/Miror-B-BW-258115890",
-                Time: "", Attempts: 0
+                Time: "2022-06-12T14:26:37.986Z", Attempts: 3
             },
             {
                 Group: "Bosses",
                 Name: "Dakim",
                 Image: "img/trainers/colosseum/dakim.png",
                 ImageSource: "http://s1305.photobucket.com/user/EliteknightTMP/media/Sprites/Cipher%20Sprites/CipherDakim.png.html",
-                Time: "", Attempts: 0
+                Time: "2022-06-13T02:11:45.004Z", Attempts: 4
             },
             {
                 Group: "Bosses",
@@ -95,7 +95,30 @@ Season9.Runs.push(
                 FirstAttemptDate: "",
                 Image: "img/ribbons/national.png"
             },
-
+            { "Group": "Pokemon", "Name": "Umbreon", "Time": "2022-06-11T21:01:36.566Z" },
+            { "Group": "Pokemon", "Name": "Espeon", "Time": "2022-06-11T21:01:36.566Z" },
+            { "Group": "Pokemon", "Name": "Furret", "Time": "2022-06-11T23:22:53.021Z" },
+            { "Group": "Pokemon", "Name": "Quilava", "Time": "2022-06-11T23:44:40.731Z" },
+            { "Group": "Pokemon", "Name": "Flaaffy", "Time": "2022-06-12T00:07:29.885Z" },
+            { "Group": "Pokemon", "Name": "Noctowl", "Time": "2022-06-12T00:17:41.178Z" },
+            { "Group": "Pokemon", "Name": "Misdreavus", "Time": "2022-06-12T00:24:00.977Z" },
+            { "Group": "Pokemon", "Name": "Whiscash", "Time": "2022-06-12T00:40:33.673Z" },
+            { "Group": "Pokemon", "Name": "Kirlia", "Time": "2022-06-12T01:08:54.445Z" },
+            { "Group": "Pokemon", "Name": "Ariados", "Time": "2022-06-12T03:08:57.683Z" },
+            { "Group": "Pokemon", "Name": "Volbeat", "Time": "2022-06-12T04:57:53.115Z" },
+            { "Group": "Pokemon", "Name": "Tropius", "Time": "2022-06-12T05:09:12.617Z" },
+            { "Group": "Pokemon", "Name": "Magmar", "Time": "2022-06-12T06:13:52.030Z" },
+            { "Group": "Pokemon", "Name": "Sunflora", "Time": "2022-06-12T06:39:06.749Z" },
+            { "Group": "Pokemon", "Name": "Sneasel", "Time": "2022-06-12T07:37:01.863Z" },
+            { "Group": "Pokemon", "Name": "Mawile", "Time": "2022-06-12T08:22:57.279Z" },
+            { "Group": "Pokemon", "Name": "Stantler", "Time": "2022-06-12T09:05:28.928Z" },
+            { "Group": "Pokemon", "Name": "Lanturn", "Time": "2022-06-12T09:25:54.437Z" },
+            { "Group": "Pokemon", "Name": "Lairon", "Time": "2022-06-12T13:11:22.377Z" },
+            { "Group": "Pokemon", "Name": "Plusle", "Time": "2022-06-12T13:21:25.777Z" },
+            { "Group": "Pokemon", "Name": "Hitmontop", "Time": "2022-06-12T15:06:51.962Z" },
+            { "Group": "Pokemon", "Name": "Ampharos", "Time": "2022-06-12T19:56:57.740Z" },
+            { "Group": "Pokemon", "Name": "Typhlosion", "Time": "2022-06-12T22:25:35.479Z" },
+            { "Group": "Pokemon", "Name": "Entei", "Time": "2022-06-13T00:34:02.865Z" }
         ]
     }
 );
