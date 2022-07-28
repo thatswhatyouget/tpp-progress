d17496f49c7db20c5c1d743a89fe4ab3de0a66b3
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2022-07-28 20:49:55 +0100

    Finished Grand Colosseum.

--- data/Season 9/grandocolo.ts
+++ data/Season 9/grandocolo.ts
@@ -4,12 +4,13 @@ Season9.Runs.push(
         RunName: "Grand Colosseum",
         ColorPrimary: "#ff9955",
         ColorSecondary: "#884422",
-        Duration: "255d",
+        Duration: "2022-06-19T03:30:00Z",
         StartDate: "2022-06-11T21:00:00Z",
         HostName: "U!!!?",
         HostImage: "img/hosts/accc.gif",
         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/cu73f6/the_hosts_of_the_voices_after_the_fate_of_hoenn/",
         Region: "Orre",
+        FinalStateLink: "./states/Season 9/grandocolo.json",
         Generation: 3,
         Events: [
             {
@@ -52,7 +53,7 @@ Season9.Runs.push(
                 Name: "Miror B.",
                 Image: "img/trainers/colosseum/mirorb.png",
                 ImageSource: "http://billyk40.deviantart.com/art/Miror-B-BW-258115890",
-                Time: "", Attempts: 0
+                Time: "2022-06-14T08:48:07.272Z", Attempts: 1
             },
             {
                 Group: "Final Bosses",
@@ -80,21 +81,68 @@ Season9.Runs.push(
                 Name: "Nascour",
                 Image: "img/trainers/colosseum/nascour.png",
                 ImageSource: "http://s1292.photobucket.com/user/Apollothemuse/media/Sprites/NascourSprite_zpse80724b9.png.html",
-                Time: "", Attempts: 0
+                Time: "2022-06-15T06:55:49.865Z", Attempts: 1
             },
             {
                 Group: "Champions",
                 Name: "Evice",
                 Image: "img/trainers/colosseum/evice.png",
                 ImageSource: "https://www.reddit.com/r/pokemon/comments/2z9gmy/cipher_head_evice_would_like_to_battle_oc_trainer/",
-                Time: "", Attempts: 0
+                Time: "2022-06-15T07:16:23.248Z", Attempts: 1
             },
+
             <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "First Game Clear", Attempts: 0, Time: "", Party: [
+                Group: "Hall of Fame", Name: "First Game Clear", Attempts: 1, Time: "2022-06-15T07:16:23.766Z", Party: [
+                    { Pokemon: "Umbreon", Nickname: "WOOMY", Level: 63, Gender: "Male" },
+                    { Pokemon: "Espeon", Nickname: "XO ", Level: 62, Gender: "Female" },
+                    { Pokemon: "Plusle", Level: 62, Gender: "Male" },
+                    { Pokemon: "Typhlosion", Nickname: "W  RR", Level: 62, Gender: "Male" },
+                    { Pokemon: "Tropius", Nickname: " FF ", Level: 61, Gender: "Male" },
+                    { Pokemon: "Ampharos", Nickname: "4 X\u2025PP", Level: 62, Gender: "Female" },
                 ],
-                FirstAttemptDate: "",
                 Image: "img/ribbons/national.png"
             },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Second Game Clear", Attempts: 1, Time: "2022-06-16T12:01:51.391Z", Party: [
+                    { Pokemon: "Ampharos", Nickname: "4 X\u2025PP", Level: 80, Gender: "Female" },
+                    { Pokemon: "Plusle", Level: 89, Gender: "Male" },
+                    { Pokemon: "Espeon", Nickname: "XO ", Level: 74, Gender: "Female" },
+                    { Pokemon: "Tropius", Nickname: " FF ", Level: 74, Gender: "Male" },
+                    { Pokemon: "Typhlosion", Nickname: "W  RR", Level: 79, Gender: "Male" },
+                    { Pokemon: "Umbreon", Nickname: "WOOMY", Level: 76, Gender: "Male" },
+                ],
+                Image: "img/ribbons/national.png"
+            },
+
+            {
+                Group: "Final Bosses Rematch",
+                Name: "Nascour",
+                Image: "img/trainers/colosseum/nascour.png",
+                ImageSource: "http://s1292.photobucket.com/user/Apollothemuse/media/Sprites/NascourSprite_zpse80724b9.png.html",
+                Time: "2022-06-17T14:58:50.001Z", Attempts: 1,
+                Class: "Rematch"
+            },
+            {
+                Group: "Champions",
+                Name: "Evice",
+                Image: "img/trainers/colosseum/evice.png",
+                ImageSource: "https://www.reddit.com/r/pokemon/comments/2z9gmy/cipher_head_evice_would_like_to_battle_oc_trainer/",
+                Time: "2022-06-17T15:12:56.750Z", Attempts: 1,
+                Class: "Rematch"
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Third Game Clear", Attempts: 1, Time: "2022-06-17T15:12:57.218Z", Party: [
+                    { Pokemon: "Plusle", Level: 100, Gender: "Male" },
+                    { Pokemon: "Umbreon", Nickname: "WOOMY", Level: 91, Gender: "Male" },
+                    { Pokemon: "Ampharos", Nickname: "4 X\u2025PP", Level: 91, Gender: "Female" },
+                    { Pokemon: "Tropius", Nickname: " FF ", Level: 86, Gender: "Male" },
+                    { Pokemon: "Espeon", Nickname: "XO ", Level: 91, Gender: "Female" },
+                    { Pokemon: "Typhlosion", Nickname: "W  RR", Level: 87, Gender: "Male" },
+                ],
+                Image: "img/ribbons/national.png"
+            },
+
             { "Group": "Pokemon", "Name": "Umbreon", "Time": "2022-06-11T21:01:36.566Z" },
             { "Group": "Pokemon", "Name": "Espeon", "Time": "2022-06-11T21:01:36.566Z" },
             { "Group": "Pokemon", "Name": "Furret", "Time": "2022-06-11T23:22:53.021Z" },
@@ -136,7 +184,23 @@ Season9.Runs.push(
             { "Group": "Pokemon", "Name": "Heracross", "Time": "2022-06-14T16:07:47.980Z" },
             { "Group": "Pokemon", "Name": "Flygon", "Time": "2022-06-14T17:23:08.671Z" },
             { "Group": "Pokemon", "Name": "Exploud", "Time": "2022-06-15T01:05:26.679Z" },
-            { "Group": "Pokemon", "Name": "Torkoal", "Time": "2022-06-15T01:49:02.935Z" }
+            { "Group": "Pokemon", "Name": "Torkoal", "Time": "2022-06-15T01:49:02.935Z" },
+            { "Group": "Pokemon", "Name": "Gardevoir", "Time": "2022-06-15T04:04:53.251Z" },
+            { "Group": "Pokemon", "Name": "Aggron", "Time": "2022-06-15T05:26:20.642Z" },
+            { "Group": "Pokemon", "Name": "Crobat", "Time": "2022-06-15T06:20:33.184Z" },
+            { "Group": "Pokemon", "Name": "Crawdaunt", "Time": "2022-06-15T06:41:57.722Z" },
+            { "Group": "Pokemon", "Name": "Croconaw", "Time": "2022-06-15T10:16:14.745Z" },
+            { "Group": "Pokemon", "Name": "Feraligatr", "Time": "2022-06-15T16:29:39.345Z" },
+            { "Group": "Pokemon", "Name": "Bayleef", "Time": "2022-06-15T17:49:43.900Z" },
+            { "Group": "Pokemon", "Name": "Corsola", "Time": "2022-06-15T19:28:10.814Z" },
+            { "Group": "Pokemon", "Name": "Gengar", "Time": "2022-06-15T20:00:35.926Z" },
+            { "Group": "Pokemon", "Name": "Milotic", "Time": "2022-06-15T20:16:07.264Z" },
+            { "Group": "Pokemon", "Name": "Salamence", "Time": "2022-06-16T08:11:48.032Z" },
+            { "Group": "Pokemon", "Name": "Meganium", "Time": "2022-06-16T11:09:59.574Z" },
+            { "Group": "Pokemon", "Name": "Metagross", "Time": "2022-06-16T11:53:01.493Z" },
+            { "Group": "Pokemon", "Name": "Tyranitar", "Time": "2022-06-16T12:01:40.061Z" },
+            { "Group": "Pokemon", "Name": "Mew", "Time": "2022-06-16T12:38:10.166Z" },
+            { "Group": "Pokemon", "Name": "Ho-Oh", "Time": "2022-06-18T17:39:08.742Z" },
         ]
     }
 );
