2d8cebae73543791f013e63dba8859071cf71518
M4 <dave.was.r@gmail.com>
2022-08-20 00:20:07 -0400

    Prepped Randomized TriHard Emerald

---
+++ data/Season 9/randotrihardemerald.ts
@@ -0,0 +1,81 @@
+/// <reference path="../tpp-data.ts" />
+Season9.Runs.push(
+    {
+        RunName: "Randomized TriHard Emerald",
+        ColorPrimary: "#07825B",
+        ColorSecondary: "#6A2400",
+        Duration: "255d",
+        StartDate: "2022-08-20T21:00:00Z",
+        HostName: "",
+        HostImage: "img/hosts/f48.png",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "National",
+        // FinalStateLink: "./states/Season 6/trihardemerald.json",
+        Events: [
+            {
+                Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Heat Badge", Image: "img/badges/heat.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Feather Badge", Image: "img/badges/feather.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Mind Badge", Image: "img/badges/mind.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Rain Badge", Image: "img/badges/rain.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four", Name: "Sidney", Image: "img/trainers/emerald/sidney.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Phoebe", Image: "img/trainers/emerald/phoebe.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Glacia", Image: "img/trainers/emerald/glacia.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Drake", Image: "img/trainers/emerald/drake.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Wallace", Image: "img/trainers/emerald/wallace.png",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "", Attempts: 0, Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+        ]
+    }
+);
