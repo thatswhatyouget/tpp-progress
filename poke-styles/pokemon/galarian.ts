4c7ab237e289bc1f414a1f594390e6f9ca1b7f8f
BornToLose <twitchplayslore@gmail.com>
2022-08-30 23:42:27 +0200

    Added Galarian forms

---
+++ poke-styles/pokemon/galarian.ts
@@ -0,0 +1,9 @@
+/// <reference path="../write-style.ts" />
+
+//make sure galarian overrides come after normal dex
+/// <reference path="natdex.ts" />
+
+var galarian = ["Meowth", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Farfetch'd", "Weezing", "Mr. Mime", "Articuno", "Zapdos", "Moltres", "Slowking", "Corsola", "Zigzagoon", "Linoone", "Darumaka", "Darmanitan", "Yamask", "Stunfisk"];
+
+addStyles(galarian, (p, i) => ".galar .pokesprite." + dexClean(p) + " img { background-image:url('../img/pokemon-galar.png')!important; background-position: 0px -" + i + "em!important; }");
+addStyles(galarian, (p, i) => ".galarian .dexEntry .pokesprite." + dexClean(p) + " img, .pokesprite.galarian." + dexClean(p) + " img { background-image:url('../img/pokemon-galar-large.png')!important; background-position: 0px -" + i + "em!important; }");
