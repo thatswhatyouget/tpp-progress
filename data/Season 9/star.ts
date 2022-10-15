22a7268e350764cab0e795f089b319c05f2cb881
M4 <dave.was.r@gmail.com>
2022-10-15 15:50:33 -0400

    Added Pokemon Star

---
+++ data/Season 9/star.ts
@@ -0,0 +1,126 @@
+/// <reference path="../tpp-data.ts" />
+Season9.Runs.push(
+    {
+        RunName: "Star",
+        ColorSecondary: "#4A2C57",
+        ColorPrimary: "#C6C6C6",
+        StartDate: "2022-10-15T21:00:00Z",
+        Duration: "255d",
+        HostName: "",
+        HostImage: "img/hosts/vrg.gif",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/do2ovk/virginia_faba_the_six_time_married_three_times/",
+        Region: "Alola",
+        Generation: 7,
+        Pokedex: "National",
+        //FinalStateLink: "./states/Season 6/rum.json",
+        Events: [
+            {
+                "Group": "Badges", "Name": "Normalium Z", "Image": "img/z-crystals/Normalium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Fightinium Z", "Image": "img/z-crystals/Fightinium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Waterium Z", "Image": "img/z-crystals/Waterium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Firium Z", "Image": "img/z-crystals/Firium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Grassium Z", "Image": "img/z-crystals/Grassium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Rockium Z", "Image": "img/z-crystals/Rockium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Electrium Z", "Image": "img/z-crystals/Electrium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Ghostium Z", "Image": "img/z-crystals/Ghostium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Darkium Z", "Image": "img/z-crystals/Darkium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Dragonium Z", "Image": "img/z-crystals/Dragonium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Fairium Z", "Image": "img/z-crystals/Fairium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Groundium Z", "Image": "img/z-crystals/Groundium Z.png",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Badges", "Name": "Surge Badge",
+                "Time": "", "Attempts": 0
+            },
+
+            {
+                "Group": "Elite Four", "Name": "Molayne", "Image": "img/trainers/sun/molayne.png", "ImageSource": "https://www.deviantart.com/art/Custom-sprite-Molayne-653031564",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Elite Four", "Name": "Olivia", "Image": "img/trainers/sun/olivia.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Elite Four", "Name": "Acerola", "Image": "img/trainers/sun/acerola.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Elite Four", "Name": "Kahili", "Image": "img/trainers/sun/kahili.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Kahili-651482881",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Champions", "Name": "Hau", "Image": "img/trainers/sun/hau.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Hau-651693374",
+                "Time": "", "Attempts": 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion-alola.png",
+                FirstAttemptDate: ""
+            },
+
+            {
+                "Group": "Elite Four Rematch", "Name": "Molayne", "Class": "rematch", "Image": "img/trainers/sun/molayne.png", "ImageSource": "https://www.deviantart.com/art/Custom-sprite-Molayne-653031564",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Elite Four Rematch", "Name": "Olivia", "Class": "rematch", "Image": "img/trainers/sun/olivia.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Elite Four Rematch", "Name": "Acerola", "Class": "rematch", "Image": "img/trainers/sun/acerola.gif", "ImageSource": "http://drawnamu.deviantart.com/art/Trial-Captains-Alola-632931621",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Elite Four Rematch", "Name": "Kahili", "Class": "rematch", "Image": "img/trainers/sun/kahili.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Kahili-651482881",
+                "Time": "", "Attempts": 0
+            },
+            {
+                "Group": "Champions", "Name": "Hau", "Class": "rematch", "Image": "img/trainers/sun/hau.gif", "ImageSource": "http://beliot419.deviantart.com/art/Custom-sprite-Hau-651693374",
+                "Time": "", "Attempts": 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/champion-alola.png",
+                FirstAttemptDate: ""
+            },
+
+        ]
+    }
+)
