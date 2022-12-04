3efaedc16bc13fbce593b56fd92560d5c0f935a1
M4 <dave.was.r@gmail.com>
2022-12-04 15:01:41 -0500

    Earned the Stone and Knuckle badges

--- data/Sidegames/sapphire.ts
+++ data/Sidegames/sapphire.ts
@@ -17,11 +17,11 @@ Sidegames.Runs.push(
         Events: [
             {
                 Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png",
-                Time: "", Attempts: 0,
+                Time: "2022-12-04T04:07:59.169Z", Attempts: 1,
             },
             {
                 Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png",
-                Time: "", Attempts: 0,
+                Time: "2022-12-04T07:37:51.704Z", Attempts: 2,
             },
             {
                 Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png",
@@ -83,6 +83,31 @@ Sidegames.Runs.push(
                 FirstAttemptDate: "",
                 Image: "img/ribbons/champion.png"
             },
+
+            { "Group": "Pokemon", "Name": "Mudkip", "Time": "2022-12-03T22:11:10.986Z" },
+            { "Group": "Pokemon", "Name": "Poochyena", "Time": "2022-12-03T22:36:58.021Z" },
+            { "Group": "Pokemon", "Name": "Zigzagoon", "Time": "2022-12-03T22:39:05.968Z" },
+            { "Group": "Pokemon", "Name": "Wurmple", "Time": "2022-12-03T22:44:46.818Z" },
+            { "Group": "Pokemon", "Name": "Ralts", "Time": "2022-12-03T23:27:17.575Z" },
+            { "Group": "Pokemon", "Name": "Wingull", "Time": "2022-12-04T00:28:47.726Z" },
+            { "Group": "Pokemon", "Name": "Shroomish", "Time": "2022-12-04T01:03:55.360Z" },
+            { "Group": "Pokemon", "Name": "Taillow", "Time": "2022-12-04T02:11:43.235Z" },
+            { "Group": "Pokemon", "Name": "Nincada", "Time": "2022-12-04T02:16:50.066Z" },
+            { "Group": "Pokemon", "Name": "Marshtomp", "Time": "2022-12-04T03:45:40.287Z" },
+            { "Group": "Pokemon", "Name": "Cascoon", "Time": "2022-12-04T10:16:15.279Z" },
+            { "Group": "Pokemon", "Name": "Dustox", "Time": "2022-12-04T11:55:06.459Z" },
+            { "Group": "Pokemon", "Name": "Minun", "Time": "2022-12-04T15:40:55.617Z" },
+            { "Group": "Pokemon", "Name": "Plusle", "Time": "2022-12-04T15:49:06.609Z" },
+            { "Group": "Pokemon", "Name": "Kirlia", "Time": "2022-12-04T16:37:13.113Z" },
+            { "Group": "Pokemon", "Name": "Swellow", "Time": "2022-12-04T19:30:31.351Z" },
+            { "Group": "Pokemon", "Name": "Aron", "Time": "2022-12-04T06:40:01.897Z" },
+            { "Group": "Pokemon", "Name": "Whismur", "Time": "2022-12-04T02:56:57.897Z" },
+            { "Group": "Pokemon", "Name": "Makuhita", "Time": "2022-12-04T06:11:29.897Z" },
+            { "Group": "Pokemon", "Name": "Sableye", "Time": "2022-12-04T06:49:53.897Z" },
+            { "Group": "Pokemon", "Name": "Oddish", "Time": "2022-12-04T14:32:00.898Z" },
+            { "Group": "Pokemon", "Name": "Electrike", "Time": "2022-12-04T13:51:24.898Z" },
+            { "Group": "Pokemon", "Name": "Skitty", "Time": "2022-12-04T10:51:13.898Z" },
+            { "Group": "Pokemon", "Name": "Gulpin", "Time": "2022-12-04T14:15:31.898Z" }
 
         ],
     }
