daf6dac6409eebae5cd9c3d70f8241ae9077aa31
BornToLose <twitchplayslore@gmail.com>
2022-12-10 02:57:37 +0100

    Added sprites for Hisuian forms

---
+++ poke-styles/pokemon/hisuian.ts
@@ -0,0 +1,9 @@
+/// <reference path="../write-style.ts" />
+
+//make sure hisuian overrides come after normal dex
+/// <reference path="natdex.ts" />
+
+var hisuian = ["Growlithe", "Arcanine", "Voltorb", "Electrode", "Typhlosion", "Qwilfish", "Sneasel", "Samurott", "Lilligant", "Zorua", "Zoroark", "Braviary", "Sliggoo", "Goodra", "Avalugg", "Decidueye"];
+
+addStyles(hisuian, (p, i) => ".hisui .pokesprite." + dexClean(p) + " img { background-image:url('../img/pokemon-hisui.png')!important; background-position: 0px -" + i + "em!important; }");
+addStyles(hisuian, (p, i) => ".hisuian .dexEntry .pokesprite." + dexClean(p) + " img, .pokesprite.hisuian." + dexClean(p) + " img { background-image:url('../img/pokemon-hisui-large.png')!important; background-position: 0px -" + i + "em!important; }");
