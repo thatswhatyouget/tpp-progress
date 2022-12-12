e0090aea2eb3755d06b71e06b8a3438b86c5dbbf
M4 <dave.was.r@gmail.com>
2022-12-12 01:13:16 -0500

    Finished Sapphire, started Yellow

---
+++ data/Sidegames/yellow.ts
@@ -0,0 +1,136 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Yellow",
+        ColorPrimary: "goldenrod",
+        ColorSecondary: "#330",
+        StartDate: "2022-12-12T04:38:30Z",
+        Duration: "2022-12-26T21:00:00Z",
+        HostName: "RITCHIE",
+        HostImage: "img/hosts/red.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Kanto",
+        Generation: 1,
+        Pokedex: "Kanto",
+        // FinalStateLink:"./states/Season 9/yellow.json", 
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Boulder Badge",
+                Image: "img/badges/boulder.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Cascade Badge",
+                Image: "img/badges/cascade.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Thunder Badge",
+                Image: "img/badges/thunder.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Rainbow Badge",
+                Image: "img/badges/rainbow.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Soul Badge",
+                Image: "img/badges/soul.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Marsh Badge",
+                Image: "img/badges/marsh.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Volcano Badge",
+                Image: "img/badges/volcano.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Earth Badge",
+                Image: "img/badges/earth.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Lorelei",
+                Image: "img/trainers/red/lorelei.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Bruno",
+                Image: "img/trainers/red/bruno.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Agatha",
+                Image: "img/trainers/red/agatha.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Lance",
+                Image: "img/trainers/red/lance.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "BLUE",
+                Image: "img/trainers/red/blue.png",
+                Time: "",
+                Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                    // { Pokemon: "Zapdos", Nickname: "AA-j", Level: 81, Type1: "Electric", Type2: "Flying" },
+                    // { Pokemon: "Nidoking", Nickname: "AAAAAAAAAA", Level: 54, Type1: "Poison", Type2: "Ground" },
+                    // { Pokemon: "Omastar", Level: 52, Type1: "Rock", Type2: "Water" },
+                    // { Pokemon: "Venomoth", Nickname: "AATTVVV", Level: 39, Type1: "Bug", Type2: "Poison" },
+                    // { Pokemon: "Lapras", Nickname: "AIIIIIIRRR", Level: 31, Type1: "Water", Type2: "Ice" },
+                    // { Pokemon: "Pidgeot", Nickname: "aaabaaajss", Level: 69, Type1: "Normal", Type2: "Flying" },
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+
+            { "Group": "Pokemon", "Name": "Pikachu", "Time": "2022-12-12T04:51:10.454Z" },
+            { "Group": "Pokemon", "Name": "NidoranF", "Time": "2022-12-12T05:25:10.646Z" },
+            { "Group": "Pokemon", "Name": "Mankey", "Time": "2022-12-12T05:28:37.497Z" },
+            { "Group": "Pokemon", "Name": "Spearow", "Time": "2022-12-12T05:36:58.175Z" },
+            { "Group": "Pokemon", "Name": "Pidgeotto", "Time": "2022-12-12T05:58:55.557Z" }
+        ]
+    }
+);
