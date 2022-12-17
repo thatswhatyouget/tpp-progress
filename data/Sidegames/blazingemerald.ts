c268079d2a8cfbf6956e92082d9832f5284263f9
M4 <dave.was.r@gmail.com>
2022-12-16 21:19:32 -0500

    Earned the Stone Badge

--- data/Sidegames/blazingemerald.ts
+++ data/Sidegames/blazingemerald.ts
@@ -19,8 +19,8 @@ Sidegames.Runs.push(
                 Group: "Badges",
                 Name: "Stone Badge",
                 Image: "img/badges/stone.png",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-17T01:49:42.346Z",
+                Attempts: 1
             },
             {
                 Group: "Badges",
@@ -108,12 +108,12 @@ Sidegames.Runs.push(
             },
             <TPP.HallOfFame>{
                 Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "61902", Attempts: 0, Party: [
-                    { Pokemon: "Weezing", Nickname: "G,.. YRMto", Level: 60, Gender: "Male", Number: 184, IDNo: "61902" },
-                    { Pokemon: "Shuckle", Nickname: "GGNN", Level: 60, Gender: "Male", Number: 249, IDNo: "61902" },
-                    { Pokemon: "Beedrill", Nickname: ",,,      ", Level: 60, Gender: "Female", Number: 111, IDNo: "61902" },
-                    { Pokemon: "Crobat", Nickname: "MASTERBAIT", Level: 60, Gender: "Female", Number: 126, IDNo: "61902" },
-                    { Pokemon: "Weavile", Nickname: "GI", Level: 60, Gender: "Male", Number: 289, IDNo: "61902" },
-                    { Pokemon: "Gliscor", Nickname: "TT     -  ", Level: 60, Gender: "Female", Number: 160, IDNo: "61902" },
+                    // { Pokemon: "Weezing", Nickname: "G,.. YRMto", Level: 60, Gender: "Male", Number: 184, IDNo: "61902" },
+                    // { Pokemon: "Shuckle", Nickname: "GGNN", Level: 60, Gender: "Male", Number: 249, IDNo: "61902" },
+                    // { Pokemon: "Beedrill", Nickname: ",,,      ", Level: 60, Gender: "Female", Number: 111, IDNo: "61902" },
+                    // { Pokemon: "Crobat", Nickname: "MASTERBAIT", Level: 60, Gender: "Female", Number: 126, IDNo: "61902" },
+                    // { Pokemon: "Weavile", Nickname: "GI", Level: 60, Gender: "Male", Number: 289, IDNo: "61902" },
+                    // { Pokemon: "Gliscor", Nickname: "TT     -  ", Level: 60, Gender: "Female", Number: 160, IDNo: "61902" },
                 ],
                 FirstAttemptDate: "",
                 Image: "img/ribbons/champion.png"
@@ -164,6 +164,23 @@ Sidegames.Runs.push(
                 FirstAttemptDate: "",
                 Image: "img/ribbons/champion.png"
             },
+            { "Group": "Pokemon", "Name": "Clefairy", "Time": "2022-12-16T21:26:52.577Z" },
+            { "Group": "Pokemon", "Name": "Wurmple", "Time": "2022-12-16T21:52:43.688Z" },
+            { "Group": "Pokemon", "Name": "Shinx", "Time": "2022-12-16T21:55:57.227Z" },
+            { "Group": "Pokemon", "Name": "Zigzagoon", "Time": "2022-12-16T22:05:40.302Z" },
+            { "Group": "Pokemon", "Name": "Poochyena", "Time": "2022-12-16T22:09:21.711Z" },
+            { "Group": "Pokemon", "Name": "Poliwag", "Time": "2022-12-16T22:42:55.822Z" },
+            { "Group": "Pokemon", "Name": "Wingull", "Time": "2022-12-16T22:54:21.497Z" },
+            { "Group": "Pokemon", "Name": "Snubbull", "Time": "2022-12-16T22:59:00.312Z" },
+            { "Group": "Pokemon", "Name": "Cascoon", "Time": "2022-12-16T23:16:33.233Z" },
+            { "Group": "Pokemon", "Name": "Dustox", "Time": "2022-12-16T23:32:37.688Z" },
+            { "Group": "Pokemon", "Name": "Ledyba", "Time": "2022-12-16T23:38:39.894Z" },
+            { "Group": "Pokemon", "Name": "Caterpie", "Time": "2022-12-16T23:42:08.718Z" },
+            { "Group": "Pokemon", "Name": "Silcoon", "Time": "2022-12-17T00:20:22.940Z" },
+            { "Group": "Pokemon", "Name": "Shroomish", "Time": "2022-12-17T00:28:58.572Z" },
+            { "Group": "Pokemon", "Name": "Venonat", "Time": "2022-12-17T00:32:17.121Z" },
+            { "Group": "Pokemon", "Name": "Beautifly", "Time": "2022-12-17T01:09:45.242Z" },
+            { "Group": "Pokemon", "Name": "Whismur", "Time": "2022-12-17T02:05:23.298Z" }
         ]
     }
 );
