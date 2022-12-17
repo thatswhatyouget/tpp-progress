3290ca36d0442286276b8a4af7634cf3f6db823b
M4 <dave.was.r@gmail.com>
2022-12-16 21:57:40 -0500

    Finished Yellow

--- data/Sidegames/yellow.ts
+++ data/Sidegames/yellow.ts
@@ -12,7 +12,7 @@ Sidegames.Runs.push(
         Region: "Kanto",
         Generation: 1,
         Pokedex: "Kanto",
-        // FinalStateLink:"./states/Season 9/yellow.json", 
+        FinalStateLink: "./states/Season 9/yellow.json",
         Events: [
             {
                 Group: "Badges",
@@ -35,94 +35,106 @@ Sidegames.Runs.push(
                 Name: "Thunder Badge",
                 Image: "img/badges/thunder.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-13T17:20:30.600Z",
+                Attempts: 1
             },
             {
                 Group: "Badges",
                 Name: "Rainbow Badge",
                 Image: "img/badges/rainbow.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-13T21:08:31.126Z",
+                Attempts: 1
             },
             {
                 Group: "Badges",
                 Name: "Soul Badge",
                 Image: "img/badges/soul.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-14T08:35:40.848Z",
+                Attempts: 1
             },
             {
                 Group: "Badges",
                 Name: "Marsh Badge",
                 Image: "img/badges/marsh.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-15T03:10:35.622Z",
+                Attempts: 2
             },
             {
                 Group: "Badges",
                 Name: "Volcano Badge",
                 Image: "img/badges/volcano.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-15T04:37:06.389Z",
+                Attempts: 1
             },
             {
                 Group: "Badges",
                 Name: "Earth Badge",
                 Image: "img/badges/earth.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-15T19:06:49.327Z",
+                Attempts: 1
             },
             {
                 Group: "Elite Four",
                 Name: "Lorelei",
                 Image: "img/trainers/red/lorelei.png",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-16T00:28:05.698Z",
+                Attempts: 1
             },
             {
                 Group: "Elite Four",
                 Name: "Bruno",
                 Image: "img/trainers/red/bruno.png",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-16T00:32:19.606Z",
+                Attempts: 1
             },
             {
                 Group: "Elite Four",
                 Name: "Agatha",
                 Image: "img/trainers/red/agatha.png",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-16T00:42:12.339Z",
+                Attempts: 1
             },
             {
                 Group: "Elite Four",
                 Name: "Lance",
                 Image: "img/trainers/red/lance.png",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-16T02:16:54.084Z",
+                Attempts: 3
             },
             {
                 Group: "Champions",
-                Name: "BLUE",
+                Name: "JOHN",
                 Image: "img/trainers/red/blue.png",
-                Time: "",
-                Attempts: 0
+                Time: "2022-12-16T02:39:52.507Z",
+                Attempts: 1
             },
             <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
-                    // { Pokemon: "Zapdos", Nickname: "AA-j", Level: 81, Type1: "Electric", Type2: "Flying" },
-                    // { Pokemon: "Nidoking", Nickname: "AAAAAAAAAA", Level: 54, Type1: "Poison", Type2: "Ground" },
-                    // { Pokemon: "Omastar", Level: 52, Type1: "Rock", Type2: "Water" },
-                    // { Pokemon: "Venomoth", Nickname: "AATTVVV", Level: 39, Type1: "Bug", Type2: "Poison" },
-                    // { Pokemon: "Lapras", Nickname: "AIIIIIIRRR", Level: 31, Type1: "Water", Type2: "Ice" },
-                    // { Pokemon: "Pidgeot", Nickname: "aaabaaajss", Level: 69, Type1: "Normal", Type2: "Flying" },
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2022-12-16T02:40:46.413Z", Attempts: 3, Party: [
+                    { Pokemon: "Articuno", Nickname: "FRIDGO", Level: 55, Type1: "Ice", Type2: "Flying" },
+                    { Pokemon: "Snorlax", Nickname: "KIRBY", Level: 42, Type1: "Normal" },
+                    { Pokemon: "Tentacruel", Nickname: "SPLATOON", Level: 50, Type1: "Water", Type2: "Poison" },
+                    { Pokemon: "Primeape", Nickname: "PunOko", Level: 46, Type1: "Fighting" },
+                    { Pokemon: "Mr. Mime", Nickname: "MILES", Level: 51, Type1: "Psychic" },
+                    { Pokemon: "Pikachu", Nickname: "SPARKY", Level: 53, Type1: "Electric" },
                 ],
-                FirstAttemptDate: "",
+                FirstAttemptDate: "3d 19h 37m 21s",
+                Image: "img/ribbons/champion.png"
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2022-12-16T19:15:00Z", Attempts: 7, Party: [
+                    { Pokemon: "Tentacruel", Nickname: "SPLATOON", Level: 51, Type1: "Water", Type2: "Poison" },
+                    { Pokemon: "Snorlax", Nickname: "KIRBY", Level: 51, Type1: "Normal" },
+                    { Pokemon: "Primeape", Nickname: "PunOko", Level: 48, Type1: "Fighting" },
+                    { Pokemon: "Mr. Mime", Nickname: "MILES", Level: 53, Type1: "Psychic" },
+                    { Pokemon: "Pikachu", Nickname: "SPARKY", Level: 52, Type1: "Electric" },
+                    { Pokemon: "Fearow", Nickname: "AVES", Level: 44, Type1: "Normal", Type2: "Flying" },
+                ],
+                FirstAttemptDate: "4d 8h 38m 59s",
                 Image: "img/ribbons/champion.png"
             },
 
@@ -139,7 +151,67 @@ Sidegames.Runs.push(
             { "Group": "Pokemon", "Name": "Clefairy", "Time": "2022-12-12T14:39:19.135Z" },
             { "Group": "Pokemon", "Name": "Geodude", "Time": "2022-12-12T15:38:01.206Z" },
             { "Group": "Pokemon", "Name": "Paras", "Time": "2022-12-12T16:09:02.603Z" },
-            { "Group": "Pokemon", "Name": "Charmander", "Time": "2022-12-12T17:54:50.312Z" }
+            { "Group": "Pokemon", "Name": "Charmander", "Time": "2022-12-12T17:54:50.312Z" },
+            { "Group": "Pokemon", "Name": "Nidoqueen", "Time": "2022-12-12T19:02:03.382Z" },
+            { "Group": "Pokemon", "Name": "Rattata", "Time": "2022-12-12T19:20:30.965Z" },
+            { "Group": "Pokemon", "Name": "Drowzee", "Time": "2022-12-12T21:52:46.162Z" },
+            { "Group": "Pokemon", "Name": "Diglett", "Time": "2022-12-12T22:29:18.983Z" },
+            { "Group": "Pokemon", "Name": "Abra", "Time": "2022-12-12T23:09:38.179Z" },
+            { "Group": "Pokemon", "Name": "Jigglypuff", "Time": "2022-12-12T23:57:53.155Z" },
+            { "Group": "Pokemon", "Name": "Fearow", "Time": "2022-12-13T02:08:37.421Z" },
+            { "Group": "Pokemon", "Name": "Mr.Mime", "Time": "2022-12-13T03:47:43.355Z" }, //MILES
+            { "Group": "Pokemon", "Name": "Bulbasaur", "Time": "2022-12-13T08:27:41.404Z" },
+            { "Group": "Pokemon", "Name": "Caterpie", "Time": "2022-12-13T11:17:04.240Z" },
+            { "Group": "Pokemon", "Name": "Metapod", "Time": "2022-12-13T11:25:46.649Z" },
+            { "Group": "Pokemon", "Name": "Sandshrew", "Time": "2022-12-13T12:04:29.675Z" },
+            { "Group": "Pokemon", "Name": "Dugtrio", "Time": "2022-12-13T13:23:54.386Z" },
+            { "Group": "Pokemon", "Name": "Primeape", "Time": "2022-12-13T17:08:54.567Z" },
+            { "Group": "Pokemon", "Name": "Squirtle", "Time": "2022-12-13T17:24:45.816Z" },
+            { "Group": "Pokemon", "Name": "Onix", "Time": "2022-12-13T18:15:13.336Z" },
+            { "Group": "Pokemon", "Name": "Eevee", "Time": "2022-12-13T19:33:30.927Z" },
+            { "Group": "Pokemon", "Name": "Jolteon", "Time": "2022-12-13T19:46:06.031Z" },
+            { "Group": "Pokemon", "Name": "Hitmonlee", "Time": "2022-12-13T21:53:24.146Z" },
+            { "Group": "Pokemon", "Name": "Snorlax", "Time": "2022-12-13T22:58:57.693Z" },
+            { "Group": "Pokemon", "Name": "Exeggcute", "Time": "2022-12-13T23:34:25.297Z" },
+            { "Group": "Pokemon", "Name": "Kangaskhan", "Time": "2022-12-14T00:10:49.658Z" },
+            { "Group": "Pokemon", "Name": "Cubone", "Time": "2022-12-14T00:27:34.363Z" },
+            { "Group": "Pokemon", "Name": "Tentacool", "Time": "2022-12-14T03:08:23.390Z" },
+            { "Group": "Pokemon", "Name": "Aerodactyl", "Time": "2022-12-14T09:28:24.984Z" },
+            { "Group": "Pokemon", "Name": "Raticate", "Time": "2022-12-14T09:37:04.014Z" },
+            { "Group": "Pokemon", "Name": "Growlithe", "Time": "2022-12-14T09:39:11.698Z" },
+            { "Group": "Pokemon", "Name": "Grimer", "Time": "2022-12-14T09:43:54.109Z" },
+            { "Group": "Pokemon", "Name": "Machop", "Time": "2022-12-14T10:37:47.794Z" },
+            { "Group": "Pokemon", "Name": "Gastly", "Time": "2022-12-14T11:55:38.466Z" },
+            { "Group": "Pokemon", "Name": "Haunter", "Time": "2022-12-14T12:20:00.008Z" },
+            { "Group": "Pokemon", "Name": "Bellsprout", "Time": "2022-12-14T13:27:01.625Z" },
+            { "Group": "Pokemon", "Name": "Oddish", "Time": "2022-12-14T13:31:46.863Z" },
+            { "Group": "Pokemon", "Name": "Weepinbell", "Time": "2022-12-14T13:37:34.224Z" },
+            { "Group": "Pokemon", "Name": "Slowpoke", "Time": "2022-12-14T13:49:00.428Z" },
+            { "Group": "Pokemon", "Name": "Farfetch'd", "Time": "2022-12-14T15:24:23.185Z" }, // DUX
+            { "Group": "Pokemon", "Name": "Gloom", "Time": "2022-12-14T15:39:05.078Z" },
+            { "Group": "Pokemon", "Name": "Venonat", "Time": "2022-12-14T18:26:45.187Z" },
+            { "Group": "Pokemon", "Name": "Tentacruel", "Time": "2022-12-14T19:37:10.683Z" },
+            { "Group": "Pokemon", "Name": "Doduo", "Time": "2022-12-14T20:34:39.818Z" },
+            { "Group": "Pokemon", "Name": "Ponyta", "Time": "2022-12-14T21:41:31.855Z" },
+            { "Group": "Pokemon", "Name": "Kabuto", "Time": "2022-12-15T00:42:37.186Z" },
+            { "Group": "Pokemon", "Name": "Lapras", "Time": "2022-12-15T02:04:26.061Z" },
+            { "Group": "Pokemon", "Name": "Horsea", "Time": "2022-12-15T07:53:17.448Z" },
+            { "Group": "Pokemon", "Name": "Seadra", "Time": "2022-12-15T08:02:17.288Z" },
+            { "Group": "Pokemon", "Name": "Butterfree", "Time": "2022-12-15T08:17:11.557Z" },
+            { "Group": "Pokemon", "Name": "Hypno", "Time": "2022-12-15T11:08:30.920Z" },
+            { "Group": "Pokemon", "Name": "Krabby", "Time": "2022-12-15T17:15:27.442Z" },
+            { "Group": "Pokemon", "Name": "Articuno", "Time": "2022-12-15T17:23:56.375Z" },
+            { "Group": "Pokemon", "Name": "Kingler", "Time": "2022-12-15T17:29:48.242Z" },
+            { "Group": "Pokemon", "Name": "Seel", "Time": "2022-12-15T17:32:20.067Z" },
+            { "Group": "Pokemon", "Name": "Staryu", "Time": "2022-12-15T18:18:52.401Z" },
+            { "Group": "Pokemon", "Name": "Nidorino", "Time": "2022-12-15T20:00:26.443Z" },
+            { "Group": "Pokemon", "Name": "Machoke", "Time": "2022-12-15T23:48:47.104Z" },
+            { "Group": "Pokemon", "Name": "Ditto", "Time": "2022-12-16T03:47:06.659Z" },
+            { "Group": "Pokemon", "Name": "Mewtwo", "Time": "2022-12-16T04:59:57.855Z" },
+            { "Group": "Pokemon", "Name": "Ivysaur", "Time": "2022-12-16T09:31:49.886Z" },
+            { "Group": "Pokemon", "Name": "Venusaur", "Time": "2022-12-16T11:19:12.453Z" },
+            { "Group": "Pokemon", "Name": "Golbat", "Time": "2022-12-16T11:57:14.995Z" },
+            { "Group": "Pokemon", "Name": "Poliwag", "Time": "2022-12-16T12:57:15.285Z" }
         ]
     }
 );
