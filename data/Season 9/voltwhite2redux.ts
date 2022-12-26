2e34af429c2158ef978102f0b6f60d62acad5b57
M4 <dave.was.r@gmail.com>
2022-12-26 16:46:54 -0500

    Added Volt White 2 Redux

---
+++ data/Season 9/voltwhite2redux.ts
@@ -0,0 +1,175 @@
+/// <reference path="../tpp-data.ts" />
+Season9.Runs.push(
+    {
+        RunName: "Volt White 2 Redux",
+        ColorPrimary: "white",
+        ColorSecondary: "black",
+        Duration: "30d",
+        StartDate: "2022-12-26T21:00:00Z",
+        HostName: "44X N Y",
+        HostImage: "img/hosts/aaaalk.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/6f6mw8/dr_alek_our_serious_and_diligent_protagonist/",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "National",
+        // FinalStateLink:"./states/Season 9/voltwhite2redux.json", 
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Basic Badge",
+                Image: "img/badges/basic.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Toxic Badge",
+                Image: "img/badges/toxic.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Insect Badge",
+                Image: "img/badges/insect.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Bolt Badge",
+                Image: "img/badges/bolt.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Quake Badge",
+                Image: "img/badges/quake.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Jet Badge",
+                Image: "img/badges/jet.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Legend Badge",
+                Image: "img/badges/legend.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Wave Badge",
+                Image: "img/badges/wave.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Shauntal",
+                Image: "img/trainers/black/shauntal.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Grimsley",
+                Image: "img/trainers/black/grimsley.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Caitlin",
+                Image: "img/trainers/black/caitlin.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Marshal",
+                Image: "img/trainers/black/marshal.png",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "♂♂♂'",
+                Image: "img/hosts/♂♂♂.png",
+                ImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/odwg8w/mars_the_tomboyish_trainer_barp_the_ampharos_and/",
+                Time: "",
+                Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "09544", Attempts: 0, Party: [
+                    { Pokemon: "Meganium", Nickname: " u", Level: 89, Gender: "Male" },
+                    { Pokemon: "Infernape", Nickname: "00k", Level: 89, Gender: "Male" },
+                    { Pokemon: "Torterra", Nickname: "fj", Level: 88, Gender: "Male" },
+                    { Pokemon: "Kricketune", Nickname: "ucrdq  2sl", Level: 84, Gender: "Female" },
+                    { Pokemon: "Mawile", Nickname: "  i", Level: 81, Gender: "Male" },
+                    { Pokemon: "Lunatone", Nickname: "Lun\u263a", Level: 82 },
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Grimsley",
+                Image: "img/trainers/black/grimsley.png",
+                Class: "Rematch",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Shauntal",
+                Image: "img/trainers/black/shauntal.png",
+                Class: "Rematch",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Caitlin",
+                Image: "img/trainers/black/caitlin.png",
+                Class: "Rematch",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Marshal",
+                Image: "img/trainers/black/marshal.png",
+                Class: "Rematch",
+                Time: "",
+                Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "Iris",
+                Image: "img/trainers/black/iris.png",
+                Class: "Rematch",
+                Time: "",
+                Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "09544", Attempts: 0, Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+
+            { "Group": "Pokemon", "Name": "Snivy", "Time": "2022-12-26T21:10:58.597Z" },
+            { "Group": "Pokemon", "Name": "Starly", "Time": "2022-12-26T21:25:11.837Z" },
+            { "Group": "Pokemon", "Name": "Pidgey", "Time": "2022-12-26T21:26:42.096Z" },
+            { "Group": "Pokemon", "Name": "Patrat", "Time": "2022-12-26T21:30:22.929Z" },
+            { "Group": "Pokemon", "Name": "Ledyba", "Time": "2022-12-26T21:31:06.725Z" }
+        ]
+    }
+);
