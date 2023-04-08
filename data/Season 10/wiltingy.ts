3fa4d7ac76adfad9b71c4d0fd051152557db1033
M4 <dave.was.r@gmail.com>
2023-04-08 15:47:18 -0400

    Added Wilting Y

---
+++ data/Season 10/wiltingy.ts
@@ -0,0 +1,102 @@
+/// <reference path="../tpp-data.ts" />
+Season10.Runs.push(
+    {
+        RunName: "Wilting Y",
+        ColorPrimary: "#e1002f",
+        ColorSecondary: "gold",
+        Duration: "255d",
+        StartDate: "2023-04-08T21:00:00Z",
+        HostName: "",
+        // HostImage: "img/hosts/266.png",
+        // HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/9f0o9s/hall_of_fame_randomized_y/e5tt7ma/",
+        Region: "Kalos",
+        Generation: 6,
+        Events: [
+            {
+                Group: "Badges", Name: "Bug Badge", Image: "img/badges/bug.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Cliff Badge", Image: "img/badges/cliff.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Rumble Badge", Image: "img/badges/rumble.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Plant Badge", Image: "img/badges/plant.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Voltage Badge", Image: "img/badges/voltage.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Fairy Badge", Image: "img/badges/fairy.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Psychic Badge", Image: "img/badges/psychic.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Iceberg Badge", Image: "img/badges/iceberg.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Siebold", Image: "img/trainers/x/siebold.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Malva", Image: "img/trainers/x/malva.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Drasna", Image: "img/trainers/x/drasna.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Wikstrom", Image: "img/trainers/x/wikstrom.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Diantha", Image: "img/trainers/x/diantha.png",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2020-05-07T16:39:38.481Z", Attempts: 8, Party: [
+                    // { Pokemon: "Heliolisk", Nickname: "Herrrdliopr", Level: 78, Gender: "Male" },
+                    // { Pokemon: "Aurorus", Nickname: "Amééépura2", Level: 77, Gender: "Male" },
+                    // { Pokemon: "Mothim", Nickname: "Moth", Level: 70, Gender: "Male" },
+                    // { Pokemon: "Goodra", Nickname: "R6", Level: 65, Gender: "Male" },
+                    // { Pokemon: "Ducklett", Nickname: "dfuucck", Level: 61, Gender: "Female" },
+                    // { Pokemon: "Clawitzer", Nickname: "8d b9 ’bmp m", Level: 56, Gender: "Female" },
+                ],
+                FirstAttemptDate: "2020-05-07T11:06:41.329Z",
+                Image: "img/ribbons/champion-kalos.png"
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Wikstrom", Image: "img/trainers/x/wikstrom.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Siebold", Image: "img/trainers/x/siebold.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Malva", Image: "img/trainers/x/malva.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Drasna", Image: "img/trainers/x/drasna.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Diantha", Image: "img/trainers/x/diantha.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+
+        ]
+    }
+);
