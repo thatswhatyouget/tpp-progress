cbe0a90cf601f2332bcfc9cdf02eed5da3b684af
M4 <dave.was.r@gmail.com>
2023-05-11 22:06:12 -0400

    Added Victory Fire

---
+++ data/Season 10/victoryfire.ts
@@ -0,0 +1,102 @@
+/// <reference path="../tpp-data.ts" />
+Season10.Runs.push(
+    {
+        RunName: "Victory Fire",
+        ColorPrimary: "#C53A21",
+        ColorSecondary: "#E6D6B5",
+        Duration: "255d",
+        StartDate: "2023-05-13T21:00:00Z",
+        HostName: "",
+        // HostImage: "img/hosts/april.png",
+        Region: "Tyron",
+        Pokedex: "Tyron",
+        // FinalStateLink: "./states/Season 7/vega.json",
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Transform Badge",
+                Image: "img/badges/victoryfire/transform.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Rising Badge",
+                Image: "img/badges/victoryfire/rising.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Black Badge",
+                Image: "img/badges/victoryfire/black.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Wave Badge",
+                Image: "img/badges/victoryfire/wave.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Blaze Badge",
+                Image: "img/badges/victoryfire/blaze.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Snowflake Badge",
+                Image: "img/badges/victoryfire/snowflake.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "MindEye Badge",
+                Image: "img/badges/victoryfire/mindeye.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Nature Badge",
+                Image: "img/badges/victoryfire/nature.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Theodore",
+                Image: "img/trainers/vega/theodore.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Vanessa",
+                Image: "img/trainers/vega/vanessa.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Irene",
+                Image: "img/trainers/vega/irene.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Cole",
+                Image: "img/trainers/vega/cole.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "Sylvia",
+                Image: "img/trainers/vega/sylvia.png",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+
+        ]
+    }
+);
