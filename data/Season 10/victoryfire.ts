b67e94ea1eb0af77604b0f8b05b60b185be902cd
M4 <dave.was.r@gmail.com>
2023-05-14 00:20:52 -0400

    Fixed Tyron regional dex

--- data/Season 10/victoryfire.ts
+++ data/Season 10/victoryfire.ts
@@ -6,8 +6,8 @@ Season10.Runs.push(
         ColorSecondary: "#E6D6B5",
         Duration: "255d",
         StartDate: "2023-05-13T21:00:00Z",
-        HostName: "",
-        // HostImage: "img/hosts/april.png",
+        HostName: "B",
+        HostImage: "img/hosts/b2.png",
         BaseGame: "Emerald",
         Region: "Tyron",
         Pokedex: "Tyron",
@@ -17,7 +17,7 @@ Season10.Runs.push(
                 Group: "Badges",
                 Name: "Transform Badge",
                 Image: "img/badges/victoryfire/transform.png",
-                Time: "", Attempts: 0
+                Time: "2023-05-14T01:02:16.811Z", Attempts: 1
             },
             {
                 Group: "Badges",
@@ -97,6 +97,22 @@ Season10.Runs.push(
                 FirstAttemptDate: "",
                 Image: "img/ribbons/champion.png"
             },
+
+            { "Group": "Pokemon", "Name": "Axew", "Time": "2023-05-13T21:18:06.621Z" },
+            { "Group": "Pokemon", "Name": "Salandit", "Time": "2023-05-13T21:24:09.029Z" },
+            { "Group": "Pokemon", "Name": "Pineco", "Time": "2023-05-13T22:18:48.972Z" },
+            { "Group": "Pokemon", "Name": "Munna", "Time": "2023-05-13T23:39:55.205Z" },
+            { "Group": "Pokemon", "Name": "Magnemite", "Time": "2023-05-13T23:46:07.993Z" },
+            { "Group": "Pokemon", "Name": "Woobat", "Time": "2023-05-13T23:49:03.589Z" },
+            { "Group": "Pokemon", "Name": "Tynamo", "Time": "2023-05-13T23:56:40.992Z" },
+            { "Group": "Pokemon", "Name": "Musharna", "Time": "2023-05-14T00:28:48.037Z" },
+            { "Group": "Pokemon", "Name": "Koffing", "Time": "2023-05-14T01:29:58.640Z" },
+            { "Group": "Pokemon", "Name": "Machop", "Time": "2023-05-14T01:35:40.464Z" },
+            { "Group": "Pokemon", "Name": "Scraggy", "Time": "2023-05-14T01:44:00.309Z" },
+            { "Group": "Pokemon", "Name": "Shuppet", "Time": "2023-05-14T02:11:49.050Z" },
+            { "Group": "Pokemon", "Name": "Yamask", "Time": "2023-05-14T02:23:30.222Z" },
+            { "Group": "Pokemon", "Name": "Litwick", "Time": "2023-05-14T03:33:19.508Z" },
+            { "Group": "Pokemon", "Name": "Gastly", "Time": "2023-05-14T03:42:06.751Z" },
 
         ]
     }
