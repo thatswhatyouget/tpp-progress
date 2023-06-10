176da2b9c9c4a114bb7c44c3c0f1018a1a31d9d9
M4 <dave.was.r@gmail.com>
2023-06-10 07:51:18 -0400

    Added Randomized Soul Silver

---
+++ data/Season 10/randomizedsoulsilver.ts
@@ -0,0 +1,139 @@
+/// <reference path="../tpp-data.ts" />
+Season10.Runs.push(
+    {
+        RunName: "Randomized Soul Silver",
+        ColorPrimary: "#7a9cff",
+        ColorSecondary: "#278fe5",
+        StartDate: "2023-06-10T12:00:00Z",
+        Duration: "255d",
+        HostName: "",
+        HostImage: "img/hosts/aaeffff.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/8ceslx/syke_sprite/",
+        Region: "Johto",
+        Pokedex: "National",
+        // AdditionalRegions: [{ Name: "Kanto", Time: "8d21h39m" }],
+        Generation: 4,
+        Events: [
+            {
+                Group: "Badges", Name: "Zephyr Badge", Image: "img/badges/zephyr.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Hive Badge", Image: "img/badges/hive.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Plain Badge", Image: "img/badges/plain.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Fog Badge", Image: "img/badges/fog.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Storm Badge", Image: "img/badges/storm.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Mineral Badge", Image: "img/badges/mineral.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Glacier Badge", Image: "img/badges/glacier.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Rising Badge", Image: "img/badges/rising.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four", Name: "Will", Image: "img/trainers/heartgold/will.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Koga", Image: "img/trainers/heartgold/koga.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Bruno", Image: "img/trainers/heartgold/bruno.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Karen", Image: "img/trainers/heartgold/karen.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Lance", Image: "img/trainers/heartgold/lance.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four Rematch", Name: "Will", Image: "img/trainers/heartgold/will.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Koga", Image: "img/trainers/heartgold/koga.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Bruno", Image: "img/trainers/heartgold/bruno.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Karen", Image: "img/trainers/heartgold/karen.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Lance", Image: "img/trainers/heartgold/lance.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, IDNo: "", Party: [
+                    // { Pokemon: "Skuntank", Nickname: "F2P", Level: 60, Gender: "Female", Met: "Sprout Tower", OT: "AAEFFFF" },
+                    // { Pokemon: "Farfetch'd", Nickname: "0011hIqU", Level: 55, Gender: "Female", Met: "Route 48", OT: "AAEFFFF" },
+                    // { Pokemon: "Lugia", Nickname: "r", Level: 58, Met: "Whirl Islands", OT: "AAEFFFF" },
+                    // { Pokemon: "Gastrodon", Nickname: "AAAAAAAAA", Level: 57, Gender: "Male", Met: "Slowpoke Well", OT: "AAEFFFF" },
+                    // { Pokemon: "Froslass", Nickname: "00      UU", Level: 68, Gender: "Female", Met: "Slowpoke Well", OT: "AAEFFFF" },
+                    // { Pokemon: "Typhlosion", Nickname: "222", Level: 82, Gender: "Male", Met: "New Bark Town", OT: "AAEFFFF" },
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: ""
+            },
+        ]
+    }
+);
