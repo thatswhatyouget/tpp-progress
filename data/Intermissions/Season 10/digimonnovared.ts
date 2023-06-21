1fc09196b905c1779bc0ebad0cf646a964d48f3a
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2023-06-21 20:11:13 +0100

    Populated Digimon Nova Red

--- data/Intermissions/Season 10/digimonnovared.ts
+++ data/Intermissions/Season 10/digimonnovared.ts
@@ -6,8 +6,7 @@ Intermissions.Runs.push(
         ColorPrimary: "#E67300",
         ColorSecondary: "#AD4200",
         StartDate: "2023-05-12T21:45:00Z",
-        // Duration: "2023-05-13T20:30:00Z", (when Victory Fire started)
-        Duration: "255d", // Split everything after the time above into a revisit 
+        Duration: "2023-05-13T20:30:00Z",
         HostName: "DDDJWV",
         HostImage: "img/hosts/pee.png",
         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/568gkg/the_ultra_crew_so_far_v_30/d8i7kwk",
@@ -16,49 +15,66 @@ Intermissions.Runs.push(
         Events: [
             {
                 Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2023-05-13T03:32:57.726Z", Attempts: 2
             },
             {
                 Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2023-05-13T08:20:00.662Z", Attempts: 1
             },
             {
                 Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2023-05-13T15:08:37.568Z", Attempts: 2
             },
             {
                 Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
-            },
-            <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2023-05-20T03:39:45.092Z", IDNo: "32286", Attempts: 31, Party: [
-                    { Pokemon: "A.Mermaimo", Nickname: "AHNNNN", Level: 86, Gender: "Female", Number: 275, IDNo: "32286" },
-                    { Pokemon: "Gallantmon", Nickname: "C.Dukemon", Level: 68, Gender: "Male", Number: 83, IDNo: "32286" },
-                    { Pokemon: "Goldramon", Nickname: "loser", Level: 76, Number: 347, IDNo: "32286" },
-                    { Pokemon: "Lilithmon", Nickname: "VLA.", Level: 66, Gender: "Female", Number: 205, IDNo: "32286" },
-                    { Pokemon: "Ophanimon", Nickname: "NIA", Level: 74, Gender: "Female", Number: 41, IDNo: "32286" },
-                    { Pokemon: "WarGreymon", Nickname: " AOLLR", Level: 82, Gender: "Male", Number: 10, IDNo: "32286" },
-                ],
-                FirstAttemptDate: "2023-05-19T12:26:10.178Z",
-                Image: "img/ribbons/champion.png"
+                Time: "2023-05-13T19:34:41.318Z", Attempts: 1
             },
 
+            { "Group": "Pokemon", "Name": "Koromon", "Time": "2023-05-12T21:51:37.541Z" },
+            { "Group": "Pokemon", "Name": "Curimon", "Time": "2023-05-12T22:26:26.771Z" },
+            { "Group": "Pokemon", "Name": "Punimon", "Time": "2023-05-12T22:56:39.183Z" },
+            { "Group": "Pokemon", "Name": "Pichimon", "Time": "2023-05-12T23:04:26.145Z" },
+            { "Group": "Pokemon", "Name": "Y.Botamon", "Time": "2023-05-12T23:15:16.357Z" },
+            { "Group": "Pokemon", "Name": "Puroromon", "Time": "2023-05-12T23:23:52.386Z" },
+            { "Group": "Pokemon", "Name": "Garurumon", "Time": "2023-05-12T23:31:29.812Z" },
+            { "Group": "Pokemon", "Name": "Wormmon", "Time": "2023-05-12T23:38:30.741Z" },
+            { "Group": "Pokemon", "Name": "Bukamon", "Time": "2023-05-12T23:38:30.741Z" },
+            { "Group": "Pokemon", "Name": "Nyokimon", "Time": "2023-05-13T00:15:40.070Z" },
+            { "Group": "Pokemon", "Name": "Chibomon", "Time": "2023-05-13T00:21:53.135Z" },
+            { "Group": "Pokemon", "Name": "Motimon", "Time": "2023-05-13T00:32:55.009Z" },
+            { "Group": "Pokemon", "Name": "Minomon", "Time": "2023-05-13T00:38:40.861Z" },
+            { "Group": "Pokemon", "Name": "Yuramon", "Time": "2023-05-13T01:03:05.120Z" },
+            { "Group": "Pokemon", "Name": "DemiVeemon", "Time": "2023-05-13T01:08:27.944Z" },
+            { "Group": "Pokemon", "Name": "Chapmon", "Time": "2023-05-13T01:43:38.371Z" },
+            { "Group": "Pokemon", "Name": "Gurimon", "Time": "2023-05-13T02:17:27.126Z" },
+            { "Group": "Pokemon", "Name": "K.Numemon", "Time": "2023-05-13T02:23:45.517Z" },
+            { "Group": "Pokemon", "Name": "Budmon", "Time": "2023-05-13T03:07:51.717Z" },
+            { "Group": "Pokemon", "Name": "Agumon", "Time": "2023-05-13T03:23:34.737Z" },
+            { "Group": "Pokemon", "Name": "Nohemon", "Time": "2023-05-13T03:26:40.924Z" },
+            { "Group": "Pokemon", "Name": "Nyaromon", "Time": "2023-05-13T03:59:22.257Z" },
+            { "Group": "Pokemon", "Name": "Manbomon", "Time": "2023-05-13T04:06:18.684Z" },
+            { "Group": "Pokemon", "Name": "Vilemon", "Time": "2023-05-13T05:02:03.592Z" },
+            { "Group": "Pokemon", "Name": "Salamon", "Time": "2023-05-13T05:36:14.539Z" },
+            { "Group": "Pokemon", "Name": "Kapurimon", "Time": "2023-05-13T07:52:04.646Z" },
+            { "Group": "Pokemon", "Name": "Petitmon", "Time": "2023-05-13T08:00:01.923Z" },
+            { "Group": "Pokemon", "Name": "Wanyamon", "Time": "2023-05-13T09:30:18.613Z" },
+            { "Group": "Pokemon", "Name": "Yokomon", "Time": "2023-05-13T09:43:52.609Z" },
+            { "Group": "Pokemon", "Name": "Sunarizamo", "Time": "2023-05-13T13:12:46.089Z" },
+            { "Group": "Pokemon", "Name": "Kudamon", "Time": "2023-05-13T13:22:26.371Z" },
+            { "Group": "Pokemon", "Name": "Scorpiomon", "Time": "2023-05-13T13:34:28.538Z" },
+            { "Group": "Pokemon", "Name": "Sunmon", "Time": "2023-05-13T13:53:13.013Z" },
+            { "Group": "Pokemon", "Name": "Gatomon", "Time": "2023-05-13T14:12:12.436Z" },
+            { "Group": "Pokemon", "Name": "Sukamon", "Time": "2023-05-13T14:27:56.946Z" },
+            { "Group": "Pokemon", "Name": "Crabmon", "Time": "2023-05-13T15:33:46.679Z" },
+            { "Group": "Pokemon", "Name": "Poromon", "Time": "2023-05-13T15:38:12.802Z" },
+            { "Group": "Pokemon", "Name": "Greymon", "Time": "2023-05-13T15:43:01.963Z" },
+            { "Group": "Pokemon", "Name": "Kamemon", "Time": "2023-05-13T16:49:01.943Z" },
+            { "Group": "Pokemon", "Name": "Monochromo", "Time": "2023-05-13T16:54:36.848Z" },
+            { "Group": "Pokemon", "Name": "Bearmon", "Time": "2023-05-13T17:03:13.939Z" },
+            { "Group": "Pokemon", "Name": "Tokomon", "Time": "2023-05-13T17:06:44.796Z" },
+            { "Group": "Pokemon", "Name": "Mermaimon", "Time": "2023-05-13T18:27:48.911Z" },
+            { "Group": "Pokemon", "Name": "Cutemon", "Time": "2023-05-13T19:40:26.455Z" },
+            { "Group": "Pokemon", "Name": "Veemon", "Time": "2023-05-13T19:58:27.028Z" },
         ]
     }
 );
---
+++ data/Revisits/digimonnovared.ts
@@ -0,0 +1,103 @@
+/// <reference path="../tpp-data.ts" />
+Revisits.Runs.push(
+    {
+        RunName: "Digimon Nova Red Revisit",
+        Class: "digimonnovared",
+        ColorPrimary: "#E67300",
+        ColorSecondary: "#AD4200",
+        StartDate: "2023-05-18T04:13:55Z",
+        Duration: "2023-05-20T03:45:00Z",
+        HostName: "DDDJWV",
+        HostImage: "img/hosts/pee.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/568gkg/the_ultra_crew_so_far_v_30/d8i7kwk",
+        BaseGame: "FireRed",
+        Pokedex: "Digimon",
+        FinalStateLink: "./states/Season 10/digimonnovared.json",
+        Events: [
+            {
+                Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2023-05-18T20:41:31.844Z", Attempts: 2
+            },
+            {
+                Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2023-05-18T23:33:35.432Z", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2023-05-19T01:59:05.546Z", Attempts: 2
+            },
+            {
+                Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2023-05-19T04:49:44.340Z", Attempts: 1
+            },
+
+            { Group: "Elite Four", Name: "Lorelei", Image: "img/trainers/red/lorelei.png", Time: "2023-05-19T17:47:45.059Z", Attempts: 6 },
+            { Group: "Elite Four", Name: "Bruno", Image: "img/trainers/red/bruno.png", Time: "2023-05-19T21:14:08.050Z", Attempts: 10 },
+            { Group: "Elite Four", Name: "Agatha", Image: "img/trainers/red/agatha.png", Time: "2023-05-19T23:10:01.338Z", Attempts: 4 },
+            { Group: "Elite Four", Name: "Lance", Image: "img/trainers/red/lance.png", Time: "2023-05-20T01:48:04.634Z", Attempts: 6 },
+            { Group: "Champions", Name: "Terry", Image: "img/trainers/red/blue.png", Time: "2023-05-20T03:38:53.053Z", Attempts: 4 },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2023-05-20T03:39:45.092Z", IDNo: "32286", Attempts: 31, Party: [
+                    { Pokemon: "A.Mermaimo", Nickname: "AHNNNN", Level: 86, Gender: "Female", Number: 275, IDNo: "32286" },
+                    { Pokemon: "Gallantmon", Nickname: "C.Dukemon", Level: 68, Gender: "Male", Number: 83, IDNo: "32286" },
+                    { Pokemon: "Goldramon", Nickname: "loser", Level: 76, Number: 347, IDNo: "32286" },
+                    { Pokemon: "Lilithmon", Nickname: "VLA.", Level: 66, Gender: "Female", Number: 205, IDNo: "32286" },
+                    { Pokemon: "Ophanimon", Nickname: "NIA", Level: 74, Gender: "Female", Number: 41, IDNo: "32286" },
+                    { Pokemon: "WarGreymon", Nickname: " AOLLR", Level: 82, Gender: "Male", Number: 10, IDNo: "32286" },
+                ],
+                FirstAttemptDate: "2023-05-19T12:26:10.178Z",
+                Image: "img/ribbons/champion.png"
+            },
+
+            { "Group": "Pokemon", "Name": "Raidramon", "Time": "2023-05-18T04:58:16.100Z" },
+            { "Group": "Pokemon", "Name": "Dracomon", "Time": "2023-05-18T05:45:10.955Z" },
+            { "Group": "Pokemon", "Name": "Digitamamo", "Time": "2023-05-18T06:11:57.426Z" },
+            { "Group": "Pokemon", "Name": "Bakemon", "Time": "2023-05-18T06:16:31.284Z" },
+            { "Group": "Pokemon", "Name": "Gammamon", "Time": "2023-05-18T08:12:49.815Z" },
+            { "Group": "Pokemon", "Name": "Monzaemon", "Time": "2023-05-18T08:59:30.222Z" },
+            { "Group": "Pokemon", "Name": "Ladydevimo", "Time": "2023-05-18T12:58:19.894Z" },
+            { "Group": "Pokemon", "Name": "M.Greymon", "Time": "2023-05-18T13:45:27.197Z" },
+            { "Group": "Pokemon", "Name": "Justimon", "Time": "2023-05-18T13:54:01.734Z" },
+            { "Group": "Pokemon", "Name": "P.Chessmon", "Time": "2023-05-18T14:30:34.947Z" },
+            { "Group": "Pokemon", "Name": "Otamamon", "Time": "2023-05-18T14:32:25.439Z" },
+            { "Group": "Pokemon", "Name": "Tinkermon", "Time": "2023-05-18T14:36:45.158Z" },
+            { "Group": "Pokemon", "Name": "Mamemon", "Time": "2023-05-18T14:54:41.399Z" },
+            { "Group": "Pokemon", "Name": "Gizamon", "Time": "2023-05-18T14:57:21.070Z" },
+            { "Group": "Pokemon", "Name": "Shoutmon", "Time": "2023-05-18T15:03:32.681Z" },
+            { "Group": "Pokemon", "Name": "Mojyamon", "Time": "2023-05-18T15:09:46.337Z" },
+            { "Group": "Pokemon", "Name": "Meicoomon", "Time": "2023-05-18T15:12:17.749Z" },
+            { "Group": "Pokemon", "Name": "Kuwagamon", "Time": "2023-05-18T15:17:04.394Z" },
+            { "Group": "Pokemon", "Name": "Chamelemon", "Time": "2023-05-18T15:25:22.017Z" },
+            { "Group": "Pokemon", "Name": "Snimon", "Time": "2023-05-18T15:32:43.213Z" },
+            { "Group": "Pokemon", "Name": "Gigimon", "Time": "2023-05-18T16:49:24.812Z" },
+            { "Group": "Pokemon", "Name": "Guilmon", "Time": "2023-05-18T17:00:18.224Z" },
+            { "Group": "Pokemon", "Name": "A.Mermaimo", "Time": "2023-05-18T17:51:15.332Z" },
+            { "Group": "Pokemon", "Name": "Growlmon", "Time": "2023-05-18T18:06:11.793Z" },
+            { "Group": "Pokemon", "Name": "Angewomon", "Time": "2023-05-18T20:31:16.177Z" },
+            { "Group": "Pokemon", "Name": "W.Growlmon", "Time": "2023-05-18T21:40:28.610Z" },
+            { "Group": "Pokemon", "Name": "Plesiomon", "Time": "2023-05-18T21:59:07.522Z" },
+            { "Group": "Pokemon", "Name": "Viximon", "Time": "2023-05-18T22:15:42.844Z" },
+            { "Group": "Pokemon", "Name": "Hagurumon", "Time": "2023-05-18T22:20:12.840Z" },
+            { "Group": "Pokemon", "Name": "Goldramon", "Time": "2023-05-18T22:30:32.757Z" },
+            { "Group": "Pokemon", "Name": "Lilithmon", "Time": "2023-05-19T00:50:50.831Z" },
+            { "Group": "Pokemon", "Name": "Tanemon", "Time": "2023-05-19T02:42:37.248Z" },
+            { "Group": "Pokemon", "Name": "Tapirmon", "Time": "2023-05-19T02:50:18.833Z" },
+            { "Group": "Pokemon", "Name": "Ophanimon", "Time": "2023-05-19T03:46:01.096Z" },
+            { "Group": "Pokemon", "Name": "Dracmon", "Time": "2023-05-19T08:03:46.488Z" },
+            { "Group": "Pokemon", "Name": "Kuramon", "Time": "2023-05-19T08:50:42.742Z" },
+            { "Group": "Pokemon", "Name": "Gallantmon", "Time": "2023-05-19T14:18:29.734Z" },
+            { "Group": "Pokemon", "Name": "ToyAgumon", "Time": "2023-05-19T15:41:45.505Z" },
+            { "Group": "Pokemon", "Name": "WarGreymon", "Time": "2023-05-19T15:48:53.471Z" },
+            { "Group": "Pokemon", "Name": "Piximon", "Time": "2023-05-19T15:52:22.655Z" },
+            { "Group": "Pokemon", "Name": "Bombmon", "Time": "2023-05-19T15:58:05.926Z" },
+            { "Group": "Pokemon", "Name": "M.Koromon", "Time": "2023-05-19T16:04:04.648Z" },
+            { "Group": "Pokemon", "Name": "Datirimon", "Time": "2023-05-19T16:13:23.165Z" },
+            { "Group": "Pokemon", "Name": "Ketomon", "Time": "2023-05-19T16:16:52.640Z" },
+            { "Group": "Pokemon", "Name": "Dorimon", "Time": "2023-05-19T16:19:36.638Z" },
+            { "Group": "Pokemon", "Name": "Upamon", "Time": "2023-05-19T16:22:15.003Z" },
+        ],
+        Revisit: { Collection: "Intermissions", Run: "Digimon Nova Red" },
+        CopyEvents: ["Digimon Nova Red"],
+    }
+);
