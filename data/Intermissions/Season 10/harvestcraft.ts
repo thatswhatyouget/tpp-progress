3c264a431d75e85671aa730908450e80d03118c9
M4 <dave.was.r@gmail.com>
2023-07-06 16:58:09 -0400

    Added HarvestCraft

---
+++ data/Intermissions/Season 10/harvestcraft.ts
@@ -0,0 +1,80 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "HarvestCraft",
+        ColorPrimary: "brown",
+        ColorSecondary: "goldenrod",
+        StartDate: "2023-07-06T22:00:00.000Z",
+        Duration: "2023-07-08T21:00:00Z",
+        HostName: "",
+        // HostImage: "img/hosts/mudkid.png",
+        // HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        Region: "HarvestCraft",
+        Generation: 3,
+        // FinalStateLink: "./states/Season 10/harvestcraft.json",
+        Events: [
+            {
+                Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Heat Badge", Image: "img/badges/heat.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png",
+                Time: "2022-12-06T19:07:06.782Z", Attempts: 1,
+            },
+            {
+                Group: "Badges", Name: "Feather Badge", Image: "img/badges/feather.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Mind Badge", Image: "img/badges/mind.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Rain Badge", Image: "img/badges/rain.png",
+                Time: "", Attempts: 0,
+            },
+
+            {
+                Group: "Elite Four", Name: "Sidney", Image: "img/trainers/emerald/sidney.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Phoebe", Image: "img/trainers/emerald/phoebe.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Glacia", Image: "img/trainers/emerald/glacia.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Drake", Image: "img/trainers/emerald/drake.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Champions", Name: "Steven", Image: "img/trainers/emerald/steven.png",
+                Time: "", Attempts: 0,
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "28882", Party: [
+                ],
+                Time: "", Attempts: 0,
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+
+        ],
+    }
+);
