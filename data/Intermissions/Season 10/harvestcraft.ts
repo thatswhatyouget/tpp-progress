7dab9aba8ee5ea8b63ddb848b70b41ed5a72988b
M4 <dave.was.r@gmail.com>
2023-07-07 13:04:10 -0400

    Earned the Blank Badge

--- data/Intermissions/Season 10/harvestcraft.ts
+++ data/Intermissions/Season 10/harvestcraft.ts
@@ -4,18 +4,18 @@ Intermissions.Runs.push(
         RunName: "HarvestCraft",
         ColorPrimary: "brown",
         ColorSecondary: "goldenrod",
-        StartDate: "2023-07-06T22:00:00.000Z",
+        StartDate: "2023-07-06T21:45:00.000Z",
         Duration: "2023-07-08T21:00:00Z",
-        HostName: "",
-        // HostImage: "img/hosts/mudkid.png",
+        HostName: "TA",
+        HostImage: "img/hosts/ta.png",
         // HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
         Region: "HarvestCraft",
         Generation: 3,
         // FinalStateLink: "./states/Season 10/harvestcraft.json",
         Events: [
             {
-                Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png",
-                Time: "", Attempts: 0,
+                Group: "Badges", Name: "Blank Badge", Image: "img/badges/stone.png",
+                Time: "2023-07-07T04:14:35.085Z", Attempts: 2,
             },
             {
                 Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png",
@@ -31,7 +31,7 @@ Intermissions.Runs.push(
             },
             {
                 Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png",
-                Time: "2022-12-06T19:07:06.782Z", Attempts: 1,
+                Time: "", Attempts: 0,
             },
             {
                 Group: "Badges", Name: "Feather Badge", Image: "img/badges/feather.png",
@@ -74,7 +74,21 @@ Intermissions.Runs.push(
                 FirstAttemptDate: "",
                 Image: "img/ribbons/champion.png"
             },
-
+            {"Group":"Pokemon","Name":"Fennekin","Time":"2023-07-06T21:53:00.367Z"},
+            {"Group":"Pokemon","Name":"Taillow","Time":"2023-07-06T22:20:54.353Z"},
+            {"Group":"Pokemon","Name":"Ralts","Time":"2023-07-07T01:10:13.970Z"},
+            {"Group":"Pokemon","Name":"Zigzagoon","Time":"2023-07-07T01:52:19.700Z"},
+            {"Group":"Pokemon","Name":"Braxien" /*sic*/,"Time":"2023-07-07T01:58:00.929Z"},
+            {"Group":"Pokemon","Name":"Budew","Time":"2023-07-07T02:06:43.810Z"},
+            {"Group":"Pokemon","Name":"Kakuna","Time":"2023-07-07T03:18:36.797Z"},
+            {"Group":"Pokemon","Name":"Pancham","Time":"2023-07-07T03:27:31.962Z"},
+            {"Group":"Pokemon","Name":"Slakoth","Time":"2023-07-07T03:33:55.887Z"},
+            {"Group":"Pokemon","Name":"Geodude","Time":"2023-07-07T04:47:31.987Z"},
+            {"Group":"Pokemon","Name":"Zubat","Time":"2023-07-07T05:42:55.994Z"},
+            {"Group":"Pokemon","Name":"Magikarp","Time":"2023-07-07T06:03:55.065Z"},
+            {"Group":"Pokemon","Name":"Dedenne","Time":"2023-07-07T07:01:07.473Z"},
+            {"Group":"Pokemon","Name":"Oddish","Time":"2023-07-07T15:52:22.751Z"},
+            {"Group":"Pokemon","Name":"Bunnelby","Time":"2023-07-07T16:41:30.888Z"}
         ],
     }
 );
