15e8ef4c9db19f06a390310b9fc2316cd2339c24
M4 <dave.was.r@gmail.com>
2023-07-08 14:09:29 -0400

    Added Resolute

---
+++ data/Season 10/resolute.ts
@@ -0,0 +1,204 @@
+/// <reference path="../tpp-data.ts" />
+Season10.Runs.push(
+    {
+        RunName: "Resolute",
+        ColorPrimary: "#E96239",
+        ColorSecondary: "#96C6CD",
+        StartDate: "2023-07-08T21:00:00Z",
+        Duration: "255d",
+        HostName: "",
+        HostImage: "",
+        BaseGame: "Emerald",
+        Region: "Sylon",
+        Pokedex: "Sylon",
+        // FinalStateLink: "./states/Season 10/resolute.json",
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Razor Badge",
+                Image: "img/badges/resolute/transform.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Imagery Badge",
+                Image: "img/badges/resolute/imagery.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Film Badge",
+                Image: "img/badges/resolute/film.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Metal Badge",
+                Image: "img/badges/resolute/metal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Belief Badge",
+                Image: "img/badges/resolute/belief.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Wisdom Badge",
+                Image: "img/badges/resolute/wisdom.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Amulet Badge",
+                Image: "img/badges/resolute/amulet.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Basis Badge",
+                Image: "img/badges/resolute/basis.png",
+                Time: "", Attempts: 0
+            },
+
+
+            {
+                Group: "Badges",
+                Name: "Transform Badge",
+                Image: "img/badges/victoryfire/transform.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Rising Badge",
+                Image: "img/badges/victoryfire/rising.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Toxic Badge",
+                Image: "img/badges/resolute/toxic.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Hard Badge",
+                Image: "img/badges/resolute/hard.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Blaze Badge",
+                Image: "img/badges/victoryfire/blaze.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Snowflake Badge",
+                Image: "img/badges/victoryfire/snowflake.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "MindEye Badge",
+                Image: "img/badges/victoryfire/mindeye.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Nature Badge",
+                Image: "img/badges/victoryfire/nature.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Badges",
+                Name: "Zephyr Badge",
+                Image: "img/badges/zephyr.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Hive Badge",
+                Image: "img/badges/hive.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Plain Badge",
+                Image: "img/badges/plain.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Fog Badge",
+                Image: "img/badges/fog.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Mineral Badge",
+                Image: "img/badges/mineral.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Storm Badge",
+                Image: "img/badges/storm.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Glacier Badge",
+                Image: "img/badges/glacier.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Rising Badge",
+                Image: "img/badges/rising.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four",
+                Name: "Cyasine",
+                Image: "img/trainers/victoryfire/cyasine.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Zach",
+                Image: "img/trainers/victoryfire/zach.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Ebby",
+                Image: "img/trainers/victoryfire/ebby.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Violet",
+                Image: "img/trainers/victoryfire/violet.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "Jacob",
+                Image: "img/trainers/victoryfire/jacob.png",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "", Party: [
+                    // { Pokemon: "", Nickname: "", Level: , Gender: "", Number: , IDNo: "" },
+                ],
+                FirstAttemptDate: "2023-05-15T20:13:27.007Z",
+                Image: "img/ribbons/champion.png",
+                Time: "", Attempts: 0
+            },
+        ]
+    }
+);
