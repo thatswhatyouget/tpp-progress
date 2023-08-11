c9ee738305e09238f62b309f20cb1418457405d0
M4 <dave.was.r@gmail.com>
2023-08-11 19:38:45 -0400

    Earned all badges in Cramorant

--- data/Intermissions/Season 10/cramorant.ts
+++ data/Intermissions/Season 10/cramorant.ts
@@ -47,40 +47,40 @@ Intermissions.Runs.push(
                 Name: "Soul Badge",
                 Image: "img/badges/soul.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2023-08-10T21:22:36.533Z", Attempts: 1
             },
             {
                 Group: "Badges",
                 Name: "Marsh Badge",
                 Image: "img/badges/marsh.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2023-08-11T13:56:26.821Z", Attempts: 1
             },
             {
                 Group: "Badges",
                 Name: "Volcano Badge",
                 Image: "img/badges/volcano.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2023-08-11T13:08:24.437Z", Attempts: 1
             },
             {
                 Group: "Badges",
                 Name: "Earth Badge",
                 Image: "img/badges/earth.png",
                 ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2023-08-11T14:52:41.262Z", Attempts: 1
             },
             {
                 Group: "Elite Four",
                 Name: "Lorelei",
                 Image: "img/trainers/red/lorelei.png",
-                Time: "", Attempts: 0
+                Time: "2023-08-11T23:27:17.964Z", Attempts: 1
             },
             {
                 Group: "Elite Four",
                 Name: "Bruno",
                 Image: "img/trainers/red/bruno.png",
-                Time: "", Attempts: 0
+                Time: "2023-08-11T23:34:03.854Z", Attempts: 1
             },
             {
                 Group: "Elite Four",
@@ -101,15 +101,15 @@ Intermissions.Runs.push(
                 Time: "", Attempts: 0
             },
             <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2", Attempts: 0, Party: [
-                    // { Pokemon: "Articuno", Nickname: "FRIDGO", Level: 55, Type1: "Ice", Type2: "Flying" },
-                    // { Pokemon: "Snorlax", Nickname: "KIRBY", Level: 42, Type1: "Normal" },
-                    // { Pokemon: "Tentacruel", Nickname: "SPLATOON", Level: 50, Type1: "Water", Type2: "Poison" },
-                    // { Pokemon: "Primeape", Nickname: "PunOko", Level: 46, Type1: "Fighting" },
-                    // { Pokemon: "Mr. Mime", Nickname: "MILES", Level: 51, Type1: "Psychic" },
-                    // { Pokemon: "Pikachu", Nickname: "SPARKY", Level: 53, Type1: "Electric" },
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", Attempts: 0, Party: [
+                    { Pokemon: "Cramorant", Nickname: "BCDEFaa", Level: 49, Type1: "Flying", Type2: "Water" },
+                    { Pokemon: "Celfable", Nickname: "BB(?------", Level: 45, Type1: "Normal" },
+                    { Pokemon: "Barraskewda", Nickname: "DVKBDDMNEF", Level: 46, Type1: "Water" },
+                    { Pokemon: "Magmar", Nickname: "BOOBER", Level: 48, Type1: "Fire" },
+                    { Pokemon: "Golem", Nickname: "AL", Level: 46, Type1: "Rock", Type2:"Ground" },
+                    { Pokemon: "Weezing", Nickname: "P..]XXMMJJ", Level: 48, Type1: "Poison" },
                 ],
-                FirstAttemptDate: "",
+                FirstAttemptDate: "2023-08-11T23:16:34.452Z",
                 Image: "img/ribbons/champion.png"
             },
         ]
