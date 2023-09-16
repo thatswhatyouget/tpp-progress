d50186e851942d37ae2b3bff53895b65931b5dd6
BornToLose <twitchplayslore@gmail.com>
2023-09-16 15:33:07 +0200

    Added data for Debug Rubin (2023)

---
+++ data/Intermissions/Season 10/rubin.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Rubin (Debug Version)",
+        ColorPrimary: "#bb595b",
+        ColorSecondary: "#893f31",
+        StartDate: "2023-09-02T13:12:32.428Z",
+        Duration: "2023-09-02T15:27:32.234Z",
+        HostName: "IYYI",
+        HostImage: "img/hosts/petra.gif",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/cofjn3/petra_scharlachrot_the_arcane_cyclist/",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "National",
+        Events: [
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2023-09-02T15:21:34.002Z", IDNo: "03205", Attempts: 1, Party: [
+                    { Pokemon: "Geradaks", Class: "Linoone", Level: 99, Number: 13, Gender: "Male", IDNo: "03205" },
+                    { Pokemon: "Jungglut", Class: "Combusken", Level: 99, Number: 5, Gender: "Male", IDNo: "03205" },
+                    { Pokemon: "Schwalboss", Class: "Swellow", Level: 95, Number: 26, Gender: "Female", IDNo: "03205" },
+                    { Pokemon: "Roselia", Class: "Roselia", Level: 3, Number: 94, Gender: "Female", IDNo: "03205" },
+                    { Pokemon: "Knilz", Class: "Shroomish", Nickname: "BE", Level: 5, Number: 34, Gender: "Female", IDNo: "03205" },
+                    { Pokemon: "Geckarbor", Class: "Treecko", Level: 1, Number: 34, Gender: "Male", IDNo: "03205" },
+                ],
+                Image: "img/ribbons/champion.png"
+            },
+
+            { "Group": "Pokemon", "Name": "Zigzagoon", "Time": "2021-05-13T07:13:04.588Z" }, // via Debug Menu
+        ],
+    }
+);
