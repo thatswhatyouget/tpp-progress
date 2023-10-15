2482cf0e5b9d56d697e7f4b053248cfc1a30a8a0
M4 <dave.was.r@gmail.com>
2023-10-15 08:37:56 -0400

    Earned the Stone and Balance badges

--- data/Season 10/snakewood.ts
+++ data/Season 10/snakewood.ts
@@ -14,6 +14,74 @@ Season10.Runs.push(
         Pokedex: "Snakewood",
         // FinalStateLink: "./states/Season 10/snakewood.json",
         Events: [
+            {
+                Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png",
+                Time: "2023-10-15T02:59:42.153Z", Attempts: 3,
+            },
+            {
+                Group: "Badges", Name: "Miasma Badge", Image: "img/badges/snakewood/miasma.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Heat Badge", Image: "img/badges/heat.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png",
+                Time: "2023-10-15T11:28:05.368Z", Attempts: 1,
+            },
+            {
+                Group: "Badges", Name: "Knight Emblem", Image: "img/badges/snakewood/knightemblem.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Comet Badge", Image: "img/badges/snakewood/comet.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Focus Badge", Image: "img/badges/snakewood/focus.png",
+                Time: "", Attempts: 0,
+            },
+
+            {
+                Group: "Elite Four", Name: "Caitlin", Image: "img/trainers/snakewood/caitlin.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Benny", Image: "img/trainers/snakewood/benny.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Glacia", Image: "img/trainers/snakewood/glacia.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Elite Four", Name: "Indigo", Image: "img/trainers/snakewood/indigo.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Champions", Name: "Feng Lei", Image: "img/trainers/snakewood/fenglei.png",
+                Time: "", Attempts: 0,
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "31529", Party: [
+                    // { Pokemon: "Manectric", Nickname: "George", Level: 53, Gender: "Male", Number: 79, IDNo: "28882" },
+                    // { Pokemon: "Swampert", Nickname: "lif", Level: 57, Gender: "Male", Number: 9, IDNo: "28882" },
+                    // { Pokemon: "Swellow", Nickname: "AYA", Level: 54, Gender: "Female", Number: 26, IDNo: "28882" },
+                    // { Pokemon: "Ninjask", Nickname: "EXCALIBUR", Level: 53, Gender: "Female", Number: 43, IDNo: "28882" },
+                    // { Pokemon: "Gardevoir", Nickname: "ALICE", Level: 54, Gender: "Female", Number: 31, IDNo: "28882" },
+                    // { Pokemon: "Armaldo", Nickname: "NOT HELIX", Level: 56, Gender: "Female", Number: 136, IDNo: "28882" },
+                ],
+                Time: "", Attempts: 0,
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+
         ]
     }
 );
