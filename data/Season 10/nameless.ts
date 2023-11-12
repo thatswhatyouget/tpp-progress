04dfcce922beffa1e2a177c7179f7280eb32c007
M4 <dave.was.r@gmail.com>
2023-11-12 01:24:01 -0500

    Added Nameless

---
+++ data/Season 10/nameless.ts
@@ -0,0 +1,18 @@
+/// <reference path="../tpp-data.ts" />
+Season10.Runs.push(
+    {
+        RunName: "Nameless",
+        ColorPrimary: "#333",
+        ColorSecondary: "#666",
+        StartDate: "2023-11-11T23:00:00Z",
+        Duration: "40d",
+        HostName: "CHRONYA",
+        HostImage: "img/hosts/chronya.png",
+        BaseGame: "FireRed",
+        Region: "Cyenn",
+        Pokedex: "Cyenn",
+        //FinalStateLink: "./states/Season 10/nameless.json",
+        Events: [
+        ],
+    }
+);
