e81862ae3c2bff89b0ce90133826f8f28f0ca2f7
M4 <dave.was.r@gmail.com>
2023-12-27 16:07:40 -0500

    Added Inverse White

---
+++ data/Season 10/inversewhite.ts
@@ -0,0 +1,106 @@
+/// <reference path="../tpp-data.ts" />
+Season10.Runs.push(
+    {
+        RunName: "Inverse White",
+        ColorPrimary: "#111",
+        ColorSecondary: "#CC9BA2",
+        Duration: "255d",
+        StartDate: "2023-12-27T21:00:00Z",
+        HostName: "  QZO  ",
+        HostImage: "img/hosts/gmyc.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        // FinalStateLink: "./states/Season 10/inversewhite.json",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "National",
+        Events: [
+            {
+                Group: "Badges", Name: "Trio Badge", Image: "img/badges/trio.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Quake Badge", Image: "img/badges/quake.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Jet Badge", Image: "img/badges/jet.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Freeze Badge", Image: "img/badges/freeze.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Legend Badge", Image: "img/badges/legend.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four", Name: "Shauntal", Image: "img/trainers/black/shauntal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Grimsley", Image: "img/trainers/black/grimsley.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Caitlin", Image: "img/trainers/black/caitlin.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Marshal", Image: "img/trainers/black/marshal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "N", Image: "img/trainers/black/n.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Ghetsis", Image: "img/trainers/black/ghetsis.png",
+                Time: "", Attempts: 0
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "First Game Clear", Time: "", IDNo: "", Attempts: 0, Party: [
+                ],
+                Image: "img/ribbons/event.png",
+                FirstAttemptDate: ""
+            },
+
+            {
+                Group: "Elite Four Rematch", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Grimsley", Image: "img/trainers/black/grimsley.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Caitlin", Image: "img/trainers/black/caitlin.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Marshal", Image: "img/trainers/black/marshal.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Champions", Name: "Alder", Image: "img/trainers/black/alder.png",
+                Time: "", Attempts: 0
+            },
+
+        ]
+    }
+);
