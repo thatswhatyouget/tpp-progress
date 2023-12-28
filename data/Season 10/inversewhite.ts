33c4d4b7b5d35c5045b0dd09ab7e29d58c72980a
M4 <dave.was.r@gmail.com>
2023-12-28 18:15:10 -0500

    Earned the Bolt Badge

--- data/Season 10/inversewhite.ts
+++ data/Season 10/inversewhite.ts
@@ -28,7 +28,7 @@ Season10.Runs.push(
             },
             {
                 Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png",
-                Time: "", Attempts: 0
+                Time: "2023-12-28T19:24:48.174Z", Attempts: 1
             },
             {
                 Group: "Badges", Name: "Quake Badge", Image: "img/badges/quake.png",
@@ -101,6 +101,44 @@ Season10.Runs.push(
                 Time: "", Attempts: 0
             },
 
+            { "Group": "Pokemon", "Name": "Tepig", "Time": "2023-12-27T21:02:27.717Z" },
+            { "Group": "Pokemon", "Name": "Minccino", "Time": "2023-12-27T21:20:49.555Z" },
+            { "Group": "Pokemon", "Name": "Lillipup", "Time": "2023-12-27T21:24:30.712Z" },
+            { "Group": "Pokemon", "Name": "Patrat", "Time": "2023-12-27T22:09:35.418Z" },
+            { "Group": "Pokemon", "Name": "Purrloin", "Time": "2023-12-27T22:19:18.241Z" },
+            { "Group": "Pokemon", "Name": "Pansage", "Time": "2023-12-27T22:50:06.272Z" },
+            { "Group": "Pokemon", "Name": "Herdier", "Time": "2023-12-28T00:14:34.207Z" },
+            { "Group": "Pokemon", "Name": "Audino", "Time": "2023-12-28T00:17:06.828Z" },
+            { "Group": "Pokemon", "Name": "Pidove", "Time": "2023-12-28T00:26:49.203Z" },
+            { "Group": "Pokemon", "Name": "Roggenrola", "Time": "2023-12-28T00:40:04.254Z" },
+            { "Group": "Pokemon", "Name": "Woobat", "Time": "2023-12-28T00:44:14.555Z" },
+            { "Group": "Pokemon", "Name": "Blitzle", "Time": "2023-12-28T01:00:08.889Z" },
+            { "Group": "Pokemon", "Name": "Pignite", "Time": "2023-12-28T01:39:48.550Z" },
+            { "Group": "Pokemon", "Name": "Tympole", "Time": "2023-12-28T02:04:46.113Z" },
+            { "Group": "Pokemon", "Name": "Timburr", "Time": "2023-12-28T02:09:07.398Z" },
+            { "Group": "Pokemon", "Name": "Throh", "Time": "2023-12-28T03:30:11.717Z" },
+            { "Group": "Pokemon", "Name": "Sewaddle", "Time": "2023-12-28T04:34:26.405Z" },
+            { "Group": "Pokemon", "Name": "Venipede", "Time": "2023-12-28T04:38:27.587Z" },
+            { "Group": "Pokemon", "Name": "Petilil", "Time": "2023-12-28T04:42:37.873Z" },
+            { "Group": "Pokemon", "Name": "Sandile", "Time": "2023-12-28T06:05:52.762Z" },
+            { "Group": "Pokemon", "Name": "Scraggy", "Time": "2023-12-28T06:39:20.636Z" },
+            { "Group": "Pokemon", "Name": "Darumaka", "Time": "2023-12-28T06:55:09.691Z" },
+            { "Group": "Pokemon", "Name": "Maractus", "Time": "2023-12-28T10:32:06.059Z" },
+            { "Group": "Pokemon", "Name": "Dwebble", "Time": "2023-12-28T10:44:11.158Z" },
+            { "Group": "Pokemon", "Name": "Yamask", "Time": "2023-12-28T14:08:56.135Z" },
+            { "Group": "Pokemon", "Name": "Tirtouga", "Time": "2023-12-28T14:49:04.230Z" },
+            { "Group": "Pokemon", "Name": "Sigilyph", "Time": "2023-12-28T15:45:19.976Z" },
+            { "Group": "Pokemon", "Name": "Solosis", "Time": "2023-12-28T16:21:28.919Z" },
+            { "Group": "Pokemon", "Name": "Krokorok", "Time": "2023-12-28T16:26:15.577Z" },
+            { "Group": "Pokemon", "Name": "Trubbish", "Time": "2023-12-28T16:33:33.770Z" },
+            { "Group": "Pokemon", "Name": "Liepard", "Time": "2023-12-28T16:41:35.215Z" },
+            { "Group": "Pokemon", "Name": "Swadloon", "Time": "2023-12-28T17:25:47.843Z" },
+            { "Group": "Pokemon", "Name": "Tranquill", "Time": "2023-12-28T17:42:04.491Z" },
+            { "Group": "Pokemon", "Name": "Vanillite", "Time": "2023-12-28T20:14:45.034Z" },
+            { "Group": "Pokemon", "Name": "Karrablast", "Time": "2023-12-28T21:43:24.140Z" },
+            { "Group": "Pokemon", "Name": "Deerling", "Time": "2023-12-28T21:43:24.140Z" },
+            { "Group": "Pokemon", "Name": "Foongus", "Time": "2023-12-28T22:03:29.582Z" },
+            { "Group": "Pokemon", "Name": "Stoutland", "Time": "2023-12-28T22:06:39.768Z" }
         ]
     }
 );
