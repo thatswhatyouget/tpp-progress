c761ccd64f8636215741ae224e720f53cd4424fe
M4 <dave.was.r@gmail.com>
2024-01-03 16:56:14 -0500

    Ended Inverse White, Began Inverse White 2

---
+++ data/Season 10/inversewhite2.ts
@@ -0,0 +1,146 @@
+/// <reference path="../tpp-data.ts" />
+Season10.Runs.push(
+    {
+        RunName: "Inverse White 2",
+        ColorPrimary: "white",
+        ColorSecondary: "black",
+        StartDate: "2024-01-03T21:00:00Z",
+        Duration: "2024-02-12T21:00:00Z",
+        HostName: "BOH  LE",
+        HostImage: "img/hosts/cly.png",
+        // HostImageSource: "https://www.deviantart.com/krizste/art/TPP-VoltWhite2Redux-forest-sprites-943447962",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "National",
+        // FinalStateLink:"./states/Season 10/inversewhite2.json", 
+        Events: [
+            {
+                Group: "Badges",
+                Name: "Basic Badge",
+                Image: "img/badges/basic.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Toxic Badge",
+                Image: "img/badges/toxic.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Insect Badge",
+                Image: "img/badges/insect.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Bolt Badge",
+                Image: "img/badges/bolt.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Quake Badge",
+                Image: "img/badges/quake.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Jet Badge",
+                Image: "img/badges/jet.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Legend Badge",
+                Image: "img/badges/legend.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Wave Badge",
+                Image: "img/badges/wave.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Shauntal",
+                Image: "img/trainers/black/shauntal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Grimsley",
+                Image: "img/trainers/black/grimsley.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Caitlin",
+                Image: "img/trainers/black/caitlin.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four",
+                Name: "Marshal",
+                Image: "img/trainers/black/marshal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "Iris",
+                Image: "img/trainers/black/iris.png",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "16413", Attempts: 0, Party: [
+
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Grimsley",
+                Image: "img/trainers/black/grimsley.png",
+                Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Shauntal",
+                Image: "img/trainers/black/shauntal.png",
+                Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Caitlin",
+                Image: "img/trainers/black/caitlin.png",
+                Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch",
+                Name: "Marshal",
+                Image: "img/trainers/black/marshal.png",
+                Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions",
+                Name: "Iris",
+                Image: "img/trainers/black/iris.png",
+                Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "16413", Attempts: 0, Party: [
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+
+        ]
+    }
+);
