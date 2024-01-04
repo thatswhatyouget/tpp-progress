dc514b72062bce6f9d5d698f297bd37fc641cab0
M4 <dave.was.r@gmail.com>
2024-01-03 22:20:53 -0500

    Earned the Basic and Toxic badges

--- data/Season 10/inversewhite2.ts
+++ data/Season 10/inversewhite2.ts
@@ -18,13 +18,13 @@ Season10.Runs.push(
                 Group: "Badges",
                 Name: "Basic Badge",
                 Image: "img/badges/basic.png",
-                Time: "", Attempts: 0
+                Time: "2024-01-04T00:26:13.229Z", Attempts: 2
             },
             {
                 Group: "Badges",
                 Name: "Toxic Badge",
                 Image: "img/badges/toxic.png",
-                Time: "", Attempts: 0
+                Time: "2024-01-04T03:17:58.226Z", Attempts: 1
             },
             {
                 Group: "Badges",
@@ -141,6 +141,24 @@ Season10.Runs.push(
                 Image: "img/ribbons/champion.png"
             },
 
+            { "Group": "Pokemon", "Name": "Tepig", "Time": "2024-01-03T21:08:11.998Z" },
+            { "Group": "Pokemon", "Name": "Lillipup", "Time": "2024-01-03T21:20:20.878Z" },
+            { "Group": "Pokemon", "Name": "Patrat", "Time": "2024-01-03T21:23:28.820Z" },
+            { "Group": "Pokemon", "Name": "Purrloin", "Time": "2024-01-03T21:28:38.644Z" },
+            { "Group": "Pokemon", "Name": "Pidove", "Time": "2024-01-03T21:54:18.117Z" },
+            { "Group": "Pokemon", "Name": "Sewaddle", "Time": "2024-01-03T21:57:12.406Z" },
+            { "Group": "Pokemon", "Name": "Sunkern", "Time": "2024-01-03T21:58:41.788Z" },
+            { "Group": "Pokemon", "Name": "Mareep", "Time": "2024-01-03T22:04:04.442Z" },
+            { "Group": "Pokemon", "Name": "Azurill", "Time": "2024-01-03T22:11:06.056Z" },
+            { "Group": "Pokemon", "Name": "Psyduck", "Time": "2024-01-03T22:55:10.757Z" },
+            { "Group": "Pokemon", "Name": "Riolu", "Time": "2024-01-03T23:27:25.768Z" },
+            { "Group": "Pokemon", "Name": "Koffing", "Time": "2024-01-04T01:20:01.506Z" },
+            { "Group": "Pokemon", "Name": "Growlithe", "Time": "2024-01-04T01:20:01.506Z" },
+            { "Group": "Pokemon", "Name": "Magnemite", "Time": "2024-01-04T01:27:55.614Z" },
+            { "Group": "Pokemon", "Name": "Elekid", "Time": "2024-01-04T01:44:05.802Z" },
+            { "Group": "Pokemon", "Name": "Flaaffy", "Time": "2024-01-04T01:52:53.538Z" },
+            { "Group": "Pokemon", "Name": "Audino", "Time": "2024-01-04T02:53:00.118Z" },
+            { "Group": "Pokemon", "Name": "Swadloon", "Time": "2024-01-04T03:17:43.535Z" }
         ]
     }
 );
