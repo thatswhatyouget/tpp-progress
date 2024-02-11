8388f0185ff4cb75b3a45405b0ade04d93eb4203
BornToLose <twitchplayslore@gmail.com>
2024-02-11 19:41:48 +0100

    Added data files for the JP sidegames

---
+++ data/Sidegames/jpblue.ts
@@ -0,0 +1,30 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Japanese Blue",
+        ColorPrimary: "#3c6cb8",
+        ColorSecondary: "#2c4c9e",
+        StartDate: "2024-01-27T04:01:03Z",
+        Duration: "2024-01-29T21:29:21Z",
+        HostName: "アアイイイ",
+        HostImage: "img/hosts/b.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/n8sv79/multiverse_champion_basil_benson_an_the_red_and/",
+        Region: "Kanto",
+        Generation: 1,
+        Pokedex: "Kanto",
+        Events: [
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2024-01-29T19:18:23.320Z", Attempts: 11, Party: [
+                    { Pokemon: "Zapdos", Nickname: "シタサスヒ", Level: 63, Type1: "Electric", Type2: "Flying" },
+                    { Pokemon: "Zapdos", Nickname: "シタサスヒ", Level: 62, Type1: "Electric", Type2: "Flying" },
+                    { Pokemon: "Parasect", Nickname: "ホイイイシ", Level: 61, Type1: "Bug", Type2: "Grass" },
+                    { Pokemon: "Blastoise", Nickname: "アキカカカ", Level: 61, Type1: "Water" },
+                    { Pokemon: "Arcanine", Nickname: "ミムフヌヘ", Level: 61, Type1: "Fire" },
+                    { Pokemon: "Marowak", Nickname: "ムハハチタ", Level: 61, Type1: "Ground" },
+                ],
+                FirstAttemptDate: "2024-01-28T22:02:59.059Z",
+                Image: "img/ribbons/champion.png"
+            },
+        ]
+    }
+);
---
+++ data/Sidegames/jpsapphire.ts
@@ -0,0 +1,42 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Japanese Sapphire",
+        ColorPrimary: "#3f4f9e",
+        ColorSecondary: "#2b2761",
+        StartDate: "2024-02-01T01:09:40Z",
+        Duration: "2024-02-06T18:27:01Z",
+        HostName: "チvテテセ",
+        HostImage: "img/hosts/b.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/n8sv79/multiverse_champion_basil_benson_an_the_red_and/",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "Hoenn",
+        Events: [
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2024-02-05T10:30:21.088Z", Attempts: 2, IDNo: "40511", Party: [
+                    { Pokemon: "Lunatone", Nickname: "オソミマオ", Level: 56, Number: 125, IDNo: "40511" },
+                    { Pokemon: "Swalot", Nickname: "エスはハフ", Level: 55, Gender: "Female", Number: 96, IDNo: "40511" },
+                    { Pokemon: "Blaziken", Nickname: "くけけVt", Level: 62, Gender: "Male", Number: 6, IDNo: "40511" },
+                    { Pokemon: "Donphan", Nickname: "くいやふに", Level: 55, Gender: "Female", Number: 166, IDNo: "40511" },
+                    { Pokemon: "Dusclops", Nickname: "サン゙ス", Level: 60, Gender: "Male", Number: 149, IDNo: "40511" },
+                    { Pokemon: "Lanturn", Nickname: "イシスハマ", Level: 55, Gender: "Male", Number: 182, IDNo: "40511" },
+                ],
+                FirstAttemptDate: "2024-02-05T01:25:50.625Z",
+                Image: "img/ribbons/champion.png"
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2024-02-05T17:06:41.930Z", Attempts: 3, IDNo: "40511", Party: [
+                    { Pokemon: "Beautifly", Nickname: "カシさうい", Level: 61, Gender: "Female", Number: 16, IDNo: "40511" },
+                    { Pokemon: "Lunatone", Nickname: "オソミマオ", Level: 61, Number: 125, IDNo: "40511" },
+                    { Pokemon: "Lanturn", Nickname: "イシスハマ", Level: 61, Gender: "Male", Number: 182, IDNo: "40511" },
+                    { Pokemon: "Donphan", Nickname: "くいやふに", Level: 60, Gender: "Female", Number: 166, IDNo: "40511" },
+                    { Pokemon: "Blaziken", Nickname: "くけけVt", Level: 66, Gender: "Male", Number: 6, IDNo: "40511" },
+                    { Pokemon: "Dusclops", Nickname: "サン゙ス", Level: 62, Gender: "Male", Number: 149, IDNo: "40511" },
+                ],
+                FirstAttemptDate: "2024-02-05T14:16:39.817Z",
+                Image: "img/ribbons/champion.png"
+            },
+        ]
+    }
+);
