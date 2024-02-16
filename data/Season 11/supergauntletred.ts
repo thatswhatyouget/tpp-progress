7a17299bb7bea8ead46c707a07e464cfe3e7e91e
M4 <dave.was.r@gmail.com>
2024-02-16 03:43:35 -0500

    Earned the Soul and Marsh Badges

--- data/Season 11/supergauntletred.ts
+++ data/Season 11/supergauntletred.ts
@@ -35,11 +35,11 @@ Season11.Runs.push(
             },
             {
                 Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2024-02-15T21:14:21.5189346Z", Attempts: 2
             },
             {
                 Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "", Attempts: 0
+                Time: "2024-02-16T08:29:44.1690811Z", Attempts: 1
             },
             {
                 Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
@@ -85,12 +85,23 @@ Season11.Runs.push(
             { Name: "Oddish", Time: "2024-02-14T01:10:11.8842271Z", Group: "Pokemon" },
             { Name: "Onix", Time: "2024-02-14T09:04:50.2750568Z", Group: "Pokemon" },
             { Name: "Eevee", Time: "2024-02-15T01:48:36.7331892Z", Group: "Pokemon" },
+            { Name: "Doduo", Time: "2024-02-15T18:22:41.5336860Z", Group: "Pokemon" },
+            { Name: "NidoranM", Time: "2024-02-15T18:24:16.5940322Z", Group: "Pokemon" },
+            { Name: "Kangaskhan", Time: "2024-02-15T18:38:44.0600093Z", Group: "Pokemon" },
+            { Name: "Exeggcute", Time: "2024-02-15T18:57:36.8736063Z", Group: "Pokemon" },
+            { Name: "Paras", Time: "2024-02-15T18:59:41.7067993Z", Group: "Pokemon" },
+            { Name: "NidoranF", Time: "2024-02-15T19:11:15.5167056Z", Group: "Pokemon" },
+            { Name: "Lapras", Time: "2024-02-15T23:21:01.7778708Z", Group: "Pokemon" },
+            { Name: "MissingNo.", Time: "2024-02-16T05:13:14.3604363Z", Group: "Pokemon" },
 
             { Name: "Kakuna", Time: "2024-02-13T14:49:48.6703971Z", Group: "Pokemon" },
             { Name: "Wartortle", Time: "2024-02-13T19:05:13.9238283Z", Group: "Pokemon" },
             { Name: "Gloom", Time: "2024-02-14T04:51:32.5620032Z", Group: "Pokemon" },
             { Name: "Gyarados", Time: "2024-02-14T19:19:55.5149320Z", Group: "Pokemon" },
             { Name: "Raticate", Time: "2024-02-14T20:51:37.1654607Z", Group: "Pokemon" },
+            { Name: "Arbok", Time: "2024-02-15T06:17:38.0154592Z", Group: "Pokemon" },
+            { Name: "Vileplume", Time: "2024-02-15T22:43:07.5374353Z", Group: "Pokemon" },
+            { Name: "Hypno", Time: "2024-02-15T16:56:13.0823135Z", Group: "Pokemon" },
 
         ]
     }
