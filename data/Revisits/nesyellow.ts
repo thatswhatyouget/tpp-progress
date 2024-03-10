382376f09fbe5831893390334afa3cda3f18008a
BornToLose <twitchplayslore@gmail.com>
2024-03-10 17:51:36 +0100

    Added files for NES Yellow

---
+++ data/Revisits/nesyellow.ts
@@ -0,0 +1,44 @@
+/// <reference path="../tpp-data.ts" />
+Revisits.Runs.push(
+    {
+        RunName: "Lei Dian Huang Bi Ka Qiu Chuan Shuo Revisit",
+        ColorPrimary: "#eb9f1c",
+        ColorSecondary: "#fd5227",
+        Duration: "2024-02-19T11:25:49Z",
+        StartDate: "2024-02-18T02:05:28Z",
+        HostName: "Ash",
+        HostImage: "img/hosts/ash.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Kanto",
+        Generation: 1,
+        Pokedex:"Kanto",
+        Events: [
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2024-02-18T22:35:29Z", Attempts: 0, Party: [
+                    { Pokemon: "Gengar", Nickname: "Gengar", Level: 64 },
+                    { Pokemon: "Blastoise", Nickname: "Blastoise", Level: 36 },
+                    { Pokemon: "Moltres", Nickname: "Moltres", Level: 54 },
+                    { Pokemon: "Marowak", Nickname: "Marowak", Level: 35 },
+                    { Pokemon: "Raichu", Nickname: "Raichu", Level: 47 },
+                    { Pokemon: "Suicune", Nickname: "Suicune", Level: 50 },
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2024-02-19T10:15:42Z", Attempts: 0, Party: [
+                    { Pokemon: "Mewtwo", Nickname: "Mewtwo", Level: 72 },
+                    { Pokemon: "Blastoise", Nickname: "Blastoise", Level: 38 },
+                    { Pokemon: "Moltres", Nickname: "Moltres", Level: 55 },
+                    { Pokemon: "Gengar", Nickname: "Gengar", Level: 73 },
+                    { Pokemon: "Suicune", Nickname: "Suicune", Level: 51 },
+                    { Pokemon: "Raichu", Nickname: "Raichu", Level: 57 },
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+        ],
+        Revisit: { Collection: "Intermissions", Run: "Lei Dian Huang Bi Ka Qiu Chuan Shuo" },
+        CopyEvents: ["Lei Dian Huang Bi Ka Qiu Chuan Shuo"],
+    }
+);
