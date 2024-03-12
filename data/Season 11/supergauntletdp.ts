bdce765a1cbdf6e76363cdeec8b4d35f367a148f
M4 <dave.was.r@gmail.com>
2024-03-12 00:09:31 -0400

    Started Dual Diamond Pearl

---
+++ data/Season 11/supergauntletdp.ts
@@ -0,0 +1,211 @@
+/// <reference path="../tpp-data.ts" />
+Season11.Runs.push(
+    {
+        RunName: "Super Gauntlet Diamond",
+        ColorPrimary: "#09C",
+        ColorSecondary: "#06A",
+        StartDate: "2024-03-11T21:00:00Z",
+        Duration: "2024-03-21T03:35:35Z",
+        HostName: "p\u2640P",
+        HostImage: "img/hosts/nqpppnl.png",
+        Region: "Sinnoh",
+        Generation: 4,
+        Pokedex: "Sinnoh",
+        FromNatDex: true,
+        Class: "dual-run",
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        //FinalStateLink: "./states/Season 11/supergauntletdp.json",
+        APIObjectName: "diamond", 
+        Events: [
+            {
+                Group: "Badges", Name: "Coal Badge", Image: "img/badges/coal.png",
+                Time: "2024-03-11T23:41:38.978Z", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Forest Badge", Image: "img/badges/forest.png",
+                Time: "2024-03-12T03:32:46.167Z", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Cobble Badge", Image: "img/badges/cobble.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Fen Badge", Image: "img/badges/fen.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Relic Badge", Image: "img/badges/relic.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Mine Badge", Image: "img/badges/mine.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Icicle Badge", Image: "img/badges/icicle.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Beacon Badge", Image: "img/badges/beacon.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Aaron", Image: "img/trainers/platinum/aaron.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Bertha", Image: "img/trainers/platinum/bertha.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Flint", Image: "img/trainers/platinum/flint.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Lucian", Image: "img/trainers/platinum/lucian.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Cynthia", Image: "img/trainers/platinum/cynthia.png",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "", Attempts: 0, Party: [
+                    // { Pokemon: "Roserade", Nickname: "O \u263aR0BAA4B", Level: 72, Gender: "Male", Met: "Route 204", OT: ".      " },
+                    // { Pokemon: "Medicham", Nickname: "Crshoigtd", Level: 55, Gender: "Male", Met: "Route 211", OT: ".      " },
+                    // { Pokemon: "Floatzel", Nickname: "\u265b\u263d\u2197\u2197\u263d", Level: 55, Gender: "Male", Met: "Valley Windworks", OT: ".      " },
+                    // { Pokemon: "Rhyperior", Nickname: ";aozbbpp  ", Level: 56, Gender: "Male", Met: "Route 214", OT: ".      " },
+                    // { Pokemon: "Scyther", Nickname: "NCZEUSELET", Level: 57, Gender: "Male", Met: "Route 210", OT: ".      " },
+                    // { Pokemon: "Lickilicky", Nickname: "E #!? %\u265a(", Level: 53, Gender: "Male", Met: "Route 215", OT: ".      " },
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion-sinnoh.png"
+            },
+            {
+                Group: "Elite Four Rematch", Class: "Rematch", Name: "Aaron", Image: "img/trainers/platinum/aaron.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Class: "Rematch", Name: "Bertha", Image: "img/trainers/platinum/bertha.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Class: "Rematch", Name: "Flint", Image: "img/trainers/platinum/flint.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Class: "Rematch", Name: "Lucian", Image: "img/trainers/platinum/lucian.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Class: "Rematch", Name: "Cynthia", Image: "img/trainers/platinum/cynthia.png",
+                Time: "", Attempts: 0
+            },
+        ]
+    }
+);
+Season11.Runs.push(
+    {
+        RunName: "Super Gauntlet Pearl",
+        ColorPrimary: "#C09",
+        ColorSecondary: "#A06",
+        StartDate: "2024-03-11T21:00:00Z",
+        Duration: "2024-03-21T03:35:35Z",
+        HostName: "AAAAAAA",
+        HostImage: "img/hosts/iecbw.png",
+        Region: "Sinnoh",
+        Generation: 4,
+        Pokedex: "Sinnoh",
+        FromNatDex: true,
+        Class: "dual-run",
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        //FinalStateLink: "./states/Season 11/supergauntletdp.json",
+        APIObjectName: "pearl",
+        Events: [
+            {
+                Group: "Badges", Name: "Coal Badge", Image: "img/badges/coal.png",
+                Time: "1h44m", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Forest Badge", Image: "img/badges/forest.png",
+                Time: "2024-03-12T03:41:42.293Z", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Cobble Badge", Image: "img/badges/cobble.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Fen Badge", Image: "img/badges/fen.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Relic Badge", Image: "img/badges/relic.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Mine Badge", Image: "img/badges/mine.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Icicle Badge", Image: "img/badges/icicle.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Beacon Badge", Image: "img/badges/beacon.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Aaron", Image: "img/trainers/platinum/aaron.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Bertha", Image: "img/trainers/platinum/bertha.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Flint", Image: "img/trainers/platinum/flint.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Lucian", Image: "img/trainers/platinum/lucian.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Cynthia", Image: "img/trainers/platinum/cynthia.png",
+                Time: "", Attempts: 0
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "", Attempts: 0, Party: [
+                    // { Pokemon: "Roserade", Nickname: "O \u263aR0BAA4B", Level: 72, Gender: "Male", Met: "Route 204", OT: ".      " },
+                    // { Pokemon: "Medicham", Nickname: "Crshoigtd", Level: 55, Gender: "Male", Met: "Route 211", OT: ".      " },
+                    // { Pokemon: "Floatzel", Nickname: "\u265b\u263d\u2197\u2197\u263d", Level: 55, Gender: "Male", Met: "Valley Windworks", OT: ".      " },
+                    // { Pokemon: "Rhyperior", Nickname: ";aozbbpp  ", Level: 56, Gender: "Male", Met: "Route 214", OT: ".      " },
+                    // { Pokemon: "Scyther", Nickname: "NCZEUSELET", Level: 57, Gender: "Male", Met: "Route 210", OT: ".      " },
+                    // { Pokemon: "Lickilicky", Nickname: "E #!? %\u265a(", Level: 53, Gender: "Male", Met: "Route 215", OT: ".      " },
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion-sinnoh.png"
+            },
+            {
+                Group: "Elite Four Rematch", Class: "Rematch", Name: "Aaron", Image: "img/trainers/platinum/aaron.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Class: "Rematch", Name: "Bertha", Image: "img/trainers/platinum/bertha.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Class: "Rematch", Name: "Flint", Image: "img/trainers/platinum/flint.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Class: "Rematch", Name: "Lucian", Image: "img/trainers/platinum/lucian.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Class: "Rematch", Name: "Cynthia", Image: "img/trainers/platinum/cynthia.png",
+                Time: "", Attempts: 0
+            },
+        ]
+    }
+);
