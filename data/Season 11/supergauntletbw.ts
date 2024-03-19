0d4613dc12e0c576d6a18de4ffae7504ae3992e9
M4 <dave.was.r@gmail.com>
2024-03-19 09:21:49 -0400

    Added Super Gauntlet Black and White

---
+++ data/Season 11/supergauntletbw.ts
@@ -0,0 +1,261 @@
+/// <reference path="../tpp-data.ts" />
+Season11.Runs.push(
+    {
+        RunName: "Super Gauntlet Black",
+        ColorPrimary: "#353535",
+        ColorSecondary: "black",
+        StartDate: "2024-03-18T21:00:00Z",
+        Duration: "2024-03-25T21:00:00Z",
+        HostName: "   \u2640Yss",
+        HostImage: "img/hosts/gmyc.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "National",
+        FromNatDex: true,
+        Class: "dual-run",
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        //FinalStateLink: "./states/Season 11/supergauntletbw.json",
+        APIObjectName: "diamond", 
+        Events: [
+            {
+                Group: "Badges", Name: "Trio Badge", Image: "img/badges/trio.png",
+                Time: "2024-03-18T22:58:18.855Z", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png",
+                Time: "2024-03-19T03:04:55.761Z", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png",
+                Time: "2024-03-19T08:30:10.577Z", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Quake Badge", Image: "img/badges/quake.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Jet Badge", Image: "img/badges/jet.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Freeze Badge", Image: "img/badges/freeze.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Legend Badge", Image: "img/badges/legend.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four", Name: "Shauntal", Image: "img/trainers/black/shauntal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Grimsley", Image: "img/trainers/black/grimsley.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Caitlin", Image: "img/trainers/black/caitlin.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Marshal", Image: "img/trainers/black/marshal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "N", Image: "img/trainers/black/n.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Ghetsis", Image: "img/trainers/black/ghetsis.png",
+                Time: "", Attempts: 0
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "First Game Clear", IDNo: "53314", Party: [
+                    { Pokemon: "Garbodor", Nickname: "Trpuebp", Level: 54, Gender: "Female" },
+                    { Pokemon: "Stoutland", Nickname: "ufzbhh♀ddc", Level: 61, Gender: "Male" },
+                    { Pokemon: "Maractus", Nickname: "Maraco", Level: 49, Gender: "Male" },
+                    { Pokemon: "Reuniclus", Nickname: "Cellmadioz", Level: 52, Gender: "Male" },
+                    { Pokemon: "Beartic", Nickname: "Cdacho", Level: 52, Gender: "Female" },
+                    { Pokemon: "Carracosta", Nickname: "Devyr", Level: 52, Gender: "Male" }
+                ],
+                Image: "img/ribbons/event.png",
+                FirstAttemptDate: "",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four Rematch", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Grimsley", Image: "img/trainers/black/grimsley.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Caitlin", Image: "img/trainers/black/caitlin.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Marshal", Image: "img/trainers/black/marshal.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Champions", Name: "Alder", Image: "img/trainers/black/alder.png",
+                Time: "", Attempts: 0
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "53314", Party: [
+                    { Pokemon: "Carracosta", Nickname: "Devyr", Level: 69, Gender: "Male" },
+                    { Pokemon: "Reuniclus", Nickname: "Cellmadioz", Level: 79, Gender: "Male" },
+                    { Pokemon: "Stoutland", Nickname: "ufzbhh♀ddc", Level: 72, Gender: "Male" },
+                    { Pokemon: "Volcarona", Nickname: "Alcamotf", Level: 70, Gender: "Male" },
+                    { Pokemon: "Beartic", Nickname: "Cdacho", Level: 68, Gender: "Female" },
+                    { Pokemon: "Zekrom", Nickname: "Zekenator", Level: 68 }
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "2024-01-01T18:45:07.079Z",
+                Time: "", Attempts: 0
+            },
+
+        ]
+    }
+);
+Season11.Runs.push(
+    {
+        RunName: "Super Gauntlet White",
+        ColorPrimary: "#E5E5E5",
+        ColorSecondary: "#C5C5C5",
+        StartDate: "2024-03-18T21:00:00Z",
+        Duration: "2024-03-25T21:00:00Z",
+        HostName: "%--\u265b\u265b\u2639 ",
+        HostImage: "img/hosts/aaq.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/beur93/aqua_kuehl_the_lonely/",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "National",
+        FromNatDex: true,
+        Class: "dual-run",
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        //FinalStateLink: "./states/Season 11/supergauntletbw.json",
+        APIObjectName: "pearl", 
+        Events: [
+            {
+                Group: "Badges", Name: "Trio Badge", Image: "img/badges/trio.png",
+                Time: "2024-03-18T22:24:07.051Z", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png",
+                Time: "2024-03-18T23:56:24.244Z", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png",
+                Time: "2024-03-19T04:08:10.397Z", Attempts: 1
+            },
+            {
+                Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Quake Badge", Image: "img/badges/quake.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Jet Badge", Image: "img/badges/jet.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Freeze Badge", Image: "img/badges/freeze.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Legend Badge", Image: "img/badges/legend.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four", Name: "Shauntal", Image: "img/trainers/black/shauntal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Grimsley", Image: "img/trainers/black/grimsley.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Caitlin", Image: "img/trainers/black/caitlin.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Marshal", Image: "img/trainers/black/marshal.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "N", Image: "img/trainers/black/n.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Ghetsis", Image: "img/trainers/black/ghetsis.png",
+                Time: "", Attempts: 0
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "First Game Clear", IDNo: "60990", Party: [
+                    { Pokemon: "Garbodor", Nickname: "Trpuebp", Level: 54, Gender: "Female" },
+                    { Pokemon: "Stoutland", Nickname: "ufzbhh♀ddc", Level: 61, Gender: "Male" },
+                    { Pokemon: "Maractus", Nickname: "Maraco", Level: 49, Gender: "Male" },
+                    { Pokemon: "Reuniclus", Nickname: "Cellmadioz", Level: 52, Gender: "Male" },
+                    { Pokemon: "Beartic", Nickname: "Cdacho", Level: 52, Gender: "Female" },
+                    { Pokemon: "Carracosta", Nickname: "Devyr", Level: 52, Gender: "Male" }
+                ],
+                Image: "img/ribbons/event.png",
+                FirstAttemptDate: "",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four Rematch", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Grimsley", Image: "img/trainers/black/grimsley.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Caitlin", Image: "img/trainers/black/caitlin.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Marshal", Image: "img/trainers/black/marshal.png", Class: "Rematch",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Champions", Name: "Alder", Image: "img/trainers/black/alder.png",
+                Time: "", Attempts: 0
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "60990", Party: [
+                    { Pokemon: "Carracosta", Nickname: "Devyr", Level: 69, Gender: "Male" },
+                    { Pokemon: "Reuniclus", Nickname: "Cellmadioz", Level: 79, Gender: "Male" },
+                    { Pokemon: "Stoutland", Nickname: "ufzbhh♀ddc", Level: 72, Gender: "Male" },
+                    { Pokemon: "Volcarona", Nickname: "Alcamotf", Level: 70, Gender: "Male" },
+                    { Pokemon: "Beartic", Nickname: "Cdacho", Level: 68, Gender: "Female" },
+                    { Pokemon: "Zekrom", Nickname: "Zekenator", Level: 68 }
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "2024-01-01T18:45:07.079Z",
+                Time: "", Attempts: 0
+            },
+
+        ]
+    }
+);
