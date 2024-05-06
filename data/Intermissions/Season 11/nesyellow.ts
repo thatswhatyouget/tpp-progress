edb6aeae4ac53ee55c6970fc88754e60866f2a1a
BornToLose <twitchplayslore@gmail.com>
2024-05-06 13:56:40 +0200

    Added clipped catches from NES Yellow

--- data/Intermissions/Season 11/nesyellow.ts
+++ data/Intermissions/Season 11/nesyellow.ts
@@ -14,6 +14,23 @@ Intermissions.Runs.push(
         Pokedex:"Kanto",
         Events: [
             { "Group": "Pokemon", "Name": "Pikachu", "Time": "2024-02-11T02:50:45Z" },
+			// Missing NidoranM
+            { "Group": "Pokemon", "Name": "Weedle", "Time": "2024-02-11T04:42:13Z" },
+            { "Group": "Pokemon", "Name": "Caterpie", "Time": "2024-02-11T05:54:37Z" },
+            { "Group": "Pokemon", "Name": "NidoranF", "Time": "2024-02-11T06:24:54Z" },
+			// Missing Charmander
+            { "Group": "Pokemon", "Name": "Charmeleon", "Time": "2024-02-11T17:16:20Z" },
+			// Missing Squirtle
+            { "Group": "Pokemon", "Name": "Wartortle", "Time": "2024-02-11T23:41:02Z" },
+            { "Group": "Pokemon", "Name": "Bulbasaur", "Time": "2024-02-12T00:35:27Z" },
+            { "Group": "Pokemon", "Name": "Farfetch'd", "Time": "2024-02-12T04:05:57Z" },
+            { "Group": "Pokemon", "Name": "Eevee", "Time": "2024-02-12T05:10:08Z" },
+			// Missing Haunter
+            { "Group": "Pokemon", "Name": "Cubone", "Time": "2024-02-12T08:26:37Z" },
+            { "Group": "Pokemon", "Name": "Marowak", "Time": "2024-02-12T09:06:49Z" },
+            { "Group": "Pokemon", "Name": "Snorlax", "Time": "2024-02-12T09:40:25Z" },
+            { "Group": "Pokemon", "Name": "Magmar", "Time": "2024-02-12T15:24:44Z" },
+            { "Group": "Pokemon", "Name": "Gengar", "Time": "2024-02-12T18:23:30Z" },
         ]
     }
 );
