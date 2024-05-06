3205b52e5eba3bd3ac2a7a323363d0561af0c65e
BornToLose <twitchplayslore@gmail.com>
2024-05-06 14:16:06 +0200

    Added clipped catches from NES Yellow Revisit

--- data/Revisits/nesyellow.ts
+++ data/Revisits/nesyellow.ts
@@ -13,6 +13,15 @@ Revisits.Runs.push(
         Generation: 1,
         Pokedex:"Kanto",
         Events: [
+		
+            { "Group": "Pokemon", "Name": "Raichu", "Time": "2024-02-18T07:55:22Z" },
+            { "Group": "Pokemon", "Name": "Moltres", "Time": "2024-02-18T11:18:32Z" },
+			// Missing Zubat, Pidgey, Rattata, Abra, Clefairy, Paras, Sandshrew, Onix, Tentacool, Horsea, Nidoking, Nidorino, Gastly, and Pinsir
+            { "Group": "Pokemon", "Name": "Grimer", "Time": "2024-02-18T11:27:00Z" },
+            { "Group": "Pokemon", "Name": "Hitmonlee", "Time": "2024-02-18T14:18:25Z" },
+            { "Group": "Pokemon", "Name": "Suicune", "Time": "2024-02-18T15:53:56Z" },
+            { "Group": "Pokemon", "Name": "Blastoise", "Time": "2024-02-18T21:12:01Z" },
+		
             <TPP.HallOfFame>{
                 Group: "Hall of Fame", Name: "Hall of Fame", Time: "2024-02-18T22:35:29Z", Attempts: 0, Party: [
                     { Pokemon: "Gengar", Nickname: "Gengar", Level: 64 },
@@ -25,6 +34,13 @@ Revisits.Runs.push(
                 FirstAttemptDate: "",
                 Image: "img/ribbons/champion.png"
             },
+			
+            { "Group": "Pokemon", "Name": "Ivysaur", "Time": "2024-02-18T22:55:36Z" },
+            { "Group": "Pokemon", "Name": "Porygon", "Time": "2024-02-19T00:15:25Z" },
+            { "Group": "Pokemon", "Name": "Mewtwo", "Time": "2024-02-19T00:22:53Z" },
+            { "Group": "Pokemon", "Name": "Ponyta", "Time": "2024-02-19T00:40:09Z" },
+            { "Group": "Pokemon", "Name": "Omanyte", "Time": "2024-02-19T03:28:53Z" },
+			
             <TPP.HallOfFame>{
                 Group: "Hall of Fame", Name: "Hall of Fame", Time: "2024-02-19T10:15:42Z", Attempts: 0, Party: [
                     { Pokemon: "Mewtwo", Nickname: "Mewtwo", Level: 72 },
