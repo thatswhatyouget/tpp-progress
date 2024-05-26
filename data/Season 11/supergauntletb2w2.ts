1f8f19e6793720b93922c974b800e21e27634864
BornToLose <twitchplayslore@gmail.com>
2024-05-26 17:43:08 +0200

    Added file for SGB2W2

--- data/Season 11/supergauntletb2w2.ts
+++ data/Season 11/supergauntletb2w2.ts
@@ -1,13 +1,13 @@
 /// <reference path="../tpp-data.ts" />
 Season11.Runs.push(
     {
-        RunName: "Super Gauntlet Black",
-        ColorPrimary: "#353535",
+        RunName: "Super Gauntlet Black 2",
+        ColorPrimary: "#b9d7e2",
         ColorSecondary: "black",
-        StartDate: "2024-03-18T21:00:00Z",
-        Duration: "2024-03-24T00:00:22Z",
-        HostName: "   \u2640Yss",
-        HostImage: "img/hosts/gmyc.png",
+        StartDate: "2024-05-13T21:00:00Z",
+        Duration: "2024-05-25T01:20:31Z",
+        HostName: "T448Z4 ",
+        HostImage: "img/hosts/cly.png",
         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
         Region: "Unova",
         Generation: 5,
@@ -15,20 +15,20 @@ Season11.Runs.push(
         FromNatDex: true,
         Class: "dual-run",
         LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
-        //FinalStateLink: "./states/Season 11/supergauntletbw.json",
-        APIObjectName: "black", 
+        //FinalStateLink: "./states/Season 11/supergauntletb2w2.json",
+        APIObjectName: "black2", 
         Events: [
             {
-                Group: "Badges", Name: "Trio Badge", Image: "img/badges/trio.png",
-                Time: "2024-03-18T22:58:18.855Z", Attempts: 1
+                Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png",
+                Time: "", Attempts: 0
             },
             {
-                Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png",
-                Time: "2024-03-19T03:04:55.761Z", Attempts: 1
+                Group: "Badges", Name: "Toxic Badge", Image: "img/badges/toxic.png",
+                Time: "", Attempts: 0
             },
             {
                 Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png",
-                Time: "2024-03-19T08:30:10.577Z", Attempts: 1
+                Time: "", Attempts: 0
             },
             {
                 Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png",
@@ -43,11 +43,11 @@ Season11.Runs.push(
                 Time: "", Attempts: 0
             },
             {
-                Group: "Badges", Name: "Freeze Badge", Image: "img/badges/freeze.png",
+                Group: "Badges", Name: "Legend Badge", Image: "img/badges/legend.png",
                 Time: "", Attempts: 0
             },
             {
-                Group: "Badges", Name: "Legend Badge", Image: "img/badges/legend.png",
+                Group: "Badges", Name: "Wave Badge", Image: "img/badges/wave.png",
                 Time: "", Attempts: 0
             },
 
@@ -67,20 +67,14 @@ Season11.Runs.push(
                 Group: "Elite Four", Name: "Marshal", Image: "img/trainers/black/marshal.png",
                 Time: "", Attempts: 0
             },
+
             {
-                Group: "Champions", Name: "N", Image: "img/trainers/black/n.png",
-                Time: "", Attempts: 0
-            },
-            {
-                Group: "Champions", Name: "Ghetsis", Image: "img/trainers/black/ghetsis.png",
+                Group: "Champions", Name: "Iris", Image: "img/trainers/black/iris.png",
                 Time: "", Attempts: 0
             },
 			
-			// Missing Sawk
-            { Name: "Throh", Time: "2024-03-19T19:10:34Z", Group: "Pokemon" },
-			
-            <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "First Game Clear", IDNo: "53314", Party: [
+            /*<TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "30102", Party: [
                     { Pokemon: "Krookodile", Nickname: "Hecatia", Level: 52, Gender: "Female" },
                     { Pokemon: "Sawk", Nickname: "OBR2ERT", Level: 53, Gender: "Male" },
                     { Pokemon: "Galvantula", Nickname: "Tiki", Level: 47, Gender: "Male" },
@@ -90,8 +84,8 @@ Season11.Runs.push(
                 ],
                 Image: "img/ribbons/event.png",
                 FirstAttemptDate: "",
-                Time: "2024-03-21T20:55:28Z", Attempts: 0
-            },
+                Time: "2024-05-21T20:55:28Z", Attempts: 0
+            },*/
 
             {
                 Group: "Elite Four Rematch", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Class: "Rematch",
@@ -111,12 +105,12 @@ Season11.Runs.push(
             },
 
             {
-                Group: "Champions", Name: "Alder", Image: "img/trainers/black/alder.png",
+                Group: "Champions", Name: "Iris", Image: "img/trainers/black/iris.png",
                 Time: "", Attempts: 0
             },
 			
-            <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "53314", Party: [
+            /*<TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "30102", Party: [
                     { Pokemon: "Bisharp", Nickname: "wii", Level: 70, Gender: "Male" },
                     { Pokemon: "Sawk", Nickname: "OBR2ERT", Level: 73, Gender: "Male" },
                     { Pokemon: "Thundurus", Nickname: "jeenee", Level: 79, Gender: "Male" },
@@ -126,27 +120,21 @@ Season11.Runs.push(
                 ],
                 Image: "img/ribbons/champion.png",
                 FirstAttemptDate: "",
-                Time: "2024-03-23T20:30:32Z", Attempts: 0
-            },
-
-			// Missing Celebi, Karrablast, Blitzle, Bisharp, Thundurus, Krookodile, Omastar, Venomoth, Purrloin, Pidove, Minccino, Roggenrola, Tympole, Cottonee, Vullaby, Venipede, Scraggy, Elgyem, Swadloon, Pansear
-            { Name: "Latias", Time: "2024-03-23T22:18:03Z", Group: "Pokemon" },
-            { Name: "Accelgor", Time: "2024-03-23T22:35:17Z", Group: "Pokemon" },
-            { Name: "Escavalier", Time: "2024-03-23T22:38:24Z", Group: "Pokemon" },
-            { Name: "Zorua", Time: "2024-03-23T23:09:03Z", Group: "Pokemon" },
+                Time: "2024-05-23T20:30:32Z", Attempts: 0
+            },*/
 
         ]
     }
 );
 Season11.Runs.push(
     {
-        RunName: "Super Gauntlet White",
-        ColorPrimary: "#E5E5E5",
-        ColorSecondary: "#C5C5C5",
-        StartDate: "2024-03-18T21:00:00Z",
-        Duration: "2024-03-24T00:00:22Z",
-        HostName: "%--\u265b\u265b\u2639 ",
-        HostImage: "img/hosts/aaq.png",
+        RunName: "Super Gauntlet White 2",
+        ColorPrimary: "#be9986",
+        ColorSecondary: "#3a3a3a",
+        StartDate: "2024-05-13T21:00:00Z",
+        Duration: "2024-05-25T01:20:31Z",
+        HostName: "3 B1A1 ",
+        HostImage: "img/hosts/r.png",
         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/beur93/aqua_kuehl_the_lonely/",
         Region: "Unova",
         Generation: 5,
@@ -154,20 +142,20 @@ Season11.Runs.push(
         FromNatDex: true,
         Class: "dual-run",
         LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
-        //FinalStateLink: "./states/Season 11/supergauntletbw.json",
-        APIObjectName: "white", 
+        //FinalStateLink: "./states/Season 11/supergauntletb2w2.json",
+        APIObjectName: "white2", 
         Events: [
             {
                 Group: "Badges", Name: "Trio Badge", Image: "img/badges/trio.png",
-                Time: "2024-03-18T22:24:07.051Z", Attempts: 1
+                Time: "", Attempts: 0
             },
             {
                 Group: "Badges", Name: "Basic Badge", Image: "img/badges/basic.png",
-                Time: "2024-03-18T23:56:24.244Z", Attempts: 1
+                Time: "", Attempts: 0
             },
             {
                 Group: "Badges", Name: "Insect Badge", Image: "img/badges/insect.png",
-                Time: "2024-03-19T04:08:10.397Z", Attempts: 1
+                Time: "", Attempts: 0
             },
             {
                 Group: "Badges", Name: "Bolt Badge", Image: "img/badges/bolt.png",
@@ -214,12 +202,9 @@ Season11.Runs.push(
                 Group: "Champions", Name: "Ghetsis", Image: "img/trainers/black/ghetsis.png",
                 Time: "", Attempts: 0
             },
-
-			// Missing Throh
-            { Name: "Sawk", Time: "2024-03-19T19:10:34Z", Group: "Pokemon" },
 			
-            <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "First Game Clear", IDNo: "60990", Party: [
+            /*<TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "08128", Party: [
                     { Pokemon: "Basculin", Nickname: "aabass", Level: 46, Gender: "Female", Form: "Blue" },
                     { Pokemon: "Throh", Nickname: "AAGG", Level: 48, Gender: "Male" },
                     { Pokemon: "Gothitelle", Nickname: "Ellen", Level: 47, Gender: "Female" },
@@ -229,8 +214,8 @@ Season11.Runs.push(
                 ],
                 Image: "img/ribbons/event.png",
                 FirstAttemptDate: "",
-                Time: "2024-03-20T07:07:20Z", Attempts: 0
-            },
+                Time: "2024-05-20T07:07:20Z", Attempts: 0
+            },*/
 
             {
                 Group: "Elite Four Rematch", Name: "Shauntal", Image: "img/trainers/black/shauntal.png", Class: "Rematch",
@@ -254,8 +239,8 @@ Season11.Runs.push(
                 Time: "", Attempts: 0
             },
 
-            <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "60990", Party: [
+            /*<TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "08128", Party: [
                     { Pokemon: "Simisear", Nickname: "AAMONK", Level: 74, Gender: "Male" },
                     { Pokemon: "Lilligant", Nickname: "Amaryhlowl", Level: 86, Gender: "Female" },
                     { Pokemon: "Basculin", Nickname: "aabass", Level: 74, Gender: "Female", Form: "Blue" },
@@ -265,27 +250,9 @@ Season11.Runs.push(
                 ],
                 Image: "img/ribbons/champion.png",
                 FirstAttemptDate: "",
-                Time: "2024-03-21T22:30:48Z", Attempts: 0
-            },
-
-            <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "Hall of Fame", IDNo: "60990", Party: [
-                    { Pokemon: "Meloetta", Level: 71 },
-                    { Pokemon: "Lilligant", Nickname: "Amaryhlowl", Level: 90, Gender: "Female" },
-                    { Pokemon: "Simisear", Nickname: "AAMONK", Level: 75, Gender: "Male" },
-                    { Pokemon: "Kyurem", Nickname: "pfreopiSZT", Level: 77 },
-                    { Pokemon: "Gothitelle", Nickname: "Ellen", Level: 73, Gender: "Female" },
-                    { Pokemon: "Basculin", Nickname: "aabass", Level: 75, Gender: "Female", Form: "Blue" },
-                ],
-                Image: "img/ribbons/champion.png",
-                FirstAttemptDate: "",
-                Time: "2024-03-22T21:05:24Z", Attempts: 0
-            },
+                Time: "2024-05-21T22:30:48Z", Attempts: 0
+            },*/
 			
-			// Missing Shelmet, Lilligant, Kyurem, Gothitelle, Basculin, Meloetta, Simisear, Timburr, Porygon, Zebstrika, Petilil, Lilipup, Pidove, Blitzle, Sandile, Beautifly, Sigilyph, Beheeyem, Mienshao, and Roggenrola
-            { Name: "Escavalier", Time: "2024-03-23T22:35:16Z", Group: "Pokemon" },
-            { Name: "Accelgor", Time: "2024-03-23T22:38:24Z", Group: "Pokemon" },
-
         ]
     }
 );
