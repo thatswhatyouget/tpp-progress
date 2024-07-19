7c790449b37ea59e8ca02ac73ad18fab19ed68cd
BornToLose <twitchplayslore@gmail.com>
2024-07-19 22:56:39 +0200

    Added data file for SGSS, and initial HoFs for each.

---
+++ data/Season 11/supergauntletswsh.ts
@@ -0,0 +1,100 @@
+/// <reference path="../tpp-data.ts" />
+Season11.Runs.push(
+    {
+        RunName: "Super Gauntlet Sword",
+        ColorSecondary: "black",
+        ColorPrimary: "#00A0E9",
+        Duration: "2024-07-17T07:29:34Z",
+        StartDate: "2024-07-08T21:10:00Z",
+        HostName: "11sdccvgg g",
+        HostImage: "img/hosts/89977re.gif",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/e1c95e/rey_collins_the_girl_with_rainbow_eyes/",
+        Region: "Galar",
+        Generation: 8,
+        Pokedex: "Galar",
+        FromNatDex: true,
+        Class: "dual-run",
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        // FinalStateLink: "./states/Season 11/supergauntletsum.json",
+        Events: [
+            { "Group": "Badges", "Name": "Grass Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/grass.png" },
+            { "Group": "Badges", "Name": "Water Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/water.png" },
+            { "Group": "Badges", "Name": "Fire Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/fire.png" },
+            { "Group": "Badges", "Name": "Fighting Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/fighting.png" },
+            { "Group": "Badges", "Name": "Fairy Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/fairy.png" },
+            { "Group": "Badges", "Name": "Rock Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/rock.png" },
+            { "Group": "Badges", "Name": "Dark Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/dark.png" },
+            { "Group": "Badges", "Name": "Dragon Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/dragon.png" },
+
+            { "Group": "Elite Four", "Name": "Bede", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/bede.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Bede" },
+            { "Group": "Elite Four", "Name": "Nessa", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/nessa.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Nessa" },
+            { "Group": "Elite Four", "Name": "Bea", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/bea.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Bea" },
+            { "Group": "Elite Four", "Name": "Raihan", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/raihan.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Raihan" },
+            { "Group": "Champions", "Name": "Leon", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/leon.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Leon" },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-12T03:21:54Z", IDNo: "831278", Attempts: 0, Party: [
+                    { Pokemon: "Crawdaunt", Nickname: "Skrelpeaterr", Gender: "Male", Level: 61, Type1: "Water", Type2: "Dark", Number: 342, OT: "11sdccvgg g", IDNo: "831278" },
+                    { Pokemon: "Slowbro", Nickname: "\"'aaaZzzzzzz", Gender: "Male", Level: 64, Type1: "Poison", Type2: "Psychic", Number: 80, OT: "11sdccvgg g", IDNo: "831278", Form: "Galarian" },
+                    { Pokemon: "Grimmsnarl", Nickname: "Oberon", Gender: "Male", Level: 64, Type1: "Dark", Type2: "Fairy", Number: 861, OT: "11sdccvgg g", IDNo: "831278" },
+                    { Pokemon: "Morpeko", Nickname: "Arnie", Gender: "Male", Level: 66, Type1: "Electric", Type2: "Dark", Number: 877, OT: "11sdccvgg g", IDNo: "831278" },
+                    { Pokemon: "Thievul", Nickname: "Lkiuu", Gender: "Male", Level: 72, Type1: "Dark", Number: 828, OT: "11sdccvgg g", IDNo: "831278" },
+                    { Pokemon: "Moltres", Nickname: "Black Doom", Level: 74, Type1: "Dark", Type2: "Flying", Number: 146, OT: "11sdccvgg g", IDNo: "831278", Form: "Galarian" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+			
+        ]
+    }
+);
+Season11.Runs.push(
+    {
+        RunName: "Super Gauntlet Shield",
+        ColorSecondary: "black",
+        ColorPrimary: "#BF014F",
+        Duration: "2024-07-17T07:29:34Z",
+        StartDate: "2024-07-08T21:10:00Z",
+        HostName: "1qqaaazzzzzz",
+        HostImage: "img/hosts/89977re.gif",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/e1c95e/rey_collins_the_girl_with_rainbow_eyes/",
+        Region: "Galar",
+        Generation: 8,
+        Pokedex: "Galar",
+        FromNatDex: true,
+        Class: "dual-run",
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        // FinalStateLink: "./states/Season 11/supergauntletsum.json",
+        Events: [
+		
+            { "Group": "Badges", "Name": "Grass Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/grass.png" },
+            { "Group": "Badges", "Name": "Water Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/water.png" },
+            { "Group": "Badges", "Name": "Fire Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/fire.png" },
+            { "Group": "Badges", "Name": "Fighting Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/fighting.png" },
+            { "Group": "Badges", "Name": "Fairy Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/fairy.png" },
+            { "Group": "Badges", "Name": "Rock Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/rock.png" },
+            { "Group": "Badges", "Name": "Dark Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/dark.png" },
+            { "Group": "Badges", "Name": "Dragon Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/dragon.png" },
+
+            { "Group": "Elite Four", "Name": "Bede", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/bede.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Bede" },
+            { "Group": "Elite Four", "Name": "Nessa", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/nessa.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Nessa" },
+            { "Group": "Elite Four", "Name": "Allister", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/allister.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Allister" },
+            { "Group": "Elite Four", "Name": "Raihan", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/raihan.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Raihan" },
+            { "Group": "Champions", "Name": "Leon", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/leon.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Leon" },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-11T02:13:21Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Hippowdon", Nickname: "Mayo", Gender: "Female", Level: 67, Type1: "Ground", Number: 450, OT: "1qqaaazzzzzz", IDNo: "719457" },
+                    { Pokemon: "Appletun", Nickname: "mwwzzz aaaa", Gender: "Male", Level: 72, Type1: "Grass", Type2: "Dragon", Number: 842, OT: "1qqaaazzzzzz", IDNo: "719457" },
+                    { Pokemon: "Corviknight", Nickname: "﹩﹩﹩", Gender: "Male", Level: 67, Type1: "Steel", Type2: "Flying", Number: 823, OT: "1qqaaazzzzzz", IDNo: "719457" },
+                    { Pokemon: "Dubwool", Nickname: "Lab", Gender: "Female", Level: 68, Type1: "Normal", Number: 832, OT: "1qqaaazzzzzz", IDNo: "719457" },
+                    { Pokemon: "Inteleon", Nickname: "Jimm", Gender: "Male", Level: 67, Type1: "Water", Number: 818, OT: "1qqaaazzzzzz", IDNo: "719457" },
+                    { Pokemon: "Hitmonchan", Nickname: "\u00a2\u00a2", Gender: "Male", Level: 69, Type1: "Fighting", Number: 107, OT: "1qqaaazzzzzz", IDNo: "719457" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+
+        ]
+    }
+);
