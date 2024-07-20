bfe1fb69dae6aa8d28c956ceb5b97b63f8b74bbf
BornToLose <twitchplayslore@gmail.com>
2024-07-20 18:30:10 +0200

    Added all known HoFs for SGSS + extra forms

--- data/Season 11/supergauntletswsh.ts
+++ data/Season 11/supergauntletswsh.ts
@@ -34,12 +34,77 @@ Season11.Runs.push(
 
             <TPP.HallOfFame>{
                 Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-12T03:21:54Z", IDNo: "831278", Attempts: 0, Party: [
-                    { Pokemon: "Crawdaunt", Nickname: "Skrelpeaterr", Gender: "Male", Level: 61, Type1: "Water", Type2: "Dark", Number: 342, OT: "11sdccvgg g", IDNo: "831278" },
-                    { Pokemon: "Slowbro", Nickname: "\"'aaaZzzzzzz", Gender: "Male", Level: 64, Type1: "Poison", Type2: "Psychic", Number: 80, OT: "11sdccvgg g", IDNo: "831278", Form: "Galarian" },
-                    { Pokemon: "Grimmsnarl", Nickname: "Oberon", Gender: "Male", Level: 64, Type1: "Dark", Type2: "Fairy", Number: 861, OT: "11sdccvgg g", IDNo: "831278" },
-                    { Pokemon: "Morpeko", Nickname: "Arnie", Gender: "Male", Level: 66, Type1: "Electric", Type2: "Dark", Number: 877, OT: "11sdccvgg g", IDNo: "831278" },
-                    { Pokemon: "Thievul", Nickname: "Lkiuu", Gender: "Male", Level: 72, Type1: "Dark", Number: 828, OT: "11sdccvgg g", IDNo: "831278" },
-                    { Pokemon: "Moltres", Nickname: "Black Doom", Level: 74, Type1: "Dark", Type2: "Flying", Number: 146, OT: "11sdccvgg g", IDNo: "831278", Form: "Galarian" },
+                    { Pokemon: "Crawdaunt", Nickname: "Skrelpeaterr", Gender: "Male", Level: 61, /*Type1: "Water", Type2: "Dark", Number: 342, OT: "11sdccvgg g", IDNo: "831278" */},
+                    { Pokemon: "Slowbro", Nickname: "\"'aaaZzzzzzz", Gender: "Male", Level: 64, /*Type1: "Poison", Type2: "Psychic", Number: 80, OT: "11sdccvgg g", IDNo: "831278",*/ Form: "Galarian" },
+                    { Pokemon: "Grimmsnarl", Nickname: "Oberon", Gender: "Male", Level: 64, /*Type1: "Dark", Type2: "Fairy", Number: 861, OT: "11sdccvgg g", IDNo: "831278" */},
+                    { Pokemon: "Morpeko", Nickname: "Arnie", Gender: "Male", Level: 66, /*Type1: "Electric", Type2: "Dark", Number: 877, OT: "11sdccvgg g", IDNo: "831278" */},
+                    { Pokemon: "Thievul", Nickname: "Lkiuu", Gender: "Male", Level: 72, /*Type1: "Dark", Number: 828, OT: "11sdccvgg g", IDNo: "831278" */},
+                    { Pokemon: "Moltres", Nickname: "Black Doom", Level: 74, /*Type1: "Dark", Type2: "Flying", Number: 146, OT: "11sdccvgg g", IDNo: "831278",*/ Form: "Galarian" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-13T11:43:54Z", IDNo: "831278", Attempts: 0, Party: [
+                    { Pokemon: "Crawdaunt", Nickname: "Skrelpeaterr", Gender: "Male", Level: 69, /*Type1: "Water", Type2: "Dark", Number: 342, OT: "11sdccvgg g", IDNo: "831278" */},
+                    { Pokemon: "Grimmsnarl", Nickname: "Oberon", Gender: "Male", Level: 73, /*Type1: "Dark", Type2: "Fairy", Number: 861, OT: "11sdccvgg g", IDNo: "831278" */},
+                    { Pokemon: "Slowbro", Nickname: "\"'aaaZzzzzzz", Gender: "Male", Level: 75, /*Type1: "Poison", Type2: "Psychic", Number: 80, OT: "11sdccvgg g", IDNo: "831278",*/ Form: "Galarian" },
+                    { Pokemon: "Morpeko", Nickname: "Arnie", Gender: "Male", Level: 74, /*Type1: "Electric", Type2: "Dark", Number: 877, OT: "11sdccvgg g", IDNo: "831278" */},
+                    { Pokemon: "Thievul", Nickname: "Lkiuu", Gender: "Male", Level: 81, /*Type1: "Dark", Number: 828, OT: "11sdccvgg g", IDNo: "831278" */},
+                    { Pokemon: "Moltres", Nickname: "Black Doom", Level: 80, /*Type1: "Dark", Type2: "Flying", Number: 146, OT: "11sdccvgg g", IDNo: "831278",*/ Form: "Galarian" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-14T02:11:12Z", IDNo: "831278", Attempts: 0, Party: [
+                    { Pokemon: "Eternatus", Level: 62 },
+                    { Pokemon: "Grimmsnarl", Nickname: "Oberon", Gender: "Male", Level: 82, },
+                    { Pokemon: "Moltres", Nickname: "Black Doom", Level: 84, Form: "Galarian" },
+                    { Pokemon: "Crawdaunt", Nickname: "Skrelpeaterr", Gender: "Male", Level: 80, },
+                    { Pokemon: "Thievul", Nickname: "Lkiuu", Gender: "Male", Level: 85 },
+                    { Pokemon: "Morpeko", Nickname: "Arnie", Gender: "Male", Level: 80 },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-14T02:51:35Z", IDNo: "831278", Attempts: 0, Party: [
+                    { Pokemon: "Eternatus", Level: 65 },
+                    { Pokemon: "Grimmsnarl", Nickname: "Oberon", Gender: "Male", Level: 83, },
+                    { Pokemon: "Moltres", Nickname: "Black Doom", Level: 85, Form: "Galarian" },
+                    { Pokemon: "Crawdaunt", Nickname: "Skrelpeaterr", Gender: "Male", Level: 80, },
+                    { Pokemon: "Thievul", Nickname: "Lkiuu", Gender: "Male", Level: 87 },
+                    { Pokemon: "Morpeko", Nickname: "Arnie", Gender: "Male", Level: 81 },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Galarian Star Tournament", Time: "2024-07-15T18:09:56Z", IDNo: "831278", Attempts: 0, Party: [
+                    { Pokemon: "Pangoro", Nickname: "Barry", Gender: "Male", Level: 86, Shiny: "true" },
+                    { Pokemon: "Crawdaunt", Nickname: "Skrelpeaterr", Gender: "Male", Level: 85 },
+                    { Pokemon: "Morpeko", Nickname: "Arnie", Gender: "Male", Level: 88 },
+                    { Pokemon: "Grimmsnarl", Nickname: "Oberon", Gender: "Male", Level: 89 },
+                    { Pokemon: "Thievul", Nickname: "Lkiuu", Gender: "Male", Level: 96 },
+                    { Pokemon: "Moltres", Nickname: "Black Doom", Level: 91, Form: "Galarian" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-16T14:24:17Z", IDNo: "831278", Attempts: 0, Party: [
+                    { Pokemon: "Pangoro", Nickname: "Barry", Gender: "Male", Level: 100, Shiny: "true" },
+                    { Pokemon: "Grimmsnarl", Nickname: "Oberon", Gender: "Male", Level: 100 },
+                    { Pokemon: "Thievul", Nickname: "Lkiuu", Gender: "Male", Level: 100 },
+                    { Pokemon: "Morpeko", Nickname: "Arnie", Gender: "Male", Level: 100 },
+                    { Pokemon: "Crawdaunt", Nickname: "Skrelpeaterr", Gender: "Male", Level: 100 },
+                    { Pokemon: "Moltres", Nickname: "Black Doom", Level: 100, Form: "Galarian" },
                 ],
                 Image: "img/ribbons/champion-galar.png",
                 FirstAttemptDate: ""
@@ -84,12 +149,116 @@ Season11.Runs.push(
 
             <TPP.HallOfFame>{
                 Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-11T02:13:21Z", IDNo: "719457", Attempts: 0, Party: [
-                    { Pokemon: "Hippowdon", Nickname: "Mayo", Gender: "Female", Level: 67, Type1: "Ground", Number: 450, OT: "1qqaaazzzzzz", IDNo: "719457" },
-                    { Pokemon: "Appletun", Nickname: "mwwzzz aaaa", Gender: "Male", Level: 72, Type1: "Grass", Type2: "Dragon", Number: 842, OT: "1qqaaazzzzzz", IDNo: "719457" },
-                    { Pokemon: "Corviknight", Nickname: "﹩﹩﹩", Gender: "Male", Level: 67, Type1: "Steel", Type2: "Flying", Number: 823, OT: "1qqaaazzzzzz", IDNo: "719457" },
-                    { Pokemon: "Dubwool", Nickname: "Lab", Gender: "Female", Level: 68, Type1: "Normal", Number: 832, OT: "1qqaaazzzzzz", IDNo: "719457" },
-                    { Pokemon: "Inteleon", Nickname: "Jimm", Gender: "Male", Level: 67, Type1: "Water", Number: 818, OT: "1qqaaazzzzzz", IDNo: "719457" },
-                    { Pokemon: "Hitmonchan", Nickname: "\u00a2\u00a2", Gender: "Male", Level: 69, Type1: "Fighting", Number: 107, OT: "1qqaaazzzzzz", IDNo: "719457" },
+                    { Pokemon: "Hippowdon", Nickname: "Mayo", Gender: "Female", Level: 67,/* Type1: "Ground", Number: 450, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Appletun", Nickname: "mwwzzz aaaa", Gender: "Male", Level: 72,/* Type1: "Grass", Type2: "Dragon", Number: 842, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Corviknight", Nickname: "﹩﹩﹩", Gender: "Male", Level: 67, /*Type1: "Steel", Type2: "Flying", Number: 823, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Dubwool", Nickname: "Lab", Gender: "Female", Level: 68, /*Type1: "Normal", Number: 832, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Inteleon", Nickname: "Jimm", Gender: "Male", Level: 67, /*Type1: "Water", Number: 818, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Hitmonchan", Nickname: "\u00a2\u00a2", Gender: "Male", Level: 69, /*Type1: "Fighting", Number: 107, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-11T13:38:56Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Dubwool", Nickname: "Lab", Gender: "Female", Level: 70, /*Type1: "Normal", Number: 832, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Hippowdon", Nickname: "Mayo", Gender: "Female", Level: 70, /*Type1: "Ground", Number: 450, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Corviknight", Nickname: "﹩﹩﹩", Gender: "Male", Level: 70, /*Type1: "Steel", Type2: "Flying", Number: 823, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Inteleon", Nickname: "Jimm", Gender: "Male", Level: 70, /*Type1: "Water", Number: 818, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Hitmonchan", Nickname: "\u00a2\u00a2", Gender: "Male", Level: 71, /*Type1: "Fighting", Number: 107, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Appletun", Nickname: "mwwzzz aaaa", Gender: "Male", Level: 75, /*Type1: "Grass", Type2: "Dragon", Number: 842, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-12T15:48:40Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Inteleon", Nickname: "Jimm", Gender: "Male", Level: 73, /*Type1: "Water", Number: 818, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Corviknight", Nickname: "﹩﹩﹩", Gender: "Male", Level: 72, /*Type1: "Steel", Type2: "Flying", Number: 823, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Hippowdon", Nickname: "Mayo", Gender: "Female", Level: 71, /*Type1: "Ground", Number: 450, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Dubwool", Nickname: "Lab", Gender: "Female", Level: 72, /*Type1: "Normal", Number: 832, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Hitmonchan", Nickname: "\u00a2\u00a2", Gender: "Male", Level: 74, /*Type1: "Fighting", Number: 107, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                    { Pokemon: "Appletun", Nickname: "mwwzzz aaaa", Gender: "Male", Level: 78, /*Type1: "Grass", Type2: "Dragon", Number: 842, OT: "1qqaaazzzzzz", IDNo: "719457" */},
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-12T17:45:17Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Alcremie", Nickname: "\\%{&œøä4#", Gender: "Female", Level: 76, Form: "matcha-ribbon" },
+                    { Pokemon: "Zapdos", Nickname: "jU88iKiqu", Level: 72, Form: "Galarian" },
+                    { Pokemon: "Articuno", Nickname: "Merlin", Level: 73, Form: "Galarian" },
+                    { Pokemon: "Inteleon", Nickname: "Jimm", Gender: "Male", Level: 74 },
+                    { Pokemon: "Dhelmise", Nickname: "uTg''\":ls", Level: 58 },
+                    { Pokemon: "Linoone", Gender: "Female", Level: 63, Form: "Galarian" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-14T00:50:52Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Articuno", Nickname: "Merlin", Level: 83, Form: "Galarian" },
+                    { Pokemon: "Zapdos", Nickname: "jU88iKiqu", Level: 79, Form: "Galarian" },
+                    { Pokemon: "Arctovish", Nickname: "helpp", Level: 61 },
+					{ Pokemon: "Dubwool", Nickname: "Lab", Gender: "Female", Level: 78 },
+                    { Pokemon: "Dhelmise", Nickname: "uTg''\":ls", Level: 78 },
+                    { Pokemon: "Alcremie", Nickname: "\\%{&œøä4#", Gender: "Female", Level: 89, Form: "matcha-ribbon" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Galarian Star Tournament", Time: "2024-07-14T03:20:38Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Articuno", Nickname: "Merlin", Level: 84, Form: "Galarian" },
+                    { Pokemon: "Zapdos", Nickname: "jU88iKiqu", Level: 81, Form: "Galarian" },
+                    { Pokemon: "Arctovish", Nickname: "helpp", Level: 66 },
+					{ Pokemon: "Dubwool", Nickname: "Lab", Gender: "Female", Level: 81 },
+                    { Pokemon: "Dhelmise", Nickname: "uTg''\":ls", Level: 82 },
+                    { Pokemon: "Alcremie", Nickname: "\\%{&œøä4#", Gender: "Female", Level: 92, Form: "matcha-ribbon" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Galarian Star Tournament", Time: "2024-07-15T20:03:24Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Zapdos", Nickname: "jU88iKiqu", Level: 92, Form: "Galarian" },
+                    { Pokemon: "Cleffa", Nickname: "Opie", Gender: "Female", Level: 46 },
+					{ Pokemon: "Stakataka", Level: 72 },
+                    { Pokemon: "Articuno", Nickname: "Merlin", Level: 94, Form: "Galarian" },
+                    { Pokemon: "Dhelmise", Nickname: "uTg''\":ls", Level: 98 },
+                    { Pokemon: "Alcremie", Nickname: "\\%{&œøä4#", Gender: "Female", Level: 100, Form: "matcha-ribbon" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Champion Tournament", Time: "2024-07-15T21:01:15Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Zapdos", Nickname: "jU88iKiqu", Level: 93, Form: "Galarian" },
+                    { Pokemon: "Clefairy", Nickname: "Opie", Gender: "Female", Level: 54 },
+					{ Pokemon: "Stakataka", Level: 73 },
+                    { Pokemon: "Articuno", Nickname: "Merlin", Level: 95, Form: "Galarian" },
+                    { Pokemon: "Dhelmise", Nickname: "uTg''\":ls", Level: 99 },
+                    { Pokemon: "Alcremie", Nickname: "\\%{&œøä4#", Gender: "Female", Level: 100, Form: "matcha-ribbon" },
+                ],
+                Image: "img/ribbons/champion-galar.png",
+                FirstAttemptDate: ""
+            },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Galarian Star Tournament", Time: "2024-07-16T20:12:23Z", IDNo: "719457", Attempts: 0, Party: [
+                    { Pokemon: "Dhelmise", Nickname: "uTg''\":ls", Level: 100 },
+                    { Pokemon: "Alcremie", Nickname: "\\%{&œøä4#", Gender: "Female", Level: 100, Form: "matcha-ribbon" },
+                    { Pokemon: "Inteleon", Nickname: "Jimm", Gender: "Male", Level: 77 },
+					{ Pokemon: "Zamazenta", Nickname: "1qqQqqqwwqQw", Level: 72, Form: "Crowned" },
+                    { Pokemon: "Dracovish", Nickname: "imfineJHGBVF", Level: 43 },
+					{ Pokemon: "Nihilego", Nickname: "Xoahjmui", Level: 72 },
                 ],
                 Image: "img/ribbons/champion-galar.png",
                 FirstAttemptDate: ""
