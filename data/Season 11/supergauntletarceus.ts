9c3d361087a58522a956e39cb056ba075084a087
BornToLose <twitchplayslore@gmail.com>
2024-08-21 10:53:41 +0200

    Created file for SGLA and added Game Clear team

---
+++ data/Season 11/supergauntletarceus.ts
@@ -0,0 +1,36 @@
+/// <reference path="../tpp-data.ts" />
+Season8.Runs.push(
+    {
+        RunName: "Legends: Arceus",
+        ColorSecondary: "#007EB5",
+        ColorPrimary: "#EBDEC9",
+        StartDate: "2024-08-05T21:00:00Z",
+        Duration: "2022-08-20T15:12:55Z",
+        HostName: `2`,
+        HostImage: "img/hosts/iecbw.png",
+        HostImageSource: "https://discord.com/channels/927292379436814406/928400303588532324/937375338739146792",
+        Region: "Hisui",
+        Events: [
+            { Group: "Noble Pokémon", Name: "Kleavor", Time: "", Attempts: 0, Class: "pokesprite bosses hisuian" },
+            { Group: "Noble Pokémon", Name: "Lilligant", Time: "", Attempts: 0, Class: "pokesprite bosses hisuian" },
+            { Group: "Noble Pokémon", Name: "Arcanine", Time: "", Attempts: 0, Class: "pokesprite bosses hisuian" },
+            { Group: "Noble Pokémon", Name: "Electrode", Time: "", Attempts: 0, Class: "pokesprite bosses hisuian" },
+            { Group: "Noble Pokémon", Name: "Avalugg", Time: "", Attempts: 0, Class: "pokesprite bosses hisuian" },
+            { Group: "Noble Pokémon", Name: "Dialga", Time: "", Attempts: 0, Class: "pokesprite bosses hisuian" },
+            { Group: "Noble Pokémon", Name: "Arceus", Time: "", Attempts: 0, Class: "pokesprite bosses hisuian" },
+
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "First Game Clear", Attempts: 0, Time: "2024-08-09T04:52:30Z", Party: [
+                    { Pokemon: "Zoroark", Nickname: "w", Level: 61, Gender: "Male", Form: "Hisuian" },
+                    { Pokemon: "Dialga", Nickname: "aAS++", Level: 65 },
+                    { Pokemon: "Braviary", Nickname: "P;AEDtgD", Level: 63, Gender: "Male", Form: "Hisuian" },
+                    { Pokemon: "Basculegion", Nickname: "Linda", Level: 65, Gender: "Female" },
+                    { Pokemon: "Decidueye", Nickname: "q/", Level: 67, Gender: "Male", Form: "Hisuian" },
+                    { Pokemon: "Lilligant", Nickname: "'liLla", Level: 72, Gender: "Female", Form: "Hisuian", Class: "alpha" },
+                ],
+                Image: "img/ribbons/national.png"
+            },
+
+        ]
+    }
+);
