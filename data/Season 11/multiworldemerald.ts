d3e57cbbd773990d9f70059e0083151d98b678ba
M4 <dave.was.r@gmail.com>
2024-09-13 04:45:28 -0400

    Created file for Multiworld Emerald

---
+++ data/Season 11/multiworldemerald.ts
@@ -0,0 +1,87 @@
+/// <reference path="../tpp-data.ts" />
+Season11.Runs.push(
+    {
+        RunName: "Multiworld Emerald",
+        ColorPrimary: "#3FB249",
+        ColorSecondary: "#3667B1",
+        StartDate: "2024-09-14T16:00:00Z",
+        Duration: "255d",
+        HostName: "",
+        //HostImage: "img/hosts/qq.png",
+        BaseGame: "Emerald",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "Hoenn",
+        FromNatDex: true,
+        //FinalStateLink: "./states/Season 11/supergauntletes.json",
+        Events: [
+            {
+                Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Heat Badge", Image: "img/badges/heat.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Feather Badge", Image: "img/badges/feather.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Mind Badge", Image: "img/badges/mind.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Rain Badge", Image: "img/badges/rain.png",
+                Time: "", Attempts: 0
+            },
+
+            {
+                Group: "Elite Four", Name: "Sidney", Image: "img/trainers/emerald/sidney.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Phoebe", Image: "img/trainers/emerald/phoebe.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Glacia", Image: "img/trainers/emerald/glacia.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Drake", Image: "img/trainers/emerald/drake.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Wallace", Image: "img/trainers/emerald/wallace.png",
+                Time: "", Attempts: 0
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "", Attempts: 0, Party: [
+                    // { Pokemon: "Minun", Nickname: "bnnuy", Level: 43, Gender: "Male", Number: 81, IDNo: "" },
+                    // { Pokemon: "Blaziken", Nickname: "WKKIII", Level: 63, Gender: "Female", Number: 6, IDNo: "" },
+                    // { Pokemon: "Zangoose", Nickname: "GGGUUvo", Level: 53, Gender: "Female", Number: 123, IDNo: "" },
+                    // { Pokemon: "Gorebyss", Nickname: "nnut…’’98D", Level: 66, Gender: "Female", Number: 178, IDNo: "" },
+                    // { Pokemon: "Grumpig", Nickname: "reyn", Level: 50, Gender: "Male", Number: 111, IDNo: "" },
+                    // { Pokemon: "Shiftry", Nickname: "IRPWWC", Level: 49, Gender: "Male", Number: 24, IDNo: "" },
+                ],
+                FirstAttemptDate: "",
+                Image: "img/ribbons/champion.png"
+            },
+			
+        ]
+    }
+);
