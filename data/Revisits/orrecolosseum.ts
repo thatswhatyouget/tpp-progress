f8e18a4df6e0f01050d7bd0e95d52359d1132cef
BornToLose <twitchplayslore@gmail.com>
2024-10-12 16:22:31 +0200

    Added Orre Colosseum Revisits data

---
+++ data/Revisits/orrecolosseum.ts
@@ -0,0 +1,87 @@
+/// <reference path="../tpp-data.ts" />
+Revisits.Runs.push(
+    {
+        RunName: "Super Gauntlet Colosseum - Orre Colosseum Revisit",
+        ColorPrimary: "#bba155",
+        ColorSecondary: "#644d2e",
+        StartDate: "2024-05-11T20:53:04Z",
+        Duration: "2024-05-12T19:39:34Z",
+        HostName: "WES",
+        HostImage: "img/hosts/aaaaaaa.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Orre",
+        Generation: 3,
+        ContainsRunsFrom: ["Revisits"],
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        //FinalStateLink: "./states/Season 11/supergauntletcolo.json",
+        Events: [
+			
+            { Name: "Tentacool", Time: "2024-05-11T21:04:50Z", Group: "Pokemon" },
+
+        ]
+    }
+);
+
+Revisits.Runs.push(
+    {
+        RunName: "Super Gauntlet Ruby - Orre Colosseum Revisit",
+        ColorPrimary: "red",
+        ColorSecondary: "darkred",
+        StartDate: "2024-05-11T21:04:12Z",
+        Duration: "2024-05-12T19:50:31Z",
+        HostName: "TERRA",
+        HostImage: "img/hosts/qq.png",
+        BaseGame: "Ruby",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "Hoenn",
+        FromNatDex: true,
+        //Class: "dual-run",
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        //FinalStateLink: "./states/Season 11/supergauntletes.json",
+        APIObjectName: "ruby", 
+        Events: [
+			
+            { Name: "Suicune", Time: "2024-05-11T21:04:50Z", Group: "Pokemon" },
+            { Name: "Metagross", Time: "2024-05-11T21:06:31Z", Group: "Pokemon" },
+            { Name: "Dusclops", Time: "2024-05-11T21:22:59Z", Group: "Pokemon" },
+            { Name: "Snorlax", Time: "2024-05-11T21:24:22Z", Group: "Pokemon" },
+            { Name: "Skitty", Time: "2024-05-11T23:47:14Z", Group: "Pokemon" },
+            { Name: "Relicanth", Time: "2024-05-12T03:30:13Z", Group: "Pokemon" },
+            { Name: "Mr. Mime", Time: "2024-05-12T05:48:31Z", Group: "Pokemon" },
+            { Name: "Scyther", Time: "2024-05-12T05:52:30Z", Group: "Pokemon" },
+            { Name: "Lapras", Time: "2024-05-12T05:54:08Z", Group: "Pokemon" },
+			
+        ],
+        Revisit: { Collection: "Season 11", Run: "Super Gauntlet Ruby - Colosseum Revisit" },
+        CopyEvents: ["Super Gauntlet Ruby - Colosseum Revisit"],
+    }
+);
+
+Revisits.Runs.push(
+    {
+        RunName: "Super Gauntlet XD - Orre Colosseum Revisit",
+        ColorPrimary: "#532245",
+        ColorSecondary: "#793165",
+        StartDate: "2024-05-11T21:09:41Z",
+        Duration: "2024-05-12T21:05:30Z",
+        HostName: "DAVID",
+        HostImage: "img/hosts/david.png",
+        //HostImageSource: "",
+        Region: "Orre",
+        Generation: 3,
+        ContainsRunsFrom: ["Revisits"],
+        LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+        //FinalStateLink: "./states/Season 11/supergauntletxd.json",
+        Events: [
+			
+            { Name: "Ho-Oh", Time: "2024-05-11T21:22:59Z", Group: "Pokemon" },
+            { Name: "Groudon", Time: "2024-05-11T21:24:21Z", Group: "Pokemon" },
+            { Name: "Suicune", Time: "2024-05-12T05:49:42Z", Group: "Pokemon" },
+            { Name: "Latios", Time: "2024-05-12T05:51:15Z", Group: "Pokemon" },
+            { Name: "Metagross", Time: "2024-05-12T05:52:30Z", Group: "Pokemon" },
+            { Name: "Latias", Time: "2024-05-12T05:54:08Z", Group: "Pokemon" },
+			
+        ]
+    }
+);
