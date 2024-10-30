a93d9f3bc5c2f53bd16754c3e4a9df792734f88d
m4-used-rollout <dave.was.r@gmail.com>
2024-10-30 22:56:43 +0000

    Removed erroneous final state link

--- data/Intermissions/Season 11/trickortreat.ts
+++ data/Intermissions/Season 11/trickortreat.ts
@@ -4,14 +4,14 @@ Intermissions.Runs.push(
         RunName: "Trick or Treat House 2022(2)",
         ColorPrimary: "orange",
         ColorSecondary: "purple",
-        StartDate: "2024-10-30T21:00:00.000Z",
+        StartDate: "2024-10-30T22:00:00.000Z",
         Duration: "2024-11-01T21:00:00.000Z",
         HostName: "River",
         HostImage: "img/hosts/treekid.png",
         // HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
         // Region: "Hoenn",
         // Generation:3,
-        FinalStateLink: "./states/Season 11/trickortreat.json",
+        //FinalStateLink: "./states/Season 11/trickortreat.json",
         Events: [
         ]
     }
