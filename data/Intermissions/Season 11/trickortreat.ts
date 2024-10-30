6c5b3cc2ca6079c0cf6d6ca96f7b9f2cad063bfc
m4-used-rollout <dave.was.r@gmail.com>
2024-10-30 15:37:14 +0000

    Added TTH 2024

---
+++ data/Intermissions/Season 11/trickortreat.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Trick or Treat House 2022(2)",
+        ColorPrimary: "orange",
+        ColorSecondary: "purple",
+        StartDate: "2024-10-30T21:00:00.000Z",
+        Duration: "2021-11-01T21:00:00.000Z",
+        // HostName: "",
+        // HostImage: "img/hosts/mudkid.png",
+        // HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        // Region: "Hoenn",
+        // Generation:3,
+        FinalStateLink: "./states/Season 10/trickortreat.json",
+        Events: [
+        ]
+    }
+);
