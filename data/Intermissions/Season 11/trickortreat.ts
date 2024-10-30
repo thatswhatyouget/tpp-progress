de315f1b2b567273eb2b96dc134eaa2858644822
m4-used-rollout <dave.was.r@gmail.com>
2024-10-30 22:36:39 +0000

    Started TTH 2024

--- data/Intermissions/Season 11/trickortreat.ts
+++ data/Intermissions/Season 11/trickortreat.ts
@@ -5,13 +5,13 @@ Intermissions.Runs.push(
         ColorPrimary: "orange",
         ColorSecondary: "purple",
         StartDate: "2024-10-30T21:00:00.000Z",
-        Duration: "2021-11-01T21:00:00.000Z",
-        // HostName: "",
-        // HostImage: "img/hosts/mudkid.png",
+        Duration: "2024-11-01T21:00:00.000Z",
+        HostName: "River",
+        HostImage: "img/hosts/treekid.png",
         // HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
         // Region: "Hoenn",
         // Generation:3,
-        FinalStateLink: "./states/Season 10/trickortreat.json",
+        FinalStateLink: "./states/Season 11/trickortreat.json",
         Events: [
         ]
     }
