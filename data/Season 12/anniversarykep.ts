94ec057314b3eb2846fb15bc09f2ec4e07f8aa2b
BornToLose <twitchplayslore@gmail.com>
2025-02-19 17:20:59 +0100

    Added milestones for KEP Anniversary

--- data/Season 12/anniversarykep.ts
+++ data/Season 12/anniversarykep.ts
@@ -2,8 +2,8 @@
 Season12.Runs.push(
     {
         RunName: "Anniversary KEP",
-        ColorPrimary: "#c0504d",
-        ColorSecondary: "#8c3836",
+        ColorPrimary: "#cf5f6e",
+        ColorSecondary: "#a54551",
         StartDate: "2025-02-12T23:18:16Z",
         Duration: "2025-02-18T00:14:39Z",
         HostName: "ARµµ-",
@@ -63,14 +63,103 @@ Season12.Runs.push(
                 Time: "2025-02-15T01:55:42.140Z", Attempts: 2
             },
             {
-                Group: "Champions", Name: "BLUE", Image: "img/trainers/red/blue.png",
+                Group: "Champions", Name: "S", Image: "img/trainers/red/blue.png",
                 Time: "2025-02-15T02:43:48.971Z", Attempts: 2
             },
             <TPP.HallOfFame>{
                 Group: "Hall of Fame", Name: "Hall of Fame", Time: "2025-02-15T02:44:48.459Z", Attempts: 4, Party: [
+                    { Pokemon: "Ramoose", Nickname: "AAAC", Level: 59, Type1: "Normal", Type2: "Grass" },
+                    { Pokemon: "Gawarhed", Nickname: "X]]]OWWW;♀", Level: 59, Type1: "Rock" },
+                    { Pokemon: "Iguanarch", Nickname: "AAAAJKA", Level: 63, Type1: "Dragon" },
+                    { Pokemon: "Pendraken", Nickname: "BLO", Level: 69, Type1: "Water", Type2: "Fairy" },
+                    { Pokemon: "Omegadge", Nickname: "JRRZ", Level: 59, Type1: "Steel" },
+                    { Pokemon: "Gorotora", Nickname: "IRRJJJRRAA", Level: 60, Type1: "Electric" },
                 ],
                 Image: "img/ribbons/champion.png",
                 FirstAttemptDate: "2025-02-15T00:01:59.549Z"
+            },
+			
+			{ Group: "Past Hosts", Name: "(((", Image: "img/hosts/ccc.png", 
+			Time: "2025-02-15T18:59:17.708Z", Attempts: 3, 
+			ImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/7xfytx/xena_winipek_cesar_cesis_cee_cee_brackets_sprites/" },
+			
+			{ Group: "Past Hosts", Name: "KAY", Image: "img/hosts/kay.png", 
+			Time: "2025-02-17T04:53:08.611Z", Attempts: 1, 
+			ImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/aq2qmu/kay_the_planeswalker/" },
+			
+			{ Group: "Past Hosts", Name: "BBBBBa", Image: "img/hosts/bbbbba.png", 
+			Time: "2025-02-17T13:14:41.805Z", Attempts: 1, 
+			ImageSource: "https://www.deviantart.com/krizste/art/TPP-CharityDualRedBlue-pk-bb-sprites-946843785" },
+			
+			{ Group: "Past Hosts", Name: "ASH", Image: "img/hosts/ash3.png", 
+			Time: "2025-02-17T15:16:20.408Z", Attempts: 1, 
+			ImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/1d4oe61/super_gauntlet_red_gold_and_silver_host_sprites/" },
+			
+			{ Group: "Past Hosts", Name: "A", Image: "img/hosts/rocket.png", 
+			Time: "2025-02-17T15:46:50.185Z", Attempts: 1, 
+			ImageSource: "" },
+			
+            {
+                Group: "Elite Four Rematch", Name: "Lorelei", Image: "img/trainers/red/lorelei.png", "Class": "Rematch",
+                Time: "2025-02-17T16:48:12.078Z", Attempts: 1
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Bruno", Image: "img/trainers/red/bruno.png", "Class": "Rematch",
+                Time: "2025-02-17T16:58:36.917Z", Attempts: 1
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Agatha", Image: "img/trainers/red/agatha.png", "Class": "Rematch",
+                Time: "2025-02-17T17:10:18.514Z", Attempts: 1
+            },
+            {
+                Group: "Elite Four Rematch", Name: "Lance", Image: "img/trainers/red/lance.png", "Class": "Rematch",
+                Time: "2025-02-17T17:21:45.221Z", Attempts: 1
+            },
+            {
+                Group: "Champions", Name: "S", Image: "img/trainers/red/blue.png", "Class": "Rematch",
+                Time: "2025-02-17T17:31:14.371Z", Attempts: 1
+            },
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2025-02-17T17:32:16.542Z", Attempts: 1, Party: [
+                    { Pokemon: "Gorotora", Nickname: "IRRJJJRRAA", Level: 96, Type1: "Electric" },
+                    { Pokemon: "Iguanarch", Nickname: "AAAAJKA", Level: 95, Type1: "Dragon" },
+                    { Pokemon: "Ramoose", Nickname: "AAAC", Level: 88, Type1: "Normal", Type2: "Grass" },
+                    { Pokemon: "Gawarhed", Nickname: "X]]]OWWW;♀", Level: 89, Type1: "Rock" },
+                    { Pokemon: "Balumba", Nickname: "MNOO[[Z TL", Level: 100, Type1: "Fairy", Type2: "Flying" },
+                    { Pokemon: "Omegadge", Nickname: "JRRZ", Level: 89, Type1: "Steel" },
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "2025-02-17T16:41:43.338Z"
+            },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2025-02-17T19:47:15.726Z", Attempts: 1, Party: [
+                    { Pokemon: "Gorotora", Nickname: "IRRJJJRRAA", Level: 96, Type1: "Electric" },
+                    { Pokemon: "Ramoose", Nickname: "AAAC", Level: 91, Type1: "Normal", Type2: "Grass" },
+                    { Pokemon: "Iguanarch", Nickname: "AAAAJKA", Level: 97, Type1: "Dragon" },
+                    { Pokemon: "Gawarhed", Nickname: "X]]]OWWW;♀", Level: 91, Type1: "Rock" },
+                    { Pokemon: "Balumba", Nickname: "MNOO[[Z TL", Level: 100, Type1: "Fairy", Type2: "Flying" },
+                    { Pokemon: "Pendraken", Nickname: "BLO", Level: 99, Type1: "Water", Type2: "Fairy" }
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: ""
+            },
+			
+			{ Group: "Past Hosts", Name: "AMANDA", Image: "img/hosts/amanda.png", 
+			Time: "2025-02-17T22:00:22.376Z", Attempts: 1, 
+			ImageSource: "" },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2025-02-18T00:10:08.628Z", Attempts: 1, Party: [
+                    { Pokemon: "Gorotora", Nickname: "IRRJJJRRAA", Level: 100, Type1: "Electric" },
+                    { Pokemon: "???", Nickname: "ABKABB", Level: 80, Type1: "Fighting", Type2: "Ghost" },
+                    { Pokemon: "???", Nickname: "CCCKKKK", Level: 80, Type1: "Fighting", Type2: "Ghost" },
+                    { Pokemon: "???", Nickname: "▓▓        ", Level: 82, Type1: "Fighting", Type2: "Ghost" },
+                    { Pokemon: "Mew", Level: 33, Type1: "Psychic" },
+                    { Pokemon: "???", Nickname: "AAACUUU)::", Level: 80, Type1: "Fighting", Type2: "Ghost" },
+                ],
+                Image: "img/ribbons/champion.png",
+                FirstAttemptDate: "2025-02-17T16:41:43.338Z"
             },
         ]
     }
