8abb02741415aaa3dd3ac452c6b184ce4282f56e
BornToLose <twitchplayslore@gmail.com>
2025-02-21 21:42:49 +0100

    Added catch data for KEP Anniversary up to Soul Badge

--- data/Season 12/anniversarykep.ts
+++ data/Season 12/anniversarykep.ts
@@ -14,25 +14,81 @@ Season12.Runs.push(
         Pokedex: "Kanto Expansion Pak",
         FinalStateLink:"./states/Season 12/anniversarykep.json",
         Events: [
+		
+			{ Name: "Squirtle", Time: "2025-02-12T23:27:36.535Z", Group: "Pokemon" },
+			{ Name: "Coinpur", Time: "2025-02-13T01:20:30Z", Group: "Pokemon" },
+			{ Name: "Kakuna", Time: "2025-02-13T01:33:47Z", Group: "Pokemon" },
+			{ Name: "Kotora", Time: "2025-02-13T01:38:24Z", Group: "Pokemon" },
+			{ Name: "Weedle", Time: "2025-02-13T02:05:29Z", Group: "Pokemon" },
+			{ Name: "Gentlarva", Time: "2025-02-13T02:19:04Z", Group: "Pokemon" },
+			{ Name: "Pupal", Time: "2025-02-13T02:39:39Z", Group: "Pokemon" },
+			{ Name: "Carapthor", Time: "2025-02-13T03:17:27Z", Group: "Pokemon" },
+			{ Name: "Bittybat", Time: "2025-02-13T04:35:35Z", Group: "Pokemon" },
+			{ Name: "NidoranM", Time: "2025-02-13T04:40:39Z", Group: "Pokemon" },
+			{ Name: "Gaotora", Time: "2025-02-13T05:45:34Z", Group: "Pokemon" },
+			{ Name: "Wartortle", Time: "2025-02-13T05:45:50Z", Group: "Pokemon" },
+			{ Name: "Jigglypuff", Time: "2025-02-13T06:17:03Z", Group: "Pokemon" },
+			{ Name: "Nidorino", Time: "2025-02-13T06:55:43Z", Group: "Pokemon" },
+			{ Name: "Nidoreign", Time: "2025-02-13T06:57:09Z", Group: "Pokemon" },
+			{ Name: "Vulpiii", Time: "2025-02-13T09:11:13Z", Group: "Pokemon" },
+		
+            {
+                Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2025-02-13T09:55:09.746Z", Attempts: 1
+            },
+			
+			{ Name: "Psyduck", Time: "2025-02-13T10:45:55Z", Group: "Pokemon" },
+			{ Name: "Poliwag", Time: "2025-02-13T10:47:43Z", Group: "Pokemon" },
+			{ Name: "Magikarp", Time: "2025-02-13T11:12:03Z", Group: "Pokemon" },
+			{ Name: "Orfry", Time: "2025-02-13T11:15:13Z", Group: "Pokemon" },
+			{ Name: "Goldeen", Time: "2025-02-13T11:56:44Z", Group: "Pokemon" },
+			{ Name: "Shellder", Time: "2025-02-13T12:03:46Z", Group: "Pokemon" },
+			{ Name: "Cheep", Time: "2025-02-13T12:05:24Z", Group: "Pokemon" },
+			{ Name: "Tentacool", Time: "2025-02-13T12:33:29Z", Group: "Pokemon" },
+			{ Name: "Blottle", Time: "2025-02-13T12:39:41Z", Group: "Pokemon" },
+			{ Name: "Meowth", Time: "2025-02-13T13:01:54.126Z", Group: "Pokemon" },
+			{ Name: "Pidgey", Time: "2025-02-13T13:13:15Z", Group: "Pokemon" },
+			{ Name: "Wiglett", Time: "2025-02-13T14:07:37Z", Group: "Pokemon" },
+			{ Name: "Diglett", Time: "2025-02-13T15:11:54Z", Group: "Pokemon" },
+			{ Name: "Onix", Time: "2025-02-13T15:15:18Z", Group: "Pokemon" },
+			{ Name: "Dugtrio", Time: "2025-02-13T15:26:14Z", Group: "Pokemon" },
+			{ Name: "Wugtrio", Time: "2025-02-13T15:39:51Z", Group: "Pokemon" },
+			{ Name: "Ribbito", Time: "2025-02-13T18:06:55Z", Group: "Pokemon" },
+			{ Name: "Gavillain", Time: "2025-02-13T19:09:50Z", Group: "Pokemon" },
+			{ Name: "Pendraken", Time: "2025-02-13T19:32:15.043Z", Group: "Pokemon" },
+			{ Name: "Eevee", Time: "2025-02-13T20:04:19Z", Group: "Pokemon" },
+			{ Name: "Ramoose", Time: "2025-02-13T20:51:06Z", Group: "Pokemon" },
+			{ Name: "Kolta", Time: "2025-02-13T21:01:27Z", Group: "Pokemon" },
+			
+            {
+                Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2025-02-13T21:37:20.928Z", Attempts: 1
+            },
+			
+			{ Name: "Squeamata", Time: "2025-02-13T22:18:19Z", Group: "Pokemon" },
+			{ Name: "Cactormus", Time: "2025-02-13T22:19:25.292Z", Group: "Pokemon" },
+			{ Name: "Gastly", Time: "2025-02-14T00:51:26Z", Group: "Pokemon" },
+			{ Name: "Cubone", Time: "2025-02-14T00:55:06Z", Group: "Pokemon" },
+			{ Name: "Balumba", Time: "2025-02-14T01:40:30Z", Group: "Pokemon" },
+			{ Name: "Bellsprout", Time: "2025-02-14T02:02:56Z", Group: "Pokemon" },
+			{ Name: "Oddish", Time: "2025-02-14T02:04:36Z", Group: "Pokemon" },
+			{ Name: "Venonat", Time: "2025-02-14T02:07:36Z", Group: "Pokemon" },
+			{ Name: "Gorotora", Time: "2025-02-14T03:32:19.320Z", Group: "Pokemon" },
+			
+            {
+                Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2025-02-14T03:42:21.693Z", Attempts: 1
+            },
+			
+			
+			
             {
                 Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
                 Time: "2025-02-14T21:05:11.442Z", Attempts: 1
             },
             {
-                Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "2025-02-13T09:55:09.746Z", Attempts: 1
-            },
-            {
-                Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "2025-02-13T21:37:20.928Z", Attempts: 1
-            },
-            {
                 Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
                 Time: "2025-02-14T18:54:49.006Z", Attempts: 1
-            },
-            {
-                Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
-                Time: "2025-02-14T03:42:21.693Z", Attempts: 1
             },
             {
                 Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
