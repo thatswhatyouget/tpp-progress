cb29be2d13bbde8f39b1d1f8f5a222025480c20a
BornToLose <twitchplayslore@gmail.com>
2025-03-09 00:30:30 +0100

    Added Karpe Diem sidegame

---
+++ data/Sidegames/karpediem.ts
@@ -0,0 +1,22 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Karpe Diem",
+        StartDate: "2025-03-04T23:44:17Z",
+        ColorPrimary: "#fd884f",
+        ColorSecondary: "#b22b11",
+        Duration: "255d",
+        HostName: "Fisherman",
+        HostImage: "img/hosts/fisherman.png",
+        // HostImageSource: "",
+        Region: "Joisey",
+        Generation: 1,
+        // LastScreenshot: "https://twitchplayspokemon.tv/sidegame_image/1330",
+        SidegameId: "karpe",
+        Events: [
+		
+            { Name: "Magikarp", Time: "2024-03-08T12:46:55Z", Group: "Pokemon" },
+			
+        ]
+    }
+);
