118be484b29c94c7c5cdb0e1c74e4166ee4c13b1
BornToLose <twitchplayslore@gmail.com>
2025-05-03 16:06:01 +0200

    Added file for ToTH 2022(2)

---
+++ data/Intermissions/Season 10/trickortreat.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Trick or Treat House 2022(2)",
+        ColorPrimary: "orange",
+        ColorSecondary: "purple",
+        StartDate: "2023-10-30T21:43:46Z",
+        Duration: "2023-11-02T11:38:24Z",
+        HostName: "    KKX",
+        HostImage: "img/hosts/aa.png",
+        HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        Region: "Hoenn",
+        Generation:3,
+        FinalStateLink: "./states/Season 10/trickortreat.json",
+        Events: [
+        ]
+    }
+);
