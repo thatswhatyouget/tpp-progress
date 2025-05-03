1666d134db69189ca898f9d2ccb47b9aff8bf6e9
BornToLose <twitchplayslore@gmail.com>
2025-05-03 15:41:49 +0200

    Added files for ToTH 2022

---
+++ data/Sidegames/trickortreat.ts
@@ -0,0 +1,20 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+		RunName: "Trick or Treat House 2022 Sidegame",
+        ColorPrimary: "orange",
+        ColorSecondary: "purple",
+        StartDate: "2022-11-02T08:44:29Z",
+        Duration: "2022-11-13T21:15:36Z",
+        HostName: "GCCEEEF",
+        HostImage: "img/hosts/aa.png",
+        HostImageSource: "https://reddit.com/r/twitchplayspokemon/comments/iewiby/the_hosts_of_the_voices_after_the_rise_of_the_king/",
+        Region: "Hoenn",
+        Generation:3,
+        FinalStateLink: "./states/Season 9/trickortreat.json",
+        Events: [
+        ],
+        Revisit: { Collection: "Intermissions", Run: "Trick or Treat House 2022" },
+        CopyEvents: ["Trick or Treat House 2022"],
+    }
+);
