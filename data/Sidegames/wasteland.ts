0f326cb89703b98eef53c6f51f2b22761614166b
BornToLose <twitchplayslore@gmail.com>
2025-05-17 16:07:04 +0200

    Added file for Wasteland sidegame

---
+++ data/Sidegames/wasteland.ts
@@ -0,0 +1,24 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Wasteland",
+        StartDate: "2025-05-15T00:27:00Z",
+        ColorPrimary: "#c4b594",
+        ColorSecondary: "#9b6e43",
+        Duration: "255d",
+        HostName: "SIM",
+        HostImage: "img/hosts/sim.png",
+        // HostImageSource: "",
+        Region: "Dakos",
+        Generation: 2,
+        Pokedex: "New",
+        FromNatDex: true,
+        FinalStateLink: "./states/Season 12/wasteland.json",
+        SidegameId: "wasteland",
+        Events: [
+		
+            { "Group": "Pokemon", "Name": "Nidorino", "Time": "2025-05-16T04:09:58Z" },
+			
+        ]
+    }
+);
