bf6c8c684b9811d6341a2b8c37d0c18c7d53686a
m4_used_rollout <dave.was.r@gmail.com>
2025-05-18 04:36:58 +0000

    Fixed fakemon fusions fighting with fuseddex fusions

--- poke-styles/other/fuseddex.ts
+++ poke-styles/other/fuseddex.ts
@@ -1,6 +1,6 @@
-/// <reference path="../write-style.ts" />
+/// <reference path="../pokemon/fakemon.ts" />
 /// <reference path="../../data/Pokedex/other/fuseddex.ts" />
 
 addSpriteSheet("fusedcrystal", "fusedcrystal", SpriteSheetType.Large);
 
-addStyles(Pokedex.Regional["Fuseddex"], defaultMapping("fusedcrystal"));
+addStyles(Pokedex.Regional["Fuseddex"], defaultMapping("")); //Empty mapping so none of the fusion styles are more specific than the fakemon fusions (from Chatty Crystal)
