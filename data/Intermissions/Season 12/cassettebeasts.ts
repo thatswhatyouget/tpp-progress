a8f2a5a7c1476e5a2013aebaca1d2c9f7bbae362
BornToLose <twitchplayslore@gmail.com>
2025-06-17 03:02:52 +0200

    Cassette Beasts data up to Poppetox

--- data/Intermissions/Season 12/cassettebeasts.ts
+++ data/Intermissions/Season 12/cassettebeasts.ts
@@ -8,7 +8,7 @@ Intermissions.Runs.push(
         ColorPrimary: "#b0ceee",
         ColorSecondary: "#293146",
         BackgroundImage: "linear-gradient(#ab75e8, #3dbc9f)",
-        // HostImage: "img/hosts/other/shigeki.png",
+        HostImage: "img/hosts/other/wrt-violet.png",
         // HostImageSource: "http://telefang-fans.deviantart.com/art/Bek-Shigeki-DS-491919868",
         HostName: "wrt-violet",
         Region: "New Wirral",
@@ -17,9 +17,44 @@ Intermissions.Runs.push(
         Events: [
 		
             { Group: "Pokemon", Name: "Bansheep", Time: "2025-04-01T02:15:37Z" },
+            { Group: "Pokemon", Name: "Sirenade", Time: "2025-04-01T03:04:25Z" },
+            { Group: "Pokemon", Name: "Dominoth", Time: "2025-04-01T05:05:11Z" },
+            { Group: "Pokemon", Name: "Traffikrab", Time: "2025-04-01T05:13:32Z" },
+            { Group: "Pokemon", Name: "Springheel", Time: "2025-04-01T05:26:20Z" },
+            { Group: "Pokemon", Name: "Carniviper", Time: "2025-04-01T05:34:57Z" },
+            { Group: "Pokemon", Name: "Squirey", Time: "2025-04-01T07:30:13Z" },
+            { Group: "Pokemon", Name: "Pombomb", Time: "2025-04-01T07:35:56Z" },
+            { Group: "Pokemon", Name: "Zombleat", Time: "2025-04-01T07:41:50Z" },
+            { Group: "Pokemon", Name: "Wingloom", Time: "2025-04-01T08:36:39Z" },
+            { Group: "Pokemon", Name: "Hopskin", Time: "2025-04-01T08:38:05Z" },
+            { Group: "Pokemon", Name: "Masquerattle", Time: "2025-04-01T08:39:00Z" },
+            { Group: "Pokemon", Name: "Decibelle", Time: "2025-04-01T10:54:28Z" },
+            { Group: "Pokemon", Name: "Lobstacle", Time: "2025-04-01T11:39:23Z" },
+            { Group: "Pokemon", Name: "Snoopin", Time: "2025-04-01T12:02:06Z" },
+            { Group: "Pokemon", Name: "Palangolin", Time: "2025-04-01T12:35:04Z" },
+            { Group: "Pokemon", Name: "Manispear", Time: "2025-04-01T12:35:29Z" },
+            { Group: "Pokemon", Name: "Spitzfyre", Time: "2025-04-01T12:35:51Z" },
+            { Group: "Pokemon", Name: "Dandylion", Time: "2025-04-01T12:52:05Z" },
+			
+            {
+                Group: "Bosses", Name: "Morgante", Image: "img/cassette/archangels/morgante1.png",
+                Time: "2025-04-01T13:07:24Z", Attempts: 1,
+            },
+			
+            { Group: "Pokemon", Name: "Elfless", Time: "2025-04-01T14:29:26Z" },
+            { Group: "Pokemon", Name: "Weevilite", Time: "2025-04-01T15:11:19Z" },
+            { Group: "Pokemon", Name: "Aeroboros", Time: "2025-04-01T15:27:17Z" },
+            { Group: "Pokemon", Name: "Puppercut", Time: "2025-04-01T16:50:03Z" },
+            { Group: "Pokemon", Name: "Faucetear", Time: "2025-04-01T18:53:37Z" },
+            { Group: "Pokemon", Name: "Terracooka", Time: "2025-04-01T19:15:00Z" },
+			
+            {
+                Group: "Bosses", Name: "Poppetox", Image: "img/cassette/archangels/poppetox.png",
+                Time: "2025-04-01T23:23:21Z", Attempts: 1,
+            },
 			
             /*{
-                Group: "Stamps", Name: "Wallace", Image: "img/badges/cassette/wallace.png",
+                Group: "Stamps", Name: "Wallace", Image: "img/cassette/stamps/wallace.png",
                 Time: "2025-04-01T05:05:55Z", Attempts: 0,
             },*/
 			
