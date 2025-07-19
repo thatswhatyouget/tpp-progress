730024257226f4892f5924a85753b9f39da03843
BornToLose <twitchplayslore@gmail.com>
2025-07-19 19:32:24 +0200

    Added Randomized Black 2-2

---
+++ data/Intermissions/Season 8/randoblack3.ts
@@ -0,0 +1,38 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "Randomized Black 2-2",
+        ColorPrimary: "#111",
+        ColorSecondary: "#697AB7",
+        Duration: "2021-07-30T03:45:45.393Z",
+        StartDate: "2021-07-29T23:35:00Z",
+        HostName: "KKK---'",
+        HostImage: "img/hosts/r.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/g4c56y/reese_the_super_pokestar/",
+        Region: "Unova",
+        Generation: 5,
+        Pokedex: "National",
+        // FinalStateLink:"./states/Season 8/randoblack3.json",
+        Events: [
+			
+            { "Group": "Pokemon", "Name": "Aron", "Time": "2021-07-29T23:43:36Z" },
+            { "Group": "Pokemon", "Name": "Tepig", "Time": "2021-07-29T23:57:28Z" },
+            { "Group": "Pokemon", "Name": "NidoranM", "Time": "2021-07-30T00:00:54Z" },
+            { "Group": "Pokemon", "Name": "Zubat", "Time": "2021-07-30T00:39:45Z" },
+            { "Group": "Pokemon", "Name": "Budew", "Time": "2021-07-30T00:42:34Z" },
+            { "Group": "Pokemon", "Name": "Marill", "Time": "2021-07-30T00:48:00Z" },
+            { "Group": "Pokemon", "Name": "Lillipup", "Time": "2021-07-30T01:00:23Z" },
+            { "Group": "Pokemon", "Name": "Larvitar", "Time": "2021-07-30T01:06:07Z" },
+            { "Group": "Pokemon", "Name": "Barboach", "Time": "2021-07-30T01:08:35Z" },
+            { "Group": "Pokemon", "Name": "Pidove", "Time": "2021-07-30T01:20:00Z" },
+            { "Group": "Pokemon", "Name": "Remoraid", "Time": "2021-07-30T01:23:57Z" },
+            { "Group": "Pokemon", "Name": "Timburr", "Time": "2021-07-30T01:35:59Z" },
+            { "Group": "Pokemon", "Name": "Silcoon", "Time": "2021-07-30T02:17:16Z" },
+            { "Group": "Pokemon", "Name": "Wingull", "Time": "2021-07-30T02:21:41Z" },
+            { "Group": "Pokemon", "Name": "Kirlia", "Time": "2021-07-30T02:23:54Z" },
+            { "Group": "Pokemon", "Name": "Snubbull", "Time": "2021-07-30T02:36:41Z" },
+            { "Group": "Pokemon", "Name": "Snivy", "Time": "2021-07-30T02:53:03Z" },
+			
+        ]
+    }
+);
