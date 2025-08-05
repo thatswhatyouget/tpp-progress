3661200ab025193426d423786247cab841a749ed
m4-used-rollout <dave.was.r@gmail.com>
2025-08-05 16:26:34 +0000

    Created entry for Unbound

---
+++ data/Season 12/unbound.ts
@@ -0,0 +1,51 @@
+/// <reference path="../tpp-data.ts" />
+Season10.Runs.push(
+    {
+        RunName: "Unbound",
+        ColorSecondary: "#7544C6",
+        ColorPrimary: "#FFB320",
+        StartDate: "2025-08-09T21:00:00Z",
+        Duration: "255d",
+        HostName: "",
+        HostImage: "",
+        HostImageSource: "",
+        BaseGame: "FireRed",
+        Region: "Borrius",
+        FinalStateLink: "./states/Season 12/unbound.json",
+        Events: [
+            {
+                Group: "Badges", Name: "Leaf Badge", Image: "img/badges/unbound/Leaf.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Vision Badge", Image: "img/badges/unbound/Vision.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Wings Badge", Image: "img/badges/unbound/Wings.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Fall Badge", Image: "img/badges/unbound/Fall.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Battery Badge", Image: "img/badges/unbound/Battery.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Ring Badge", Image: "img/badges/unbound/Ring.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Swamp Badge", Image: "img/badges/unbound/Swamp.png",
+                Time: "", Attempts: 0,
+            },
+            {
+                Group: "Badges", Name: "Time Badge", Image: "img/badges/unbound/Time.png",
+                Time: "", Attempts: 0,
+            },
+
+        ]
+    }
+);
