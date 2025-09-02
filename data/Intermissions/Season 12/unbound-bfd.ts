2f6dee526ab9d0ca2de67c5b5b7ca2311e78194a
BornToLose <twitchplayslore@gmail.com>
2025-09-02 19:38:23 +0200

    Added events for Unbound BFD

--- data/Intermissions/Season 12/unbound-bfd.ts
+++ data/Intermissions/Season 12/unbound-bfd.ts
@@ -13,6 +13,31 @@ Intermissions.Runs.push(
         Region: "Borrius",
         //FinalStateLink: "./states/Season 12/unbound-bfd.json",
         Events: [
+		
+            { "Group": "Pokemon", "Name": "Eevee", "Time": "2025-08-08T03:00:15.196Z" },
+            { "Group": "Pokemon", "Name": "Ponyta", "Time": "2025-08-08T03:00:15.196Z" },
+            { "Group": "Pokemon", "Name": "Phanpy", "Time": "2025-08-08T03:00:15.267Z" },
+            { "Group": "Pokemon", "Name": "Nidorina", "Time": "2025-08-08T09:19:18.691Z" },
+            { "Group": "Pokemon", "Name": "Wooloo", "Time": "2025-08-08T12:04:51.164Z" },
+            { "Group": "Pokemon", "Name": "Frosmoth", "Time": "2025-08-08T14:01:53.576Z" },
+            { "Group": "Pokemon", "Name": "Snom", "Time": "2025-08-08T14:19:46.107Z" },
+			
+            /*{
+                Group: "Prints",
+                Name: "Silver Tower Symbol",
+                Image: "img/badges/symbols/unbound/towerS.png",
+                Time: "2025-08-08T20:52:34.557Z",
+                Attempts: 1 // Undefeated
+            },*/
+			
+            { "Group": "Pokemon", "Name": "Zamazenta", "Time": "2025-08-08T20:58:13.416Z" },
+            { "Group": "Pokemon", "Name": "Ferrothorn", "Time": "2025-08-09T01:28:20.777Z" },
+            { "Group": "Pokemon", "Name": "Reshiram", "Time": "2025-08-09T03:30:23.932Z" },
+            { "Group": "Pokemon", "Name": "Zangoose", "Time": "2025-08-09T12:14:13.867Z" },
+            { "Group": "Pokemon", "Name": "Manaphy", "Time": "2025-08-09T13:09:43.339Z" },
+            { "Group": "Pokemon", "Name": "Zeraora", "Time": "2025-08-09T16:29:33.079Z" },
+            { "Group": "Pokemon", "Name": "Mewtwo", "Time": "2025-08-09T17:53:45.005Z" },
+			
         ]
     }
 );
