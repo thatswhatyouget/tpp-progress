e0e46485690d4223e923ff3b7a0c06e03eb4f857
m4_used_rollout <dave.was.r@gmail.com>
2025-10-18 18:07:01 +0000

    Created bar for Z-A

---
+++ data/Season 12/z-a.ts
@@ -0,0 +1,18 @@
+/// <reference path="../tpp-data.ts" />
+Season12.Runs.push(
+    {
+        RunName: "Legends: Z-A",
+        ColorSecondary: "#3f3e40",
+        ColorPrimary: "#89c97c",
+        StartDate: "2025-10-18T21:00:00Z",
+        Duration: "255d",
+        HostName: '',
+        //HostImage: "img/hosts/iecbw.png",
+        HostImageSource: "https://discord.com/channels/927292379436814406/928400303588532324/937375338739146792",
+		Region: "Kalos",
+		Generation: 9,
+        Events: [
+
+        ]
+    }
+);
