2269c9c3a6542107501ce7655dafbd6387a7b783
m4-used-rollout <dave.was.r@gmail.com>
2025-11-07 06:39:50 +0000

    Added entry for Unbound Revisit sidegame

---
+++ data/Sidegames/unbound-revisit.ts
@@ -0,0 +1,19 @@
+/// <reference path="../tpp-data.ts" />
+Sidegames.Runs.push(
+    {
+        RunName: "Unbound Revisit",
+        ColorSecondary: "#7544C6",
+        ColorPrimary: "#FFB320",
+        StartDate: "2025-11-07T02:03:00Z",
+        Duration: "2025-11-22T21:00:00Z",
+        HostName: "Adeuu",
+        HostImage: "img/hosts/adeuu.png",
+        HostImageSource: "",
+        BaseGame: "FireRed",
+        Region: "Borrius",
+        //FinalStateLink: "./states/Season 12/unbound.json",
+        Events: [
+        ],
+        CopyEvents: ["Unbound"]
+    }
+);
