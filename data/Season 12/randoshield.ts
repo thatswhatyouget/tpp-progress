c4476908e2ad0d7fdb1aa520d79537685cc3da9e
m4-used-rollout <dave.was.r@gmail.com>
2025-12-13 22:49:15 +0000

    Added Z-A revisit catches, randoshield

---
+++ data/Season 12/randoshield.ts
@@ -0,0 +1,41 @@
+/// <reference path="../tpp-data.ts" />
+Season12.Runs.push(
+	{
+		RunName: "Randomized Shield",
+		ColorSecondary: "black",
+		ColorPrimary: "#E9004E",
+		StartDate: "2025-12-13T21:00:00Z",
+		Duration: "255d",
+		HostName: "2",
+		HostImage: "img/hosts/89977re.gif",
+		Region: "Galar",
+		Generation: 8,
+		Pokedex: "Galar",
+		ExtraDexes: ["Isle of Armor", "Crown Tundra"],
+		LiveUpdaterArchive: "https://www.reddit.com/live/1ci491antkthk/",
+		Events: [
+			{ "Group": "Badges", "Name": "Grass Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/grass.png" },
+			{ "Group": "Badges", "Name": "Water Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/water.png" },
+			{ "Group": "Badges", "Name": "Fire Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/fire.png" },
+			{ "Group": "Badges", "Name": "Ghost Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/ghost.png" },
+			{ "Group": "Badges", "Name": "Fairy Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/fairy.png" },
+			{ "Group": "Badges", "Name": "Ice Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/ice.png" },
+			{ "Group": "Badges", "Name": "Dark Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/dark.png" },
+			{ "Group": "Badges", "Name": "Dragon Badge", "Time": "", "Attempts": 0, "Image": "img/badges/galar/dragon.png" },
+
+			{ "Group": "Elite Four", "Name": "Bede", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/bede.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Bede" },
+			{ "Group": "Elite Four", "Name": "Nessa", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/nessa.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Nessa" },
+			{ "Group": "Elite Four", "Name": "Allister", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/allister.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Allister" },
+			{ "Group": "Elite Four", "Name": "Raihan", "Time": "", "Attempts": 0, "Image": "img/trainers/sword/raihan.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Raihan" },
+
+			{ "Group": "Champions", "Name": "Leon", "Time": "", "Attempts":0, "Image": "img/trainers/sword/leon.png", "ImageSource": "https://bulbapedia.bulbagarden.net/wiki/Leon" },
+
+			<TPP.HallOfFame>{
+				Group: "Hall of Fame", Name: "Champion Tournament", Time: "", IDNo: "", Attempts: 0, Party: [
+				],
+				Image: "img/ribbons/champion-galar.png",
+				FirstAttemptDate: ""
+			},
+		]
+	}
+);
