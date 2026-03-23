6b2e40970ef43193092ed0fe3fdb0b3047d3c6e0
m4_used_rollout <dave.was.r@gmail.com>
2026-03-23 06:20:36 +0000

    Added new anniversary run

---
+++ data/Season 13/anniversaryemerald.ts
@@ -0,0 +1,51 @@
+/// <reference path="../tpp-data.ts" />
+Season13.Runs.push(
+    {
+        RunName: "Anniversary Emerald",
+        ColorPrimary: "#fff",
+        ColorSecondary: "#00a10b",
+        StartDate: "2026-03-22T21:00:00Z",
+        Duration: "255d",
+        HostName: "mg0000",
+        HostImage: "img/hosts/a.png",
+        BaseGame: "Emerald",
+        Region: "Hoenn",
+        Generation: 3,
+        Pokedex: "Hoenn",
+        // FinalStateLink: "./states/Season 13/anniversaryemerald.json",
+        Events: [
+            {
+                Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Heat Badge", Image: "img/badges/heat.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Feather Badge", Image: "img/badges/feather.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Mind Badge", Image: "img/badges/mind.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Rain Badge", Image: "img/badges/rain.png",
+                Time: "", Attempts: 0
+            },
+        ]
+    }
+);
