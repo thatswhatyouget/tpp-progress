036dabaa8ae881b1662411ddcf166de3da2e82e0
M4 <dave.was.r@gmail.com>
2026-03-25 02:57:03 -0400

    Earned the first four badges

--- data/Season 13/anniversaryemerald.ts
+++ data/Season 13/anniversaryemerald.ts
@@ -12,27 +12,29 @@ Season13.Runs.push(
         Region: "Hoenn",
         Generation: 3,
         Pokedex: "Hoenn",
+        FromNatDex: true,
+        ExtraDexes: ["National"],
         // FinalStateLink: "./states/Season 13/anniversaryemerald.json",
         Events: [
             {
                 Group: "Badges", Name: "Stone Badge", Image: "img/badges/stone.png",
-                Time: "", Attempts: 0
+                Time: "2026-03-23T09:29:52Z", Attempts: 3
             },
             {
                 Group: "Badges", Name: "Knuckle Badge", Image: "img/badges/knuckle.png",
-                Time: "", Attempts: 0
+                Time: "2026-03-23T21:02:43Z", Attempts: 2
             },
             {
                 Group: "Badges", Name: "Dynamo Badge", Image: "img/badges/dynamo.png",
-                Time: "", Attempts: 0
+                Time: "2026-03-24T07:27:12Z", Attempts: 7
             },
             {
                 Group: "Badges", Name: "Heat Badge", Image: "img/badges/heat.png",
-                Time: "", Attempts: 0
+                Time: "2026-03-25T02:18:03Z", Attempts: 2
             },
             {
                 Group: "Badges", Name: "Balance Badge", Image: "img/badges/balance.png",
-                Time: "", Attempts: 0
+                Time: "", Attempts: 5
             },
             {
                 Group: "Badges", Name: "Feather Badge", Image: "img/badges/feather.png",
