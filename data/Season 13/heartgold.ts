5bd1356b74739833dbcf5fc30062c25b32695648
m4-used-rollout <dave.was.r@gmail.com>
2026-05-09 20:47:41 +0000

    Added HeartGold 101

---
+++ data/Season 13/heartgold.ts
@@ -0,0 +1,19 @@
+/// <reference path="../tpp-data.ts" />
+Season13.Runs.push(
+    {
+        RunName: "HeartGold",
+        ColorPrimary: "#ffbc1b",
+        ColorSecondary: "#bb8e1b",
+        Duration: "255d",
+        StartDate: "2026-05-09T21:00:00Z",
+        HostName: "",
+        HostImage: "img/hosts/aoooo.png",
+        HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
+        Region: "Johto",
+        //AdditionalRegions: [{ Name: "Kanto", Time: "11d 15h 9m" }],
+        Generation: 4,
+        Pokedex: "Johto",
+        Events: [
+        ]
+    }
+);
