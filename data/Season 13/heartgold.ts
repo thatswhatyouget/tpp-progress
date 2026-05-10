2ee87bfcfb19128ad52679ed67ef150400b0f9db
m4-used-rollout <dave.was.r@gmail.com>
2026-05-10 05:23:07 +0000

    Earned the Zephyr and Hive badges

--- data/Season 13/heartgold.ts
+++ data/Season 13/heartgold.ts
@@ -6,14 +6,26 @@ Season13.Runs.push(
         ColorSecondary: "#bb8e1b",
         Duration: "255d",
         StartDate: "2026-05-09T21:00:00Z",
-        HostName: "",
+        HostName: "Lindy",
         HostImage: "img/hosts/aoooo.png",
         HostImageSource: "https://www.reddit.com/r/twitchplayspokemon/comments/3xj7tq/the_hosts_of_the_voices/",
         Region: "Johto",
         //AdditionalRegions: [{ Name: "Kanto", Time: "11d 15h 9m" }],
         Generation: 4,
-        Pokedex: "Johto",
+        //Pokedex: "Johto",
         Events: [
+            {
+                Group: "Badges",
+                Name: "Zephyr Badge",
+                Image: "img/badges/zephyr.png",
+                Time: "2026-05-10T01:42:29.578Z", Attempts: 1
+            },
+                        {
+                Group: "Badges",
+                Name: "Hive Badge",
+                Image: "img/badges/hive.png",
+                Time: "2026-05-10T04:29:35.276Z", Attempts: 3
+            },
         ]
     }
 );
