d126bda65795b7524b98caca16464f5901e27bd7
M4 <dave.was.r@gmail.com>
2026-05-11 15:44:10 -0400

    Earned the Plain, Fog, Storm, and Glacier badges

--- data/Season 13/heartgold.ts
+++ data/Season 13/heartgold.ts
@@ -20,11 +20,47 @@ Season13.Runs.push(
                 Image: "img/badges/zephyr.png",
                 Time: "2026-05-10T01:42:29.578Z", Attempts: 1
             },
-                        {
+            {
                 Group: "Badges",
                 Name: "Hive Badge",
                 Image: "img/badges/hive.png",
                 Time: "2026-05-10T04:29:35.276Z", Attempts: 3
+            },
+            {
+                Group: "Badges",
+                Name: "Plain Badge",
+                Image: "img/badges/plain.png",
+                Time: "2026-05-10T13:04:07.422Z", Attempts: 2
+            },
+            {
+                Group: "Badges",
+                Name: "Fog Badge",
+                Image: "img/badges/fog.png",
+                Time: "2026-05-10T18:44:55.716Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Storm Badge",
+                Image: "img/badges/storm.png",
+                Time: "2026-05-11T03:50:54.188Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Mineral Badge",
+                Image: "img/badges/mineral.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges",
+                Name: "Glacier Badge",
+                Image: "img/badges/glacier.png",
+                Time: "2026-05-11T19:30:17.554Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Rising Badge",
+                Image: "img/badges/rising.png",
+                Time: "", Attempts: 0
             },
         ]
     }
