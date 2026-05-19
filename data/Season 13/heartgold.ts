fde9855727782385ffd09d092ff05e3ac5851343
M4 <dave.was.r@gmail.com>
2026-05-19 16:56:35 -0400

    Defeated Lance and earned all Kanto badges

--- data/Season 13/heartgold.ts
+++ data/Season 13/heartgold.ts
@@ -10,7 +10,7 @@ Season13.Runs.push(
         HostImage: "img/hosts/lindy.png",
         HostImageSource: "https://www.tumblr.com/krizste/816436904216150016",
         Region: "Johto",
-        //AdditionalRegions: [{ Name: "Kanto", Time: "11d 15h 9m" }],
+        AdditionalRegions: [{ Name: "Kanto", Time: "2026-05-13T21:55:13.567Z" }],
         Generation: 4,
         Pokedex: "National",
         Events: [
@@ -65,33 +65,95 @@ Season13.Runs.push(
             {
                 Group: "Elite Four",
                 Name: "Will",
-                Image: "img/trainers/crystal/will.png",
+                Image: "img/trainers/heartgold/will.png",
                 Time: "2026-05-13T14:09:25.214Z", Attempts: 2
             },
             {
                 Group: "Elite Four",
                 Name: "Koga",
-                Image: "img/trainers/crystal/koga.png",
+                Image: "img/trainers/heartgold/koga.png",
                 Time: "2026-05-13T14:15:21.672Z", Attempts: 1
             },
             {
                 Group: "Elite Four",
                 Name: "Bruno",
-                Image: "img/trainers/crystal/bruno.png",
+                Image: "img/trainers/heartgold/bruno.png",
                 Time: "2026-05-13T14:39:18.449Z", Attempts: 2
             },
             {
                 Group: "Elite Four",
                 Name: "Karen",
-                Image: "img/trainers/crystal/karen.png",
+                Image: "img/trainers/heartgold/karen.png",
                 Time: "2026-05-13T17:23:27.666Z", Attempts: 3
             },
             {
                 Group: "Champions",
                 Name: "Lance",
-                Image: "img/trainers/crystal/lance.png",
+                Image: "img/trainers/heartgold/lance.png",
+                Time: "2026-05-13T21:06:28.488Z", Attempts: 4
+            },
+            {
+                Group: "Badges",
+                Name: "Boulder Badge",
+                Image: "img/badges/boulder.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2026-05-15T02:44:19.738Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Cascade Badge",
+                Image: "img/badges/cascade.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2026-05-15T01:43:01.448Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Thunder Badge",
+                Image: "img/badges/thunder.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2026-05-14T22:58:03.474Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Rainbow Badge",
+                Image: "img/badges/rainbow.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2026-05-14T20:37:58.780Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Soul Badge",
+                Image: "img/badges/soul.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2026-05-14T20:03:27.101Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Marsh Badge",
+                Image: "img/badges/marsh.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2026-05-15T00:19:27.900Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Volcano Badge",
+                Image: "img/badges/volcano.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2026-05-15T03:43:38.461Z", Attempts: 1
+            },
+            {
+                Group: "Badges",
+                Name: "Earth Badge",
+                Image: "img/badges/earth.png",
+                ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "2026-05-15T04:29:23.835Z", Attempts: 2
+            },
+            {
+                Group: "Champions",
+                Name: "Red",
+                Image: "img/trainers/heartgold/red.png",
                 Time: "", Attempts: 0
-            }
+            },
         ]
     }
 );
