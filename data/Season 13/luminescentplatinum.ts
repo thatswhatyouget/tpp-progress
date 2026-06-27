945e674903a135f4d89b43f53eb11d0c46e17f27
M4 <dave.was.r@gmail.com>
2026-06-27 12:00:22 -0400

    Added Luminescent Platinum

---
+++ data/Season 13/luminescentplatinum.ts
@@ -0,0 +1,70 @@
+/// <reference path="../tpp-data.ts" />
+Season13.Runs.push(
+    {
+        RunName: "Luminescent Platinum",
+        ColorPrimary: "#727281",
+        ColorSecondary: "#51515D",
+        BackgroundImage: "linear-gradient(#51515D 0%, #727281 50%, #51515D 100%)",
+        StartDate: "2026-06-27T21:00:00Z",
+        Duration: "255d",
+        HostName: "",
+        //HostImage: "img/hosts/dot.png",
+        Region: "Sinnoh",
+        Generation: 8,
+        Pokedex: "Enhanced Sinnoh",
+        Events: [
+            {
+                Group: "Badges", Name: "Coal Badge", Image: "img/badges/coal.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Forest Badge", Image: "img/badges/forest.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Cobble Badge", Image: "img/badges/cobble.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Fen Badge", Image: "img/badges/fen.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Relic Badge", Image: "img/badges/relic.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Mine Badge", Image: "img/badges/mine.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Icicle Badge", Image: "img/badges/icicle.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Beacon Badge", Image: "img/badges/beacon.png", 
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Aaron", Image: "img/trainers/platinum/aaron.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Bertha", Image: "img/trainers/platinum/bertha.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Flint", Image: "img/trainers/platinum/flint.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Lucian", Image: "img/trainers/platinum/lucian.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "Cynthia", Image: "img/trainers/platinum/cynthia.png",
+                Time: "", Attempts: 0
+            },
+        ]
+    }
+);
