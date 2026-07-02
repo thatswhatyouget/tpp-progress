f6e208681e1ecc34636741edbf0b52fd7a891971
BornToLose <twitchplayslore@gmail.com>
2026-07-02 15:09:18 +0200

    Added Contest victory for LumiPlat

--- data/Season 13/luminescentplatinum.ts
+++ data/Season 13/luminescentplatinum.ts
@@ -65,6 +65,24 @@ Season13.Runs.push(
                 Group: "Champions", Name: "Cynthia", Image: "img/trainers/platinum/cynthia.png",
                 Time: "", Attempts: 0
             },
+			
+            <TPP.HallOfFame>{
+                Group: "Hall of Fame", Name: "Hall of Fame", Time: "", IDNo: "00000", Attempts: 0, Party: [
+                    //{ Pokemon: "Gardevoir", Level: 69 },
+                    //{ Pokemon: "Manaphy", Nickname: "112drfic", Level: 77 },
+                    //{ Pokemon: "Houndoom", Nickname: "££puuuuyyn", Level: 70 },
+                    //{ Pokemon: "Pikachu", Nickname: "££££££££xS", Level: 69 },
+                    //{ Pokemon: "Drapion", Nickname: "1", Level: 70 },
+                    //{ Pokemon: "Toxicroak", Level: 76 }
+                ],
+                FirstAttemptDate: "2024-03-15T17:26:33",
+                Image: "img/ribbons/champion-sinnoh.png"
+            },
+			
+            {
+                Group: "Contests", Name: "Coolness Normal", Image: "img/badges/ribbons/bdsp/coolness_a.png",
+                Time: "2026-07-02T12:26:50Z"
+            },
 
             { "Group": "Pokemon", "Name": "Piplup", "Time": "2026-06-27T21:17:02Z" },
             { "Group": "Pokemon", "Name": "Turtwig", "Time": "2026-06-27T21:36:51Z" },
