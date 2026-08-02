65cd647f24b1a945481c44fe8f4b413b10e02429
m4-used-rollout <dave.was.r@gmail.com>
2026-08-02 21:37:07 +0000

    Set up for Peridot

---
+++ data/Season 13/peridot.ts
@@ -0,0 +1,52 @@
+/// <reference path="../tpp-data.ts" />
+Season13.Runs.push(
+	{
+		RunName: "Peridot",
+		ColorPrimary: "#83ac00",
+		ColorSecondary: "#4f6800",
+		StartDate: "2026-08-08T21:00:00Z",
+		Duration: "255d",
+		HostName: "",
+		//HostImage: "img/hosts/",
+		//HostImageSource: "",
+		Region: "Peridot",
+		Generation: 2,
+		Pokedex: "Peridot",
+		//FinalStateLink: "./states/Season 13/peridot.json",
+		Events: [
+			{
+				Group: "Badges", Name: "Fist Badge", Image: "img/badges/fist.png",
+				Time: "", Attempts: 0
+			},
+			{
+				Group: "Badges", Name: "Wave Badge", Image: "img/badges/wave.png",
+				Time: "", Attempts: 0
+			},
+			{
+				Group: "Badges", Name: "Mystic Badge", Image: "img/badges/mystic.png",
+				Time: "", Attempts: 0
+			},
+			{
+				Group: "Badges", Name: "Skull Badge", Image: "img/badges/skull.png",
+				Time: "", Attempts: 0
+			},
+			{
+				Group: "Badges", Name: "Cog Badge", Image: "img/badges/cog.png",
+				Time: "", Attempts: 0
+			},
+			{
+				Group: "Badges", Name: "Root Badge", Image: "img/badges/root.png",
+				Time: "", Attempts: 0
+			},
+			{
+				Group: "Badges", Name: "Terra Badge", Image: "img/badges/terra.png",
+				Time: "", Attempts: 0
+			},
+			{
+				Group: "Badges", Name: "Wyrm Badge", Image: "img/badges/wyrm.png",
+				Time: "", Attempts: 0
+			},
+
+		]
+	}
+);
