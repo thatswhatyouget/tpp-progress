7b3ae31a8676a249ec0ed5f7fc523a35bcdc6569
m4_used_rollout <dave.was.r@gmail.com>
2026-08-12 00:48:07 +0000

    Peridot Gym Badges

--- data/Season 13/peridot.ts
+++ data/Season 13/peridot.ts
@@ -6,7 +6,7 @@ Season13.Runs.push(
 		ColorSecondary: "#4f6800",
 		StartDate: "2026-08-08T21:00:00Z",
 		Duration: "255d",
-		HostName: "",
+		HostName: "B",
 		//HostImage: "img/hosts/",
 		//HostImageSource: "",
 		Region: "Peridot",
@@ -16,31 +16,31 @@ Season13.Runs.push(
 		Events: [
 			{
 				Group: "Badges", Name: "Fist Badge", Image: "img/badges/fist.png",
-				Time: "", Attempts: 0
+				Time: "2026-08-09T00:27:12.829Z", Attempts: 2
 			},
 			{
 				Group: "Badges", Name: "Wave Badge", Image: "img/badges/wave.png",
-				Time: "", Attempts: 0
+				Time: "2026-08-09T06:16:36.001Z", Attempts: 1
 			},
 			{
 				Group: "Badges", Name: "Mystic Badge", Image: "img/badges/mystic.png",
-				Time: "", Attempts: 0
+				Time: "2026-08-09T13:45:40.962Z", Attempts: 1
 			},
 			{
 				Group: "Badges", Name: "Skull Badge", Image: "img/badges/skull.png",
-				Time: "", Attempts: 0
+				Time: "2026-08-10T11:24:23.543Z", Attempts: 1
 			},
 			{
 				Group: "Badges", Name: "Cog Badge", Image: "img/badges/cog.png",
-				Time: "", Attempts: 0
+				Time: "2026-08-10T22:49:13.015Z", Attempts: 2
 			},
 			{
 				Group: "Badges", Name: "Root Badge", Image: "img/badges/root.png",
-				Time: "", Attempts: 0
+				Time: "2026-08-11T00:43:21.991Z", Attempts: 1
 			},
 			{
 				Group: "Badges", Name: "Terra Badge", Image: "img/badges/terra.png",
-				Time: "", Attempts: 0
+				Time: "2026-08-11T19:06:23.910Z", Attempts: 1
 			},
 			{
 				Group: "Badges", Name: "Wyrm Badge", Image: "img/badges/wyrm.png",
