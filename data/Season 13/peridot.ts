f3edee305e34a45a57858bb285b1793c1ffc80fe
m4_used_rollout <dave.was.r@gmail.com>
2026-08-12 03:10:15 +0000

    Fixed badge images

--- data/Season 13/peridot.ts
+++ data/Season 13/peridot.ts
@@ -7,7 +7,7 @@ Season13.Runs.push(
 		StartDate: "2026-08-08T21:00:00Z",
 		Duration: "255d",
 		HostName: "B",
-		//HostImage: "img/hosts/",
+		HostImage: "img/hosts/b2.png",
 		//HostImageSource: "",
 		Region: "Peridot",
 		Generation: 2,
@@ -15,35 +15,35 @@ Season13.Runs.push(
 		//FinalStateLink: "./states/Season 13/peridot.json",
 		Events: [
 			{
-				Group: "Badges", Name: "Fist Badge", Image: "img/badges/fist.png",
+				Group: "Badges", Name: "Fist Badge", Image: "img/badges/peridot/fist.png",
 				Time: "2026-08-09T00:27:12.829Z", Attempts: 2
 			},
 			{
-				Group: "Badges", Name: "Wave Badge", Image: "img/badges/wave.png",
+				Group: "Badges", Name: "Wave Badge", Image: "img/badges/peridot/wave.png",
 				Time: "2026-08-09T06:16:36.001Z", Attempts: 1
 			},
 			{
-				Group: "Badges", Name: "Mystic Badge", Image: "img/badges/mystic.png",
+				Group: "Badges", Name: "Mystic Badge", Image: "img/badges/peridot/mystic.png",
 				Time: "2026-08-09T13:45:40.962Z", Attempts: 1
 			},
 			{
-				Group: "Badges", Name: "Skull Badge", Image: "img/badges/skull.png",
+				Group: "Badges", Name: "Skull Badge", Image: "img/badges/peridot/skull.png",
 				Time: "2026-08-10T11:24:23.543Z", Attempts: 1
 			},
 			{
-				Group: "Badges", Name: "Cog Badge", Image: "img/badges/cog.png",
+				Group: "Badges", Name: "Cog Badge", Image: "img/badges/peridot/cog.png",
 				Time: "2026-08-10T22:49:13.015Z", Attempts: 2
 			},
 			{
-				Group: "Badges", Name: "Root Badge", Image: "img/badges/root.png",
+				Group: "Badges", Name: "Root Badge", Image: "img/badges/peridot/root.png",
 				Time: "2026-08-11T00:43:21.991Z", Attempts: 1
 			},
 			{
-				Group: "Badges", Name: "Terra Badge", Image: "img/badges/terra.png",
+				Group: "Badges", Name: "Terra Badge", Image: "img/badges/peridot/terra.png",
 				Time: "2026-08-11T19:06:23.910Z", Attempts: 1
 			},
 			{
-				Group: "Badges", Name: "Wyrm Badge", Image: "img/badges/wyrm.png",
+				Group: "Badges", Name: "Wyrm Badge", Image: "img/badges/peridot/wyrm.png",
 				Time: "", Attempts: 0
 			},
 
